1a4de32bd24d3719d2a465b705fc2b91f9759c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:53 +0000

    Add package testwizard.android-set-top-box/3.2.10/testwizard.android-set-top-box-3.2.10.tar.gz

---
+++ packages/testwizard.android-set-top-box/testwizard.android-set-top-box-3.2.10.tar.gz/testwizard.android-set-top-box-3.2.10/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.android-set-top-box",
+    version="3.2.10",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for Android set-top box testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.android_set_top_box'],
+    install_requires=[
+        'testwizard.test==3.2.10',
+        'testwizard.testobjects-core==3.2.10',
+        'testwizard.commands-audio==3.2.10',
+        'testwizard.commands-mobile==3.2.10',
+        'testwizard.commands-powerswitch==3.2.10',
+        'testwizard.commands-remotecontrol==3.2.10',
+        'testwizard.commands-video==3.2.10'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+

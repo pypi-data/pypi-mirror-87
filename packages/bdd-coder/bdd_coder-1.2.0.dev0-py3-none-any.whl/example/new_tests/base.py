f6d5074ac6cc23f0184e06b6c4f8e1f6b44bf831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package bdd-coder/1.2.0.dev0/bdd_coder-1.2.0.dev0-py3-none-any.whl

---
+++ packages/bdd-coder/bdd_coder-1.2.0.dev0-py3-none-any.whl/example/new_tests/base.py
@@ -0,0 +1,20 @@
+from bdd_coder.tester import decorators
+from bdd_coder.tester import tester
+
+from . import aliases
+
+steps = decorators.Steps(aliases.MAP, 'example/tests')
+scenario = decorators.Scenario(steps)
+
+
+@steps
+class BddTester(tester.BddTester):
+    pass
+
+
+class BaseTestCase(tester.BaseTestCase):
+    def board__is_added_to_the_game(self, *args):
+        pass
+
+    def user_is_welcome(self, *args):
+        pass

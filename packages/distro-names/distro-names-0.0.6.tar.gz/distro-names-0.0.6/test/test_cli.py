39269a1e04a700f062819608cbb288a8f64efadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:32 +0000

    Add package distro-names/0.0.6/distro-names-0.0.6.tar.gz

---
+++ packages/distro-names/distro-names-0.0.6.tar.gz/distro-names-0.0.6/test/test_cli.py
@@ -0,0 +1,16 @@
+# import pytest
+
+from distro.distro import main
+
+
+class TestDistroSearch(object):
+    def test_empty_search(self):
+        main()
+        raise NotImplementedError
+
+    def test_single_term_no_result(self):
+        main()
+        raise NotImplementedError
+
+    def test_version(self):
+        raise NotImplementedError

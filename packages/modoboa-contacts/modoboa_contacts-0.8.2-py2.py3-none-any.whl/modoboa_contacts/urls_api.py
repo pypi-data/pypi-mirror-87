3828e22cf954048004b3a16ca890d93c2b28a49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:08 +0000

    Add package modoboa-contacts/0.8.2/modoboa_contacts-0.8.2-py2.py3-none-any.whl

---
+++ packages/modoboa-contacts/modoboa_contacts-0.8.2-py2.py3-none-any.whl/modoboa_contacts/urls_api.py
@@ -0,0 +1,16 @@
+"""Contacts API urls."""
+
+from rest_framework import routers
+
+from . import viewsets
+
+
+router = routers.SimpleRouter()
+router.register(
+    r"address-books", viewsets.AddressBookViewSet, basename="addressbook")
+router.register(r"categories", viewsets.CategoryViewSet, basename="category")
+router.register(r"contacts", viewsets.ContactViewSet, basename="contact")
+router.register(
+    r"emails", viewsets.EmailAddressViewSet, basename="emailaddress")
+
+urlpatterns = router.urls

224137470086a610e49697640aee75a65b96b055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package MPh/0.7.6/MPh-0.7.6-py3-none-any.whl

---
+++ packages/MPh/MPh-0.7.6-py3-none-any.whl/mph/__init__.py
@@ -0,0 +1,15 @@
+﻿"""Pythonic scripting interface for Comsol Multiphysics."""
+
+# Meta information
+__title__     = 'MPh'
+__version__   = '0.7.6'
+__date__      = '2020–11–29'
+__author__    = 'John Hennig'
+__copyright__ = 'John Hennig'
+__license__   = 'MIT'
+
+# Public interface
+from .model   import Model
+from .client  import Client
+from .server  import Server
+from .backend import inspect

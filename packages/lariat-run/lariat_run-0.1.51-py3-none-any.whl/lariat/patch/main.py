d0e15d9fdd275e5d5c0f0b78198f92c107b9b7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:43 +0000

    Add package lariat-run/0.1.51/lariat_run-0.1.51-py3-none-any.whl

---
+++ packages/lariat-run/lariat_run-0.1.51-py3-none-any.whl/lariat/patch/main.py
@@ -0,0 +1,26 @@
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def patch_all():
+    try:
+        import lariat.patch.pandas as patch_pandas
+
+        patch_pandas.patch()
+    except Exception:
+        log.warning("Couldn't patch module pandas")
+
+    try:
+        import lariat.patch.sklearn as patch_sklearn
+
+        patch_sklearn.patch()
+    except Exception:
+        log.warning("Couldn't patch module sklearn")
+
+    try:
+        import lariat.patch.subprocess as patch_subprocess
+
+        patch_subprocess.patch()
+    except Exception:
+        log.warning("Couldn't patch module subprocess")

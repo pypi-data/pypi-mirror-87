1395e3acc3b3f7b5613e1a8bd9766c4087b4bbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package bn-gs-prob-dist/1.0/bn_gs_prob_dist-1.0.tar.gz

---
+++ packages/bn-gs-prob-dist/bn_gs_prob_dist-1.0.tar.gz/bn_gs_prob_dist-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='bn_gs_prob_dist',
+    version='1.0',
+    description='Binomial & gaussian distributions',
+    packages=['bn_gs_prob_dist'],
+    author='Shaun Cox',
+    author_email='shaun@something.com',
+    zip_safe=False,
+)

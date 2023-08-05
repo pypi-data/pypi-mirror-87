7fa62bf4ffe22ea4292cc9f890a3d3bf0f20f1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:16 +0000

    Add package sicp/0.0.3b121.dev0/sicp-0.0.3b121.dev0.tar.gz

---
+++ packages/sicp/sicp-0.0.3b121.dev0.tar.gz/sicp-0.0.3b121.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+
+setup(
+    name="sicp",
+    version="0.0.3b121.dev0",
+    author="Rahul Arya",
+    author_email="rahularya@berkeley.edu",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    licence="MIT",
+    packages=find_packages(include=["sicp", "common"]),
+    package_data={"": ["**/*.tex"]},
+    include_package_data=True,
+    entry_points={"console_scripts": ["sicp=sicp.__main__:cli"]},
+    python_requires=">=3.8",
+    install_requires=["click", "watchdog", "crcmod"],
+)

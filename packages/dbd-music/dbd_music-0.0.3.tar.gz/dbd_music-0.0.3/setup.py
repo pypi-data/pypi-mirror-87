ee7177eabbbd598e2a6aa6d2f625fa29f457863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:06 +0000

    Add package dbd-music/0.0.3/dbd_music-0.0.3.tar.gz

---
+++ packages/dbd-music/dbd_music-0.0.3.tar.gz/dbd_music-0.0.3/setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+setuptools.setup(
+    name="dbd_music",
+    version="0.0.3",
+    author="SilentJungle399",
+    description="Simplest thing you could use for music in python.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

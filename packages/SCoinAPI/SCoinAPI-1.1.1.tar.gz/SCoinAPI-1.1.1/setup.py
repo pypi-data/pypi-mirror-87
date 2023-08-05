50ec5d20ba3f0ce603af9bb5c6fa93c0081cf00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package SCoinAPI/1.1.1/SCoinAPI-1.1.1.tar.gz

---
+++ packages/SCoinAPI/SCoinAPI-1.1.1.tar.gz/SCoinAPI-1.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md','r') as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'SCoinAPI',
+    version = '1.1.1',
+    description = "The light package for the SCU blockchain in the IOTA system.",
+    py_modules = ['SCoinAPI'],
+    package_dir = {'' : 'src'},
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    install_requires = ["requests>=2.23.0",],
+    url = 'https://github.com/sefx5ever/SCoinAPI.git',
+    author = 'Wyne Tan',
+    author_email = 'sefx5ever@gmail.com',
+)
+
+# 創建 whl 檔 【python setup.py bdist_wheel】

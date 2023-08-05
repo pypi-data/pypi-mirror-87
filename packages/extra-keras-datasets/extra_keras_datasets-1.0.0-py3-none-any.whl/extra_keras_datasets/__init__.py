6475478eb0cfdedecb9e31c49a339223349792a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package extra-keras-datasets/1.0.0/extra_keras_datasets-1.0.0-py3-none-any.whl

---
+++ packages/extra-keras-datasets/extra_keras_datasets-1.0.0-py3-none-any.whl/extra_keras_datasets/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+__version__ = 'v1.0.0'
+
+from . import emnist
+from . import kmnist
+from . import svhn
+from . import stl10
+from . import iris
+
+__all__ = ['emnist', 'kmnist', 'svhn', 'stl10', 'iris']

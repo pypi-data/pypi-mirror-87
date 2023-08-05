36bf5569a609094d8f8e4861087e460f349a8b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:28 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc33/odoo12-addon-somconnexio-12.0.0.0.0rc33.tar.gz

---
+++ packages/odoo12-addon-somconnexio/odoo12-addon-somconnexio-12.0.0.0.0rc33.tar.gz/odoo12-addon-somconnexio-12.0.0.0.0rc33/odoo/addons/somconnexio/wizards/__init__.py
@@ -0,0 +1,6 @@
+from . import crm_leads_validate
+from . import contract_iban_change
+from . import contract_holder_change
+from . import contract_email_change
+from . import contract_address_change
+from . import contract_one_shot_request

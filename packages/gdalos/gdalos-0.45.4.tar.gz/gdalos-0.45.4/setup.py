26eb5cbb3c789fb111625a260b22e08234d18201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:07 +0000

    Add package gdalos/0.45.4/gdalos-0.45.4.tar.gz

---
+++ packages/gdalos/gdalos-0.45.4.tar.gz/gdalos-0.45.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from src.gdalos_data.__data__ import (
+    __name__,
+    __author__,
+    __author_email__,
+    __license__,
+    __url__,
+    __version__,
+)
+
+setup(
+    name=__name__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
+    url=__url__,
+    description="a simple gdal translate/warp/addo python wrapper for raster batch processing",
+    # packages=["gdalos", "gdalos_data", "gdalos_qt"],
+    packages=find_packages("src"),  # include all packages under src
+    package_dir={"": "src"},   # tell distutils packages are under src
+    install_requires=["gdal"],
+    extras_require={"PyQt": ["fidget", "PyQt5"], "PySide": ["fidget", "PySide2"]},
+    python_requires=">=3.6.0",
+    include_package_data=True,
+    data_files=[("", ["README.rst", "LICENSE"])],
+)

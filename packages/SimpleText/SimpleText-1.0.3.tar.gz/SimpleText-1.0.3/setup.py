88443d9fca264aacb80fff46cb9a9f542d1d3a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:03 +0000

    Add package SimpleText/1.0.3/SimpleText-1.0.3.tar.gz

---
+++ packages/SimpleText/SimpleText-1.0.3.tar.gz/SimpleText-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SimpleText",
+    version="1.0.3",
+    author="Paresh Sharma",
+    author_email="paresh7903@gmail.com",
+    description="A package to manage textual data in a simple fashion.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Paresh95/SimpleText",
+    keywords=['Pre-processing', 'Text Analysis', 'NLP'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+

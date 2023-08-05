5b8a619341ab688202218d558d852bca8a89ce18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:08 +0000

    Add package mollie-api-extract/0.0.6/mollie-api-extract-0.0.6.tar.gz

---
+++ packages/mollie-api-extract/mollie-api-extract-0.0.6.tar.gz/mollie-api-extract-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mollie-api-extract", # Replace with your own username
+    version="0.0.6",
+    author="Amod Kumar",
+    author_email="amodk@outlook.com",
+    description="Fetch data from your Mollie account",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amodk/mollie-api-extract",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

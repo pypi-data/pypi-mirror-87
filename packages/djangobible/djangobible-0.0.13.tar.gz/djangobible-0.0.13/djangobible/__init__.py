4aa951a0b3ca0fa23b6496414a1ce71e74ff22ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:07 +0000

    Add package djangobible/0.0.13/djangobible-0.0.13.tar.gz

---
+++ packages/djangobible/djangobible-0.0.13.tar.gz/djangobible-0.0.13/djangobible/__init__.py
@@ -0,0 +1,10 @@
+"""
+djangobible python library.
+
+djangobible lets you easily associate Django models with Bible Scripture references and search accordingly.
+"""
+
+__version__ = "0.0.13"
+
+# This import statement allows djangobible to be a wrapper for pythonbible.
+from pythonbible import *

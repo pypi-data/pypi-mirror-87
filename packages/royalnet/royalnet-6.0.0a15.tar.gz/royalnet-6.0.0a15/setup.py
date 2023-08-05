f2616b2dbe3fdf1d775dcef2e3d213b2e5b6aae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package royalnet/6.0.0a15/royalnet-6.0.0a15.tar.gz

---
+++ packages/royalnet/royalnet-6.0.0a15.tar.gz/royalnet-6.0.0a15/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['royalnet',
+ 'royalnet.alchemist',
+ 'royalnet.campaigns',
+ 'royalnet.campaigns.tests',
+ 'royalnet.scrolls',
+ 'royalnet.typing']
+
+package_data = \
+{'': ['*'], 'royalnet': ['.pytest_cache/*', '.pytest_cache/v/cache/*']}
+
+install_requires = \
+['sqlalchemy>=1.3.19,<2.0.0', 'toml>=0.10.1,<0.11.0']
+
+setup_kwargs = {
+    'name': 'royalnet',
+    'version': '6.0.0a15',
+    'description': 'A multipurpose bot and web framework',
+    'long_description': None,
+    'author': 'Stefano Pigozzi',
+    'author_email': 'ste.pigozzi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

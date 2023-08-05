1df1d27b559775b1d33e068c48d3634368ba0932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:10 +0000

    Add package finnemner/1.0.2/finnemner-1.0.2.tar.gz

---
+++ packages/finnemner/finnemner-1.0.2.tar.gz/finnemner-1.0.2/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: WYR。
+"""
+
+from setuptools import setup
+
+setup(
+    name= 'finnemner',
+    version= '1.0.2',
+    packages = ['finnemner'],
+    author='WYRZL',
+    author_email='wuuyanru@163.com',
+    license='Apache',
+    description = 'nem and ner'
+)

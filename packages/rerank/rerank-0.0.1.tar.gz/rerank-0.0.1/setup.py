a4f1394c80eb78ff9729c8701cfe5beb004a7224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package rerank/0.0.1/rerank-0.0.1.tar.gz

---
+++ packages/rerank/rerank-0.0.1.tar.gz/rerank-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="rerank",
+    version="0.0.1",
+    author="Sahil Prasad",
+    author_email="sahilxprasad@gmail.com",
+    description="Some description",
+    url="https://rerank.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

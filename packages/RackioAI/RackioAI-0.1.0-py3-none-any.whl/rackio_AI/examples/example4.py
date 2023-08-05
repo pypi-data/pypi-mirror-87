13d4174e50e45cd1d4223270fe19edefe2a569fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:39 +0000

    Add package RackioAI/0.1.0/RackioAI-0.1.0-py3-none-any.whl

---
+++ packages/RackioAI/RackioAI-0.1.0-py3-none-any.whl/rackio_AI/examples/example4.py
@@ -0,0 +1,19 @@
+import os
+from rackio_AI import RackioAI
+from rackio import Rackio
+
+app = Rackio()
+
+RackioAI(app)
+
+os.chdir('../..')
+cwd = os.getcwd()
+
+base_path = os.path.join(cwd, 'rackio_AI', 'data')
+
+filename = os.path.join(base_path, 'Leak')
+
+RackioAI.load(filename)
+
+url_to_save = os.path.join(base_path, 'name.csv')
+RackioAI.reader.tpl.to('csv', filename=url_to_save)

7a1c8c40a49c5c6df5286791210465955a7de92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:00 +0000

    Add package tatspy/0.0.2/tatspy-0.0.2.tar.gz

---
+++ packages/tatspy/tatspy-0.0.2.tar.gz/tatspy-0.0.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 29 21:42:14 2020
+
+@author: Felipe
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tatspy",
+    version="0.0.2",
+    author="Felipe Buchbinder",
+    author_email="felbuch@gmail.com",
+    description="Technical Analysis Time Series for Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['STOCK', 'TECHNICAL ANALYSIS INDICATORS', 'HISTORICAL DATA' ,'TIME SERIES', 'MACHINE LEARNING', 'ALGORITHMIC TRADING'],   
+    download_url='https://github.com/felbuch/tatspy/archive/v0.0.2.tar.gz',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','pandas','datetime','investpy','ta']
+)

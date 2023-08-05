aee3c4c272f2061a12f5bed1e9a74b8293b07532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:54 +0000

    Add package sparkautomapper.fhir/0.1.65/sparkautomapper.fhir-0.1.65-py3-none-any.whl

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.1.65-py3-none-any.whl/spark_auto_mapper_fhir/resources/immunization_recommendation.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from pyspark.sql.types import StructType
+from spark_fhir_schemas.r4.resources.immunizationrecommendation import ImmunizationRecommendationSchema
+
+from spark_auto_mapper_fhir.extensions.extension_base import ExtensionBase
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+from spark_auto_mapper_fhir.fhir_types.list import FhirList
+from spark_auto_mapper_fhir.resources.fhir_resource_base import FhirResourceBase
+
+
+class ImmunizationRecommendation(FhirResourceBase):
+    def __init__(
+        self,
+        id_: FhirId,
+        extension: Optional[FhirList[ExtensionBase]] = None
+    ) -> None:
+        """
+        ImmunizationRecommendation Resource in FHIR
+        https://hl7.org/FHIR/datatypes.html#ImmunizationRecommendation
+
+
+        :param id_: id of resource
+        """
+        super().__init__(
+            resourceType="ImmunizationRecommendation",
+            id_=id_,
+            extension=extension
+        )
+
+    def get_schema(self, include_extension: bool) -> Optional[StructType]:
+        return ImmunizationRecommendationSchema.get_schema(
+            include_extension=include_extension
+        )

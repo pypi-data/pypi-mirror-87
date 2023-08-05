68cecdf70e2140ea97f5623329e167a986e23a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package django-gaston/0.3.0/django_gaston-0.3.0-py3-none-any.whl

---
+++ packages/django-gaston/django_gaston-0.3.0-py3-none-any.whl/gaston/templatetags/gaston_tags.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('gaston/menu.html', takes_context=True)
+def menu(context, menu, depth_count=None):
+    depth_count = depth_count + 1 if depth_count is not None else 0
+
+    return {
+        'menu': menu,
+        'request': context['request'],
+        'depth_count': depth_count,
+    }
+
+
+@register.simple_tag(takes_context=True)
+def get_item_css_classes(context, item):
+    request = context['request']
+    return item.get_all_css_classes(request)

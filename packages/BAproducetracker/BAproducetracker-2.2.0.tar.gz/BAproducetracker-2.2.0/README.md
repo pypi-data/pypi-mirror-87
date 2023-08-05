551ee1e56226914e2de4cb6da87cb11268343056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package BAproducetracker/2.2.0/BAproducetracker-2.2.0.tar.gz

---
+++ packages/BAproducetracker/BAproducetracker-2.2.0.tar.gz/BAproducetracker-2.2.0/README.md
@@ -0,0 +1,62 @@
+
+# BadApplesProject
+
+### Why
+Have you ever forgotten you have something in the fridge until after it expires? According to a report by the Natural Resources Defense Council, America throws out more than 1,250 calories per day per person. That is a lot of wasted resources. Our application will solve this problem by tracking produce.
+
+### What
+Produce tracker is an user-friendly application that will help you keep track of potential expired products to minimize waste. It will allow you to keep track of your pantry items and notify you upon imminent expirations. 
+
+### How
+
+The first step to use our application is to upload grocery or item receipts. Reciepts can either be scanned via the camera, or manually entered in the Pantry. The application will automatically use optical character recognition (OCR, pytesseract) to match receipts to items in our database. Our database pulls information from reputable food expiration websites (such as https://www.eatbydate.com/) and matches the receipt item with one of the items in this database.
+
+After the upload process, the user will be able to look at their items and view information of interest (such as expiration dates) on the Pantry. The user's produce is also stored in the database, ensuring that their data is persistent across all states of the application. There will be notifications implemented in the future, so the user is notified when the item is about to expire. 
+
+Currently, our application can be tested with the following produce: fresh apples, blueberries, grapes, and bananas.
+
+
+
+![Landing Screen](https://user-images.githubusercontent.com/55852769/96529371-f90e1500-1252-11eb-84a2-0e31fe436143.png) ![Succesful Scan](https://user-images.githubusercontent.com/55852769/96529454-35417580-1253-11eb-9931-19e696cafc71.png) 
+![Pantry Screen](https://user-images.githubusercontent.com/55852769/96529531-6c178b80-1253-11eb-84f1-3b3b2a913b1e.png) 
+![Manual Entry Screen](https://user-images.githubusercontent.com/55852769/96529598-9a956680-1253-11eb-8eb8-d9b14b797f50.png)
+<p align="center">
+  <img width="399" height="299" src="https://user-images.githubusercontent.com/55852769/96529666-cc0e3200-1253-11eb-8c75-8d779ab9d405.png">
+</p>
+
+
+
+
+### Technologies Used
+- Kivy v2.0.0
+- Python v3.8
+- SQLite3
+- Pytesseract
+- Numpy
+
+### Pitch
+
+If you would like a quick overview of the concept/idea before development, you can view this [elevator pitch](https://www.youtube.com/watch?v=OB2ZTpWcwHo) about the project.
+
+# Package Name
+
+The package is hosted in PyPi, called [BAproducetracker](https://pypi.org/project/BAproducetracker/)
+
+
+# Initialization & Run Commands
+We are testing the project using an image of Ubuntu 20.0.4 in VMware, using this [build](https://www.osboxes.org/ubuntu/#ubuntu-20-04-vmware).
+
+Make sure your camera is working and accessible on the VM. Our application uses the camera, so it'll crash without a camera provider.
+
+    # Make sure you have pip3 installed
+    
+    # install the pip module and dependences
+    pip3 install BAproducetracker==0.0.16
+    
+    # Run the application (*)
+    ~/.local/bin/bapt
+    
+(*) This step will also install a couple dependencies (Tesseract, xclip). It will ask for user password to install these dependencies. If the GUI says 'Python is not responding', don't press 'Force Quit' for the application, instead press 'Wait'. 
+
+# Source Code
+https://github.com/jd3builds/BadApplesJJP
---
+++ packages/BAproducetracker/BAproducetracker-2.2.0.tar.gz/BAproducetracker-2.2.0/producetracker/scraper.py
@@ -0,0 +1,153 @@
+import requests
+from bs4 import BeautifulSoup
+from bs4 import Tag
+from database import insert_general_table
+
+counter = 0
+
+def normalize_name(name):
+    if ' lasts' in name:
+        name = name.replace(' lasts', '')
+    if ' last' in name:
+        name = name.replace(' last', '')
+    if '(hard)*' in name:
+        name = name.replace('(hard)*', '')
+    return name
+
+def get_unopened(raw):
+    val = None
+    if raw == '(Unopened/Opened)' or raw == '-':
+        val = None
+    elif "unopened" in raw.lower():
+        val = False
+    elif "opened" in raw.lower():
+        val = True
+    return val
+
+# Date amount to days (ex: 1 Year -> 365 days)
+def get_bound(normalized, units):
+    index = None
+    for i, c in enumerate(normalized):
+        if not c.isdigit():
+            index = i
+            break
+
+    return int(normalized[0:index]) * units[normalized[index:]]
+
+# Returns lower bound, upper bound, unit type
+def get_lower_and_upper_range(raw):
+    # Infinite amount of time
+    if raw == 'Indefinite':
+        return (99999, 99999, 'Days')
+    # Edge case: contains + in name (ex: 2+ Years -> 2 Years)
+    if '+' in raw:
+        raw = raw.replace('+', '')
+    # Edge case: unsure 'Cook First' means
+    if raw == 'Cook first':
+        return None
+    if '(' in raw:
+        raw = raw[0: raw.index('(')]
+    # Fix this case
+    if '--' in raw:
+        return None
+    if '-' == raw:
+        return None
+
+    # Contains several units of measurement
+    if "Year" in raw and "Months" in raw:
+        normalized = raw.replace(" ", "").lower()
+        units = {
+            "year": 365,
+            "years": 365,
+            "month": 30,
+            "months": 30
+        }
+        dash = normalized.index('-')
+        return (get_bound(normalized[0:dash], units), get_bound(normalized[dash + 1:], units), "Days")
+
+    if "date" in raw:
+        raw = ' '.join(raw.split()[0:2])
+
+    if raw == 'Same Day':
+        return (1, 1, 'Day')
+    elif '-' in raw:
+        index = raw.index('-')
+        return (int(raw[0:index]), int(raw[index + 1:index + 2]), raw[index + 3:].replace(" ", ""))
+    else:
+        index = raw.index(' ')
+        return (int(raw[0:index]), None, raw[index + 1:].replace(" ", ""))
+
+
+def scrape_category_of_items(URL, category, subcategory):
+    r = requests.get(url=URL)
+
+    soup = BeautifulSoup(r.text)
+
+    for list in soup.find_all('div', {'class': 'arrow-list'}):
+        for li in filter(lambda x: type(x) is Tag, list.ul.contents):
+            name = li.a.contents[0]
+            url = li.a['href']
+            scrape_single_item_from_page(url, name, category, subcategory)
+
+# Working for fruit pages
+def scrape_single_item_from_page(URL, general_name, category, subcategory):
+    global counter
+    r = requests.get(url=URL)
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    categories = []
+    items = []
+
+    possible_storages = ['Counter', 'Refrigerator', 'Freezer', 'Pantry', 'Shelf', 'Fridge']
+
+    for tr in filter(lambda x: type(x) is Tag, soup.select('div table')[0].tbody.children):
+        current_contents = list(filter(lambda x: type(x) is Tag, tr.contents))
+
+        if len(current_contents) == 0:
+            if len(tr.contents) != 0 and "opened" in str(tr.contents[0]).lower():
+                categories[0] = str(tr.contents[0])
+            continue
+        # Current row is an invalid header (Past printed date)
+        if len(current_contents[1].contents) != 0 and str(current_contents[1].contents[0]) == 'Past Printed Date':
+            continue
+        # Current row is a header (unopened, pantry, etc...) containing non-product info
+        if len(current_contents[1].contents) != 0 and str(current_contents[1].contents[0]) in possible_storages:
+            categories.clear()
+            for th in current_contents:
+                categories.extend(list(map(lambda x: str(x), th.contents)))
+            if len(current_contents[0].contents) == 0:
+                categories.insert(0, '-')
+        # Current row is a product row (item)
+        else:
+            if "Date" in current_contents[1].contents[0]:
+                continue
+            name = normalize_name(current_contents[0].contents[0].contents[0])
+            for i in range(1, len(categories)):
+                unopened = get_unopened(categories[0])
+                ranges = get_lower_and_upper_range(current_contents[i].contents[0])
+                if ranges is None:
+                    print("Unable to add item " + name + " - " + categories[i] + " (range: "  + current_contents[i].contents[0] + ")")
+                else:
+                    item = (name, counter, category, subcategory, categories[i], unopened, ranges[0], ranges[1], ranges[2])
+                    items.append(items)
+                    insert_general_table(item)
+                    print("Inserted: " + str(item))
+                    counter += 1
+
+
+if __name__ == '__main__':
+    scrape_single_item_from_page("https://www.eatbydate.com/fruits/fresh/tomatoes-shelf-life-expiration-date//", "Apples", "Fruits", "Fresh Fruits")
+    scrape_category_of_items("https://www.eatbydate.com/fruits/fresh/", "Fruits", "Fresh Fruits")
+    scrape_category_of_items("https://www.eatbydate.com/proteins/beans-peas/", "Proteins", "Beans & Peas")
+    scrape_single_item_from_page("https://www.eatbydate.com/proteins/meats/deli-meat-shelf-life-expiration-date/", "Deli Meat", "Proteins", "Deli Meat")
+    scrape_category_of_items("https://www.eatbydate.com/proteins/meats/", "Proteins", "Meats")
+    scrape_category_of_items("https://www.eatbydate.com/proteins/nuts/", "Proteins", "Nuts and Seeds")
+    scrape_category_of_items("https://www.eatbydate.com/proteins/poultry/", "Proteins", "Poultry")
+    scrape_category_of_items("https://www.eatbydate.com/proteins/poultry/", "Proteins", "Seafood")
+
+    scrape_single_item_from_page("https://www.eatbydate.com/vegetables/fresh-vegetables/spinach/", "Vegetables", "Fresh Vegetables", "Spinach")
+    scrape_single_item_from_page("https://www.eatbydate.com/vegetables/fresh-vegetables/potatoes-shelf-life-expiration-date/", "Vegetables", "Fresh Vegetables", "Potato")
+    scrape_single_item_from_page("https://www.eatbydate.com/vegetables/fresh-vegetables/how-long-do-onions-last-shelf-life/", "Vegetables", "Fresh Vegetables", "Onion")
+    scrape_single_item_from_page("https://www.eatbydate.com/vegetables/fresh-vegetables/salad-shelf-life-expiration-date/", "Vegetables", "Fresh Vegetables", "Salad")
+
+    print("Added a total of " + str(counter) + " items to the database")

db9246bfe03719266d116450f42fd92c6c8c9115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:49 +0000

    Add package qinteract/0.1.1/qinteract-0.1.1.tar.gz

---
+++ packages/qinteract/qinteract-0.1.1.tar.gz/qinteract-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qinteract',
+    version='0.1.1',
+    author='qux-bbb',
+    author_email='1147635419@qq.com',
+    url='https://github.com/qux-bbb/qinteract',
+    description='Process/Socket interactive',
+    long_description=open('README.md', 'r', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages()
+)

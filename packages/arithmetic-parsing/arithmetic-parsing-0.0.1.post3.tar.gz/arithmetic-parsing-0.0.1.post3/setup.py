7b8622679cea9eb1b51ebcbcc0b2ac11e7c28718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:14 +0000

    Add package arithmetic-parsing/0.0.1.post3/arithmetic-parsing-0.0.1.post3.tar.gz

---
+++ packages/arithmetic-parsing/arithmetic-parsing-0.0.1.post3.tar.gz/arithmetic-parsing-0.0.1.post3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arithmetic-parsing", # Replace with your own username
+    version="0.0.1-3",
+    author="Riley Wilton",
+    author_email="riley.j.wilton@gmail.com",
+    description="A simple, easy to use, arithmetic parsing tool with minimal dependencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wireboy5/arithmeticParsing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)

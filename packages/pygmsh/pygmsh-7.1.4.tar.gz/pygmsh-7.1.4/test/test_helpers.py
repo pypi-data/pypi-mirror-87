d646b61a2afc474d593d38e6a4bda0d3da58a2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:00 +0000

    Add package pygmsh/7.1.4/pygmsh-7.1.4.tar.gz

---
+++ packages/pygmsh/pygmsh-7.1.4.tar.gz/pygmsh-7.1.4/test/test_helpers.py
@@ -0,0 +1,17 @@
+"""Tests module for helpers in tests."""
+import numpy as np
+from helpers import compute_volume
+
+import pygmsh
+
+
+def test():
+    with pygmsh.geo.Geometry() as geom:
+        geom.add_circle([0, 0, 0], 1, 0.1, make_surface=False)
+        mesh = geom.generate_mesh()
+        ref = 2 * np.pi
+        assert np.abs(compute_volume(mesh) - ref) < 1e-2 * ref
+
+
+if __name__ == "__main__":
+    test()

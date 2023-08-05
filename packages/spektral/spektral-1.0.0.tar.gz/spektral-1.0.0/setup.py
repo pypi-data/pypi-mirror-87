5aa9eacef1a71dee48ae91b97bb19d5d43401173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:37 +0000

    Add package spektral/1.0.0/spektral-1.0.0.tar.gz

---
+++ packages/spektral/spektral-1.0.0.tar.gz/spektral-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='spektral',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['tensorflow>=2.1.0',
+                      'networkx',
+                      'pandas',
+                      'lxml',
+                      'joblib',
+                      'numpy',
+                      'scipy',
+                      'requests',
+                      'scikit-learn'],
+    url='https://github.com/danielegrattarola/spektral',
+    license='MIT',
+    author='Daniele Grattarola',
+    author_email='daniele.grattarola@gmail.com',
+    description='Graph Neural Networks with Keras and Tensorflow 2.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.5"
+    ],
+)

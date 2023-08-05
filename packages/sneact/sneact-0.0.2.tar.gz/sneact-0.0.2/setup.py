9cc9398c391a1e615a4cde95b736327a18ed74aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:11 +0000

    Add package sneact/0.0.2/sneact-0.0.2.tar.gz

---
+++ packages/sneact/sneact-0.0.2.tar.gz/sneact-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sneact']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sneact',
+    'version': '0.0.2',
+    'description': 'Sneact is a python library for building user interfaces',
+    'long_description': None,
+    'author': 'Kiselev Nikolay',
+    'author_email': 'ceo@machineand.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://machineand.me/sneact?f=pypi',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3,<4',
+}
+
+
+setup(**setup_kwargs)

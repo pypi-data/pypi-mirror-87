d87ae9d756a0adab3ef4e057db8dee79163add4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:22 +0000

    Add package conectividadeapp/0.0.25/conectividadeapp-0.0.25.tar.gz

---
+++ packages/conectividadeapp/conectividadeapp-0.0.25.tar.gz/conectividadeapp-0.0.25/conectividadeapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+
+    path('listagem/', views.ListConectividadeView.as_view(), name='list'),
+    # path('addactivity/<int:pk>/',views.Createactivity.as_view(), name= 'addactivity'),
+    # path('addactivity/', views.Createactivity.as_view(), name='device_activity'),
+    # path('list', listagem, name='url_listagem'),
+
+
+    # Activity Filter URLs
+    path('activity/', views.ActivityAllListView.as_view(), name='activity_list_all'),
+    re_path(r'^activity/(?P<year>[0-9]{4})/$', views.ActivityYearListView.as_view(), name='activity_list_year'),
+    re_path(r'^activity/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.ActivityMonthListView.as_view(), name='activity_list_month'),
+    re_path(r'^activity/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/$', views.ActivityDayListView.as_view(), name='activity_list_day'),
+    path('addactivity/', views.CreateactivityView.as_view(), name='addactivity'),
+    path('addactor/', views.CreateActor.as_view(), name='addactor'),
+]

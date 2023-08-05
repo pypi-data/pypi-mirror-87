5887df72a81f4b9c63622b4a0341fb1130901650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package wisdoms/2.5.22/wisdoms-2.5.22.tar.gz

---
+++ packages/wisdoms/wisdoms-2.5.22.tar.gz/wisdoms-2.5.22/setup.py
@@ -0,0 +1,28 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wisdoms",
+    version="2.5.22",
+    author="tzxd group",
+    author_email="whosqays@qq.com",
+    description="add my rpc for catch exception",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/soft-project/wisdom-lib",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyYaml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

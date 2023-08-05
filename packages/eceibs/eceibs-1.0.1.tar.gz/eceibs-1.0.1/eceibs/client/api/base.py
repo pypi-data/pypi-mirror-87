295312315acde9f830423fe1a3fa277111e7a732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package eceibs/1.0.1/eceibs-1.0.1.tar.gz

---
+++ packages/eceibs/eceibs-1.0.1.tar.gz/eceibs-1.0.1/eceibs/client/api/base.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+
+class CecibsBaseAPI(object):
+
+    API_BASE_URL = None
+
+    def __init__(self, client=None):
+        self._client = client
+
+    def _get(self, url, params=None, **kwargs):
+        if self.API_BASE_URL and 'api_base_url' not in kwargs:
+            kwargs['api_base_url'] = self.API_BASE_URL
+        return self._client.get(url, params, **kwargs)
+
+    def _post(self, url, data=None, params=None, **kwargs):
+        if self.API_BASE_URL and 'api_base_url' not in kwargs:
+            kwargs['api_base_url'] = self.API_BASE_URL
+        return self._client.post(url, data, params, **kwargs)
+
+    def _gen_url(self, url, params=None, **kwargs):
+        if self.API_BASE_URL and 'api_base_url' not in kwargs:
+            kwargs['api_base_url'] = self.API_BASE_URL
+        return self._client.gen_url(url, params, **kwargs)
+
+    def add_sign(self, data, random_str_key="randomstr", sign_key="sign"):
+        return self._client.add_sign(data, random_str_key, sign_key)
+
+    @property
+    def company_code(self):
+        return self._client.company_code
---
+++ packages/eceibs/eceibs-1.0.1.tar.gz/eceibs-1.0.1/eceibs/core/utils.py
@@ -0,0 +1,87 @@
+# encoding: utf-8
+
+import copy
+import hashlib
+import json
+import random
+import string
+
+
+class ObjectDict(dict):
+    """Makes a dictionary behave like an object, with attribute-style access.
+    """
+
+    def __getattr__(self, key):
+        if key in self:
+            return self[key]
+        return None
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class AllInPaySigner(object):
+    """AllInPay data signer"""
+
+    def __init__(self, delimiter=b'', key=None):
+        self._key = key
+        self._data = []
+        self._delimiter = to_binary(delimiter)
+
+    def add_data(self, *args):
+        """Add data to signer"""
+        for data in args:
+            self._data.append(to_binary(data))
+
+
+class AllInPayMd5Signer(AllInPaySigner):
+
+    @property
+    def signature(self):
+        """Get data signature"""
+        data = copy.copy(self._data)
+        if self._key:
+            data.append(to_binary(self._key))
+        data.sort()
+        str_to_sign = self._delimiter.join(data)
+        return hashlib.md5(str_to_sign).hexdigest().lower()
+
+
+def to_text(value, encoding='utf-8'):
+    """Convert value to unicode, default encoding is utf-8
+
+    :param value: Value to be converted
+    :param encoding: Desired encoding
+    """
+    if value is None:
+        return ''
+    if isinstance(value, str):
+        return value
+    if isinstance(value, bytes):
+        return value.decode(encoding)
+    return str(value)
+
+
+def to_binary(value, encoding='utf-8'):
+    """Convert value to binary string, default encoding is utf-8
+
+    :param value: Value to be converted
+    :param encoding: Desired encoding
+    """
+    if value is None:
+        return b''
+    if isinstance(value, bytes):
+        return value
+    if isinstance(value, str):
+        return value.encode(encoding)
+    return to_text(value).encode(encoding)
+
+
+def random_string(length=16):
+    rule = string.ascii_letters + string.digits
+    rand_list = random.sample(rule, length)
+    return ''.join(rand_list)
+
+
+def json_loads(s, object_hook=ObjectDict, **kwargs):
+    return json.loads(s, object_hook=object_hook, **kwargs)

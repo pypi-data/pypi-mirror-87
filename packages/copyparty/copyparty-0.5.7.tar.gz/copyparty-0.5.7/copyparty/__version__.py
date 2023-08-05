a71f0d33c68553e92f78db0345941d8aee9df936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:28 +0000

    Add package copyparty/0.5.7/copyparty-0.5.7.tar.gz

---
+++ packages/copyparty/copyparty-0.5.7.tar.gz/copyparty-0.5.7/copyparty/__version__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+VERSION = (0, 5, 7)
+CODENAME = "fuse jelly"
+BUILD_DT = (2020, 11, 30)
+
+S_VERSION = ".".join(map(str, VERSION))
+S_BUILD_DT = "{0:04d}-{1:02d}-{2:02d}".format(*BUILD_DT)
+
+__version__ = S_VERSION
+__build_dt__ = S_BUILD_DT
+
+# I'm all ears

63a22a3f670e99e4148dfbff24f5afa53979dcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package tap-typeform/1.2.0/tap-typeform-1.2.0.tar.gz

---
+++ packages/tap-typeform/tap-typeform-1.2.0.tar.gz/tap-typeform-1.2.0/README.md
@@ -0,0 +1,65 @@
+# tap-typeform
+
+This is a [Singer](https://singer.io) tap that produces JSON-formatted data following the [Singer spec](https://github.com/singer-io/getting-started/blob/master/docs/SPEC.md).
+
+This tap:
+
+- Pulls raw data from TypeForms's [API](https://api.typeform.com/forms)
+- Extracts the following resources from TypeForm
+  - [Responses](https://developer.typeform.com/responses)
+      - List of questions on each form added to the configuration in the tap.
+      - List of landings of users onto each form added to the configuration in the tap.
+      - List of answers completed during each landing onto each form added to the configuration in the tap.
+- Outputs the schema for each resource
+
+## Setup
+
+Building follows the conventional Singer setup:
+
+python3 ./setup.py clean
+python3 ./setup.py build
+python3 ./setup.py install
+
+## Configuration
+
+This tap requires a `config.json` which specifies details regarding an [Authentication token](https://developer.typeform.com/get-started/convert-keys-to-access-tokens/), a list of form ids, a start date for syncing historical data (date format of YYYY-MM-DDTHH:MI:SSZ), and a time period range [daily,hourly] to control what incremental extract date ranges are. See [example.config.json](example.config.json) for an example.
+
+Create the catalog:
+
+```bash
+› tap-typeform --config config.json --discover > catalog.json
+```
+
+Then to run the extract:
+
+```bash
+› tap-typeform --config config.json --catalog catalog.json --state state.json
+```
+
+Note that a typical state file looks like this:
+
+```json
+{"bookmarks": {"team_table": {"date_to_resume": "2018-08-01 00:00:00"}}}
+```
+
+## Replication
+
+With each run of the integration, the following data set is extracted and replicated to the data warehouse:
+
+- **Questions**: A list of question titles and ids that can then be used to link to answers.
+
+- **Landings**: A list of form landings and supporting data since the last completed run of the tap through the most recent day or hour respectively. On the first run, ALL increments since the **Start Date** will be replicated.
+
+- **Answers**: A list of form answers with ids that can be used to link to landings and questions since the last completed run of the integration) through the most recent day or hour respectively. On the first run, ALL increments since the **Start Date** will be replicated.
+
+## Troubleshooting / Other Important Info
+
+- **Question Data**: The form definitions are quite robust, but we have chosen to limit the fields to just those needed for responses analysis.
+
+- **Form Data**: The raw response data is not fully normalized and the tap output reflects this by breaking it into landings and answers.  Answers could potentially be normalized further, but the redundant data is quite small so it seemed better to keep it flat.  The hidden field was left a JSON structure since it could have any sorts or numbers of custom elements.  
+
+- **Timestamps**: All timestamp columns are in yyyy-MM-ddTHH:mm:ssZ format.  Resume_date state parameter are Unix timestamps.
+
+---
+
+Copyright &copy; 2018 Stitch
---
+++ packages/tap-typeform/tap-typeform-1.2.0.tar.gz/tap-typeform-1.2.0/tap_typeform/__init__.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+
+import singer
+from singer import utils
+from singer.catalog import Catalog, CatalogEntry, Schema
+from . import streams
+from .context import Context
+from . import schemas
+
+REQUIRED_CONFIG_KEYS = ["token", "forms", "incremental_range"]
+
+LOGGER = singer.get_logger()
+
+#def check_authorization(atx):
+#    atx.client.get('/settings')
+
+
+# Some taps do discovery dynamically where the catalog is read in from a
+#  call to the api but with the typeform structure, we won't do that here
+#  because it's always the same so we just pull it from file we never use
+#  atx in here since the schema is from file but we would use it if we
+#  pulled schema from the API def discover(atx):
+def discover():
+    catalog = Catalog([])
+    for tap_stream_id in schemas.STATIC_SCHEMA_STREAM_IDS:
+        #print("tap stream id=",tap_stream_id)
+        schema = Schema.from_dict(schemas.load_schema(tap_stream_id))
+        metadata = []
+        for field_name in schema.properties.keys():
+            #print("field name=",field_name)
+            if field_name in schemas.PK_FIELDS[tap_stream_id]:
+                inclusion = 'automatic'
+            else:
+                inclusion = 'available'
+            metadata.append({
+                'metadata': {
+                    'inclusion': inclusion
+                },
+                'breadcrumb': ['properties', field_name]
+            })
+        catalog.streams.append(CatalogEntry(
+            stream=tap_stream_id,
+            tap_stream_id=tap_stream_id,
+            key_properties=schemas.PK_FIELDS[tap_stream_id],
+            schema=schema,
+            metadata=metadata
+        ))
+    return catalog
+
+
+# this is already defined in schemas.py though w/o dependencies.  do we keep this for the sync?
+def load_schema(tap_stream_id):
+    path = "schemas/{}.json".format(tap_stream_id)
+    schema = utils.load_json(get_abs_path(path))
+    dependencies = schema.pop("tap_schema_dependencies", [])
+    refs = {}
+    for sub_stream_id in dependencies:
+        refs[sub_stream_id] = load_schema(sub_stream_id)
+    if refs:
+        singer.resolve_schema_references(schema, refs)
+    return schema
+
+
+def sync(atx):
+
+    # write schemas for selected streams\
+    for stream in atx.catalog.streams:
+        if stream.tap_stream_id in atx.selected_stream_ids:
+            schemas.load_and_write_schema(stream.tap_stream_id)
+
+    # since there is only one set of schemas for all forms, they will always be selected
+    streams.sync_forms(atx)
+
+    LOGGER.info('--------------------')
+    for stream_name, stream_count in atx.counts.items():
+        LOGGER.info('%s: %d', stream_name, stream_count)
+    LOGGER.info('--------------------')
+
+
+@utils.handle_top_exception(LOGGER)
+def main():
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+    atx = Context(args.config, args.state)
+    if args.discover:
+        # the schema is static from file so we don't need to pass in atx for connection info.
+        catalog = discover()
+        json.dump(catalog.to_dict(), sys.stdout)
+    else:
+        atx.catalog = Catalog.from_dict(args.properties) \
+            if args.properties else discover()
+        sync(atx)
+
+if __name__ == "__main__":
+    main()

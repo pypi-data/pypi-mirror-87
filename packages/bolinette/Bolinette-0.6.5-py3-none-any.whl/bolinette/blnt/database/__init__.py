dd0c86686ae904282394908fa415d4abdea14218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:47 +0000

    Add package bolinette/0.6.5/Bolinette-0.6.5-py3-none-any.whl

---
+++ packages/bolinette/Bolinette-0.6.5-py3-none-any.whl/bolinette/blnt/database/__init__.py
@@ -0,0 +1,5 @@
+from bolinette.blnt.database.pagination import Pagination
+from bolinette.blnt.database.engine import DatabaseEngine
+from bolinette.blnt.database.relational import RelationalDatabase
+from bolinette.blnt.database.collection import CollectionDatabase
+from bolinette.blnt.database.manager import DatabaseManager
---
+++ packages/bolinette/Bolinette-0.6.5-py3-none-any.whl/bolinette/mail/sender.py
@@ -0,0 +1,31 @@
+from bolinette.utils import logger
+
+from bolinette import blnt
+from bolinette.mail.providers import Mailgun
+
+_providers = {
+    'mailgun': Mailgun
+}
+
+
+class Sender:
+    def __init__(self, context: 'blnt.BolinetteContext'):
+        self.context = context
+        self.provider = None
+
+    def init_app(self):
+        provider = self.context.env['mail_provider']
+        if provider:
+            provider = provider.lower()
+            if provider not in _providers:
+                logger.warning(f'Unknown "{provider}" mail provider. '
+                               f'Available: {", ".join(_providers.keys())}')
+            else:
+                self.provider = _providers[provider](self.context)
+
+    async def send(self, to, subject, content):
+        if self.provider:
+            try:
+                self.provider.send(to, subject, content)
+            except Exception as ex:
+                logger.error(str(ex))

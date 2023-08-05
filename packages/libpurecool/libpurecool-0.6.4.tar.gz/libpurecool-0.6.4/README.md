151dd01df40c6972a6fc761f52a07811c913ff37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:39 +0000

    Add package libpurecool/0.6.4/libpurecool-0.6.4.tar.gz

---
+++ packages/libpurecool/libpurecool-0.6.4.tar.gz/libpurecool-0.6.4/README.md
@@ -0,0 +1,87 @@
+# Dyson Pure Cool Python library
+
+[![Build Status](https://api.travis-ci.org/etheralm/libpurecool.svg?branch=master)](https://travis-ci.org/etheralm/libpurecool) [![Coverage Status](https://coveralls.io/repos/github/etheralm/libpurecool/badge.svg?branch=master)](https://coveralls.io/github/etheralm/libpurecool?branch=master) [![PyPI](https://img.shields.io/pypi/v/libpurecool.svg)](https://pypi.python.org/pypi/libpurecool) [![Documentation Status](https://readthedocs.org/projects/libpurecool/badge/?version=latest)](http://libpurecool.readthedocs.io/en/latest/?badge=latest)
+
+[official documentation](http://libpurecool.readthedocs.io)
+
+## Fork of Charles Blonde's libpurecoolink library: [link](https://github.com/CharlesBlonde/libpurecoollink)
+
+This Python 3.4+ library allows you to control [Dyson fan/purifier devices](http://www.dyson.com/air-treatment/purifiers/dyson-pure-hot-cool-link.aspx) and [Dyson 360 Eye robot vacuum device](http://www.dyson.com/vacuum-cleaners/robot/dyson-360-eye.aspx).
+
+[official documentation](http://libpurecool.readthedocs.io)
+
+## Status
+
+This library is becoming quite stable but backward compatibility is not yet guaranteed.
+
+Device firmware is occasionally updated along with the Dyson API, so functionality may be disrupted while we interpret the changes. 
+
+## Full documentation
+
+http://libpurecool.readthedocs.io
+
+### Supported devices
+
+* Dyson pure cool tower (TP04)
+* Dyson pure cool desk (DP04)
+* Dyson pure cool link devices (Tower and Desk)
+* Dyson pure cool+hot devices (HP02 and HP04)
+* Dyson 360 Eye robot vacuum
+
+## Features
+
+The following feature are supported:
+
+* Purifier/fan devices
+    * Connect to the device using discovery or manually with IP Address
+    * Turn on/off
+    * Set speed
+    * Turn on/off oscillation
+    * Set Auto mode
+    * Set night mode
+    * Set sleep timer
+    * Set Air Quality target (Normal, High, Better)
+    * Enable/disable standby monitoring (the device continue to update sensors when in standby)
+    * Reset filter life
+    * Adjust oscillation angle (TP04/DP04)
+* Cool+Hot purifier/fan devices
+    * Set heat mode
+    * Set heat target
+    * Set fan focus mode (HP02)
+* 360 Eye device (robot vacuum)
+    * Set power mode (Quiet/Max)
+    * Start cleaning
+    * Pause cleaning
+    * Resume cleaning
+    * Abort cleaning
+
+The following sensors are available for fan/purifier devices:
+
+* Humidity
+* Temperature in Kelvin
+* Dust (unknown metric)
+* Air quality (unknown metric)
+
+## Quick start
+
+Please read [official documentation](http://libpurecoollink.readthedocs.io)
+
+## How it works
+
+Dyson devices use many different protocols in order to work:
+
+* HTTPS to Dyson API in order to get devices information (credentials, historical data, etc ...)
+* MDNS to discover devices on the local network
+* MQTT (with auth) to get device status and send commands
+
+To my knowledge, no public technical information about API/MQTT are available so all the work was done by testing and a lot of properties are unknown at this time.
+
+This library comes with a modified version of [Zeroconf](https://github.com/jstasiak/python-zeroconf) because the Dyson MDNS implementation is not valid.
+
+This [documentation](https://github.com/shadowwa/Dyson-MQTT2RRD) helped me understand some of return values.
+
+## Work to do
+
+* Better protocol understanding
+* Better technical documentation on how it is working
+* Get historical data from the API (air quality, etc ...)
---
+++ packages/libpurecool/libpurecool-0.6.4.tar.gz/libpurecool-0.6.4/libpurecool/dyson.py
@@ -0,0 +1,116 @@
+"""Dyson Pure Cool Link library."""
+
+# pylint: disable=too-many-public-methods,too-many-instance-attributes
+
+import logging
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+import urllib3
+
+from .dyson_pure_cool import DysonPureCool
+from .dyson_pure_hotcool import DysonPureHotCool
+from .utils import is_360_eye_device, \
+    is_heating_device, is_dyson_pure_cool_device, is_heating_device_v2
+
+from .dyson_360_eye import Dyson360Eye
+from .dyson_pure_cool_link import DysonPureCoolLink
+from .dyson_pure_hotcool_link import DysonPureHotCoolLink
+from .exceptions import DysonNotLoggedException
+
+_LOGGER = logging.getLogger(__name__)
+
+DYSON_API_URL = "appapi.cp.dyson.com"
+DYSON_API_URL_CN = "appapi.cp.dyson.cn"
+DYSON_API_USER_AGENT = "DysonLink/29019 CFNetwork/1188 Darwin/20.0.0"
+
+
+class DysonAccount:
+    """Dyson account."""
+
+    def __init__(self, email, password, country):
+        """Create a new Dyson account.
+
+        :param email: User email
+        :param password: User password
+        :param country: 2 characters language code
+        """
+        self._email = email
+        self._password = password
+        self._country = country
+        self._logged = False
+        self._auth = None
+        self._headers = {'User-Agent': DYSON_API_USER_AGENT}
+        if country == "CN":
+            self._dyson_api_url = DYSON_API_URL_CN
+        else:
+            self._dyson_api_url = DYSON_API_URL
+
+    def login(self):
+        """Login to dyson web services."""
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        _LOGGER.debug("Disabling insecure request warnings since "
+                      "dyson are using a self signed certificate.")
+
+        request_body = {
+            "Email": self._email,
+            "Password": self._password
+        }
+        login = requests.post(
+            "https://{0}/v1/userregistration/authenticate?country={1}".format(
+                self._dyson_api_url, self._country),
+            headers=self._headers,
+            data=request_body,
+            verify=False
+        )
+        # pylint: disable=no-member
+        if login.status_code == requests.codes.ok:
+            json_response = login.json()
+            self._auth = HTTPBasicAuth(json_response["Account"],
+                                       json_response["Password"])
+            self._logged = True
+        else:
+            self._logged = False
+        return self._logged
+
+    def devices(self):
+        """Return all devices linked to the account."""
+        if self._logged:
+            device_response = requests.get(
+                "https://{0}/v1/provisioningservice/manifest".format(
+                    self._dyson_api_url),
+                headers=self._headers,
+                verify=False,
+                auth=self._auth)
+            device_v2_response = requests.get(
+                "https://{0}/v2/provisioningservice/manifest".format(
+                    self._dyson_api_url),
+                headers=self._headers,
+                verify=False,
+                auth=self._auth)
+            devices = []
+            for device in device_response.json():
+                if is_360_eye_device(device):
+                    dyson_device = Dyson360Eye(device)
+                elif is_heating_device(device):
+                    dyson_device = DysonPureHotCoolLink(device)
+                else:
+                    dyson_device = DysonPureCoolLink(device)
+                devices.append(dyson_device)
+
+            for device_v2 in device_v2_response.json():
+                if is_dyson_pure_cool_device(device_v2):
+                    devices.append(DysonPureCool(device_v2))
+                elif is_heating_device_v2(device_v2):
+                    devices.append(DysonPureHotCool(device_v2))
+
+            return devices
+
+        _LOGGER.warning("Not logged to Dyson Web Services.")
+        raise DysonNotLoggedException()
+
+    @property
+    def logged(self):
+        """Return True if user is logged, else False."""
+        return self._logged

2a743975b09c05dd4dedfe842be880383624b224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package pokejdr/0.1.0/pokejdr-0.1.0-py3-none-any.whl

---
+++ packages/pokejdr/pokejdr-0.1.0-py3-none-any.whl/pokejdr/_version.py
@@ -0,0 +1,9 @@
+__title__ = "pokejdr"
+__description__ = "Un package utilitaire pour notre JDR pokemon."
+__url__ = "https://github.com/TRobine/Pkmn_Rmstrd"
+__version__ = "0.1.0"
+__author__ = "T. Robine & F. Soubelet"
+__author_email__ = ""
+__license__ = "MIT"
+
+__all__ = [__version__]
---
+++ packages/pokejdr/pokejdr-0.1.0-py3-none-any.whl/pokejdr/constants.py
@@ -0,0 +1,6 @@
+LOGURU_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+    "<level>{level: <8}</level> | "
+    "<cyan>{name}</cyan>:<cyan>{line}</cyan> - "
+    "<level>{message}</level>"
+)

7acafaf6c1382f53822fd3907d200f38cb0304a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:20 +0000

    Add package foursight-core/0.1.0b9/foursight_core-0.1.0b9.tar.gz

---
+++ packages/foursight-core/foursight_core-0.1.0b9.tar.gz/foursight_core-0.1.0b9/foursight_core/checks/helpers/confchecks.py
@@ -0,0 +1,7 @@
+# Create a new confchecks.py for a new project (e.g. foursight-cgap)
+from ...decorators import Decorators
+deco = Decorators('placeholder_prefix')
+CheckResult = deco.CheckResult
+ActionResult = deco.ActionResult
+check_function = deco.check_function
+action_function = deco.action_function

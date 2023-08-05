ac3c4e348525ea48c0b1d5c7e4b80bb738c3e2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package Gaussian-Binomial-1/0.2/Gaussian_Binomial_1-0.2.tar.gz

---
+++ packages/Gaussian-Binomial-1/Gaussian_Binomial_1-0.2.tar.gz/Gaussian_Binomial_1-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaussian_Binomial_1',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      author = 'Xu Wang',
+      author_email='xu.wang.pa@gmail.com',
+      packages=['Gaussian_Binomial_1'],
+      zip_safe=False)

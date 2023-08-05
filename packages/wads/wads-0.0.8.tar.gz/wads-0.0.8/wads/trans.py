16b6679c9a913fd0f8d7df4f11099f3ecc030977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package wads/0.0.8/wads-0.0.8.tar.gz

---
+++ packages/wads/wads-0.0.8.tar.gz/wads-0.0.8/wads/trans.py
@@ -0,0 +1,9 @@
+
+
+# TODO: notebook -> md with more parametrization, normalization, and cleaning up
+
+# TODO: Get rid of html junk in ipynb -> md
+
+# TODO: Transform markdown (md) relative image reference to full url ones so that they're rendered in pypi
+# Example: ![png](img/output_72_1.png) -> ![png](https://github.com/thorwhalen/invest/tree/master/img/output_72_1.png)
+

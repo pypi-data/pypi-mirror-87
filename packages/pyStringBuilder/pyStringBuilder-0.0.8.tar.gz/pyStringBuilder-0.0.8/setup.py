e495f8bf1012d4c42942a1c860d89c3dd35efd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:13 +0000

    Add package pyStringBuilder/0.0.8/pyStringBuilder-0.0.8.tar.gz

---
+++ packages/pyStringBuilder/pyStringBuilder-0.0.8.tar.gz/pyStringBuilder-0.0.8/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyStringBuilder',
+    packages = ['pyStringBuilder'],
+    version = '0.0.8',
+    license = 'GNU General Public License v3 (GPLv3)',
+    description = 'A python implementation of StringBuilder class found on Java and C-Sharp.',
+    author = 'LMongoose',
+    author_email = 'lukaspellezario@hotmail.com',
+    url = 'https://github.com/LMongoose/py-stringbuilder',
+    download_url = 'https://github.com/LMongoose/py-stringbuilder/0.0.1.tar.gz',
+    keywords = [
+        'string',
+        'building',
+        'append',
+        'concatenation'
+    ],
+    python_requires='>=3.6',
+    install_requires = [],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+    ]
+)

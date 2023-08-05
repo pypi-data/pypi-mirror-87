de54c2348de4551421d55a74f5380d7f84e9a3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:05 +0000

    Add package yolov5processor/1.1/yolov5processor-1.1.tar.gz

---
+++ packages/yolov5processor/yolov5processor-1.1.tar.gz/yolov5processor-1.1/README.md
@@ -0,0 +1,5 @@
+# Yolo v5 Inference Engine
+Basically a wrapper for the detection program for ease of use.
+
+## How to install:
+pip install yolov5processor
---
+++ packages/yolov5processor/yolov5processor-1.1.tar.gz/yolov5processor-1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yolov5processor",
+    version="1.1",
+    author="Akshay G",
+    author_email="akshayganes@gmail.com",
+    description="Wrapper package for yolov5 inference:v3.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AkshayGanesh/yolov5processor/tree/v1.0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires =[
+        "Cython",
+        "matplotlib",
+        "numpy",
+        "pillow",
+        "PyYAML",
+        "scipy>=1.4.1",
+        "tensorboard",
+        "tqdm",
+        "torch==1.6.0",
+        "torchvision==0.7.0"
+    ]
+)

266c835dfe8bfb10f87a3da53b35b83a8204527d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:52 +0000

    Add package nornir-pyxl/0.1.3/nornir_pyxl-0.1.3.tar.gz

---
+++ packages/nornir-pyxl/nornir_pyxl-0.1.3.tar.gz/nornir_pyxl-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nornir_pyxl', 'nornir_pyxl.plugins', 'nornir_pyxl.plugins.tasks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nornir>=3.0.0,<4.0.0', 'openpyxl>=3.0.5,<4.0.0']
+
+setup_kwargs = {
+    'name': 'nornir-pyxl',
+    'version': '0.1.3',
+    'description': 'OpenPyxl plugin for nornir',
+    'long_description': None,
+    'author': 'Hugo',
+    'author_email': 'hugotinoco@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

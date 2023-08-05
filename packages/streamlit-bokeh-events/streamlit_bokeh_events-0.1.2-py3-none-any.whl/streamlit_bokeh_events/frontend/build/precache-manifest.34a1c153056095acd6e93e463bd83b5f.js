3170b5cce4565495c0a8b47bbd61ae7f95b70eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:47 +0000

    Add package streamlit-bokeh-events/0.1.2/streamlit_bokeh_events-0.1.2-py3-none-any.whl

---
+++ packages/streamlit-bokeh-events/streamlit_bokeh_events-0.1.2-py3-none-any.whl/streamlit_bokeh_events/frontend/build/precache-manifest.34a1c153056095acd6e93e463bd83b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffb6a485f0b38c80648328434a372d31",
+    "url": "./index.html"
+  },
+  {
+    "revision": "367baaa4f678b6c88be5",
+    "url": "./static/js/2.5dec615c.chunk.js"
+  },
+  {
+    "revision": "07cec5cc4d188bb136df0e4204b4b983",
+    "url": "./static/js/2.5dec615c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba32f2b95dd6bf35a22",
+    "url": "./static/js/3.0bfdbef4.chunk.js"
+  },
+  {
+    "revision": "10aa13938d377205d569",
+    "url": "./static/js/main.67f64778.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.67f64778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48144209078fedf40077",
+    "url": "./static/js/runtime-main.06e5fdf4.js"
+  }
+]);
---
+++ packages/streamlit-bokeh-events/streamlit_bokeh_events-0.1.2-py3-none-any.whl/streamlit_bokeh_events/frontend/build/precache-manifest.f645f80c42315c3252bfe1dc4cb12568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66766494319b115b7b1fe41d3d460b67",
+    "url": "./index.html"
+  },
+  {
+    "revision": "367baaa4f678b6c88be5",
+    "url": "./static/js/2.5dec615c.chunk.js"
+  },
+  {
+    "revision": "07cec5cc4d188bb136df0e4204b4b983",
+    "url": "./static/js/2.5dec615c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba32f2b95dd6bf35a22",
+    "url": "./static/js/3.0bfdbef4.chunk.js"
+  },
+  {
+    "revision": "27259c07ddb1d4f324d3",
+    "url": "./static/js/main.5184dd40.chunk.js"
+  },
+  {
+    "revision": "6515c66d2a8747a146d578e1c038a822",
+    "url": "./static/js/main.5184dd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48144209078fedf40077",
+    "url": "./static/js/runtime-main.06e5fdf4.js"
+  }
+]);

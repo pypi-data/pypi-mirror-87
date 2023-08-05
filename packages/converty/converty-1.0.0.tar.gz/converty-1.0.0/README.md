129c827f31bb6cd5bfefd009f74d0a025e01a36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package converty/1.0.0/converty-1.0.0.tar.gz

---
+++ packages/converty/converty-1.0.0.tar.gz/converty-1.0.0/README.md
@@ -0,0 +1,23 @@
+
+# About
+
+A collection of programs to convert various units of measurement. Works offline.
+
+* lengths.py: Lengths conversions (kilometers, miles, etc).
+* mass.py: Mass conversions (pounds, kilograms, etc).
+* dosimetry.py: Ionizing radiation conversions (Rems, Sieverts, etc).
+* temperature.py: Temperature conversions (Celsius, Fahrenheit, Kelvin).
+* astronomy.py: Astronomical conversions (Metric, imperial, light years, AU, parsecs).
+* planck.py: Conversions for Planck units.
+
+## Requirements
+
+* Python 3.6, or higher.
+
+## Installation
+
+To install, run the following command from a terminal:
+
+```
+pip install converty
+```
---
+++ packages/converty/converty-1.0.0.tar.gz/converty-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import pathlib
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="converty",
+    version="1.0.0",
+    description="A library for unit conversions.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/D-Bits/Converty",
+    author="Dana Lockwood",
+    author_email="lockwood.da88@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+)

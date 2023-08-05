63262ce243e3b3e39a4e3cd6c9893ba377849b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:14 +0000

    Add package sciviso/1.0.1/sciviso-1.0.1-py3-none-any.whl

---
+++ packages/sciviso/sciviso-1.0.1-py3-none-any.whl/sciviso/heatmap.py
@@ -0,0 +1,58 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from sciviso import Vis
+
+
+class Heatmap(Vis):
+
+    def __init__(self, df: pd.DataFrame, chart_columns: list, row_index: str, title='', xlabel='', ylabel='',
+                 cluster_rows=True, cluster_cols=True, row_colours=None, col_colours=None, vmin=None, vmax=None,
+                 cmap_str='RdBu_r'):
+        super().__init__(df)
+        self.chart_columns = chart_columns
+        self.row_index = row_index
+        self.title = title
+        self.cluster_rows = cluster_rows
+        self.cluster_cols = cluster_cols
+        self.col_colours = col_colours
+        self.row_colours = row_colours
+        self.vmin = vmin
+        self.vmax = vmax
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.cmap_str = cmap_str
+
+    def plot(self) -> None:
+        self.check_args_in_columns([self.chart_columns, [self.row_index]])
+        df_dists = pd.DataFrame(self.df[self.chart_columns].values)
+        df_dists.columns = self.chart_columns
+        df_dists.index = self.df[self.row_index].values
+
+        ax = sns.clustermap(df_dists, col_cluster=self.cluster_cols, row_cluster=self.cluster_rows, col_colors=self.col_colours,
+                             row_colors=self.row_colours, cmap=self.cmap_str, vmax=self.vmax, vmin=self.vmin, yticklabels=1)
+        plt.title(self.title)
+        plt.setp(ax.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
+        plt.setp(ax.ax_heatmap.xaxis.get_majorticklabels(), rotation=45, horizontalalignment='right')
+
+        self.add_labels(title=False, x=False)
+        ax.fig.suptitle(self.title, fontsize=self.title_font_size, fontweight=self.title_font_weight)
+        return ax

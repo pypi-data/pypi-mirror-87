bf7c0e8bd9a1caffb3b4b7a545ae343e9b18ec24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:49 +0000

    Add package template-string-generator/0.0.4/template_string_generator-0.0.4-py3-none-any.whl

---
+++ packages/template-string-generator/template_string_generator-0.0.4-py3-none-any.whl/template_string_generator/__init__.py
@@ -0,0 +1,10 @@
+from .app import StringGenerator
+
+
+def get_default_replacements():
+    return StringGenerator.default_place_holders()
+
+
+def create_strings(template: str, replacements: dict = None) -> list:
+    string_generator = StringGenerator(template, replacements)
+    return string_generator.create_strings()

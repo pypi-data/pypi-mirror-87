3830e9e7b49c2a3bd4aa78926f95276c9cbda44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:21 +0000

    Add package iRabbitPro/16.0.0/iRabbitPro-16.0.0.tar.gz

---
+++ packages/iRabbitPro/iRabbitPro-16.0.0.tar.gz/iRabbitPro-16.0.0/iRabbitPro/proxy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time
+import requests
+
+
+def get_proxy(num):
+    url = f'http://api.hailiangip.com:8422/api/getIp?type=1&num={num}&pid=-1&unbindTime=60&cid=-1&orderId=O20100301555849667354&time={int(time.time())}&sign=327524bf470ff93d6ec893f759472aa3&noDuplicate=1&dataType=1&lineSeparator=0&singleIp=0'
+    resp = requests.get(url)
+    if ':' in resp.text:
+        print(f'获取到{num}个代理IP:' + resp.text)
+        return resp.text
+    else:
+        print(f'重新获取代理IP..' + resp.text)
+        get_proxy(num)

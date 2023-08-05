5630200227082e03ceb67f266e8d9599ff4a2fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:55 +0000

    Add package hash-256/0.0/hash_256-0.0.tar.gz

---
+++ packages/hash-256/hash_256-0.0.tar.gz/hash_256-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hash_256',
+      version='0.0',
+      description='Hash algorithm SHA-256',
+      packages=['hash_256'],
+      author_email='formydiablo3@mail.ru',
+      zip_safe=False)

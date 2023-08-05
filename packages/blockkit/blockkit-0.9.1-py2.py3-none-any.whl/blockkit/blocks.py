c33f581befa90be6a6a91c1f10162871650da5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:08 +0000

    Add package blockkit/0.9.1/blockkit-0.9.1-py2.py3-none-any.whl

---
+++ packages/blockkit/blockkit-0.9.1-py2.py3-none-any.whl/blockkit/blocks.py
@@ -0,0 +1,105 @@
+from . import Text
+from .components import Component
+from .elements import (
+    Button,
+    Checkboxes,
+    DatePicker,
+    Element,
+    Image,
+    Overflow,
+    PlainTextInput,
+    RadioButtons,
+    Select,
+)
+from .fields import (
+    ArrayField,
+    BooleanField,
+    ObjectField,
+    StringField,
+    TextField,
+    UrlField,
+    ValidationError,
+)
+
+
+class Block(Component):
+    type = StringField()
+    block_id = StringField(max_length=255)
+
+
+class Section(Block):
+    text = TextField(max_length=3000)
+    fields = ArrayField(Text, max_items=10)
+    accessory = ObjectField(Element)
+
+    def __init__(self, text=None, block_id=None, fields=None, accessory=None):
+        if not any((text, fields)):
+            raise ValidationError("Provide either text or fields.")
+
+        super().__init__("section", block_id, text, fields, accessory)
+
+
+class Divider(Block):
+    def __init__(self, block_id=None):
+        super().__init__("divider", block_id)
+
+
+class ImageBlock(Block):
+    image_url = UrlField(max_length=3000)
+    alt_text = StringField(max_length=2000)
+    title = TextField(plain=True, max_length=2000)
+
+    def __init__(self, image_url, alt_text, title=None, block_id=None):
+        super().__init__("image", block_id, image_url, alt_text, title)
+
+
+class Actions(Block):
+    elements = ArrayField(
+        Select, Button, Overflow, DatePicker, Checkboxes, RadioButtons, max_items=5
+    )
+
+    def __init__(self, elements, block_id=None):
+        super().__init__("actions", block_id, elements)
+
+
+class Context(Block):
+    elements = ArrayField(Text, Image, max_items=10)
+
+    def __init__(self, elements, block_id=None):
+        super().__init__("context", block_id, elements)
+
+
+class Input(Block):
+    label = TextField(plain=True, max_length=2000)
+    element = ObjectField(PlainTextInput, Select, DatePicker, Checkboxes, RadioButtons)
+    dispatch_action = BooleanField()
+    hint = TextField(plain=True, max_length=2000)
+    optional = BooleanField()
+
+    def __init__(
+        self,
+        label,
+        element,
+        dispatch_action=None,
+        block_id=None,
+        hint=None,
+        optional=None,
+    ):
+        super().__init__(
+            "input", block_id, label, element, dispatch_action, hint, optional
+        )
+
+
+class File(Block):
+    external_id = StringField()
+    source = StringField()
+
+    def __init__(self, external_id, source="remote", block_id=None):
+        super().__init__("file", block_id, external_id, source)
+
+
+class Header(Block):
+    text = TextField(plain=True, max_length=3000)
+
+    def __init__(self, text, block_id=None):
+        super().__init__("header", block_id, text)
---
+++ packages/blockkit/blockkit-0.9.1-py2.py3-none-any.whl/blockkit/objects.py
@@ -0,0 +1,108 @@
+from .components import Component
+from .fields import (
+    ArrayField,
+    BooleanField,
+    StringField,
+    TextField,
+    UrlField,
+    ValidationError,
+)
+
+
+class Text(Component):
+    markdown = "mrkdwn"
+    plain = "plain_text"
+
+    text = StringField()
+    type = StringField(options=[markdown, plain])
+    emoji = BooleanField()
+    verbatim = BooleanField()
+
+    def __init__(self, text, type_, emoji=None, verbatim=None):
+        super().__init__(text, type_, emoji, verbatim)
+
+        if type_ == self.markdown and emoji:
+            raise ValidationError(
+                f"'emoji' field isn't allowed when type is \"{self.plain}\"."
+            )
+
+        if type_ == self.plain and verbatim:
+            raise ValidationError(
+                f"'verbatim' field isn't allowd when type is \"{self.markdown}\"."
+            )
+
+
+class MarkdownText(Text):
+    def __init__(self, text, verbatim=None):
+        super().__init__(text, self.markdown, None, verbatim)
+
+
+class PlainText(Text):
+    def __init__(self, text, emoji=None):
+        super().__init__(text, self.plain, emoji, None)
+
+
+class Confirm(Component):
+    title = TextField(max_length=100, plain=True)
+    text = TextField(max_length=300)
+    confirm = TextField(max_length=30, plain=True)
+    deny = TextField(max_length=30, plain=True)
+
+    def __init__(self, title, text, confirm, deny):
+        super().__init__(title, text, confirm, deny)
+
+
+class Option(Component):
+    text = TextField(max_length=75, plain=True)
+    value = StringField(max_length=75)
+    url = UrlField(max_length=3000)
+    description = TextField(max_length=75, plain=True)
+
+    def __init__(self, text, value, url=None, description=None):
+        super().__init__(text, value, url, description)
+
+
+class OptionGroup(Component):
+    label = TextField(max_length=75, plain=True)
+    options = ArrayField(Option, max_items=100)
+
+    def __init__(self, label, options):
+        super().__init__(label, options)
+
+
+class Filter(Component):
+    include_options = ("im", "mpim", "private", "public")
+
+    include = ArrayField(str)
+    exclude_external_shared_channels = BooleanField()
+    exclude_bot_users = BooleanField()
+
+    def __init__(
+        self,
+        include=None,
+        exclude_external_shared_channels=None,
+        exclude_bot_users=False,
+    ):
+        if not any((include, exclude_external_shared_channels, exclude_bot_users)):
+            raise ValidationError("You should provide at least one argument.")
+
+        if set(include) - set(self.include_options):
+            raise ValidationError("Incorrect include options.")
+
+        super().__init__(include, exclude_external_shared_channels, exclude_bot_users)
+
+
+class DispatchActionConfig(Component):
+    enter_pressed = "on_enter_pressed"
+    char_entered = "on_character_entered"
+    _triggers = [enter_pressed, char_entered]
+
+    trigger_actions_on = ArrayField(str, min_items=1, max_items=2)
+
+    def __init__(self, trigger_actions_on):
+        if not set(trigger_actions_on) <= set(self._triggers):
+            raise ValidationError(
+                f"trigger_actions_on should be a subset of {self._triggers}."
+            )
+
+        super().__init__(trigger_actions_on)

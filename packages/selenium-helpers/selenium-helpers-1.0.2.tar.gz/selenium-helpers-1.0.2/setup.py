c96c9bcde3094762c86cb460c0d47199b560eecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:06 +0000

    Add package selenium-helpers/1.0.2/selenium-helpers-1.0.2.tar.gz

---
+++ packages/selenium-helpers/selenium-helpers-1.0.2.tar.gz/selenium-helpers-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setup(
+    name='selenium-helpers',
+    version='1.0.2',
+    description='Tools to make certain selenium tasks more straightforward.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ExSidius/selenium_helpers',
+    author='ExSidius',
+    author_email='mukul.ram97@gmail.com',
+    license='MIT',
+    install_requires=['selenium'],
+    include_package_data=True,
+    zip_safe=False,
+)

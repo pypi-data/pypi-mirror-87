000327f1b654d5d4ec2e241d16f864511863eda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:46 +0000

    Add package pygit-annek/0.1.0/pygit_annek-0.1.0.tar.gz

---
+++ packages/pygit-annek/pygit_annek-0.1.0.tar.gz/pygit_annek-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pygit_annek']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyGithub>=1.53,<2.0', 'click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['pygit = pygit_annek.cli:main']}
+
+setup_kwargs = {
+    'name': 'pygit-annek',
+    'version': '0.1.0',
+    'description': 'A CLI wrapper around Pygithub',
+    'long_description': None,
+    'author': 'Michael MacKenna',
+    'author_email': 'mmackenna@unitedfiregroup.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

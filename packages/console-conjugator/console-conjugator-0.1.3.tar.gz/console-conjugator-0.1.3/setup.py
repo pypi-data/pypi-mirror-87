e1c90ce1d46fd22478c96c8e89769e3db7da0d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:01 +0000

    Add package console-conjugator/0.1.3/console-conjugator-0.1.3.tar.gz

---
+++ packages/console-conjugator/console-conjugator-0.1.3.tar.gz/console-conjugator-0.1.3/setup.py
@@ -0,0 +1,37 @@
+"""
+This application conjugates the verbs of multiple languages
+Copyright (C) 2020 Konjugators
+See LICENSE for more information
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="console-conjugator",
+    version="0.1.3",
+    author="Konjugators",
+    author_email="shynn.lawrence@gmail.com",
+    description="Versatile command line conjugator for german",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Konjugators/console-conjugator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: German",
+        "Topic :: Education", 
+        "Environment :: Console",
+    ],
+    python_requires=">=3.4",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "konjugier=Deutschconjugation.cli_scripts:main",
+        ]
+    },
+)

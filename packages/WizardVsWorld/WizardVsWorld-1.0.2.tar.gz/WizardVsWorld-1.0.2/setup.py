0dfe8249ff01d2cebf8e2d0c4dc976d96af71656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:12 +0000

    Add package WizardVsWorld/1.0.2/WizardVsWorld-1.0.2.tar.gz

---
+++ packages/WizardVsWorld/WizardVsWorld-1.0.2.tar.gz/WizardVsWorld-1.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="WizardVsWorld",
+    version= "1.0.2",
+    author="Logan D.G. Smith",
+    author_email="loganda.smith@ufl.edu",
+    description="2D tactical game involving magic and malice.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukeherczeg/WizardvsWorld",
+    packages=setuptools.find_namespace_packages(include=['WizardVsWorld', 'WizardVsWorld.*', 'WizardVsWorld.assets/*', 'WizardVsWorld.classes/*']),
+    package_data={
+        '':['*.png', '*.ogg', '*.PNG', '*.txt']
+    },
+    install_requires=[
+        "pygame==1.9.6"
+    ],
+    entry_points={
+        'console_scripts':['WizardVsWorld=classes.game:main']
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.7, <3.8",
+)

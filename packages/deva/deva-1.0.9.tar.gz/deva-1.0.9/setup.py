1ceeb3d5439677b708daae0d4e6f0a1830be67f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:03 +0000

    Add package deva/1.0.9/deva-1.0.9.tar.gz

---
+++ packages/deva/deva-1.0.9.tar.gz/deva-1.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os.path import exists
+
+setup(
+    name='deva',
+    version='1.0.9',
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires='>=3.5',
+    author='spark',
+    author_email='zjw0358@gmail.com',
+    url='https://github.com/sostc/deva',
+    license='http://www.apache.org/licenses/LICENSE-2.0.html',
+    description='data eval in future',
+    long_description=(open('README.rst').read() if exists('README.rst')
+                      else ''),
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
+    zip_safe=False
+
+)

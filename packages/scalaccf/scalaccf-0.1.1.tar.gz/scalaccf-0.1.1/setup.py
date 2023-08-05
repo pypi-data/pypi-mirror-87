06b9722f29a361fcc48f1cc0f3ad814a0f7979dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package scalaccf/0.1.1/scalaccf-0.1.1.tar.gz

---
+++ packages/scalaccf/scalaccf-0.1.1.tar.gz/scalaccf-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="scalaccf",
+    packages=["scalaccf"],
+    entry_points={
+        "console_scripts": ['scalaccf = scalaccf.ScalaCCF:main']
+    },
+    version='0.1.1',
+    description="ScalaCCF - utility to fix style and documentation comments in Scala files",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Taisiia Velychko",
+    author_email="justataya@gmail.com",
+    url="https://github.com/JustTaya/MetaProg/tree/lab-2/Lab2",
+)

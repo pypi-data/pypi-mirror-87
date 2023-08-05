1b48f59e19862e55167de0e45e2b6df9252234f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package GymBuddy/2.1.6/GymBuddy-2.1.6.tar.gz

---
+++ packages/GymBuddy/GymBuddy-2.1.6.tar.gz/GymBuddy-2.1.6/GymBuddyApp/gymBuddyApp/urls.py
@@ -0,0 +1,48 @@
+"""gymBuddyApp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from GymBuddyApp.users import views as user_views
+#we do not need to import views from pages since pages already has a urls.py file
+
+
+urlpatterns = [
+    path('', include('GymBuddyApp.pages.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
+    path('register/', user_views.register, name='register'),
+    path('weight/', user_views.weight, name='weight'),
+    path('macros/', user_views.macros, name='macros'),
+    path('exercises/', user_views.exercises, name='exercises'),
+    path('exercises/<int:active_exercises>', user_views.exercises, name='exercises'),
+    path('meals/', user_views.meals, name='meals'),
+    path('login/', user_views.login2, name='login'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete')
+]
---
+++ packages/GymBuddy/GymBuddy-2.1.6.tar.gz/GymBuddy-2.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages,find_namespace_packages
+import GymBuddyApp
+
+setup(
+
+    name = 'GymBuddy',
+    version = '2.1.6',
+    packages= find_namespace_packages(),
+    url = 'https://github.com/CTKogstrom/GymBuddyApp',
+    install_requires= ['asgiref==3.2.10','beautifulsoup4==4.9.3', 'bs4==0.0.1','certifi==2020.6.20','cycler==0.10.0','Django==3.1.2','django-crispy-forms==1.9.2','kiwisolver==1.2.0','matplotlib==3.3.2','numpy==1.19.2','Pillow==7.2.0','pyparsing==2.4.7','python-dateutil==2.8.1','pytz==2020.1','requests==2.24.0','six==1.15.0','soupsieve==2.0.1','sqlparse==0.3.1','urllib3==1.25.11','webencodings==0.5.1','whitenoise==5.2.0','cchardet==2.1.7', 'lxml==4.6.1'],
+
+    package_data= {'': ['*.html','*.json','*.css','*.jpg','*.png','*.zip','*.txt','README.md', '*.sqlite3'],
+                   },
+    entry_points={
+        "console_scripts":['launch= GymBuddyApp.manage:main']
+    },
+    python_requires='>=3.6',
+)

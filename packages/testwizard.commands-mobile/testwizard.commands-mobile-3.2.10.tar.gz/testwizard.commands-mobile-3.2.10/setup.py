58370af267d966731847971adebf49cad4836f50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package testwizard.commands-mobile/3.2.10/testwizard.commands-mobile-3.2.10.tar.gz

---
+++ packages/testwizard.commands-mobile/testwizard.commands-mobile-3.2.10.tar.gz/testwizard.commands-mobile-3.2.10/setup.py
@@ -0,0 +1,71 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.commands-mobile",
+    version="3.2.10",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard Mobile commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.commands_mobile'],
+    install_requires=[
+        'testwizard.commands-core==3.2.10'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

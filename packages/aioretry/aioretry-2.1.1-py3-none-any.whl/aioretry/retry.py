8174ad9314600f40f2d3163a9a184acb273decee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:28 +0000

    Add package aioretry/2.1.1/aioretry-2.1.1-py3-none-any.whl

---
+++ packages/aioretry/aioretry-2.1.1-py3-none-any.whl/aioretry/retry.py
@@ -0,0 +1,131 @@
+from typing import (
+    Tuple,
+    Union,
+    Callable,
+    Awaitable,
+    Optional,
+    TypeVar,
+    Union
+)
+
+import inspect
+import asyncio
+
+
+RetryPolicyStrategy = Tuple[bool, Union[int, float]]
+
+RetryPolicy = Callable[[int], RetryPolicyStrategy]
+AfterFailure = Callable[[Exception, int], Optional[Awaitable]]
+
+ParamRetryPolicy = Union[RetryPolicy, str]
+ParamAfterFailure = Union[AfterFailure, str]
+
+TargetFunction = Callable[..., Awaitable]
+Exceptions = Tuple[Exception, ...]
+ExceptionsOrException = Union[Exceptions, Exception]
+
+T = TypeVar('T', RetryPolicy, AfterFailure)
+
+
+async def await_coro(coro):
+    if inspect.isawaitable(coro):
+        return await coro
+
+    return coro
+
+
+async def perform(
+    fails: int,
+    fn: TargetFunction,
+    retry_policy: RetryPolicy,
+    before_retry: Optional[AfterFailure],
+    on_exceptions: Optional[Exceptions],
+    except_exceptions: Optional[Exceptions],
+    *args,
+    **kwargs
+):
+    while True:
+        try:
+            return await fn(*args, **kwargs)
+        except Exception as e:
+            if on_exceptions is not None:
+                if not isinstance(e, on_exceptions):
+                    raise e
+            elif except_exceptions is not None and isinstance(
+                e, except_exceptions
+            ):
+                raise e
+
+            fails += 1
+            abandon, delay = retry_policy(fails)
+
+            if abandon:
+                raise e
+
+            if before_retry is not None:
+                try:
+                    await await_coro(before_retry(e, fails))
+                except Exception as e:
+                    raise RuntimeError(
+                        f'[aioretry] before_retry failed, reason: {e}'
+                    )
+
+            # `delay` could be 0
+            if delay > 0:
+                await asyncio.sleep(delay)
+
+
+def get_method(
+    target: Union[T, str],
+    args: Tuple,
+    name: str,
+) -> T:
+    if type(target) is not str:
+        return target
+
+    if len(args) == 0:
+        raise RuntimeError(
+            f'[aioretry] decorator should be used for instance method if {name} as a str `"{target}"` '
+        )
+
+    self = args[0]
+
+    return getattr(self, target)  # type: ignore
+
+
+def retry(
+    retry_policy: ParamRetryPolicy,
+    before_retry: Optional[ParamAfterFailure] = None,
+    on_exceptions: Optional[Exceptions] = None,
+    except_exceptions: Optional[Exceptions] = None,
+) -> Callable[[TargetFunction], TargetFunction]:
+    if on_exceptions is not None and type(on_exceptions) is not tuple:
+        on_exceptions = (on_exceptions,)
+
+    if except_exceptions is not None and type(except_exceptions) is not tuple:
+        except_exceptions = (except_exceptions,)
+
+    def wrapper(fn: TargetFunction) -> TargetFunction:
+        async def wrapped(*args, **kwargs):
+            return await perform(
+                0,
+                fn,
+                get_method(
+                    retry_policy,
+                    args,
+                    'retry_policy'
+                ),
+                get_method(
+                    before_retry,
+                    args,
+                    'before_retry'
+                ) if before_retry is not None else None,
+                on_exceptions,
+                except_exceptions,
+                *args,
+                **kwargs
+            )
+
+        return wrapped
+
+    return wrapper

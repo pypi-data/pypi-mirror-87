465ec91b1bc46ca90635aeea1bc9de4353c63476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package clangTooling/0.0.5/clangTooling-0.0.5-cp37-cp37m-win_amd64.whl

---
+++ packages/clangTooling/clangTooling-0.0.5-cp37-cp37m-win_amd64.whl/clangTooling/__init__.py
@@ -0,0 +1,13 @@
+'''clangTooling libraries and headers.'''
+
+from .lib import (
+    get_lib_ext,
+    library_dir,
+    library_list,
+    llvm_library_list,
+)
+from .include import (
+    header_dir,
+    llvm_includes,
+    clang_includes,
+)
---
+++ packages/clangTooling/clangTooling-0.0.5-cp37-cp37m-win_amd64.whl/clangTooling/include/headers/build/include/llvm/Support/VCSRevision.h
@@ -0,0 +1,2 @@
+#define LLVM_REVISION "ba0fe76b7eb87f91499931e76317ddd1cb493aa1"
+#define LLVM_REPOSITORY "https://github.com/llvm/llvm-project.git"

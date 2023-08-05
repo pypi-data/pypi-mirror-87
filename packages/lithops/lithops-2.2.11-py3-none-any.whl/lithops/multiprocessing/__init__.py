a3ed19eac2b2400025e794fc68fa872069b279dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:35 +0000

    Add package lithops/2.2.11/lithops-2.2.11-py3-none-any.whl

---
+++ packages/lithops/lithops-2.2.11-py3-none-any.whl/lithops/multiprocessing/__init__.py
@@ -0,0 +1,27 @@
+#
+# Package analogous to 'threading.py' but using processes
+#
+# multiprocessing/__init__.py
+#
+# This package is intended to duplicate the functionality (and much of
+# the API) of threading.py but uses processes instead of threads.  A
+# subpackage 'multiprocessing.dummy' has the same API but is a simple
+# wrapper for 'threading'.
+#
+# Copyright (c) 2006-2008, R Oudkerk
+# Licensed to PSF under a Contributor Agreement.
+#
+# Modifications Copyright (c) 2020 Cloudlab URV
+#
+
+import sys
+from . import context
+
+#
+# Copy stuff from default context
+#
+
+_names = [x for x in dir(context._default_context) if x[0] != "_"]
+globals().update((name, getattr(context._default_context, name))
+                 for name in _names)
+__all__ = _names + []
---
+++ packages/lithops/lithops-2.2.11-py3-none-any.whl/lithops/storage/backends/localhost/localhost.py
@@ -0,0 +1,222 @@
+#
+# (C) Copyright Cloudlab URV 2020
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import io
+import glob
+import shutil
+import logging
+from lithops.storage.utils import StorageNoSuchKeyError
+from lithops.constants import LITHOPS_TEMP_DIR
+
+
+logger = logging.getLogger(__name__)
+
+
+class LocalhostStorageBackend:
+    """
+    A wrap-up around Localhost filesystem APIs.
+    """
+
+    def __init__(self, localhost_config):
+        logger.debug("Creating Localhost storage client")
+        self.localhost_config = localhost_config
+        logger.debug("Localhost storage client created successfully")
+
+    def get_client(self):
+        # Simulate boto3 client
+        class LocalhostBoto3Client():
+            def __init__(self, backend):
+                self.backend = backend
+
+            def put_object(self, Bucket, Key, Body, **kwargs):
+                self.backend.put_object(Bucket, Key, Body)
+
+            def get_object(self, Bucket, Key, **kwargs):
+                body = self.backend.get_object(Bucket, Key, stream=True, extra_get_args=kwargs)
+                return {'Body': body}
+
+            def list_objects(self, Bucket, Prefix=None, **kwargs):
+                return self.backend.list_objects(Bucket, Prefix)
+
+            def list_objects_v2(self, Bucket, Prefix=None, **kwargs):
+                return self.backend.list_objects(Bucket, Prefix)
+
+        return LocalhostBoto3Client(self)
+
+    def put_object(self, bucket_name, key, data):
+        """
+        Put an object in localhost filesystem.
+        Override the object if the key already exists.
+        :param key: key of the object.
+        :param data: data of the object
+        :type data: str/bytes
+        :return: None
+        """
+        try:
+            data_type = type(data)
+            file_path = os.path.join(LITHOPS_TEMP_DIR, bucket_name, key)
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            if data_type == bytes:
+                with open(file_path, "wb") as f:
+                    f.write(data)
+            else:
+                with open(file_path, "w") as f:
+                    f.write(data)
+        except Exception as e:
+            raise(e)
+
+    def get_object(self, bucket_name, key, stream=False, extra_get_args={}):
+        """
+        Get object from localhost filesystem with a key.
+        Throws StorageNoSuchKeyError if the given key does not exist.
+        :param key: key of the object
+        :return: Data of the object
+        :rtype: str/bytes
+        """
+        buffer = None
+        try:
+            file_path = os.path.join(LITHOPS_TEMP_DIR, bucket_name, key)
+            with open(file_path, "rb") as f:
+                if 'Range' in extra_get_args:
+                    byte_range = extra_get_args['Range'].replace('bytes=', '')
+                    first_byte, last_byte = map(int, byte_range.split('-'))
+                    f.seek(first_byte)
+                    buffer = io.BytesIO(f.read(last_byte-first_byte+1))
+                else:
+                    buffer = io.BytesIO(f.read())
+            if stream:
+                return buffer
+            else:
+                return buffer.read()
+        except Exception:
+            raise StorageNoSuchKeyError(os.path.join(LITHOPS_TEMP_DIR, bucket_name), key)
+
+    def head_object(self, bucket_name, key):
+        """
+        Head object from local filesystem with a key.
+        Throws StorageNoSuchKeyError if the given key does not exist.
+        :param key: key of the object
+        :return: Data of the object
+        :rtype: str/bytes
+        """
+        pass
+
+    def delete_object(self, bucket_name, key):
+        """
+        Delete an object from storage.
+        :param bucket: bucket name
+        :param key: data key
+        """
+        file_path = os.path.join(LITHOPS_TEMP_DIR, bucket_name, key)
+        try:
+            file_dir = os.path.dirname(file_path)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            shutil.rmtree(file_dir, ignore_errors=True)
+        except Exception:
+            pass
+
+    def delete_objects(self, bucket_name, key_list):
+        """
+        Delete a list of objects from storage.
+        :param bucket: bucket name
+        :param key_list: list of keys
+        """
+        dirs = set()
+        for key in key_list:
+            file_dir = os.path.dirname(key)
+            dirs.add(file_dir)
+            # dirs.add("/".join(file_dir.split("/", 2)[:2]))
+            self.delete_object(bucket_name, key)
+
+        for file_dir in dirs:
+            shutil.rmtree(os.path.join(LITHOPS_TEMP_DIR, file_dir), ignore_errors=True)
+
+    def head_bucket(self, bucket_name):
+        """
+        Head localhost dir with a name.
+        Throws StorageNoSuchKeyError if the given bucket does not exist.
+        :param bucket_name: name of the bucket
+        :return: Metadata of the bucket
+        :rtype: str/bytes
+        """
+        raise NotImplementedError
+
+    def list_objects(self, bucket_name, prefix=None):
+        """
+        Return a list of objects for the prefix.
+        :param bucket_name: Name of the bucket.
+        :param prefix: Prefix to filter object names.
+        :return: List of objects in bucket that match the given prefix.
+        :rtype: list of str
+        """
+        obj_list = []
+
+        if prefix:
+            if prefix.endswith('/'):
+                root = os.path.join(LITHOPS_TEMP_DIR,
+                                    bucket_name,
+                                    prefix, '**')
+            else:
+                root = os.path.join(LITHOPS_TEMP_DIR,
+                                    bucket_name,
+                                    prefix+'*', '**')
+        else:
+            root = os.path.join(LITHOPS_TEMP_DIR,
+                                bucket_name, '**')
+
+        for file_name in glob.glob(root, recursive=True):
+            if os.path.isfile(file_name):
+                if file_name.endswith(prefix):
+                    continue
+                size = os.stat(file_name).st_size
+                base_dir = os.path.join(LITHOPS_TEMP_DIR, bucket_name, '')
+                obj_list.append({'Key': file_name.replace(base_dir, '').replace('\\', '/'), 'Size': size})
+
+        return obj_list
+
+    def list_keys(self, bucket_name, prefix=None):
+        """
+        Return a list of keys for the given prefix.
+        :param bucket_name: Name of the bucket.
+        :param prefix: Prefix to filter object names.
+        :return: List of keys in bucket that match the given prefix.
+        :rtype: list of str
+        """
+        key_list = []
+
+        if prefix:
+            if prefix.endswith('/'):
+                root = os.path.join(LITHOPS_TEMP_DIR,
+                                    bucket_name,
+                                    prefix, '**')
+            else:
+                root = os.path.join(LITHOPS_TEMP_DIR,
+                                    bucket_name,
+                                    prefix+'*', '**')
+        else:
+            root = os.path.join(LITHOPS_TEMP_DIR,
+                                bucket_name, '**')
+
+        for file_name in glob.iglob(root, recursive=True):
+            if os.path.isfile(file_name):
+                if file_name.endswith(prefix):
+                    continue
+                base_dir = os.path.join(LITHOPS_TEMP_DIR, bucket_name, '')
+                key_list.append(file_name.replace(base_dir, '').replace('\\', '/'))
+
+        return key_list

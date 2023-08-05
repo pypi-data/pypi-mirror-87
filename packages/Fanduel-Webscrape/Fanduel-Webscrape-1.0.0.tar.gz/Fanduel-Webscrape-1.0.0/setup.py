7f1eb76e941d90c45820e642e13913a78ab4ddf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:09 +0000

    Add package Fanduel-Webscrape/1.0.0/Fanduel-Webscrape-1.0.0.tar.gz

---
+++ packages/Fanduel-Webscrape/Fanduel-Webscrape-1.0.0.tar.gz/Fanduel-Webscrape-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+setup(
+    name="Fanduel-Webscrape",
+    version="1.0.0",
+    description="A python script that will webscrape historical NBA Fanduel salary info.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nathanhilton/NBAFanduel",
+    author="Nathan Hilton",
+    author_email="nathanhilton24@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["Fanduel_Webscrape"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "Fanduel-Webscrape=Fanduel_Webscrape.MyApp:main",
+        ]
+    },
+)

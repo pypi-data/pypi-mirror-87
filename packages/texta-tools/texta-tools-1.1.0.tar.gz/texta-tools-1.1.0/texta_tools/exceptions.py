0f52a98bef3762ece4ee1d1e0e999358b0e26c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:22 +0000

    Add package texta-tools/1.1.0/texta-tools-1.1.0.tar.gz

---
+++ packages/texta-tools/texta-tools-1.1.0.tar.gz/texta-tools-1.1.0/texta_tools/exceptions.py
@@ -0,0 +1,15 @@
+class MLPNotAvailableError(Exception):
+    """Raised when MLP is not available.""" 
+    pass
+
+class MLPFailedError(Exception):
+    """Raised when MLP processing fails."""
+    pass
+
+class InvalidInputError(Exception):
+    """Raised when something incorrect given to trainers."""
+    pass
+
+class OutOfVocabError(Exception):
+    """Raised when word not present in the vocabulary of embedding."""
+    pass

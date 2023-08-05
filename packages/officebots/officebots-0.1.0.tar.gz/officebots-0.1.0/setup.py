311a2b9ca4907dc81e7172d1198ce87efed3a988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:39 +0000

    Add package officebots/0.1.0/officebots-0.1.0.tar.gz

---
+++ packages/officebots/officebots-0.1.0.tar.gz/officebots-0.1.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="officebots",
+    version="0.1.0",
+    author="Séverin Lemaignan",
+    author_email="severin.lemaignan@brl.ac.uk",
+    description="Python API for the OfficeBots game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.brl.ac.uk/s-lemaignan/officebots-python",
+    package_dir = {"":"src"},
+    packages=["officebots"],
+    scripts = ["scripts/officebots-ros"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

1a1d524011a0574feaeea68d1dab18d7790bf68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package pewlib/0.6.2/pewlib-0.6.2.tar.gz

---
+++ packages/pewlib/pewlib-0.6.2.tar.gz/pewlib-0.6.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setup(
+    name="pewlib",
+    description="Import, processing and export library for LA-ICP-MS data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/djdt/pewlib",
+    author="T. Lockwood",
+    author_email="thomas.lockwood@uts.edu.au",
+    version="0.6.2",
+    packages=find_packages(include=["pewlib", "pewlib.*"]),
+    install_requires=["numpy"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    project_urls={
+        "Documentation": "https://djdt.github.io/pewlib",
+        "Source": "https://gtihub.com/djdt/pewlib",
+    },
+)

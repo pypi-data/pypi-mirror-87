c38133870d5954233f8534618597d11149dcab7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:20 +0000

    Add package psconfig-client/0.1.5/psconfig-client-0.1.5.tar.gz

---
+++ packages/psconfig-client/psconfig-client-0.1.5.tar.gz/psconfig-client-0.1.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+import psconfig
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psconfig-client",
+    version=psconfig.VERSION,
+    author=psconfig.AUTHOR,
+    author_email=psconfig.AUTHOR_EMAIL,
+    description="psconfig/PWA configuration parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marian-babik/psconfig-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+    install_requires=['requests'],
+)

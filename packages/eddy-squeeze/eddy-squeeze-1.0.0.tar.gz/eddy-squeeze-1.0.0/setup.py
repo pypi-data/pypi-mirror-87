a4782198012d48ef166eba6585e6f0065515c066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:02 +0000

    Add package eddy-squeeze/1.0.0/eddy-squeeze-1.0.0.tar.gz

---
+++ packages/eddy-squeeze/eddy-squeeze-1.0.0.tar.gz/eddy-squeeze-1.0.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from os.path import join
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements= open('requirements.txt').read().split()
+
+setuptools.setup(
+    name="eddy-squeeze", # Replace with your own username
+    version="1.0.0",
+    author="Kevin Cho",
+    author_email="kevincho@bwh.harvard.edu",
+    description="Visualize extra information from FSL 6.0.1 eddy outputs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnlbwh/eddy-squeeze",
+    packages=setuptools.find_packages(),
+    package_data={'eddy_squeeze':[
+        join('html_templates','*'),
+        ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements,
+    scripts=['bin/eddy_squeeze']
+)

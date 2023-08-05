9b2aa723ae64ef5cea992a627c3be68fe9a77d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:53 +0000

    Add package childespy/1.0.1/childespy-1.0.1.tar.gz

---
+++ packages/childespy/childespy-1.0.1.tar.gz/childespy-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="childespy",
+    version="1.0.1",
+    url="https://github.com/langcog/childespy",
+    author="Jessica Mankewitz",
+    author_email="jmank@stanford.edu",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["rpy2>=3.3.5", "numpy>=1.19.2", "pandas>=1.1.2"],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU Affero General Public License v3",
+    "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

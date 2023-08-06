83ba41db6b858c5118ad649cf159bbc732af88d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:11 +0000

    Add package decavision/1.1.3/decavision-1.1.3.tar.gz

---
+++ packages/decavision/decavision-1.1.3.tar.gz/decavision-1.1.3/decavision/dataset_preparation/__init__.py
@@ -0,0 +1,2 @@
+from . import data_augmentation
+from . import generate_tfrecords
---
+++ packages/decavision/decavision-1.1.3.tar.gz/decavision-1.1.3/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open('requirements.txt', 'r') as req:
+    requirements = []
+    for line in req:
+        requirements.append(line.strip())
+
+setuptools.setup(
+    name="decavision",
+    version="1.1.3",
+    author="Décathlon Canada",
+    author_email="sportvisionapi@decathlon.com",
+    description="A package to easily train powerful image classification models using colab's free TPUs.",
+    long_description="""The AI team at Décathlon Canada developed a library to help with the training of image 
+                        classification models. It is specially made to exploit the free TPUs that are offered
+                        in Google colab notebooks. You can find the full documentation 
+                        [here](https://decavision-doc.herokuapp.com/)
+                        
+                        
+                        ## Version 1.1.3
+                        Link to public repository
+                        
+                        ## Version 1.1.2
+                        Change name of package
+                        
+                        ## Version 1.1.1
+                        Fix typo in split_train
+                        
+                        ## Version 1.1.0
+                        Remove google scraping
+                        
+                        ## Version 1.0.0
+                        Original release""",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+    url='https://github.com/Decathlon/decavision.git'
+)

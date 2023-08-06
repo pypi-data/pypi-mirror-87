701e2ea1e565d5c04ea5f3c1c27d71ca2a28d6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:05 +0000

    Add package aigar/0.0.2/aigar-0.0.2.tar.gz

---
+++ packages/aigar/aigar-0.0.2.tar.gz/aigar-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read()
+
+setup(
+    name='aigar',
+    version = "0.0.2",
+    author = "Anton Wiehe",
+    author_email = "antonwiehe@gmail.com",
+    description = ("Gym-compatible environments that replicate the aigar.io game to a certain extent."),
+    license = "MIT",
+    keywords = "reinforcement learning, environments, gym",
+    url = "https://github.com/NotNANtoN/aigar",
+    packages = setuptools.find_packages(exclude=['tests', 'tests.*']),
+    install_requires = requirements,
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
+

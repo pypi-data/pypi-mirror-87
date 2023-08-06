a45dfd3d8f88128502882a0b0c1faeaf4341f927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:47 +0000

    Add package fabric-mgmt-cli/0.3/fabric-mgmt-cli-0.3.tar.gz

---
+++ packages/fabric-mgmt-cli/fabric-mgmt-cli-0.3.tar.gz/fabric-mgmt-cli-0.3/fabric/credmgr/swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from fabric.credmgr.swagger_client.api.default_api import DefaultApi
+from fabric.credmgr.swagger_client.api.tokens_api import TokensApi

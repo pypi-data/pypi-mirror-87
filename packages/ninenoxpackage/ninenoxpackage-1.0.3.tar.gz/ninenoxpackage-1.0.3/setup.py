d15d0c7ecf31b347610c4da30f9d4d0dfc05f28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package ninenoxpackage/1.0.3/ninenoxpackage-1.0.3.tar.gz

---
+++ packages/ninenoxpackage/ninenoxpackage-1.0.3.tar.gz/ninenoxpackage-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(name='ninenoxpackage',
+      version='1.0.3',
+      description='Test',
+      author='Ninenox',
+      author_email='nisit15@hotmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)

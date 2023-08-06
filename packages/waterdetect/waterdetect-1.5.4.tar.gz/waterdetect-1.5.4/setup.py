a8da6979815238b2c920822e977203ff18f08d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:24 +0000

    Add package waterdetect/1.5.4/waterdetect-1.5.4.tar.gz

---
+++ packages/waterdetect/waterdetect-1.5.4.tar.gz/waterdetect-1.5.4/setup.py
@@ -0,0 +1,38 @@
+from builtins import RuntimeError
+import setuptools
+
+short_description = 'WaterDetect generates open water cover mask for L2A Sentinel 2 imagery without any a priori knowledge on the scene.'\
+                    ' It can also be used for Landsat 8 images and for other multispectral clustering/segmentation tasks.'
+
+long_description = short_description
+
+setuptools.setup(
+    name="waterdetect", # Replace with your own username
+    version="1.5.4",
+    author="Maurício Cordeiro",
+    author_email="cordmaur@gmail.com",
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cordmaur/WaterDetect",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': ['waterdetect=waterdetect.WaterDetect:main'],
+    },
+    include_package_data=True,
+    package_data={'waterdetect': ['WaterDetect.ini']},
+    install_requires=[
+        'numpy>=1.17',
+        'scikit_learn>=0.23',
+        'matplotlib>=3.3',
+        'PyPDF2>=1.26',
+        'lxml>=4.5.0',
+        'pillow>=7.0.0'
+    ]
+)

de97b48b6139c21bfe44992a0623b10b8bd92611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:45 +0000

    Add package altprint/0.0.1/altprint-0.0.1.tar.gz

---
+++ packages/altprint/altprint-0.0.1.tar.gz/altprint-0.0.1/README.md
@@ -0,0 +1,3 @@
+# altprint
+alternative 3d printing path generator
+create and modify gcode files for use in FFF printers.
---
+++ packages/altprint/altprint-0.0.1.tar.gz/altprint-0.0.1/altprint/flow.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def calculate(w = 0.48, h = 0.2, df = 1.75, adjust = 1):
+    """
+    Calculates the flow multiplier factor, using the rounded rectangle model.
+
+    ARGS:
+    w: raster width (default 0.48mm) (float)
+    h: raster height (default 0.2mm) (float)
+    df: filament diameter (default 1.75mm) (float)
+    adjust: adjust factor (default 100%) (float)
+
+    RETURNS:
+    Flow multiplier factor
+    """
+    a = 4 * w * h + (np.pi - 4) * h**2
+    b = np.pi * df**2
+    flow = adjust * a / b
+    return flow
+
+
+def extrude(x, y, flow):
+    """
+    Generates the extrusion coordinate array.
+
+    ARGS:
+    x: x array (array)
+    y: y array (array)
+    flow: flow multiplier (float)
+    RETURNS:
+    Extrusion coordinate array (array)
+    """
+    extrusion = np.zeros(len(x))
+    for i in range(1, len(x)):
+        dx = abs(x[i] - x[i - 1])
+        dy = abs(y[i] - y[i - 1])
+        extrusion[i] = np.sqrt((dx**2) + (dy**2)) * flow + extrusion[i-1]
+    return extrusion

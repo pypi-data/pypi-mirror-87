8f2768e4f59dd56e7762a58899009809ea56491e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:37 +0000

    Add package psconfig-client/0.1.7/psconfig_client-0.1.7-py3-none-any.whl

---
+++ packages/psconfig-client/psconfig_client-0.1.7-py3-none-any.whl/psconfig/__init__.py
@@ -0,0 +1,8 @@
+AUTHOR = "Marian Babik"
+AUTHOR_EMAIL = "<marian.babik@cern.ch>"
+COPYRIGHT = "Copyright (C) 2015 CERN"
+VERSION = "0.1.7"
+DATE = "24 Nov 2020"
+__author__ = AUTHOR
+__version__ = VERSION
+__date__ = DATE

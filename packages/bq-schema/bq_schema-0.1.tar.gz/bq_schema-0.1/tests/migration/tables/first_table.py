af7bbfbe1c4bf0ac7165a5439249dd911156cca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:44 +0000

    Add package bq-schema/0.1/bq_schema-0.1.tar.gz

---
+++ packages/bq-schema/bq_schema-0.1.tar.gz/bq_schema-0.1/tests/migration/tables/first_table.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from bq_schema.bigquery_table import BigqueryTable
+
+
+@dataclass
+class FirstTableSchema:
+    a_column: int
+
+
+class FirstTable(BigqueryTable):
+    name = "first_table"
+    schema = FirstTableSchema
---
+++ packages/bq-schema/bq_schema-0.1.tar.gz/bq_schema-0.1/tests/migration/tables/nested_modules/second_table.py
@@ -0,0 +1,7 @@
+from google.cloud.bigquery import SchemaField
+from bq_schema.bigquery_table import BigqueryTable
+
+
+class SecondTable(BigqueryTable):
+    name = "second_table"
+    schema = [SchemaField("b_column", "FLOAT")]

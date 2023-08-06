26a0c277c99eb665c3f0774cf22044785eb1f233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:19 +0000

    Add package fyle-rest-auth/0.6.0/fyle-rest-auth-0.6.0.tar.gz

---
+++ packages/fyle-rest-auth/fyle-rest-auth-0.6.0.tar.gz/fyle-rest-auth-0.6.0/fyle_rest_auth/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class AuthToken(models.Model):
+    """
+    Fyle auth tokens
+    """
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT, help_text='User table relation')
+    refresh_token = models.TextField(help_text='Fyle refresh token')

9e150b061cac324def0ce6ff7d3ea69751dde142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:47 +0000

    Add package vcfsynonymous/0.1.3/vcfsynonymous-0.1.3.tar.gz

---
+++ packages/vcfsynonymous/vcfsynonymous-0.1.3.tar.gz/vcfsynonymous-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+
+    name='vcfsynonymous',
+    version='0.1.3',    
+    author="Pierre-Edouard GUERIN",
+    author_email="pierre-edouard.guerin@cefe.cnrs.fr",
+    description="Detect synonymous genetic variants in VCF",
+    licence="MIT",
+    url="https://github.com/Grelot/vcfsynonymous",
+    packages=setuptools.find_packages(),
+    install_requires=['argparse', 'numpy', 'biopython', 'bcbio-gff', 'pyfaidx', 'gffutils', 'PyVCF'],
+
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent"
+     ]
+)

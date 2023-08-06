5ddf375dbf29288c0e347676562493c886a4243a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:37 +0000

    Add package vpype-embroidery/0.0.4/vpype-embroidery-0.0.4.tar.gz

---
+++ packages/vpype-embroidery/vpype-embroidery-0.0.4.tar.gz/vpype-embroidery-0.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="vpype-embroidery",
+    version="0.0.4",
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Tatarize",
+    url="https://github.com/embroidepy/vpype-embroidery/",
+    license=license,
+    packages=["vpype_embroidery"],
+    install_requires=[
+        "click",
+        "vpype",
+        "numpy",
+        "pyembroidery",
+    ],
+    entry_points="""
+            [vpype.plugins]
+            read_emb=vpype_embroidery.read_emb:read_emb
+            write_emb=vpype_embroidery.write_emb:write_emb
+            efill=vpype_embroidery.efill:efill
+        """,
+)

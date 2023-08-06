ff85ba63b05be5dd0c0c5560891ca2327d75a8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:45 +0000

    Add package onednn-devel-cpu-dpcpp-gpu-dpcpp/2021.1.1/onednn_devel_cpu_dpcpp_gpu_dpcpp-2021.1.1-py2.py3-none-win_amd64.whl

---
+++ packages/onednn-devel-cpu-dpcpp-gpu-dpcpp/onednn_devel_cpu_dpcpp_gpu_dpcpp-2021.1.1-py2.py3-none-win_amd64.whl/onednn_devel_cpu_dpcpp_gpu_dpcpp-2021.1.1.data/data/Library/lib/cmake/dnnl/dnnl-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "DNNL::dnnl" for configuration "release"
+set_property(TARGET DNNL::dnnl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(DNNL::dnnl PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/dnnl.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/dnnl.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS DNNL::dnnl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_DNNL::dnnl "${_IMPORT_PREFIX}/lib/dnnl.lib" "${_IMPORT_PREFIX}/bin/dnnl.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)

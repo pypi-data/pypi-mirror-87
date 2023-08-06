b296ba64ad4a944baad020339326548a6cbc7157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package hypernets/0.1.6/hypernets-0.1.6-py3-none-any.whl

---
+++ packages/hypernets/hypernets-0.1.6-py3-none-any.whl/hypernets/model/hyper_model.py
@@ -0,0 +1,206 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+import hashlib
+import time
+import traceback
+from collections import UserDict
+
+from ..core.meta_learner import MetaLearner
+from ..core.trial import *
+from ..dispatchers import get_dispatcher
+from ..utils import logging
+
+logger = logging.get_logger(__name__)
+
+
+class HyperModel():
+    def __init__(self, searcher, dispatcher=None, callbacks=[], reward_metric=None, task=None):
+        # self.searcher = self._build_searcher(searcher, space_fn)
+        self.searcher = searcher
+        self.dispatcher = dispatcher
+        self.callbacks = callbacks
+        self.reward_metric = reward_metric
+        self.history = TrailHistory(searcher.optimize_direction)
+        self.start_search_time = None
+        self.task = task
+
+    def _get_estimator(self, space_sample):
+        raise NotImplementedError
+
+    def load_estimator(self, model_file):
+        raise NotImplementedError
+
+    def _run_trial(self, space_sample, trail_no, X, y, X_val, y_val, model_file, **fit_kwargs):
+
+        start_time = time.time()
+        estimator = self._get_estimator(space_sample)
+
+        for callback in self.callbacks:
+            callback.on_build_estimator(self, space_sample, estimator, trail_no)
+        #     callback.on_trail_begin(self, space_sample, trail_no)
+        fit_succeed = False
+        try:
+            estimator.fit(X, y, **fit_kwargs)
+            fit_succeed = True
+        except Exception as e:
+            logger.error('Estimator fit failed!')
+            logger.error(e)
+            track = traceback.format_exc()
+            logger.error(track)
+
+        if fit_succeed:
+            metrics = estimator.evaluate(X_val, y_val, metrics=[self.reward_metric])
+            reward = self._get_reward(metrics, self.reward_metric)
+
+            if model_file is None or len(model_file) == 0:
+                model_file = '%05d_%s.pkl' % (trail_no, space_sample.space_id)
+            estimator.save(model_file)
+
+            elapsed = time.time() - start_time
+
+            trail = Trail(space_sample, trail_no, reward, elapsed, model_file)
+
+            # improved = self.history.append(trail)
+
+            self.searcher.update_result(space_sample, reward)
+
+            # for callback in self.callbacks:
+            #     callback.on_trail_end(self, space_sample, trail_no, reward, improved, elapsed)
+        else:
+            # for callback in self.callbacks:
+            #     callback.on_trail_error(self, space_sample, trail_no)
+
+            elapsed = time.time() - start_time
+            trail = Trail(space_sample, trail_no, 0, elapsed)
+
+        return trail
+
+    def _get_reward(self, value, key=None):
+        def cast_float(value):
+            try:
+                fv = float(value)
+                return fv
+            except TypeError:
+                return None
+
+        if key is None:
+            key = 'reward'
+
+        fv = cast_float(value)
+        if fv is not None:
+            reward = fv
+        elif (isinstance(value, dict) or isinstance(value, UserDict)) and key in value and cast_float(
+                value[key]) is not None:
+            reward = cast_float(value[key])
+        else:
+            raise ValueError(
+                f'[value] should be a numeric or a dict which has a key named "{key}" whose value is a numeric.')
+        return reward
+
+    def get_best_trail(self):
+        return self.history.get_best()
+
+    @property
+    def best_reward(self):
+        best = self.get_best_trail()
+        if best is not None:
+            return best.reward
+        else:
+            return None
+
+    def get_top_trails(self, top_n):
+        return self.history.get_top(top_n)
+
+    def _before_search(self):
+        pass
+
+    def _after_search(self, last_trail_no):
+        pass
+
+    def search(self, X, y, X_eval, y_eval, max_trails=10, dataset_id=None, trail_store=None, **fit_kwargs):
+        self.start_search_time = time.time()
+
+        self.task, _ = self.infer_task_type(y)
+
+        if dataset_id is None:
+            dataset_id = self.generate_dataset_id(X, y)
+        if self.searcher.use_meta_learner:
+            self.searcher.set_meta_learner(MetaLearner(self.history, dataset_id, trail_store))
+
+        self._before_search()
+
+        dispatcher = self.dispatcher if self.dispatcher else get_dispatcher(self)
+        trail_no = dispatcher.dispatch(self, X, y, X_eval, y_eval, max_trails, dataset_id, trail_store, **fit_kwargs)
+
+        self._after_search(trail_no)
+
+    def generate_dataset_id(self, X, y):
+        repr = ''
+        if X is not None:
+            if isinstance(X, list):
+                repr += f'X len({len(X)})|'
+            if hasattr(X, 'shape'):
+                repr += f'X shape{X.shape}|'
+            if hasattr(X, 'dtypes'):
+                repr += f'x.dtypes({list(X.dtypes)})|'
+
+        if y is not None:
+            if isinstance(y, list):
+                repr += f'y len({len(y)})|'
+            if hasattr(y, 'shape'):
+                repr += f'y shape{y.shape}|'
+
+            if hasattr(y, 'dtype'):
+                repr += f'y.dtype({y.dtype})|'
+
+        sign = hashlib.md5(repr.encode('utf-8')).hexdigest()
+        return sign
+
+    def final_train(self, space_sample, X, y, **kwargs):
+        estimator = self._get_estimator(space_sample)
+        estimator.fit(X, y, **kwargs)
+        return estimator
+
+    def export_configuration(self, trials):
+        configurations = []
+        for trail in trials:
+            configurations.append(self.export_trail_configuration(trail))
+        return configurations
+
+    def export_trail_configuration(self, trail):
+        raise NotImplementedError
+
+    def infer_task_type(self, y):
+        if len(y.shape) > 1 and y.shape[-1] > 1:
+            labels = list(range(y.shape[-1]))
+            task = 'multilable'
+            return task, labels
+
+        uniques = set(y)
+        n_unique = len(uniques)
+        labels = []
+
+        if n_unique == 2:
+            logger.info(f'2 class detected, {uniques}, so inferred as a [binary classification] task')
+            task = 'binary'  # TASK_BINARY
+            labels = sorted(uniques)
+        else:
+            if y.dtype == 'float':
+                logger.info(f'Target column type is float, so inferred as a [regression] task.')
+                task = 'regression'
+            else:
+                if n_unique > 1000:
+                    if 'int' in y.dtype:
+                        logger.info(
+                            'The number of classes exceeds 1000 and column type is int, so inferred as a [regression] task ')
+                        task = 'regression'
+                    else:
+                        raise ValueError(
+                            'The number of classes exceeds 1000, please confirm whether your predict target is correct ')
+                else:
+                    logger.info(f'{n_unique} class detected, inferred as a [multiclass classification] task')
+                    task = 'multiclass'
+                    labels = sorted(uniques)
+        return task, labels
---
+++ packages/hypernets/hypernets-0.1.6-py3-none-any.whl/hypernets/searchers/evolution_searcher.py
@@ -0,0 +1,168 @@
+# -*- coding:utf-8 -*-
+"""
+
+"""
+import numpy as np
+
+from ..core.searcher import Searcher, OptimizeDirection
+from ..utils import logging
+
+logger = logging.get_logger(__name__)
+
+
+class Individual(object):
+    def __init__(self, space_sample, reward):
+        self.space_sample = space_sample
+        self.reward = reward
+
+    def mutate(self):
+        pass
+
+
+class Population(object):
+    def __init__(self, size=50, optimize_direction=OptimizeDirection.Minimize):
+        assert isinstance(size, int)
+        assert size > 0
+        self.size = size
+        self.populations = []
+        self.optimize_direction = optimize_direction
+
+    @property
+    def initializing(self):
+        return not len(self.populations) >= self.size
+
+    @property
+    def length(self):
+        return len(self.populations)
+
+    def append(self, space_sample, reward):
+        individual = Individual(space_sample, reward)
+        self.populations.append(individual)
+
+    def sample_best(self, sample_size, random_state=np.random.RandomState()):
+        if sample_size > self.length:
+            sample_size = self.length
+        indices = sorted(random_state.choice(range(self.length), sample_size))
+        samples = [self.populations[i] for i in indices]
+        best = \
+            sorted(samples,
+                   key=lambda i: i.reward if self.optimize_direction == OptimizeDirection.Minimize else -i.reward)[
+                0]
+        return best
+
+    def eliminate(self, num=1, regularized=False):
+        eliminates = []
+        for i in range(num):
+            if self.length <= 0:
+                break
+            if regularized:
+                # eliminate oldest
+                eliminates.append(self.populations.pop(0))
+            else:
+                # eliminate worst
+                worst = sorted(self.populations, key=lambda
+                    i: -i.reward if self.optimize_direction == OptimizeDirection.Minimize else i.reward)[
+                    0]
+                self.populations.remove(worst)
+                eliminates.append(worst)
+        return eliminates
+
+    def shuffle(self):
+        np.random.shuffle(self.populations)
+
+    def mutate(self, parent_space, offspring_space):
+        assert parent_space.all_assigned
+        parent_params = parent_space.get_assigned_params()
+        pos = np.random.randint(0, len(parent_params))
+        for i, hp in enumerate(offspring_space.params_iterator):
+            if i > (len(parent_params) - 1) or not parent_params[i].same_config(hp):
+                hp.random_sample()
+            else:
+                if i == pos:
+                    new_value = hp.random_sample(assign=False)
+                    while new_value == parent_params[i].value:
+                        new_value = hp.random_sample(assign=False)
+                    hp.assign(new_value)
+                else:
+                    hp.assign(parent_params[i].value)
+        return offspring_space
+
+
+class EvolutionSearcher(Searcher):
+    def __init__(self, space_fn, population_size, sample_size, regularized=False,
+                 candidates_size=10, optimize_direction=OptimizeDirection.Minimize, use_meta_learner=True,
+                 space_sample_validation_fn=None):
+        Searcher.__init__(self, space_fn=space_fn, optimize_direction=optimize_direction,
+                          use_meta_learner=use_meta_learner, space_sample_validation_fn=space_sample_validation_fn)
+        self.population = Population(size=population_size, optimize_direction=optimize_direction)
+        self.sample_size = sample_size
+        self.regularized = regularized
+        self.candidate_size = candidates_size
+
+    @property
+    def parallelizable(self):
+        return True
+
+    def sample(self):
+        if self.population.initializing:
+            space_sample = self._sample_and_check(self._random_sample)
+            return space_sample
+        else:
+            best = self.population.sample_best(self.sample_size)
+            offspring = self._get_offspring(best.space_sample)
+            if offspring is not None:
+                return offspring
+            else:
+                self.population.populations.remove(best)
+                space_sample = self._sample_and_check(self._random_sample)
+                return space_sample
+
+    def _get_offspring(self, space_sample):
+        if self.use_meta_learner and self.meta_learner is not None:
+            candidates = []
+            scores = []
+            no = 0
+            for i in range(self.candidate_size):
+                new_space = self.space_fn()
+                try:
+                    candidate = self._sample_and_check(lambda: self.population.mutate(space_sample, new_space))
+                    candidates.append(candidate)
+                    scores.append((no, self.meta_learner.predict(candidate)))
+                    no += 1
+                except:
+                    pass
+            if len(candidates) <= 0:
+                return None
+
+            topn = sorted(scores,
+                          key=lambda s: s[1] if self.optimize_direction == OptimizeDirection.Minimize else -s[1])[
+                   :int(len(candidates) * 0.3)]
+            best = topn[np.random.choice(range(len(topn)))]
+
+            if logger.is_info_enabled():
+                logger.info(f'get_offspring scores:{best[1]}, index:{best[0]}')
+
+            return candidates[best[0]]
+        else:
+            new_space = self.space_fn()
+            try:
+                candidate = self._sample_and_check(lambda: self.population.mutate(space_sample, new_space))
+                return candidate
+            except:
+                return None
+
+    def update_result(self, space_sample, result):
+        if not self.population.initializing:
+            self.population.eliminate(regularized=self.regularized)
+        self.population.append(space_sample, result)
+        if self.use_meta_learner and self.meta_learner is not None:
+            assert self.meta_learner is not None
+            self.meta_learner.new_sample(space_sample)
+
+    def summary(self):
+        summary = '\n'.join(
+            [f'vectors:{",".join([str(v) for v in individual.space_sample.vectors])}     reward:{individual.reward} '
+             for
+             individual in
+             self.population.populations])
+        return summary

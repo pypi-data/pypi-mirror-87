a033859f69440aa756aa72b691a4613f5e33f210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:26 +0000

    Add package redis-rate-limiter/0.1.3/redis-rate-limiter-0.1.3.tar.gz

---
+++ packages/redis-rate-limiter/redis-rate-limiter-0.1.3.tar.gz/redis-rate-limiter-0.1.3/redis_rate_limiter/redis_client.py
@@ -0,0 +1,20 @@
+from redis import Redis
+
+from redis_rate_limiter.exceptions import ConfigError
+
+from .config import settings
+
+wrapper = dict(client=None)
+
+
+def get_redis_client():
+    if wrapper["client"]:
+        return wrapper["client"]
+    if not settings.redis_url:
+        raise ConfigError()
+    client = Redis.from_url(settings.redis_url)
+    assert client.ping()
+    return client
+
+
+redis_client = get_redis_client()

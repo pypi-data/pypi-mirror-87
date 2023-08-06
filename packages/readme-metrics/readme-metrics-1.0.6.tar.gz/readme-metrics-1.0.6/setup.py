f4a409b08a1c08e64ff4dc5edb780df091e12bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:20 +0000

    Add package readme-metrics/1.0.6/readme-metrics-1.0.6.tar.gz

---
+++ packages/readme-metrics/readme-metrics-1.0.6.tar.gz/readme-metrics-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="readme-metrics",
+    version="1.0.6",
+    author="ReadMe",
+    author_email="support@readme.io",
+    description="ReadMe API Metrics WSGI SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/readmeio/metrics-sdks/tree/master/packages/python",
+    packages=["readme_metrics"],
+    install_requires=["werkzeug", "requests"],
+)

4f98b0c76a72c49177754c517109fd18286aee5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:00 +0000

    Add package auraxium/0.1.0a3/auraxium-0.1.0a3.tar.gz

---
+++ packages/auraxium/auraxium-0.1.0a3.tar.gz/auraxium-0.1.0a3/setup.py
@@ -0,0 +1,31 @@
+# type: ignore
+"""Package deployment script."""
+
+import setuptools
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setuptools.setup(name='auraxium',
+                 version='0.1.0a3',
+                 author='Leonhard S.',
+                 author_email='leonhard-sei@outlook.com',
+                 description='A python wrapper for the PlanetSide 2 Census API.',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 keywords='auraxium python daybreak census planetside ps2',
+                 url='https://github.com/leonhard-s/auraxium',
+                 packages=setuptools.find_packages(),
+                 package_data={'auraxium': ['py.typed']},
+                 classifiers=['Development Status :: 3 - Alpha',
+                              'Programming Language :: Python :: 3.8',
+                              'License :: OSI Approved :: MIT License',
+                              'Operating System :: OS Independent'],
+                 install_requires=['aiohttp',
+                                   'backoff',
+                                   'pydantic',
+                                   'yarl',
+                                   'websockets'],
+                 license='MIT',
+                 include_package_data=True,
+                 zip_safe=False)

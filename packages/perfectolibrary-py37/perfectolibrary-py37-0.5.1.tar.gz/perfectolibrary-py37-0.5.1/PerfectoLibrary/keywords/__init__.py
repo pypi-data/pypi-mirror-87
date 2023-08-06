da221eceae76d858ef7c104c3fdeb3df9326cbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:20 +0000

    Add package perfectolibrary-py37/0.5.1/perfectolibrary-py37-0.5.1.tar.gz

---
+++ packages/perfectolibrary-py37/perfectolibrary-py37-0.5.1.tar.gz/perfectolibrary-py37-0.5.1/PerfectoLibrary/keywords/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+from ._devices import _DeviceKeywords
+from ._rest_acts import _RestKeywords
+from ._general  import _GeneralKeywords
+
+__all__ = ["_DeviceKeywords","_RestKeywords","_GeneralKeywords"]

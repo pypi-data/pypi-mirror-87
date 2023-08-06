49616c3c21c7bba957e62ca528406bbda9104ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:10 +0000

    Add package maestral/1.3.2/maestral-1.3.2-py3-none-any.whl

---
+++ packages/maestral/maestral-1.3.2-py3-none-any.whl/maestral/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+
+The following APIs should remain stable for frontends:
+
+* maestral.main.Maestral
+* maestral.constants
+* maestral.daemon
+* maestral.errors
+* maestral.utils.appdirs
+* maestral.utils.autostart
+
+"""
+
+import warnings
+
+__version__ = "1.3.2"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/SamSchott/maestral"
+
+
+# suppress Python 3.9 warning from rubicon-objc
+warnings.filterwarnings("ignore", module="rubicon", category=UserWarning)

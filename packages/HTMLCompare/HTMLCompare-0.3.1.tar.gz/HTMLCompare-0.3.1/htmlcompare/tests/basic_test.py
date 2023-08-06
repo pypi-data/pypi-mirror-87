8bc666f0cd42922ba80504c0dfd0a24dbc99d0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:17 +0000

    Add package HTMLCompare/0.3.1/HTMLCompare-0.3.1.tar.gz

---
+++ packages/HTMLCompare/HTMLCompare-0.3.1.tar.gz/HTMLCompare-0.3.1/htmlcompare/tests/basic_test.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from unittest.case import TestCase
+
+from .. import assert_different_html, assert_same_html
+
+
+class BasicTest(TestCase):
+    def test_can_compare_single_tag(self):
+        assert_same_html('<div></div>', '<div></div>')
+
+    def test_can_compare_trivial_html(self):
+        expected_html = '''<html>
+        <body>
+            Foo bar
+        </body>
+        </html>'''
+
+        actual_html = '''
+        <html><body>
+         Foo bar</body></html>
+        '''
+        assert_same_html(expected_html, actual_html)
+
+    def test_can_detect_different_tags(self):
+        expected_html = '<foo />'
+        actual_html   = '<bar />'
+        assert_different_html(expected_html, actual_html)
+
+    def test_can_detect_different_attributes(self):
+        expected_html = '<div style="color: green" />'
+        actual_html = '<div style="color: red" />'
+        assert_different_html(expected_html, actual_html)
+
+    def test_ignores_attribute_ordering(self):
+        expected_html = '<div data-hidden="true" class="hidden" />'
+        actual_html = '<div class="hidden" data-hidden="true" />'
+        assert_same_html(expected_html, actual_html)
+
+    def test_can_detect_missing_tag(self):
+        expected_html = '<div><b>foo</b></div>'
+        actual_html = '<div>foo</div>'
+        assert_different_html(expected_html, actual_html)
+        with self.assertRaises(AssertionError, msg='check for stringification errors'):
+            assert_same_html(expected_html, actual_html, verbose=False)
+
+    def test_can_detect_missing_tag_contents(self):
+        expected_html = '<div><b>foo</b></div>'
+        actual_html = '<div></div>'
+        assert_different_html(expected_html, actual_html)
+        with self.assertRaises(AssertionError, msg='check for stringification errors'):
+            assert_same_html(expected_html, actual_html, verbose=False)
+
+    def test_can_detect_missing_characters(self):
+        expected_html = '<div>foo</div>'
+        actual_html = '<div></div>'
+        assert_different_html(expected_html, actual_html)
+        with self.assertRaises(AssertionError, msg='check for stringification errors'):
+            assert_same_html(expected_html, actual_html, verbose=False)
+
+    def test_can_ignore_whitespace_after_comment(self):
+        actual_html = '''<div>
+            <!-- comment with extra whitespace before next tag -->
+            
+            <b>foo</b>
+        </div>'''
+        expected_html = '<div><b>foo</b></div>'
+        assert_same_html(expected_html, actual_html)
+
---
+++ packages/HTMLCompare/HTMLCompare-0.3.1.tar.gz/HTMLCompare-0.3.1/htmlcompare/tests/html_style_attribute_test.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: MIT
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+from unittest.case import skip, skipIf as skip_if, TestCase
+
+from .. import assert_different_html, assert_same_html
+from ..compare_css import has_tinycss
+
+
+class HTMLStyleAttributeTest(TestCase):
+    def test_can_ignore_empty_style_attribute(self):
+        assert_different_html('<div style="color: red" />', '<div />')
+        assert_same_html('<div style="" />', '<div />')
+        # also check "EmptyTag"
+        assert_same_html('<img style="">', '<img>')
+
+    def test_can_ignore_additional_semicolon_in_style(self):
+        assert_same_html('<div style="color: red;" />', '<div style="color: red" />')
+        # also check "EmptyTag"
+        assert_same_html('<img style="color: red;">', '<img style="color: red" >')
+
+    @skip_if(not has_tinycss, reason='test requires tinycss2')
+    def test_ignores_ordering_of_style_declarations(self):
+        assert_same_html(
+            '<div style="color: red; font-weight: bold" />',
+            '<div style="font-weight:bold; color: red" />')
+
+    @skip('shorthand hex color matching not yet implemented')
+    def test_can_handle_shorthand_hex_colors(self):
+        assert_same_html('<div style="color: #f60" />', '<div style="color: #ff6600" />')
+

29087b28c202e0a331fd6eb7169c89e6d214bc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package handy-utilities/2.0.0/handy_utilities-2.0.0-py3-none-any.whl

---
+++ packages/handy-utilities/handy_utilities-2.0.0-py3-none-any.whl/my_utilities/NestedLists.py
@@ -0,0 +1,23 @@
+"""
+
+This function prints all elements of a list and its child lists if any.
+Before executing it also checks if the root argument is a list or not.
+
+"""
+
+def printNestedList(meraList, indent=False, level=0, whereToWrite=sys.stdout):
+
+    if(isinstance(meraList, list)):
+
+        for each_item in meraList:
+            if isinstance(each_item, list):
+                printNestedList(each_item, indent, level+1, whereToWrite)
+            else:
+                if(indent):
+                    for i in range(level):
+                        print("\t", end='', file=whereToWrite)
+                    print(each_item, file=whereToWrite)
+                else:
+                    print(each_item, file=whereToWrite)
+    else:
+        print(meraList+" is not a list")

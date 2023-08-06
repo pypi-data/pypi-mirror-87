1e1b7098c3e48079e99941769d85d840ac1bc8df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:41 +0000

    Add package grant/1.2.1/grant-1.2.1-py3-none-any.whl

---
+++ packages/grant/grant-1.2.1-py3-none-any.whl/grant/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+作者：姜铄
+日期：2020年12月01日
+"""
+
+# !/usr/bin/env python
+
+
+def run():
+    print('This is a package that empowers users！')
+    print('Please run the main function directly！')
+
+
+if __name__ == '__main__':
+    run()

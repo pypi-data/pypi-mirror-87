d018d017c8dae82e0ec10992b0d47d2f8437728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:52 +0000

    Add package SCoinAPI/1.1.7/SCoinAPI-1.1.7.tar.gz

---
+++ packages/SCoinAPI/SCoinAPI-1.1.7.tar.gz/SCoinAPI-1.1.7/test.py
@@ -0,0 +1,8 @@
+# from src import centralbank
+
+# ck = centralbank.Central_Bank()
+# res = ck.send_token('nUjymFTzrJcuqizbswdKACJtpRwMZtEkwXzQDxBk','cb','marmotbank',40)
+# res = ck.set_central_bank('marmotbank','jiayingfengtingweijie')
+# res = ck.create_did('marmotbank','jiayingfengtingweijie')
+# res = ck.get_balance('sefx2ever')
+# print(res)

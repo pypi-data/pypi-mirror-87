5b2b85ce7d9fe9685162895945e6043251c964d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:49 +0000

    Add package pycmc/0.0.11/pycmc-0.0.11.tar.gz

---
+++ packages/pycmc/pycmc-0.0.11.tar.gz/pycmc-0.0.11/pycmc/__init__.py
@@ -0,0 +1,32 @@
+from . import credentials
+
+# set the creds dir to HOME
+
+import os
+
+from . import credentials_manager
+
+from .charts import (
+    youtube,
+    applemusic,
+    spotify,
+    shazam,
+    soundcloud,
+    beatport,
+    itunes,
+    youtube,
+    deezer,
+    cm_score,
+    amazon,
+    qq,
+)
+
+from . import track
+from . import album
+from . import artist
+from . import utilities
+from . import curator
+from . import playlist
+from . import chart_cleaners
+from . import recommendation
+from . import search_engine

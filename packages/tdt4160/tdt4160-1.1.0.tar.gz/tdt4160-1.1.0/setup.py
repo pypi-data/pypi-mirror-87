eeb59b46d32c5da5e281a3f7b89c7161337b9750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:58 +0000

    Add package tdt4160/1.1.0/tdt4160-1.1.0.tar.gz

---
+++ packages/tdt4160/tdt4160-1.1.0.tar.gz/tdt4160-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tdt4160",
+    version="1.1.0",
+    author="Fisherman's Friend",
+    author_email="fish@waifu.club",
+    description="Easy simulations for lower level tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitgud.io/fish/tdt4160",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.7"
+)

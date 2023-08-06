9525be2bf6be26c7254cde91f4edf754b209ee81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:18 +0000

    Add package texsurgery/0.0.5/texsurgery-0.0.5-py3-none-any.whl

---
+++ packages/texsurgery/texsurgery-0.0.5-py3-none-any.whl/tests/test_python3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import logging
+
+from texsurgery.texsurgery import TexSurgery
+
+class TestPython3Surgery(unittest.TestCase):
+    """ Tests TexSurgery.code_surgery for the python3 kernel"""
+
+    def test_simple_addition(self):
+        """ Tests a simple addition"""
+        tex_source = r'\usepackage[python3]{texsurgery}2+2=\eval{2+2}'
+        tex_out = r'2+2=4'
+        ts = TexSurgery(tex_source).code_surgery()
+        self.assertEqual(ts.src, tex_out)
+        del ts #shutdow kernel
+
+    def test_division(self):
+        """ Tests a simple addition"""
+        tex_source = r'\usepackage[python3]{texsurgery}1/2=\eval{1/2}'
+        tex_out = r'1/2=0.5'
+        ts = TexSurgery(tex_source).code_surgery()
+        self.assertEqual(ts.src, tex_out)
+        del ts #shutdow kernel
+
+    def test_nested_brackets(self):
+        """ Tests an expression eval{ with {nested} brackets }"""
+        tex_source = r"\usepackage[sagemath]{texsurgery}The first prime number is \eval{sorted({7,3,5,2})[0]}"
+        tex_out = r'The first prime number is 2'
+        ts = TexSurgery(tex_source).code_surgery()
+        self.assertEqual(ts.src, tex_out)
+        del ts #shutdow kernel
+
+    def test_return_str(self):
+        """evalstr{'python string'} should not return the quotes"""
+        tex_source = r"\usepackage[sagemath]{texsurgery}My favourite colour is \evalstr{'blue'}"
+        tex_out = r'My favourite colour is blue'
+        ts = TexSurgery(tex_source).code_surgery()
+        self.assertEqual(ts.src, tex_out)
+        del ts #shutdow kernel

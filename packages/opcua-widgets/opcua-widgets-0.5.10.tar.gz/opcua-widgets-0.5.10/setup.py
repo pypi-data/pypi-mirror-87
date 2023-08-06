c1456669df7809112ca5318a3ea156427a6244f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:58 +0000

    Add package opcua-widgets/0.5.10/opcua-widgets-0.5.10.tar.gz

---
+++ packages/opcua-widgets/opcua-widgets-0.5.10.tar.gz/opcua-widgets-0.5.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name="opcua-widgets",
+      version="0.5.10",
+      description="OPC-UA Widgets",
+      author="Olivier R-D et al.",
+      url='https://github.com/FreeOpcUa/opcua-widgets',
+      packages=["uawidgets"],
+      license="GNU General Public License",
+      install_requires=["opcua"],
+      )

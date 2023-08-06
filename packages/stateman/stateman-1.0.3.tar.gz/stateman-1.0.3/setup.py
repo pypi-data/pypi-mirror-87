212e31df27893c3cadf00b54f1fa8142472ee524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:12 +0000

    Add package stateman/1.0.3/stateman-1.0.3.tar.gz

---
+++ packages/stateman/stateman-1.0.3.tar.gz/stateman-1.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['stateman']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['APPLICATION-NAME = entry:main']}
+
+setup_kwargs = {
+    'name': 'stateman',
+    'version': '1.0.3',
+    'description': 'Creating binary patch files containing changes in the directory',
+    'long_description': None,
+    'author': 'pukhov-vi',
+    'author_email': 'vipukhov@sberbank.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

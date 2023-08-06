3dd40f5431478fa2f3c9fd0e5da72aee66a46b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:10 +0000

    Add package zkg/2.5.0/zkg-2.5.0-py2.py3-none-any.whl

---
+++ packages/zkg/zkg-2.5.0-py2.py3-none-any.whl/bropkg/manager.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import sys
+
+from zeekpkg.manager import *
+
+print("Warning: the 'bropkg.manager' module is deprecated use 'zeekpkg.manager' instead.",
+      file=sys.stderr)
---
+++ packages/zkg/zkg-2.5.0-py2.py3-none-any.whl/bropkg/package.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import sys
+
+from zeekpkg.package import *
+
+print("Warning: the 'bropkg.package' module is deprecated use 'zeekpkg.package' instead.",
+      file=sys.stderr)

62e708df2873f107847f72cb32c545a3a9cc852c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:13 +0000

    Add package romaincornuconsulting.static-website/0.0.3/romaincornuconsulting.static-website-0.0.3.tar.gz

---
+++ packages/romaincornuconsulting.static-website/romaincornuconsulting.static-website-0.0.3.tar.gz/romaincornuconsulting.static-website-0.0.3/static_website/static_website.py
@@ -0,0 +1,9 @@
+from aws_cdk.core import Construct, CfnOutput
+
+
+class StaticWebsite(Construct):
+
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+
+        CfnOutput(self, "Output", value='output')

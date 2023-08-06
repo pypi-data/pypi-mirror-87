12cdacd7c255f07cf4836cf236de57f6781052cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:02 +0000

    Add package python-liquid/0.5.5/python-liquid-0.5.5.tar.gz

---
+++ packages/python-liquid/python-liquid-0.5.5.tar.gz/python-liquid-0.5.5/setup.py
@@ -0,0 +1,34 @@
+import re
+import setuptools
+
+
+with open("liquid/__init__.py", "r") as fd:
+    match = re.search(r'__version__ = "([0-5\.]+)"', fd.read())
+    __version__ = match.group(1)
+
+with open("README.rst", "r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="python-liquid",
+    version=__version__,
+    description="A Python template engine for the Liquid markup language.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/jg-rp/liquid",
+    packages=setuptools.find_packages(
+        include=[
+            "liquid",
+            "liquid.builtin",
+            "liquid.utils",
+            "liquid.builtin.tags",
+            "liquid.builtin.drops",
+            "liquid.builtin.filters",
+        ]
+    ),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["python-dateutil>=2.8.1"],
+    test_suite="tests",
+    python_requires=">=3.8",
+)

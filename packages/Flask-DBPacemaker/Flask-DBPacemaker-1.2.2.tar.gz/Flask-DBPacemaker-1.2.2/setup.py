350b7345033f5557366514e52019a1436dd3498f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:46 +0000

    Add package Flask-DBPacemaker/1.2.2/Flask-DBPacemaker-1.2.2.tar.gz

---
+++ packages/Flask-DBPacemaker/Flask-DBPacemaker-1.2.2.tar.gz/Flask-DBPacemaker-1.2.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Flask-DBPacemaker",
+    version="1.2.2",
+    author="Ron Chang",
+    author_email="ron.hsien.chang@gmail.com",
+    description="Hook db connection when you're running a long term sleep crawler assignment.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ron-Chang/flask_dbpacemaker",
+    packages=find_packages(),
+    license='MIT',
+    python_requires='>=3.6',
+    exclude_package_date={'':['.gitignore', 'dev', 'test', 'setup.py']},
+    install_requires=[
+        'Flask>=1.0.2',
+        'Flask-APScheduler>=1.11.0',
+        'Flask-SQLAlchemy>=2.3.2'
+    ]
+)

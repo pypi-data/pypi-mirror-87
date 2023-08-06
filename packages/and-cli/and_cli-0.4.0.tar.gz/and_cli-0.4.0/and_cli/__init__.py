e2bbdd3857967756a4efe740052fd74cfab13363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:44 +0000

    Add package and-cli/0.4.0/and_cli-0.4.0.tar.gz

---
+++ packages/and-cli/and_cli-0.4.0.tar.gz/and_cli-0.4.0/and_cli/__init__.py
@@ -0,0 +1,24 @@
+from importlib.metadata import version
+
+__version__ = version(__name__)
+
+NUM_COLS = 80
+
+CPP_TEMPLATE = """\
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    /* code */
+}
+"""

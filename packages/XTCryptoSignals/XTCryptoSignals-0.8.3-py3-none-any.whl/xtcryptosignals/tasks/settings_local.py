d807c411345965d0b498f1c32567278001200422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:10 +0000

    Add package XTCryptoSignals/0.8.3/XTCryptoSignals-0.8.3-py3-none-any.whl

---
+++ packages/XTCryptoSignals/XTCryptoSignals-0.8.3-py3-none-any.whl/xtcryptosignals/tasks/settings_local.py
@@ -0,0 +1,22 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = [
+    "Paulo Antunes",
+]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+# Add the following Personal Exchange keys in order to be able to Trade
+
+BINANCE_API_KEY = ""
+BINANCE_API_SECRET = ""
+
+KUCOIN_API_KEY = ""
+KUCOIN_API_SECRET = ""
+KUCOIN_API_PASSPHRASE = ""
+
+IDEX_API_KEY = ""
+IDEX_ADDRESS = ""
+IDEX_PRIVATE_KEY = ""

6392be4fd872dadde8fbd56214c744ef397b48a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:30 +0000

    Add package exetera/0.4.0/exetera-0.4.0.tar.gz

---
+++ packages/exetera/exetera-0.4.0.tar.gz/exetera-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+from exetera import __version__
+
+setup(
+    name='exetera',
+    version=__version__,
+    description='High-volume key-value store and analytics, based on hdf5',
+    long_description="file: README.md",
+    long_description_content_type = "text/markdown",
+    url='https://github.com/kcl-bmeis/ExeTera',
+    author='Ben Murray',
+    author_email='benjamin.murray@kcl.ac.uk',
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    packages=['exetera', 'exetera.core', 'exetera.processing'],
+    scripts=['exetera/bin/exetera'],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'numba',
+        'pandas',
+        'h5py'
+    ]
+)

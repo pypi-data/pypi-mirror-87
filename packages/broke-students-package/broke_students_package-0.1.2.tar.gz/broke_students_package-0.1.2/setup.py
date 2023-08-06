c3122c945825418eadca2d42f22bc85c91695597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:17 +0000

    Add package broke-students-package/0.1.2/broke_students_package-0.1.2.tar.gz

---
+++ packages/broke-students-package/broke_students_package-0.1.2.tar.gz/broke_students_package-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='broke_students_package',
+    packages=find_packages(include=['broke_students_package']),
+    version='0.1.2',
+    description='A Library that shall make you Money....., Or atleast plot it',
+    author='The Broke Student Brokers',
+    license='MIT',
+    install_requires=['datetime','numpy','alpaca_trade_api','pandas','matplotlib'],
+)

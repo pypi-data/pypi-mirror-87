c55dfa8ab6d152e4d9316e4b28856cd29a3f508e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:55 +0000

    Add package insnail_ai_tools/0.0.16/insnail_ai_tools-0.0.16-py3-none-any.whl

---
+++ packages/insnail_ai_tools/insnail_ai_tools-0.0.16-py3-none-any.whl/tests/test_decorator.py
@@ -0,0 +1,25 @@
+import pytest
+import time
+from insnail_ai_tools.decorator import timeit_decorator
+
+
+@timeit_decorator
+def func1():
+    time.sleep(1)
+    return 1
+
+
+@timeit_decorator
+async def func2():
+    time.sleep(1)
+    return 1
+
+
+def test_timeit_decorator():
+    assert func1() == 1
+
+
+@pytest.mark.asyncio
+async def test_timeit_decorator_async():
+    result = await func2()
+    assert result == 1

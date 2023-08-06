8e83cec84f2f70ec69e130548e4ac90e717633a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package django-lightning/1.0.0/django-lightning-1.0.0.tar.gz

---
+++ packages/django-lightning/django-lightning-1.0.0.tar.gz/django-lightning-1.0.0/lightning/urls.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from django.http import FileResponse, HttpResponseNotFound
+from django.urls import re_path, path, include
+from django.conf import settings
+
+static_path = getattr(settings, 'LIGHTING_STATIC_PATH', Path(__file__).absolute().parent.joinpath('static'))
+
+
+def index_view(request):
+    return FileResponse(static_path.joinpath('index.html').open('rb'))
+
+
+def asset_view(request, asset_path):
+    p = static_path.joinpath(asset_path)
+    if not p.is_file():
+        return HttpResponseNotFound()
+    return FileResponse(p.open('rb'))
+
+
+urlpatterns = [
+    path('basebone/storage/', include('storage.urls')),
+    re_path(r'^lightning/(?P<asset_path>.*$)', asset_view),
+    path('', include('api_basebone.urls')),
+    re_path('^(?!basebone).*$', index_view)
+]

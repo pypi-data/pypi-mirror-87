73ea77c6199093e7cf6824dcf1deafac645f1776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:43 +0000

    Add package fdk-rdf-parser/0.2.10/fdk-rdf-parser-0.2.10.tar.gz

---
+++ packages/fdk-rdf-parser/fdk-rdf-parser-0.2.10.tar.gz/fdk-rdf-parser-0.2.10/src/fdk_rdf_parser/classes/__init__.py
@@ -0,0 +1,20 @@
+from .catalog import Catalog
+from .contactpoint import ContactPoint
+from .data_distribution_service import DataDistributionService
+from .dataservice import DataService
+from .dataset import Dataset, PartialDataset
+from .dcat_resource import PartialDcatResource
+from .distribution import Distribution
+from .event import Event
+from .harvest_meta_data import HarvestMetaData
+from .info_model import InformationModel
+from .public_service import PublicService
+from .publisher import Publisher
+from .qualified_attribution import QualifiedAttribution
+from .quality_annotation import QualityAnnotation
+from .references import Reference
+from .skos_code import SkosCode
+from .skos_concept import SkosConcept
+from .subject import Subject
+from .temporal import Temporal
+from .theme import ConceptSchema, ThemeEU, ThemeLOS

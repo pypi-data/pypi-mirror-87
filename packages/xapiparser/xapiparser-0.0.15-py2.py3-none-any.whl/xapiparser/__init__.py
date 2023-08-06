54ecbbe5c924d301722516fffb04730b0f64df73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:03 +0000

    Add package xapiparser/0.0.15/xapiparser-0.0.15-py2.py3-none-any.whl

---
+++ packages/xapiparser/xapiparser-0.0.15-py2.py3-none-any.whl/xapiparser/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.15'
+
+from xapiparser.parser import parse
+
+__all__ = ['parse']
---
+++ packages/xapiparser/xapiparser-0.0.15-py2.py3-none-any.whl/xapiparser/cli.py
@@ -0,0 +1,17 @@
+import argparse
+
+import lxml.etree as etree
+
+from xapiparser import __version__
+from xapiparser import parse
+
+parser = argparse.ArgumentParser(description='xAPI ssh command parser.')
+parser.add_argument('command', metavar='cmd', nargs=1,
+                    help="xAPI ssh command")
+parser.add_argument('--version', action='version',
+                    version='%(prog)s v{version}'.format(version=__version__))
+
+
+def main(args=None):
+    args = parser.parse_args(args=args)
+    print(etree.tostring(parse(args.command[0]), pretty_print=True, encoding='unicode'))

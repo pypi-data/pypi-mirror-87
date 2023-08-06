53759dc7cc633add93fcc92b0edbc6595a32c50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:12 +0000

    Add package Bert_clear_title/0.0.0.1.1/Bert_clear_title-0.0.0.1.1.tar.gz

---
+++ packages/Bert_clear_title/Bert_clear_title-0.0.0.1.1.tar.gz/Bert_clear_title-0.0.0.1.1/README.md
@@ -0,0 +1,18 @@
+# 一个创建库的demo
+
+
+
+自动提取标记信息
+使用Bert模型进行训练
+初始化模型
+可以从这里下载模型
+
+https://www.kaggle.com/terrychanorg/bert-title/output
+## 快速上传操作
+可以自动查找依赖，然后上传
+```
+sh upload.sh
+```
+
+文档查看
+https://www.terrychan.org/python_libs_demo/
---
+++ packages/Bert_clear_title/Bert_clear_title-0.0.0.1.1.tar.gz/Bert_clear_title-0.0.0.1.1/test/test.py
@@ -0,0 +1,29 @@
+
+#encoding=utf-8
+from __future__ import unicode_literals
+import sys
+# 切换到上级目录
+sys.path.append("../")
+# 引入本地库
+import Bert_clear_title
+
+Demo =Bert_clear_title.Marker(model_path="/mnt/data/dev/model/Bert_clear_title/model/")
+Demo.load_model()
+
+
+text="可见其成是什么意思 - 百度知道"
+
+print(Demo.pre(text))
+# [{'text': ['[CLS]', '可', '见', '其', '成', '是', '什', '么', '意', '思', '-', '百', '度', '知', '道', '[SEP]'], 'label': ['O', 'B-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'E-title', 'O', 'O', 'O', 'O', 'O', 'O']}]
+
+one=Demo.pre(text)
+print(one)
+
+#>>> [{'text': ['[CLS]', '可', '见', '其', '成', '是', '什', '么', '意', '思', '-', '百', '度', '知', '道', '[SEP]'], 'label': ['O', 'B-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'M-title', 'E-title', 'O', 'O', 'O', 'O', 'O', 'O']}]
+
+print(Demo.get_mark_data(one[0]))
+
+
+# {'title': ['可见其成是什么意思']}
+
+

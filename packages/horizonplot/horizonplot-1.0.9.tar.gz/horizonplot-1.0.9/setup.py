7254697775b0f406af6b86720a82ff0a0cfca58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:10 +0000

    Add package horizonplot/1.0.9/horizonplot-1.0.9.tar.gz

---
+++ packages/horizonplot/horizonplot-1.0.9.tar.gz/horizonplot-1.0.9/setup.py
@@ -0,0 +1,18 @@
+
+import setuptools
+
+setuptools.setup(name='horizonplot',
+      version='1.0.9',
+      author='Kasper Munch',
+      description='Generates horizon plots.',
+      # long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/kaspermunch/horizonplot',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.6',
+      install_requires=[
+      'pandas>=1.0',
+      'numpy>=1.1',
+      'seaborn>=0.1',
+      'matplotlib>=3.0',
+      ])

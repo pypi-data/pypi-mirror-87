b7c14df24f4ef5d2801d3259f257a348bad0260a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:54 +0000

    Add package johnnydep/1.7/johnnydep-1.7-py2.py3-none-any.whl

---
+++ packages/johnnydep/johnnydep-1.7-py2.py3-none-any.whl/johnnydep/lib.py
@@ -0,0 +1,413 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+import json
+import os
+import re
+import shutil
+import tempfile
+from collections import OrderedDict
+from collections import defaultdict
+from zipfile import ZipFile
+
+import anytree
+import distlib.wheel
+import pkg_resources
+import pkginfo
+import toml
+import tabulate
+import wimpy
+from cachetools.func import ttl_cache
+from packaging.markers import default_environment
+from packaging.utils import canonicalize_name
+from packaging.utils import canonicalize_version
+from structlog import get_logger
+from wimpy import cached_property
+
+from johnnydep import pipper
+from johnnydep.compat import oyaml
+
+__all__ = ["JohnnyDist", "gen_table", "flatten_deps"]
+
+
+logger = get_logger(__name__)
+
+
+class OrderedDefaultListDict(OrderedDict):
+    def __missing__(self, key):
+        self[key] = value = []
+        return value
+
+
+class JohnnyDist(anytree.NodeMixin):
+    def __init__(self, req_string, parent=None, index_url=None, env=None, extra_index_url=None):
+        log = self.log = logger.bind(dist=req_string)
+        log.info("init johnnydist", parent=parent and str(parent.req))
+        self.parent = parent
+        self.index_url = index_url
+        self.env = env
+        self.extra_index_url = extra_index_url
+        self._recursed = False
+
+        if req_string.endswith(".whl") and os.path.isfile(req_string):
+            whl = distlib.wheel.Wheel(req_string)
+            self.name = canonicalize_name(whl.name)
+            self.specifier = "==" + canonicalize_version(whl.version)
+            self.req = pkg_resources.Requirement.parse(self.name + self.specifier)
+            self.import_names = _discover_import_names(req_string)
+            self.metadata = _extract_metadata(req_string)
+        else:
+            self.req = pkg_resources.Requirement.parse(req_string)
+            self.name = canonicalize_name(self.req.name)
+            self.specifier = str(self.req.specifier)
+            log.debug("fetching best wheel")
+            self.import_names, self.metadata = _get_info(
+                dist_name=req_string,
+                index_url=index_url,
+                env=env,
+                extra_index_url=extra_index_url,
+            )
+
+        self.extras_requested = sorted(self.req.extras)
+        if parent is None:
+            if env:
+                log.debug("root node target env", **dict(env))
+            self.required_by = []
+        else:
+            self.required_by = [str(parent.req)]
+
+    @property
+    def requires(self):
+        """Just the strings (name and spec) for my immediate dependencies. Cheap."""
+        all_requires = self.metadata.get("requires_dist", [])
+        if not all_requires:
+            return []
+        result = []
+        if self.env is None:
+            env_data = default_environment()
+        else:
+            env_data = dict(self.env)
+        for req_str in all_requires:
+            req = pkg_resources.Requirement.parse(req_str)
+            req_short, _sep, _marker = str(req).partition(";")
+            if req.marker is None:
+                # unconditional dependency
+                result.append(req_short)
+                continue
+            # conditional dependency - must be evaluated in environment context
+            for extra in [None] + self.extras_requested:
+                if req.marker.evaluate(dict(env_data, extra=extra)):
+                    self.log.debug("included conditional dep", req=req_str)
+                    result.append(req_short)
+                    break
+            else:
+                self.log.debug("dropped conditional dep", req=req_str)
+        result = sorted(set(result))  # this makes the dep tree deterministic/repeatable
+        return result
+
+    @property
+    def children(self):
+        """my immediate deps, as a tuple of johnnydists"""
+        if not self._recursed:
+            self.log.debug("populating dep tree")
+            for dep in self.requires:
+                JohnnyDist(
+                    req_string=dep,
+                    parent=self,
+                    index_url=self.index_url,
+                    env=self.env,
+                    extra_index_url=self.extra_index_url,
+                )
+            self._recursed = True
+        return super(JohnnyDist, self).children
+
+    @property
+    def homepage(self):
+        try:
+            return self.metadata["home_page"]
+        except KeyError:
+            for k in "python.details", "python.project":
+                try:
+                    return self.metadata["extensions"][k]["project_urls"]["Home"]
+                except KeyError:
+                    pass
+        self.log.info("unknown homepage")
+
+    @property
+    def summary(self):
+        text = self.metadata.get("summary") or ""
+        result = text.lstrip("#").strip()
+        return result
+
+    @property
+    def license(self):
+        result = self.metadata.get("license") or ""
+        # sometimes people just put the license in a trove classifier instead
+        # for a list of valid classifiers:
+        #   requests.get('https://pypi.python.org/pypi', params={':action': 'list_classifiers'}).text.splitlines()
+        self.log.debug("metadata license is not set, checking trove classifiers")
+        for classifier in self.metadata.get("classifiers", []):
+            if classifier.startswith("License :: "):
+                crap, result = classifier.rsplit(" :: ", 1)
+                break
+        if not result:
+            self.log.info("unknown license")
+        return result
+
+    @cached_property
+    def versions_available(self):
+        return pipper.get_versions(
+            self.project_name,
+            index_url=self.index_url,
+            env=self.env,
+            extra_index_url=self.extra_index_url,
+        )
+
+    @cached_property
+    def version_installed(self):
+        self.log.debug("checking if installed already")
+        try:
+            dist = pkg_resources.get_distribution(self.name)
+        except pkg_resources.DistributionNotFound:
+            self.log.debug("not installed")
+            return
+        self.log.debug("existing installation found", version=dist.version)
+        return dist.version
+
+    @property
+    def version_latest(self):
+        if self.versions_available:
+            return self.versions_available[-1]
+
+    @property
+    def version_latest_in_spec(self):
+        avail = list(reversed(self.versions_available))
+        for v in avail:
+            if v in self.req.specifier:
+                return v
+        # allow to get a pre-release if that's all the index has for us
+        for v in avail:
+            if self.req.specifier.contains(v, prereleases=True):
+                return v
+
+    @property
+    def extras_available(self):
+        extras = {x for x in self.metadata.get("provides_extras", []) if x}
+        for req_str in self.metadata.get("requires_dist", []):
+            req = pkg_resources.Requirement.parse(req_str)
+            extras |= set(re.findall(r'extra == "(.*?)"', str(req.marker)))
+        return sorted(extras)
+
+    @property
+    def project_name(self):
+        return self.metadata.get("name", self.name)
+
+    @property
+    def pinned(self):
+        if self.extras_requested:
+            extras = "[{}]".format(",".join(self.extras_requested))
+        else:
+            extras = ""
+        version = self.version_latest_in_spec
+        if version is None:
+            raise Exception("Can not pin because no version available is in spec")
+        result = "{}{}=={}".format(self.project_name, extras, version)
+        return result
+
+    @cached_property
+    def _best(self):
+        return pipper.get(
+            self.pinned,
+            index_url=self.index_url,
+            env=self.env,
+            extra_index_url=self.extra_index_url,
+        )
+
+    @property
+    def download_link(self):
+        return self._best["url"]
+
+    @property
+    def checksum(self):
+        return self._best["checksum"]
+
+    def serialise(self, fields=("name", "summary"), recurse=True, format=None):
+        if format == "pinned":
+            # user-specified fields are ignored/invalid in this case
+            fields = ("pinned",)
+        data = [OrderedDict([(f, getattr(self, f, None)) for f in fields])]
+        if format == "human":
+            table = gen_table(self, extra_cols=fields)
+            tabulate.PRESERVE_WHITESPACE = True
+            return tabulate.tabulate(table, headers="keys")
+        if recurse and self.requires:
+            deps = flatten_deps(self)
+            next(deps)  # skip over root
+            data += [d for dep in deps for d in dep.serialise(fields=fields, recurse=False)]
+        if format is None or format == "python":
+            result = data
+        elif format == "json":
+            result = json.dumps(data, indent=2, default=str, separators=(",", ": "))
+        elif format == "yaml":
+            result = oyaml.dump(data)
+        elif format == "toml":
+            result = "\n".join([toml.dumps(d) for d in data])
+        elif format == "pinned":
+            result = "\n".join([d["pinned"] for d in data])
+        else:
+            raise Exception("Unsupported format")
+        return result
+
+    serialize = serialise
+
+    def _repr_pretty_(self, p, cycle):
+        # hook for IPython's pretty-printer
+        if cycle:
+            p.text(repr(self))
+        else:
+            fullname = self.name + self.specifier
+            if self.extras_requested:
+                fullname += "[{}]".format(",".join(self.extras_requested))
+            p.text("<{} {} at {}>".format(type(self).__name__, fullname, hex(id(self))))
+
+
+def gen_table(johnnydist, extra_cols=()):
+    extra_cols = OrderedDict.fromkeys(extra_cols)  # de-dupe and preserve ordering
+    extra_cols.pop("name", None)  # this is always included anyway, no need to ask for it
+    johnnydist.log.debug("generating table")
+    for pre, _fill, node in anytree.RenderTree(johnnydist):
+        row = OrderedDict()
+        name = str(node.req)
+        if "specifier" in extra_cols:
+            name = wimpy.strip_suffix(name, str(node.specifier))
+        row["name"] = pre + name
+        for col in extra_cols:
+            val = getattr(node, col, "")
+            if isinstance(val, list):
+                val = ", ".join(val)
+            row[col] = val
+        yield row
+
+
+def flatten_deps(johnnydist):
+    # TODO: add the check for infinite recursion in here (traverse parents)
+    johnnydist.log.debug("resolving dep tree")
+    dist_map = OrderedDefaultListDict()
+    spec_map = defaultdict(str)
+    extra_map = defaultdict(set)
+    required_by_map = defaultdict(list)
+    for dep in anytree.iterators.LevelOrderIter(johnnydist):
+        dist_map[dep.name].append(dep)
+        spec_map[dep.name] = dep.req.specifier & spec_map[dep.name]
+        extra_map[dep.name] |= set(dep.extras_requested)
+        required_by_map[dep.name] += dep.required_by
+    for name, dists in dist_map.items():
+        spec = spec_map[name]
+        spec.prereleases = True
+        extras = extra_map[name]
+        required_by = list(OrderedDict.fromkeys(required_by_map[name]))  # order preserving de-dupe
+        for dist in dists:
+            if dist.version_latest_in_spec in spec and set(dist.extras_requested) >= extras:
+                dist.required_by = required_by
+                johnnydist.log.info(
+                    "resolved",
+                    name=dist.name,
+                    required_by=required_by,
+                    v=dist.version_latest_in_spec,
+                    spec=str(spec) or "ANY",
+                )
+                yield dist
+                break
+        else:
+            nameset = {dist.name for dist in dists}
+            assert len(nameset) == 1  # name attributes were canonicalized by JohnnyDist.__init__
+            [name] = nameset
+            johnnydist.log.info("merged specs", name=name, spec=spec, extras=extras)
+            req_string = "{name}{extras}{spec}".format(
+                name=name,
+                extras="[{}]".format(",".join(sorted(extras))) if extras else "",
+                spec=spec,
+            )
+            dist = JohnnyDist(
+                req_string=req_string,
+                index_url=johnnydist.index_url,
+                env=johnnydist.env,
+                extra_index_url=johnnydist.extra_index_url,
+            )
+            dist.required_by = required_by
+            yield dist
+            # TODO: check if this new version causes any new reqs!!
+
+
+def _discover_import_names(whl_file):
+    log = logger.bind(whl_file=whl_file)
+    logger.debug("finding import names")
+    zipfile = ZipFile(file=whl_file)
+    namelist = zipfile.namelist()
+    try:
+        [top_level_fname] = [x for x in namelist if x.endswith("top_level.txt")]
+    except ValueError:
+        log.debug("top_level absent, trying metadata")
+        metadata = _extract_metadata(whl_file)
+        try:
+            public_names = metadata["python.exports"]["modules"] or []
+        except KeyError:
+            # this dist was packaged by a dinosaur, exports is not in metadata.
+            # we gotta do it the hard way ...
+            public_names = []
+            for name in namelist:
+                if ".dist-info/" not in name and ".egg-info/" not in name:
+                    parts = name.split(os.sep)
+                    if len(parts) == 2 and parts[1] == "__init__.py":
+                        # found a top-level package
+                        public_names.append(parts[0])
+                    elif len(parts) == 1:
+                        # TODO: find or make an exhaustive list of file extensions importable
+                        name, ext = os.path.splitext(parts[0])
+                        if ext == ".py" or ext == ".so":
+                            # found a top level module
+                            public_names.append(name)
+    else:
+        all_names = zipfile.read(top_level_fname).decode("utf-8").strip().splitlines()
+        public_names = [n for n in all_names if not n.startswith("_")]
+    return public_names
+
+
+def _extract_metadata(whl_file):
+    logger.debug("searching metadata", whl_file=whl_file)
+    info = pkginfo.get_metadata(whl_file)
+    if info is None:
+        raise Exception("failed to get metadata")
+    data = {k.lower(): v for k,v in vars(info).items()}
+    data.pop("filename", None)
+    return data
+
+
+@ttl_cache(maxsize=512, ttl=60 * 5)
+def _get_info(dist_name, index_url=None, env=None, extra_index_url=None):
+    log = logger.bind(dist_name=dist_name)
+    tmpdir = tempfile.mkdtemp()
+    log.debug("created scratch", tmpdir=tmpdir)
+    try:
+        with wimpy.working_directory(tmpdir):
+            data = pipper.get(
+                dist_name,
+                index_url=index_url,
+                env=env,
+                extra_index_url=extra_index_url,
+                tmpdir=".",
+            )
+        dist_path = data["path"]
+        # extract any info we may need from downloaded dist right now, so the
+        # downloaded file can be cleaned up immediately
+        import_names = _discover_import_names(dist_path)
+        metadata = _extract_metadata(dist_path)
+    finally:
+        log.debug("removing scratch", tmpdir=tmpdir)
+        shutil.rmtree(tmpdir)
+    return import_names, metadata
+
+
+# TODO: multi-line progress bar?
+# TODO: upload test dists to test PyPI index, document pip existing failure modes
+# TODO: don't infinitely recurse on circular dep tree
---
+++ packages/johnnydep/johnnydep-1.7-py2.py3-none-any.whl/johnnydep/pipper.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import hashlib
+import json
+import os
+import subprocess
+import sys
+import tempfile
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import pip
+import pkg_resources
+from cachetools.func import ttl_cache
+from wimpy import working_directory
+from structlog import get_logger
+
+from johnnydep.compat import urlparse, urlretrieve
+from johnnydep.logs import configure_logging
+from johnnydep.util import python_interpreter
+
+log = get_logger(__name__)
+
+
+DEFAULT_INDEX = "https://pypi.org/simple/"
+
+
+def compute_checksum(target, algorithm="sha256", blocksize=2 ** 13):
+    hashtype = getattr(hashlib, algorithm)
+    hash_ = hashtype()
+    log.debug("computing checksum", target=target, algorithm=algorithm)
+    with open(target, "rb") as f:
+        for chunk in iter(lambda: f.read(blocksize), b""):
+            hash_.update(chunk)
+    result = hash_.hexdigest()
+    log.debug("computed checksum", result=result)
+    return result
+
+
+def _get_wheel_args(index_url, env, extra_index_url):
+    args = [
+        sys.executable,
+        "-m",
+        "pip",
+        "wheel",
+        "-vvv",  # --verbose x3
+        "--no-deps",
+        "--no-cache-dir",
+        "--disable-pip-version-check",
+    ]
+    if index_url is not None:
+        args += ["--index-url", index_url]
+        if index_url != DEFAULT_INDEX:
+            args += ["--trusted-host", urlparse(index_url).hostname]
+    if extra_index_url is not None:
+        args += ["--extra-index-url", extra_index_url, "--trusted-host", urlparse(extra_index_url).hostname]
+    if env is None:
+        pip_version = pip.__version__
+    else:
+        pip_version = dict(env)["pip_version"]
+        args[0] = dict(env)["python_executable"]
+    pip_major, pip_minor = pip_version.split(".")[0:2]
+    pip_major = int(pip_major)
+    pip_minor = int(pip_minor)
+    if pip_major >= 10:
+        args.append("--progress-bar=off")
+    if (pip_major, pip_minor) >= (20, 3):
+        # See https://github.com/pypa/pip/issues/9139#issuecomment-735443177
+        args.append("--use-deprecated=legacy-resolver")
+    return args
+
+
+def _download_dist(url, scratch_file, index_url, extra_index_url):
+    auth = None
+    if index_url:
+        parsed = urlparse(index_url)
+        if parsed.username and parsed.password and parsed.hostname == urlparse(url).hostname:
+            # handling private PyPI credentials in index_url
+            auth = (parsed.username, parsed.password)
+    if extra_index_url:
+        parsed = urlparse(extra_index_url)
+        if parsed.username and parsed.password and parsed.hostname == urlparse(url).hostname:
+            # handling private PyPI credentials in extra_index_url
+            auth = (parsed.username, parsed.password)
+    target, _headers = urlretrieve(url, scratch_file, auth=auth)
+    return target, _headers
+
+
+@ttl_cache(maxsize=512, ttl=60 * 5)
+def get_versions(dist_name, index_url=None, env=None, extra_index_url=None):
+    bare_name = pkg_resources.Requirement.parse(dist_name).name
+    log.debug("checking versions available", dist=bare_name)
+    args = _get_wheel_args(index_url, env, extra_index_url) + [dist_name + "==showmethemoney"]
+    try:
+        out = subprocess.check_output(args, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as err:
+        # expected. we forced this by using a non-existing version number.
+        out = getattr(err, "output", b"")
+    else:
+        log.warning(out)
+        raise Exception("Unexpected success:" + " ".join(args))
+    out = out.decode("utf-8")
+    lines = []
+    msg = "Could not find a version that satisfies the requirement"
+    for line in out.splitlines():
+        if msg in line:
+            lines.append(line)
+    [line] = lines
+    prefix = "(from versions: "
+    start = line.index(prefix) + len(prefix)
+    stop = line.rfind(")")
+    versions = line[start:stop].split(",")
+    versions = [v.strip() for v in versions if v.strip()]
+    log.debug("found versions", dist=bare_name, versions=versions)
+    return versions
+
+
+@ttl_cache(maxsize=512, ttl=60 * 5)
+def get(dist_name, index_url=None, env=None, extra_index_url=None, tmpdir=None):
+    args = _get_wheel_args(index_url, env, extra_index_url) + [dist_name]
+    scratch_dir = tempfile.mkdtemp(dir=tmpdir)
+    log.debug("wheeling and dealing", scratch_dir=os.path.abspath(scratch_dir), args=" ".join(args))
+    try:
+        out = subprocess.check_output(args, stderr=subprocess.STDOUT, cwd=scratch_dir)
+    except subprocess.CalledProcessError as err:
+        output = getattr(err, "output", b"").decode("utf-8")
+        log.warning(output)
+        raise
+    log.debug("wheel command completed ok", dist_name=dist_name)
+    out = out.decode("utf-8")
+    links = []
+    lines = out.splitlines()
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if line.startswith("Downloading from URL "):
+            parts = line.split()
+            link = parts[3]
+            links.append(link)
+        elif line.startswith("Downloading "):
+            parts = line.split()
+            last = parts[-1]
+            if len(parts) == 3 and last.startswith("(") and last.endswith(")"):
+                link = parts[-2]
+            elif len(parts) == 4 and parts[-2].startswith("(") and last.endswith(")"):
+                link = parts[-3]
+                if not urlparse(link).scheme:
+                    # newest pip versions have changed to not log the full url
+                    # in the download event. it is becoming more and more annoying
+                    # to preserve compatibility across a wide range of pip versions
+                    next_line = lines[i + 1].strip()
+                    if next_line.startswith("Added ") and " to build tracker" in next_line:
+                        link = next_line.split(" to build tracker")[0].split()[-1]
+            else:
+                link = last
+            links.append(link)
+        elif line.startswith("Source in ") and "which satisfies requirement" in line:
+            link = line.split()[-1]
+            links.append(link)
+    links = list(OrderedDict.fromkeys(links))  # order-preserving dedupe
+    if not links:
+        log.warning("could not find download link", out=out)
+        raise Exception("failed to collect dist")
+    if len(links) == 2:
+        # sometimes we collect the same link, once with a url fragment/checksum and once without
+        first, second = links
+        if first.startswith(second):
+            del links[1]
+        elif second.startswith(first):
+            del links[0]
+    if len(links) > 1:
+        log.debug("more than 1 link collected", out=out, links=links)
+        # Since PEP 517, maybe an sdist will also need to collect other distributions
+        # for the build system, even with --no-deps specified. pendulum==1.4.4 is one
+        # example, which uses poetry and doesn't publish any python37 wheel to PyPI.
+        # However, the dist itself should still be the first one downloaded.
+    link = links[0]
+    with working_directory(scratch_dir):
+        [whl] = [os.path.abspath(x) for x in os.listdir(".") if x.endswith(".whl")]
+    url, _sep, checksum = link.partition("#")
+    url = url.replace("/%2Bf/", "/+f/")  # some versions of pip did not unquote this fragment in the log
+    if not checksum.startswith("md5=") and not checksum.startswith("sha256="):
+        # PyPI gives you the checksum in url fragment, as a convenience. But not all indices are so kind.
+        algorithm = "md5"
+        if os.path.basename(whl) == url.rsplit("/")[-1]:
+            target = whl
+        else:
+            scratch_file = os.path.join(scratch_dir, os.path.basename(url))
+            target, _headers = _download_dist(url, scratch_file, index_url, extra_index_url)
+        checksum = compute_checksum(target=target, algorithm=algorithm)
+        checksum = "=".join([algorithm, checksum])
+    result = {"path": whl, "url": url, "checksum": checksum}
+    return result
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("dist_name")
+    parser.add_argument("--index-url", "-i")
+    parser.add_argument("--extra-index-url")
+    parser.add_argument("--for-python", "-p", dest="env", type=python_interpreter)
+    parser.add_argument("--verbose", "-v", default=1, type=int, choices=range(3))
+    debug = {
+        "sys.argv": sys.argv,
+        "sys.executable": sys.executable,
+        "sys.version": sys.version,
+        "sys.path": sys.path,
+        "pip.__version__": pip.__version__,
+        "pip.__file__": pip.__file__,
+    }
+    args = parser.parse_args()
+    configure_logging(verbosity=args.verbose)
+    log.debug("runtime info", **debug)
+    result = get(dist_name=args.dist_name, index_url=args.index_url, env=args.env, extra_index_url=args.extra_index_url)
+    text = json.dumps(result, indent=2, sort_keys=True, separators=(",", ": "))
+    print(text)
+
+
+if __name__ == "__main__":
+    main()

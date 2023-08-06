c9037eaa108419cec54c4de44fec57af5531705c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:00 +0000

    Add package tyadmin-api-cli/0.7.3/tyadmin_api_cli-0.7.3-py2.py3-none-any.whl

---
+++ packages/tyadmin-api-cli/tyadmin_api_cli-0.7.3-py2.py3-none-any.whl/tyadmin_api_cli/utils.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+import django
+from django.utils import translation
+
+
+def init_django_env(project_name_settings):
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    # 将项目目录加入setting
+    sys.path.append(pwd)
+    # manage.py中
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{project_name_settings}")
+
+    django.setup()
+
+
+def get_lower_case_name(text):
+    lst = []
+    for index, char in enumerate(text):
+        if char.isupper() and index != 0:
+            lst.append("_")
+        lst.append(char)
+
+    return "".join(lst).lower()
+
+
+def trans(txt):
+    cur_language = translation.get_language()
+    try:
+        translation.activate(cur_language)
+        text = translation.gettext(txt)
+    finally:
+        translation.activate(cur_language)
+    return text
+
+
+import re
+
+zh_pattern = re.compile(u'[\u4e00-\u9fa5]+')
+
+
+def contain_zh(word):
+    global zh_pattern
+    match = zh_pattern.search(word)
+
+    return match

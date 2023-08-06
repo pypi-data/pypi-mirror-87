80938aab8b0a4d31ec2a1f8e03fa462a626c54c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:42 +0000

    Add package ai2xlcore/0.0.1/ai2xlcore-0.0.1.tar.gz

---
+++ packages/ai2xlcore/ai2xlcore-0.0.1.tar.gz/ai2xlcore-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ai2xlcore',
+    version='0.0.1',
+    packages=['ai2xlcore'],
+    url='https://ai2xl.github.io',
+    author='ai2xl',
+    author_email='info@ai2xl.com',
+    description='ai2xl is a free Python library that allows using Excel for ML data preparation, zero dependency ML '
+                'model deployment, model debugging, model explainability and collaboration.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=['pandas', 'numpy', 'nyoka', 'py4j', 'python-dateutil', 'SciPy', 'scikit-learn',
+                      'pandas-profiling', 'sweetviz']
+)

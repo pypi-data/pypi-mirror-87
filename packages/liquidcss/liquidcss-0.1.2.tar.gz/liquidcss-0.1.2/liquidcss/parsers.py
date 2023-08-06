890b9b7b4b7291a4de41663f22fd8aca54ccd714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package liquidcss/0.1.2/liquidcss-0.1.2.tar.gz

---
+++ packages/liquidcss/liquidcss-0.1.2.tar.gz/liquidcss-0.1.2/liquidcss/parsers.py
@@ -0,0 +1,33 @@
+import cssutils
+from cssutils.css import (
+    CSSRule, 
+    CSSStyleRule,
+    CSSMediaRule,
+)
+
+
+class Css_Parser(object):
+
+    def __init__(self):
+        pass
+
+
+    def parse_css(self, css_string: str) -> (list, 'sheet'):
+        css_rules = []
+        sheet = cssutils.parseString(css_string)
+        for rule in sheet:
+            if isinstance(rule, CSSMediaRule):
+                rules = rule.cssRules
+                css_rules = [*css_rules, *rules]
+            if isinstance(rule, CSSStyleRule):
+                css_rules.append(rule)
+        return css_rules, sheet
+
+
+    def from_file(self, path: str) -> (list, 'sheet'):
+        css_string = ''
+        with open(path, 'r') as file:
+            css_string = file.read()
+        return self.parse_css(css_string = css_string)
+
+
---
+++ packages/liquidcss/liquidcss-0.1.2.tar.gz/liquidcss-0.1.2/liquidcss/structure.py
@@ -0,0 +1,50 @@
+import os
+
+
+class StructureManager(object):
+
+
+    folder_struct = dict(
+        root_folder_path = os.path.join('liquidcss_'),
+        css = os.path.join('liquidcss_', 'css'),
+        html = os.path.join('liquidcss_', 'html'),
+    )
+
+
+    def __init__(self, base_dir: str):
+        self.base_dir = base_dir
+
+
+    def _create_folder(self, folder_struct_key: str):
+        os.mkdir(
+            os.path.join(
+                self.base_dir, 
+                StructureManager.folder_struct[folder_struct_key]
+            )
+        )
+
+
+    @property
+    def _missing_key(self: str) -> str:
+        for key, value in StructureManager.folder_struct.items():
+            if os.path.isdir(os.path.join(self.base_dir, value)):
+                continue
+            else:
+                return key
+
+
+    def create_file(self, type_: str, file_name: str, string: str):
+        path = os.path.join(self.base_dir, StructureManager.folder_struct[type_], file_name)
+        with open(path, 'w') as file:
+            file.write(string.decode('ascii'))
+
+
+    def validate_structure(self):
+        missing_key = self._missing_key
+        if missing_key:
+            self._create_folder(folder_struct_key = missing_key)
+            self.validate_structure()
+
+
+
+    

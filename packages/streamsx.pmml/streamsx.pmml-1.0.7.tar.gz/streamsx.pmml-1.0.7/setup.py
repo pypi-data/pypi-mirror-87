8c0ab292894f45398db7bdf69625d6834a0d8655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:19 +0000

    Add package streamsx.pmml/1.0.7/streamsx.pmml-1.0.7.tar.gz

---
+++ packages/streamsx.pmml/streamsx.pmml-1.0.7.tar.gz/streamsx.pmml-1.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import streamsx.pmml
+setup(
+  name = 'streamsx.pmml',
+  packages = ['streamsx.pmml'],
+  include_package_data=True,
+  version = streamsx.pmml.__version__,
+  description = 'PMML integration for IBM Streams',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'hegermar@de.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/pypi.streamsx.pmml',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'pmml'],
+  classifiers = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+  install_requires=['streamsx>=1.12.10'],
+  
+  test_suite='nose.collector',
+  tests_require=['nose']
+)

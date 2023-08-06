43313a8ab2631772c78f037d2f71509248cb6ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package pyblish-qml/1.11.5/pyblish-qml-1.11.5.tar.gz

---
+++ packages/pyblish-qml/pyblish-qml-1.11.5.tar.gz/pyblish-qml-1.11.5/pyblish_qml/__main__.py
@@ -0,0 +1,29 @@
+"""Pyblish QML command-line interface"""
+
+import sys
+import argparse
+
+from . import app
+from pyblish import api
+
+
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--demo", action="store_true")
+    parser.add_argument("--aschild", action="store_true",
+                        help="Run as child of another process")
+    parser.add_argument(
+        "--targets",
+        nargs="*",
+        help="Targets to run plugins against."
+    )
+
+    kwargs = parser.parse_args()
+
+    if kwargs.targets is None:
+        kwargs.targets = ["default"] + api.registered_targets()
+
+    return app.main(**kwargs.__dict__)
+
+
+sys.exit(cli())

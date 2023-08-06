b06c3366d3249909efdb1f25d3f9164e8db1fcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:47 +0000

    Add package simc-support/9.0.2.6/simc_support-9.0.2.6-py3-none-any.whl

---
+++ packages/simc-support/simc_support-9.0.2.6-py3-none-any.whl/simc_support/game_data/Source.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class Source(enum.Enum):
+    DUNGEON = "Dungeon"
+    PROFESSION = "Profession"
+    PVP = "PvP"
+    RAID = "Raid"
+    UNKNOWN = "Unknown"
+    WORLD_BOSS = "World Boss"
+    WORLD_DROP = "World Drop"
+    WORLD_QUEST = "World Quest"

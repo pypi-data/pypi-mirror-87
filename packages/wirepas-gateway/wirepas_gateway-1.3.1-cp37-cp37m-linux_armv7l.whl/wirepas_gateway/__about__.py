e34df3e16a7984c42ad0ff8a2eb7fa05372c4618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:38 +0000

    Add package wirepas-gateway/1.3.1/wirepas_gateway-1.3.1-cp37-cp37m-linux_armv7l.whl

---
+++ packages/wirepas-gateway/wirepas_gateway-1.3.1-cp37-cp37m-linux_armv7l.whl/wirepas_gateway/__about__.py
@@ -0,0 +1,23 @@
+"""
+    .. Copyright:
+        Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
+        See file LICENSE for full license details.
+"""
+
+__author__ = "Wirepas Ltd"
+__author_email__ = "opensource@wirepas.com"
+__classifiers__ = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3",
+]
+__copyright__ = "2019 Wirepas Ltd"
+__description__ = "Wirepas gateway transport service that connects the local dbus to a remote MQTT broker."
+__license__ = "Apache-2"
+__pkg_name__ = "wirepas_gateway"
+__title__ = "Wirepas Gateway Transport Service"
+__url__ = "https://github.com/wirepas/gateway"
+__version__ = "1.3.1"
+__keywords__ = "wirepas connectivity iot mesh"
---
+++ packages/wirepas-gateway/wirepas_gateway-1.3.1-cp37-cp37m-linux_armv7l.whl/wirepas_gateway/__init__.py
@@ -0,0 +1,30 @@
+"""
+    Wirepas Gateway
+    ===============
+
+    .. Style guidelines:
+       http://google.github.io/styleguide/pyguide.html
+
+    .. Copyright:
+        Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
+        See file LICENSE for full license details.
+"""
+# flake8: noqa
+
+from . import dbus
+from . import protocol
+from . import utils
+
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __classifiers__,
+    __copyright__,
+    __description__,
+    __license__,
+    __pkg_name__,
+    __title__,
+    __url__,
+    __version__,
+    __keywords__,
+)

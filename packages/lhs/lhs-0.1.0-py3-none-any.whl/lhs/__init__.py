0bb2227a64a9ef8ee3796cf00dc956ef616b3117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package lhs/0.1.0/lhs-0.1.0-py3-none-any.whl

---
+++ packages/lhs/lhs-0.1.0-py3-none-any.whl/lhs/__init__.py
@@ -0,0 +1,10 @@
+"""A Latin hypercube sampler for scenario modelling."""
+
+from . import version
+
+__package_name__ = u'lhs'
+__author__ = u'Rob Moss'
+__email__ = u'rgmoss@unimelb.edu.au'
+__copyright__ = u'2020, Rob Moss'
+__license__ = u'BSD 3-Clause License'
+__version__ = version.__version__

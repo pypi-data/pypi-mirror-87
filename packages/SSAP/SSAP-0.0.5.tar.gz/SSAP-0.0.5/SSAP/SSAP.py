663ce9bb2cae292a5fbdd60accbf23d05dccaf94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:39 +0000

    Add package SSAP/0.0.5/SSAP-0.0.5.tar.gz

---
+++ packages/SSAP/SSAP-0.0.5.tar.gz/SSAP-0.0.5/SSAP/SSAP.py
@@ -0,0 +1,5 @@
+def info():
+    print('Welcome to SSAP! SSAP→[Self Service Analytics Platform]. There are different analytics function '
+          'modules to support you complete different data analytics scenarios. '
+          'Please enjoy! Any question or support needed, please raise in Big Data Community(BDC).')
+

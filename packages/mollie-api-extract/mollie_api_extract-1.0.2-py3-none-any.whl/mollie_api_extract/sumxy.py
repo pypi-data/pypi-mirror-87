5b234aaba6827de1b008df8fbdab4f9424b6e8a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package mollie-api-extract/1.0.2/mollie_api_extract-1.0.2-py3-none-any.whl

---
+++ packages/mollie-api-extract/mollie_api_extract-1.0.2-py3-none-any.whl/mollie_api_extract/sumxy.py
@@ -0,0 +1,11 @@
+def my_sum(x, y):     
+    """calculates the sum of x and y.     
+    Parameters:     
+       x (int): a number
+       y (int): another number    
+    Returns:     
+       int: sum of x and y
+    """         
+    return(x+y)
+
+

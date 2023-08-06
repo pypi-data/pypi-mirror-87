2273962e84f45a621e9839c2006fb84667e8399b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:21 +0000

    Add package gg-distributions/0.1/gg_distributions-0.1.tar.gz

---
+++ packages/gg-distributions/gg_distributions-0.1.tar.gz/gg_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gg_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gg_distributions'],
+      zip_safe=False)

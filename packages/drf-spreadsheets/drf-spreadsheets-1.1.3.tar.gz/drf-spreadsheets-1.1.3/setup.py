f56b00df4bbb88a20e29617c8204b73578e23df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:36 +0000

    Add package drf-spreadsheets/1.1.3/drf-spreadsheets-1.1.3.tar.gz

---
+++ packages/drf-spreadsheets/drf-spreadsheets-1.1.3.tar.gz/drf-spreadsheets-1.1.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drf-spreadsheets",
+    version="1.1.3",
+    author="Joe Paavola",
+    author_email="joe@workhere.com",
+    description="A Django REST Framework package allowing views to be rendered as CSV or XLSX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/workhere/drf-spreadsheets",
+    packages=setuptools.find_packages(),
+    install_requires=["djangorestframework>=3.6", "openpyxl>=2.4"],
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Development Status :: 3 - Alpha"
+    ],
+    python_requires='>=3.6',
+
+)

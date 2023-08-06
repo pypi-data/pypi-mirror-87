77ea21c421a8c9060695f7d772c5c9d81775320b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:38 +0000

    Add package PyCocoa/20.12.10/PyCocoa-20.12.10.zip

---
+++ packages/PyCocoa/PyCocoa-20.12.10.zip/PyCocoa-20.12.10/test/list_ivalues.py
@@ -0,0 +1,50 @@
+
+# -*- coding: utf-8 -*-
+
+# List all the instance variables of an Objective-C class.
+
+from pycocoa import get_class, get_ivar, get_ivars, leaked2, sortuples
+
+__version__ = '18.11.02'
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    if len(sys.argv) < 2:
+        print('USAGE: python list_ivalues.py <Obj-C Class> [prefix] ...]')
+        exit(1)
+
+    clstr, prefs = sys.argv[1], sys.argv[2:]
+
+    cls, n = get_class(clstr), 0
+    for name, encoding, ctype, _ in sortuples(get_ivars(cls, *prefs)):
+        n += 1
+        value = get_ivar(cls, name, ctype)
+        t = getattr(ctype, '__name__', ctype)
+        print('%s %s %s: %r' % (name, encoding, t, value))
+
+    print('%s %s instance variables total %s' % (n, clstr, leaked2()))
+
+# MIT License <https://OpenSource.org/licenses/MIT>
+#
+# Copyright (C) 2017-2021 -- mrJean1 at Gmail -- All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.

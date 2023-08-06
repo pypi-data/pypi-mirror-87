51a77c1a061235ab4344d0428ef11b0e58b753af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:27 +0000

    Add package ro-py/0.0.1/ro-py-0.0.1.tar.gz

---
+++ packages/ro-py/ro-py-0.0.1.tar.gz/ro-py-0.0.1/ro_py/groups.py
@@ -0,0 +1,35 @@
+from ro_py import User
+import requests
+
+endpoint = "https://groups.roblox.com/"
+
+
+class Shout:
+    def __init__(self, shout_data):
+        self.body = shout_data["body"]
+        self.poster = User(shout_data["poster"]["userId"])
+
+
+class Group:
+    def __init__(self, group_id):
+        self.id = group_id
+        group_info_req = requests.get(endpoint + f"v1/groups/{self.id}")
+        group_info = group_info_req.json()
+        self.name = group_info["name"]
+        self.description = group_info["description"]
+        self.owner = User(group_info["owner"]["userId"])
+
+        self.member_count = group_info["memberCount"]
+        self.is_builders_club_only = group_info["isBuildersClubOnly"]
+        self.public_entry_allowed = group_info["publicEntryAllowed"]
+        # self.is_locked = group_info["isLocked"]
+
+    @property
+    def shout(self):
+        group_info_req = requests.get(endpoint + f"v1/groups/{self.id}")
+        group_info = group_info_req.json()
+
+        if group_info["shout"]:
+            return Shout(group_info["shout"])
+        else:
+            return None
---
+++ packages/ro-py/ro-py-0.0.1.tar.gz/ro-py-0.0.1/ro_py/users.py
@@ -0,0 +1,21 @@
+import requests
+
+endpoint = "https://users.roblox.com/"
+
+
+class User:
+    def __init__(self, user_id):
+        self.id = user_id
+        user_info_req = requests.get(endpoint + f"v1/users/{self.id}")
+        user_info = user_info_req.json()
+        self.description = user_info["description"]
+        self.is_banned = user_info["isBanned"]
+        self.name = user_info["name"]
+        self.display_name = user_info["displayName"]
+        has_premium_req = requests.get(f"https://premiumfeatures.roblox.com/v1/users/{self.id}/validate-membership")
+        self.has_premium = has_premium_req
+
+    @property
+    def status(self):
+        status_req = requests.get(endpoint + f"v1/users/{self.id}/status")
+        return status_req.json()["status"]

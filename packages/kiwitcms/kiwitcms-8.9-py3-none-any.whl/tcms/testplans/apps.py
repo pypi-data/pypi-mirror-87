b104e2d917daaa895087a25917c6e3dbbfac641b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package kiwitcms/8.9/kiwitcms-8.9-py3-none-any.whl

---
+++ packages/kiwitcms/kiwitcms-8.9-py3-none-any.whl/tcms/testplans/apps.py
@@ -0,0 +1,16 @@
+# pylint: disable=import-outside-toplevel
+from django.apps import AppConfig as DjangoAppConfig
+
+
+class AppConfig(DjangoAppConfig):
+    name = 'tcms.testplans'
+
+    def ready(self):
+        from django.db.models.signals import post_save, pre_delete, pre_save
+        from .models import TestPlan
+        from tcms import signals
+
+        pre_save.connect(signals.pre_save_clean, TestPlan)
+        post_save.connect(signals.handle_emails_post_plan_save, TestPlan)
+        post_save.connect(signals.handle_attachments_post_save, sender=TestPlan)
+        pre_delete.connect(signals.handle_attachments_pre_delete, sender=TestPlan)

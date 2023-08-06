c3fd96468d8a7c409f7f88ed887ea4f792261147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:52 +0000

    Add package Rune/0.3.0/Rune-0.3.0-py3-none-any.whl

---
+++ packages/Rune/Rune-0.3.0-py3-none-any.whl/rune_basis/resources.py
@@ -0,0 +1,17 @@
+PERMS = [
+    'MENU-ADMIN-BASIS',
+    'BASIS_ADMIN-LIST',
+    'BASIS_ADMIN-INSTALL',
+    'BASIS_ADMIN-UNINSTALL',
+    'BASIS_ADMIN-PERMISSIONS-CLEAN',
+]
+
+
+def run_install():
+    """Is run by `Rune-Basis` when install is clicked."""
+    pass
+
+
+def run_remove():
+    """Is run by `Rune-Basis` when uninstall is clicked."""
+    pass

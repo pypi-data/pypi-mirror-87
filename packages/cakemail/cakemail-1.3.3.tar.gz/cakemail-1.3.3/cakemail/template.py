1004e9eab5d6a1d1a85cdefb1698d18a09339d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:00 +0000

    Add package cakemail/1.3.3/cakemail-1.3.3.tar.gz

---
+++ packages/cakemail/cakemail-1.3.3.tar.gz/cakemail-1.3.3/cakemail/template.py
@@ -0,0 +1,22 @@
+from cakemail_openapi import TemplateApi
+from cakemail.wrapper import WrappedApi
+
+
+class Template(WrappedApi):
+    create: TemplateApi.create_template
+    delete: TemplateApi.delete_template
+    get: TemplateApi.get_template
+    list: TemplateApi.list_templates
+    update: TemplateApi.patch_template
+
+    def __init__(self, superclass):
+        super().__init__(
+            superclass=superclass,
+            namemap={
+                'create': 'create_template',
+                'delete': 'delete_template',
+                'get': 'get_template',
+                'list': 'list_templates',
+                'update': 'patch_template',
+            }
+        )

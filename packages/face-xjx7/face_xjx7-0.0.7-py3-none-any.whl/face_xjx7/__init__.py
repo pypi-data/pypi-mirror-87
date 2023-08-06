b77d29974cf5a7978e9e264dee8d0add5566774a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:15 +0000

    Add package face-xjx7/0.0.7/face_xjx7-0.0.7-py3-none-any.whl

---
+++ packages/face-xjx7/face_xjx7-0.0.7-py3-none-any.whl/face_xjx7/__init__.py
@@ -0,0 +1,20 @@
+from .face_xjx7 import *
+from .utils import *
+from .sample_api import *
+from .logs import *
+from .core import *
+from .config import *
+from .config import logging
+from .config import model_conf
+
+
+
+# from .face_xjx7.config import *
+# from face_xjx7.core import *
+# from face_xjx7.logs import *
+# from face_xjx7.models import *
+# from face_xjx7.sample_api import *
+# from face_xjx7.utils import *
+# from face_xjx7 import *
+
+name = "face_xjx7"

23e3def05e52b9c16311f72dea6c4476fc63d217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:58 +0000

    Add package pie-extended/0.0.23/pie_extended-0.0.23.tar.gz

---
+++ packages/pie-extended/pie_extended-0.0.23.tar.gz/pie_extended-0.0.23/pie_extended/models/fro/__init__.py
@@ -0,0 +1,20 @@
+from ...utils import Metadata, File, get_path
+
+DESC = Metadata(
+    "Ancien Francais (ENC)",
+    "fro",
+    ["Jean-Baptiste Camps", "Ariane Pinche", "Thibault Clérice", "Frédéric Duval", "Lucence Ing"],
+    "Model trained on diverse corpora (hagiography, geste, etc.)",
+    "https://github.com/chartes/deucalion-model-af"
+)
+
+DOWNLOADS = [
+    File("https://github.com/chartes/deucalion-model-af/raw/master/morph.tar", "morph.tar"),
+    File("https://github.com/chartes/deucalion-model-af/raw/master/lemma-pos.tar", "lemma-pos.tar")
+]
+
+
+Models = "<{},MODE,TEMPS,PERS,NOMB,GENRE,CAS,DEGRE><{},lemma,pos>".format(
+    get_path("fro", "morph.tar"),
+    get_path("fro", "lemma-pos.tar")
+)

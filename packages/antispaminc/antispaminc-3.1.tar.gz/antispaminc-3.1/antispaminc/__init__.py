bb802abd39b803108593e355d569df7e9d7bfd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:18 +0000

    Add package antispaminc/3.1/antispaminc-3.1.tar.gz

---
+++ packages/antispaminc/antispaminc-3.1.tar.gz/antispaminc-3.1/antispaminc/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+#
+#                    GNU GENERAL PUBLIC LICENSE
+#                      Version 3, 29 June 2007
+
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+__all__ = 'antispaminc',
+__version__ = '3.0'
+
+
+from .connect import Connect
+from .class_types import ClassTypes

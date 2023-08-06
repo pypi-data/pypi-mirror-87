396a24fba167c18f9822da8cfe3d4de12d3377e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:55 +0000

    Add package shipsnake/0.0.50/shipsnake-0.0.50-py3-none-any.whl

---
+++ packages/shipsnake/shipsnake-0.0.50-py3-none-any.whl/shipsnake/actions.py
@@ -0,0 +1,41 @@
+import shipsnake.build
+import toml
+import sys
+import os
+
+try:
+	from github_release import gh_release_create
+except:
+	os.system('pip install github_release')
+	try:
+		from github_release import gh_release_create
+	except:
+			os.system('pip3 install github_release')
+			try:
+				from github_release import gh_release_create
+			except:
+				exit()
+		
+with open('shipsnake.toml') as file:
+	data = toml.loads(file.read())
+
+if sys.platform.startswith('win'):
+
+	shipsnake.build.main(data['latest_build'],[],nointeraction=True)
+
+	with open('shipsnake.toml') as file:
+		data = toml.loads(file.read())
+
+	gh_release_create(data['build']['github']+" ", data['latest_build'], publish=True, name="Version"+data['latest_build'], asset_pattern=f"./dist/pyinstaller/{data['name']}.dmg")
+
+elif sys.platform.startswith('darwin') or True:
+
+	shipsnake.build.main(data['latest_build'],[],nointeraction=True)
+
+	with open('shipsnake.toml') as file:
+		data = toml.loads(file.read())
+
+	gh_release_create(data['build']['github']+" ", data['latest_build'], publish=True, name="Version"+data['latest_build'], asset_pattern=f"./dist/pyinstaller/{data['name']}.dmg")
+
+else:
+	print('This is not being run on Mac and Windows.')

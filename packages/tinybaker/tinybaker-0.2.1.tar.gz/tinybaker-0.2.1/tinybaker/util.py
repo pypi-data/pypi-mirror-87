4415439bf15de83553e58d1b8d772ea8760b575d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:46 +0000

    Add package tinybaker/0.2.1/tinybaker-0.2.1.tar.gz

---
+++ packages/tinybaker/tinybaker-0.2.1.tar.gz/tinybaker-0.2.1/tinybaker/util.py
@@ -0,0 +1,12 @@
+from .workarounds.annot import is_fileset
+
+
+def get_files_in_path_dict(pathdict):
+    retval = set()
+    for tag in pathdict:
+        if is_fileset(tag):
+            for file in pathdict[tag]:
+                retval.add(file)
+        else:
+            retval.add(pathdict[tag])
+    return retval

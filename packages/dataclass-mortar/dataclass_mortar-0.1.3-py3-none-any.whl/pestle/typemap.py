2b68e30d2d5bc07785fe5e49aaf5ae3d83143329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:40 +0000

    Add package dataclass-mortar/0.1.3/dataclass_mortar-0.1.3-py3-none-any.whl

---
+++ packages/dataclass-mortar/dataclass_mortar-0.1.3-py3-none-any.whl/pestle/typemap.py
@@ -0,0 +1,38 @@
+from typing import Type, Union, Text, List, get_args, get_origin, Any
+from sqlalchemy.sql.sqltypes import NullType, DateTime, Float, JSON, Integer, String
+from .uttil import Datetime
+from dataclasses import dataclass, is_dataclass, fields, Field, field
+from sqlalchemy.types import TypeEngine
+
+alchamey_type = {
+    Text: String,
+    List: String,
+    Datetime: DateTime,
+    float: Float,
+    int: Integer,
+    dict: JSON
+}
+
+
+def python_to_alechamy(a_type):
+    generic_type = get_origin(a_type)
+    if generic_type:
+        if generic_type in (dict, list):
+            return JSON
+        template = list(get_args(a_type))
+        if generic_type == Union:
+            try:
+                template.remove(type(None))
+            except ValueError:
+                ...  # no worries if it isn't in there.
+            if len(template) == 1:
+                return python_to_alechamy(template[0])
+        else:
+            a_type = generic_type
+    try:
+        if issubclass(a_type, TypeEngine) or is_dataclass(a_type):
+            return a_type
+    except TypeError:
+        ...
+
+    return alchamey_type.get(a_type, String)

f9ccc1224f9f74f75da1e722c1dce6c5cd6cb71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package freud-api-crawler/0.11.0/freud_api_crawler-0.11.0-py2.py3-none-any.whl

---
+++ packages/freud-api-crawler/freud_api_crawler-0.11.0-py2.py3-none-any.whl/freud_api_crawler/string_utils.py
@@ -0,0 +1,40 @@
+import re
+
+CLEAN_UP_PATTERNS = [
+    ('-<br />', '<lb break="no"/>'),
+    ('<br />', '<lb />'),
+    ('\n', ''),
+]
+
+
+def clean_markup(source, patterns=CLEAN_UP_PATTERNS):
+    """ applies search and replace
+    :param source: A string to clean
+    :type source: str
+    :param patterns: a list of tuples `[('-<br />', '<lb break="no"/>'),`]
+    :type patterns: list
+
+    :return: the cleaned string
+    :rtype: string
+
+    """
+    for x in patterns:
+        source = source.replace(x[0], x[1])
+    return source
+
+
+def extract_page_nr(some_string):
+    """ extracts the page number from a string like `Seite 21`
+
+    :param some_string: e.g. `Seite 21`
+    :type some_string: string
+
+    :return: The page number e.g. `21`
+    :rtype: str
+    """
+
+    page_nr = re.findall(r'\d+', some_string)
+    if len(page_nr) > 0:
+        return "-".join(page_nr)
+    else:
+        return some_string

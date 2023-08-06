99ff2207852fbfa471df0d7bce1ba06d01e74fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:32 +0000

    Add package platform-utility/1.0.5/platform_utility-1.0.5.tar.gz

---
+++ packages/platform-utility/platform_utility-1.0.5.tar.gz/platform_utility-1.0.5/utilitylibrary/upload.py
@@ -0,0 +1,24 @@
+
+from utilitylibrary.unique import gennerate_unique
+
+'''
+    Get file type from filename.
+    @filename name of file.
+    return mimeType
+'''
+
+def split_mime_type(filename):
+    mimeType = filename.split(".")
+    return mimeType[1]
+
+
+'''
+    Upload list of file.
+    @f temp of file.
+    @destination_path pathupload with out filename.
+'''
+def handle_uploaded_file(f,destination_path):
+    with open(destination_path+gennerate_unique()+'.'+split_mime_type(f.name), 'wb+') as destination:  
+        for chunk in f.chunks():  
+            destination.write(chunk)
+            

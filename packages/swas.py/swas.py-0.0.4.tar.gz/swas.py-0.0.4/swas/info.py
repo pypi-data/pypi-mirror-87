3f99dc0a8b4bdf8152d2df758e1576b4b2949e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:23 +0000

    Add package swas.py/0.0.4/swas.py-0.0.4.tar.gz

---
+++ packages/swas.py/swas.py-0.0.4.tar.gz/swas.py-0.0.4/swas/info.py
@@ -0,0 +1,8 @@
+def yt():
+  return "https://www.youtube.com/channel/UC2ITRZ4_Di-KMHSIylTQbBA"
+
+class userinfo():
+  def __init__(self):
+    self.name = "Swas.py"
+    self.discriminator = 7370
+    self.id = 556119013298667520    

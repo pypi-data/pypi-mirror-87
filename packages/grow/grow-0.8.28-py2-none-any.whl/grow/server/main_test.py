0c0f009004d16f5d7d0f3069151cd413e8a45200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package grow/0.8.28/grow-0.8.28-py2-none-any.whl

---
+++ packages/grow/grow-0.8.28-py2-none-any.whl/grow/server/main_test.py
@@ -0,0 +1,108 @@
+"""Tests for the main grow development server."""
+
+import unittest
+import webapp2
+from grow.pods import pods
+from grow.server import main
+from grow.testing import testing
+
+
+class PodHandlerTestCase(unittest.TestCase):
+
+    def test_request(self):
+        dir_path = testing.create_test_pod_dir()
+        pod = pods.Pod(dir_path)
+        pod.router.add_all(use_cache=False)
+
+        # When serving a pod, should 200.
+        app = main.create_wsgi_app(pod, 'localhost', 8080)
+        request = webapp2.Request.blank('/')
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+
+        # Verify 404 is sent for page not found.
+        request = webapp2.Request.blank('/dummy/page/')
+        response = request.get_response(app)
+        self.assertEqual(404, response.status_int)
+
+        # Verify 206 for partial content.
+        headers = {'Range': 'bytes=0-4'}
+        request = webapp2.Request.blank('/public/file.txt', headers=headers)
+        response = request.get_response(app)
+        self.assertEqual(206, response.status_int)
+        self.assertEqual('bytes 0-4/13', response.headers['Content-Range'])
+        self.assertEqual('Hello', response.body)
+
+        headers = {'Range': 'bytes=5-13'}
+        request = webapp2.Request.blank('/public/file.txt', headers=headers)
+        response = request.get_response(app)
+        self.assertEqual('bytes 5-12/13', response.headers['Content-Range'])
+        self.assertEqual(' World!\n', response.body)
+
+        # Verify full response when headers omitted.
+        request = webapp2.Request.blank('/public/file.txt')
+        response = request.get_response(app)
+        self.assertEqual('Hello World!\n', response.body)
+
+        # Verify 304.
+        url_path = '/public/file.txt'
+        matched = pod.router.routes.match(url_path)
+        controller = pod.router.get_render_controller(
+            url_path, matched.value, params=matched.params)
+        response_headers = controller.get_http_headers()
+        headers = {'If-None-Match': response_headers['Last-Modified']}
+        request = webapp2.Request.blank(url_path, headers=headers)
+        response = request.get_response(app)
+        self.assertEqual(304, response.status_int)
+        self.assertEqual('', response.body)
+
+        response = request.get_response(app)
+        self.assertEqual(304, response.status_int)
+        self.assertEqual('', response.body)
+
+        # Verify sitemap on server.
+        path = '/root/sitemap.xml'
+        request = webapp2.Request.blank(path)
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+        self.assertEqual('application/xml', response.headers['Content-Type'])
+
+    def test_admin(self):
+        dir_path = testing.create_test_pod_dir()
+        pod = pods.Pod(dir_path)
+        pod.router.add_all(use_cache=False)
+        app = main.create_wsgi_app(pod, 'localhost', 8080)
+
+        # Verify routes are served.
+        request = webapp2.Request.blank('/_grow/routes')
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+        js_sentinel = '<h2>Routes</h2>'
+        self.assertIn(js_sentinel, response.body)
+
+        request = webapp2.Request.blank('/_grow/ui/css/admin.min.css')
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+
+    def test_ui(self):
+        dir_path = testing.create_test_pod_dir()
+        pod = pods.Pod(dir_path)
+        pod.router.add_all(use_cache=False)
+        app = main.create_wsgi_app(pod, 'localhost', 8080)
+
+        # Verify JS and CSS are served.
+        request = webapp2.Request.blank('/_grow/ui/js/ui.min.js')
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+        js_sentinel = 'setAttribute("target","_blank")'
+        self.assertIn(js_sentinel, response.body)
+
+        request = webapp2.Request.blank('/_grow/ui/css/ui.min.css')
+        response = request.get_response(app)
+        self.assertEqual(200, response.status_int)
+        css_sentinel = '#grow'
+        self.assertIn(css_sentinel, response.body)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/grow/grow-0.8.28-py2-none-any.whl/grow/testing/testing.py
@@ -0,0 +1,61 @@
+"""Testing resources for grow."""
+
+import os
+import shutil
+import tempfile
+import unittest
+from grow.pods import pods
+from grow.common import utils
+
+
+TESTDATA_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), 'testdata'))
+
+
+# root is relative to testdata/ dir
+def create_test_pod_dir(root='pod'):
+    dest_dir = tempfile.mkdtemp()
+    dest_dir = os.path.join(dest_dir, 'test_pod')
+    source_dir = os.path.join(TESTDATA_DIR, root)
+    shutil.copytree(source_dir, dest_dir)
+    return dest_dir
+
+
+def create_test_pod(root='pod'):
+    return pods.Pod(create_test_pod_dir(root))
+
+
+def create_pod():
+    root = tempfile.mkdtemp()
+    return pods.Pod(root)
+
+
+def get_testdata_dir():
+    return TESTDATA_DIR
+
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self, *args, **kwargs):
+        self.is_appengine = utils.is_appengine()
+        if self.is_appengine:
+            # pylint: disable=import-error
+            from google.appengine.ext import testbed
+            self.testbed = testbed.Testbed()
+            self.testbed.activate()
+            self.testbed.init_datastore_v3_stub()
+            self.testbed.init_memcache_stub()
+
+    def tearDown(self, *args, **kwargs):
+        if self.is_appengine:
+            self.testbed.deactivate()
+
+def render_path(pod, path):
+    """Given a pod and a path render the path."""
+    matched = pod.match(path)
+    controller = pod.router.get_render_controller(
+        matched.path, matched.value, params=matched.params)
+    jinja_env = pod.render_pool.get_jinja_env(
+        controller.doc.locale) if controller.use_jinja else None
+    rendered_document = controller.render(jinja_env=jinja_env)
+    return rendered_document.read()

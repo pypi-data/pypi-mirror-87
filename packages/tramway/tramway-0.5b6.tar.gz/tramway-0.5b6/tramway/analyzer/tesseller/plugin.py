8b3f467aa6b2f1bbbfa50587974d9c6b9dc3ad6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:46 +0000

    Add package tramway/0.5b6/tramway-0.5b6.tar.gz

---
+++ packages/tramway/tramway-0.5b6.tar.gz/tramway-0.5b6/tramway/analyzer/tesseller/plugin.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2020, Institut Pasteur
+#   Contributor: François Laurent
+
+# This file is part of the TRamWAy software available at
+# "https://github.com/DecBayComp/TRamWAy" and is distributed under
+# the terms of the CeCILL license as circulated at the following URL
+# "http://www.cecill.info/licenses.en.html".
+
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+
+from .abc import *
+from .proxy import *
+from tramway.tessellation import plugins
+
+
+class TessellerPlugin(TessellerProxy):
+    """
+    Wraps any plugin referenced in `tramway.tessellation.plugins`.
+    """
+    __slots__ = ('_module','_setup')
+    def __init__(self, name, **kwargs):
+        try:
+            setup, module = plugins[name]
+        except KeyError:
+            raise KeyError("no such tesseller plugin: '{}'".format(name))
+        cls = setup['make']
+        if isinstance(cls, str):
+            cls = getattr(module, cls)
+        TessellerProxy.__init__(self, cls, **kwargs)
+        self._module, self._setup = module, setup
+        self._init_kwargs = { attr: None for attr in \
+                ('min_distance','avg_distance','max_distance',
+                 'min_probability','avg_probability','max_probability',
+                ) }
+
+
+def tesseller_plugin(name):
+    """
+    Translate plugin names into `RWAnalyzer`-ready tessellers as
+    defined in the *tessellers* module, if available,
+    else returns a generic `TessellerPlugin` initializer.
+    """
+    import proxied
+    if name == 'grid':
+        return proxied.Squares
+    elif name == 'hexagon':
+        return proxied.Hexagons
+    elif name == 'kmeans':
+        return proxied.KMeans
+    elif name in ('gwr', 'gas'):
+        return proxied.GWR
+    else:
+        def init(**kwargs):
+            return TessellerPlugin(name, **kwargs)
+        return init
+
+__all__ = ['TessellerPlugin', 'tesseller_plugin']
+

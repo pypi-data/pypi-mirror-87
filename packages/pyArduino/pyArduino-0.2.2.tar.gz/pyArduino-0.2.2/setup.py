bb56e65742f865bf6ddfb7d2f48ddad2a351c5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:37 +0000

    Add package pyArduino/0.2.2/pyArduino-0.2.2.tar.gz

---
+++ packages/pyArduino/pyArduino-0.2.2.tar.gz/pyArduino-0.2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyArduino", # Replace with your own username
+    version="0.2.2",
+    author="Alex Vergara",
+    author_email="pyryzyab@tutanota.com",
+    description="Python module to handle Arduino port serial comunication",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'pyserial',
+          'pyfirmata'
+      ],
+    url="https://github.com/Pyryzyab/pyArduino",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

bf18307fde9a81ac7897976edce3b518c439f3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package stheno/0.4.2/stheno-0.4.2.tar.gz

---
+++ packages/stheno/stheno-0.4.2.tar.gz/stheno-0.4.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "numpy>=1.16",
+    "fdm",
+    "plum-dispatch>=0.2.3",
+    "backends>=0.4.3",
+    "backends-matrix>=0.3.0",
+]
+
+setup(
+    packages=find_packages(exclude=["docs"]),
+    python_requires=">=3.6",
+    install_requires=requirements,
+    include_package_data=True,
+)

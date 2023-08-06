0d81265f7f6b84f17ab7eeda76dfb9c501b1f7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:38 +0000

    Add package autogalaxy/0.16.5/autogalaxy-0.16.5-py3-none-any.whl

---
+++ packages/autogalaxy/autogalaxy-0.16.5-py3-none-any.whl/autogalaxy/pipeline/phase/dataset/phase.py
@@ -0,0 +1,223 @@
+from astropy import cosmology as cosmo
+
+import autofit as af
+from autoarray.inversion import pixelizations as pix
+from autoarray.inversion import regularization as reg
+from autogalaxy.hyper import hyper_data as hd
+from autogalaxy.pipeline.phase import abstract
+from autogalaxy.pipeline.phase import extensions
+from autogalaxy.pipeline.phase.dataset.result import Result
+
+
+class PhaseDataset(abstract.AbstractPhase):
+    galaxies = af.PhaseProperty("galaxies")
+
+    Result = Result
+
+    def __init__(self, settings, search, galaxies=None, cosmology=cosmo.Planck15):
+        """
+
+        A phase in an lens pipeline. Uses the set non_linear search to try to fit models and hyper_galaxies
+        passed to it.
+
+        Parameters
+        ----------
+        search: class
+            The class of a non_linear search
+        """
+
+        super().__init__(
+            search=search, settings=settings, galaxies=galaxies, cosmology=cosmology
+        )
+
+        self.hyper_name = None
+        self.use_as_hyper_dataset = False
+        self.is_hyper_phase = False
+
+    def run(
+        self,
+        dataset,
+        mask,
+        results=None,
+        info=None,
+        pickle_files=None,
+        log_likelihood_cap=None,
+    ):
+        """
+        Run this phase.
+
+        Parameters
+        ----------
+        mask: Mask2D
+            The default masks passed in by the pipeline
+        results: autofit.tools.pipeline.ResultsCollection
+            An object describing the results of the last phase or None if no phase has been executed
+        dataset: scaled_array.ScaledSquarePixelArray
+            An masked_imaging that has been masked
+
+        Returns
+        -------
+        result: AbstractPhase.Result
+            A result object comprising the best fit model and other hyper_galaxies.
+        """
+
+        self.model = self.model.populate(results)
+
+        results = results or af.ResultsCollection()
+
+        self.modify_dataset(dataset=dataset, results=results)
+        self.modify_settings(dataset=dataset, results=results)
+        self.modify_search_paths()
+
+        analysis = self.make_analysis(dataset=dataset, mask=mask, results=results)
+
+        result = self.run_analysis(
+            analysis=analysis,
+            info=info,
+            pickle_files=pickle_files,
+            log_likelihood_cap=log_likelihood_cap,
+        )
+
+        return self.make_result(result=result, analysis=analysis)
+
+    def make_analysis(self, dataset, mask, results=None):
+        """
+        Returns an lens object. Also calls the prior passing and masked_imaging modifying functions to allow child
+        classes to change the behaviour of the phase.
+
+        Parameters
+        ----------
+        mask: Mask2D
+            The default masks passed in by the pipeline
+        dataset: im.Imaging
+            An masked_imaging that has been masked
+        results: autofit.tools.pipeline.ResultsCollection
+            The result from the previous phase
+
+        Returns
+        -------
+        lens : Analysis
+            An lens object that the `NonLinearSearch` calls to determine the fit of a set of values
+        """
+        raise NotImplementedError()
+
+    def modify_dataset(self, dataset, results):
+        pass
+
+    def modify_settings(self, dataset, results):
+        pass
+
+    def modify_search_paths(self):
+        """
+        Modify the output paths of the phase before the non-linear search is run, so that the output path can be
+        customized using the tags of the phase.
+        """
+        if self.hyper_name is None:
+            hyper_tag = ""
+        else:
+            hyper_tag = f"{self.hyper_name}__"
+
+        if not self.has_pixelization:
+            self.search.paths.tag = f"{hyper_tag}{self.settings.phase_tag_no_inversion}"
+        else:
+            self.search.paths.tag = (
+                f"{hyper_tag}{self.settings.phase_tag_with_inversion}"
+            )
+
+    def extend_with_inversion_phase(self, hyper_search, inversion_pixels_fixed=None):
+
+        return extensions.InversionPhase(
+            phase=self,
+            hyper_search=hyper_search,
+            model_classes=(pix.Pixelization, reg.Regularization),
+            inversion_pixels_fixed=inversion_pixels_fixed,
+        )
+
+    def extend_with_multiple_hyper_phases(
+        self, setup_hyper, include_inversion=False, inversion_pixels_fixed=None
+    ):
+
+        self.use_as_hyper_dataset = True
+
+        hyper_phases = []
+
+        if include_inversion:
+            if self.has_pixelization and setup_hyper.inversion_search:
+
+                if (
+                    not setup_hyper.hyper_image_sky
+                    and not setup_hyper.hyper_background_noise
+                ):
+                    phase_inversion = extensions.InversionPhase(
+                        phase=self,
+                        hyper_search=setup_hyper.inversion_search,
+                        model_classes=(pix.Pixelization, reg.Regularization),
+                        inversion_pixels_fixed=inversion_pixels_fixed,
+                    )
+                elif (
+                    setup_hyper.hyper_image_sky
+                    and not setup_hyper.hyper_background_noise
+                ):
+                    phase_inversion = extensions.InversionPhase(
+                        phase=self,
+                        hyper_search=setup_hyper.inversion_search,
+                        model_classes=(
+                            pix.Pixelization,
+                            reg.Regularization,
+                            hd.HyperImageSky,
+                        ),
+                        inversion_pixels_fixed=inversion_pixels_fixed,
+                    )
+                elif (
+                    not setup_hyper.hyper_image_sky
+                    and setup_hyper.hyper_background_noise
+                ):
+                    phase_inversion = extensions.InversionPhase(
+                        phase=self,
+                        hyper_search=setup_hyper.inversion_search,
+                        model_classes=(
+                            pix.Pixelization,
+                            reg.Regularization,
+                            hd.HyperBackgroundNoise,
+                        ),
+                        inversion_pixels_fixed=inversion_pixels_fixed,
+                    )
+                else:
+                    phase_inversion = extensions.InversionPhase(
+                        phase=self,
+                        hyper_search=setup_hyper.inversion_search,
+                        model_classes=(
+                            pix.Pixelization,
+                            reg.Regularization,
+                            hd.HyperImageSky,
+                            hd.HyperBackgroundNoise,
+                        ),
+                        inversion_pixels_fixed=inversion_pixels_fixed,
+                    )
+
+                hyper_phases.append(phase_inversion)
+
+        if setup_hyper.hyper_galaxies_search is not None:
+            phase_hyper_galaxy = extensions.HyperGalaxyPhase(
+                phase=self,
+                hyper_search=setup_hyper.hyper_galaxies_search,
+                include_sky_background=setup_hyper.hyper_image_sky,
+                include_noise_background=setup_hyper.hyper_background_noise,
+                hyper_galaxy_names=setup_hyper.hyper_galaxy_names,
+            )
+            hyper_phases.append(phase_hyper_galaxy)
+
+        if setup_hyper.hyper_galaxy_phase_first:
+            if (
+                include_inversion and setup_hyper.inversion_search is not None
+            ) and setup_hyper.hyper_galaxies_search is not None:
+                hyper_phases = [phase for phase in reversed(hyper_phases)]
+
+        if len(hyper_phases) == 0:
+            return self
+        else:
+            return extensions.CombinedHyperPhase(
+                phase=self,
+                hyper_search=setup_hyper.hyper_combined_search,
+                hyper_phases=hyper_phases,
+            )
---
+++ packages/autogalaxy/autogalaxy-0.16.5-py3-none-any.whl/autogalaxy/pipeline/phase/extensions/hyper_phase.py
@@ -0,0 +1,91 @@
+from os import path
+import autofit as af
+from autofit.tools.phase import Dataset
+from autogalaxy.pipeline.phase import abstract
+
+
+class HyperPhase:
+    def __init__(self, phase: abstract.AbstractPhase, hyper_search, hyper_name: str):
+        """
+        Abstract HyperPhase. Wraps a phase, performing that phase before performing the action
+        specified by the run_hyper.
+
+        Parameters
+        ----------
+        phase
+            A phase
+        """
+        self.phase = phase
+        self.hyper_name = hyper_name
+        self.hyper_search = hyper_search
+
+    def run_hyper(self, *args, **kwargs) -> af.Result:
+        """
+        Run the hyper_galaxies phase.
+
+        Parameters
+        ----------
+        args
+        kwargs
+
+        Returns
+        -------
+        result
+            The result of the hyper_galaxies phase.
+        """
+        raise NotImplementedError()
+
+    def make_hyper_phase(self) -> abstract.AbstractPhase:
+        """
+        Returns
+        -------
+        hyper_phase
+            A copy of the original phase with a modified name and path
+        """
+        self.phase.search = self.hyper_search.copy_with_name_extension(
+            extension=self.phase.name, path_prefix=self.phase.paths.path_prefix
+        )
+        self.phase.hyper_name = self.hyper_name
+        return self.phase
+
+    def run(
+        self,
+        dataset: Dataset,
+        results: af.ResultsCollection,
+        info=None,
+        pickle_files=None,
+        **kwargs,
+    ) -> af.Result:
+        """
+        Run the hyper phase and then the hyper_galaxies phase.
+
+        Parameters
+        ----------
+        dataset
+            Data
+        results
+            Results from previous phases.
+        kwargs
+
+        Returns
+        -------
+        result
+            The result of the phase, with a hyper_galaxies result attached as an attribute with the hyper_name of this
+            phase.
+        """
+        result = self.phase.run(
+            dataset, results=results, info=info, pickle_files=pickle_files, **kwargs
+        )
+        results.add(self.phase.paths.name, result)
+        hyper_result = self.run_hyper(
+            dataset=dataset,
+            results=results,
+            info=info,
+            pickle_files=pickle_files,
+            **kwargs,
+        )
+        setattr(result, self.hyper_name, hyper_result)
+        return result
+
+    def __getattr__(self, item):
+        return getattr(self.phase, item)

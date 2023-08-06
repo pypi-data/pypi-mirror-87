bcfc4858ad0d373d12e0fb78b1f1f0a45e97769b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package databricks-bundle/0.5.10b2/databricks-bundle-0.5.10b2.tar.gz

---
+++ packages/databricks-bundle/databricks-bundle-0.5.10b2.tar.gz/databricks-bundle-0.5.10b2/src/databricksbundle/spark/BasicSessionFactory.py
@@ -0,0 +1,5 @@
+from databricksbundle.spark.ScriptSessionFactory import ScriptSessionFactory
+
+# @deprecated, use ScriptSessionFactory instead
+class BasicSessionFactory(ScriptSessionFactory):
+    pass

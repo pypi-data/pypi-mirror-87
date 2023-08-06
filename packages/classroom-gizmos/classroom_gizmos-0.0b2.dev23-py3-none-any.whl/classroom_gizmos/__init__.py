a2c356ee1a82a4606883c05340044b678679f219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:17 +0000

    Add package classroom-gizmos/0.0b2.dev23/classroom_gizmos-0.0b2.dev23-py3-none-any.whl

---
+++ packages/classroom-gizmos/classroom_gizmos-0.0b2.dev23-py3-none-any.whl/classroom_gizmos/__init__.py
@@ -0,0 +1,9 @@
+## See https://packaging.python.org/guides/single-sourcing-package-version/
+
+'''A collection of functions for ipython use in the classroom
+
+   -------   Time-stamp: <2020-11-24T18:06:53.308934-05:00 hedfp>   ------  '''
+
+
+__version__ = "0.0b2.dev23" # X.YaN.devM format for now
+

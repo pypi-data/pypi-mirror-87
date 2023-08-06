08ba267a75d5da7f3c874d7544207226fbed313e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:08 +0000

    Add package kshingle/0.2.0/kshingle-0.2.0.tar.gz

---
+++ packages/kshingle/kshingle-0.2.0.tar.gz/kshingle-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def read(fname):
+    import os
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='kshingle',
+      version='0.2.0',
+      description='lSplit strings into (character-based) k-shingles',
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
+      url='http://github.com/ulf1/kshingle',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['kshingle'],
+      install_requires=[
+          'setuptools>=40.0.0',
+          'numba>=0.52.0'
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)

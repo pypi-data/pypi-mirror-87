98db1bc66d020771149834cc35cb0621474a7ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:14 +0000

    Add package timeseries-fastai/2.1.1/timeseries_fastai-2.1.1-py3-none-any.whl

---
+++ packages/timeseries-fastai/timeseries_fastai-2.1.1-py3-none-any.whl/timeseries_fastai/imports.py
@@ -0,0 +1,6 @@
+from fastai.basics import *
+from fastai.torch_basics import *
+from fastai.torch_core import *
+from fastai.callback.all import *
+from fastai.data.all import *
+from fastai.vision.data import get_grid

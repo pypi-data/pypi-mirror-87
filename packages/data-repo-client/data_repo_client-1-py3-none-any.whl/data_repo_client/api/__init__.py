2e6d54179494269bfba6ac72cef2f86d555ad174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package data-repo-client/1/data_repo_client-1-py3-none-any.whl

---
+++ packages/data-repo-client/data_repo_client-1-py3-none-any.whl/data_repo_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.data_repository_service_api import DataRepositoryServiceApi
+from openapi_client.api.repository_api import RepositoryApi
+from openapi_client.api.resources_api import ResourcesApi
+from openapi_client.api.unauthenticated_api import UnauthenticatedApi

aad32733c44bd8b5b8aa8fc17af96677e7b51274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:55 +0000

    Add package etcd-import/0.0.1/etcd-import-0.0.1.tar.gz

---
+++ packages/etcd-import/etcd-import-0.0.1.tar.gz/etcd-import-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='etcd-import',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Michael Drupvesky',
+    author_email='michael.dru@outlook.com',
+    description='etcd3 API importer'
+)

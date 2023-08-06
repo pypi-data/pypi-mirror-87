854b4e62cfd418df7c41e779f89da0e8c7a094ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:05 +0000

    Add package kubemq/2.2.1/kubemq-2.2.1.tar.gz

---
+++ packages/kubemq/kubemq-2.2.1.tar.gz/kubemq-2.2.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+
+setuptools.setup(name='kubemq',
+      version='2.2.1',
+      description='KubeMQ SDK for Python',
+	  long_description=README,
+	  long_description_content_type="text/markdown",
+      url='https://github.com/kubemq-io/kubemq-Python',
+      author='KubeMQ',
+      author_email='info@kubemq.io',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'future>=0.18',
+          'grpcio>=1.27.2',
+          'protobuf>=3.13.0',
+          'setuptools~=40.8.0',
+          'PyJWT>=1.7.1',
+      ],
+      zip_safe=False)

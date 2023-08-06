c3962ba8f0aba7b3f4c53df502c0abc173c62641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:16 +0000

    Add package dataclass-mortar/0.1.0/dataclass-mortar-0.1.0.tar.gz

---
+++ packages/dataclass-mortar/dataclass-mortar-0.1.0.tar.gz/dataclass-mortar-0.1.0/mortar/util.py
@@ -0,0 +1,23 @@
+import datetime
+from sqlalchemy.orm.session import sessionmaker
+
+Datetime = datetime.datetime
+
+class _Empty: ...
+
+
+def operation(func):
+    @wraps(func)
+    def wrapper(instance: MetaData, op, session=None):
+        instance.ensure_table_exists(op)
+
+        commit = session is None
+        session: Session = session or sessionmaker(bind=instance.bind)()
+        ret = func(instance, op, session)
+        if commit:
+            session.commit()
+        if ret == _Empty:
+            return None
+        return ret or instance
+
+    return wrapper

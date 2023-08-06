fefb4f04a2c3a6730759320b1a0b13f65c67bc8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:52 +0000

    Add package x-rand/0.10/x_rand-0.10.tar.gz

---
+++ packages/x-rand/x_rand-0.10.tar.gz/x_rand-0.10/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'x_rand',
+  packages = ['x_rand'],
+  version = '0.10',
+  license='MIT',
+  description = 'Random Problem Creation for edX',
+  author = 'Connor Makowski',
+  author_email = 'connor.m.makowski@gmail.com',
+  url = 'https://github.com/connor-makowski/x_rand',
+  download_url = 'https://github.com/connor-makowski/x_rand/blob/master/dist/x_rand-0.8.tar.gz',
+  keywords = ['x_rand', 'edX', 'randomization', 'random', 'cheating', 'prevention', 'detection'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.7',
+  ],
+  install_requires=[
+          'random2>=1',
+      ],
+)

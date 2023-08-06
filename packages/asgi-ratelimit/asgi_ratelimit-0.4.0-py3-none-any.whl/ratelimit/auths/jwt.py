c559aef723cb5764593703d231711871afac3d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:11 +0000

    Add package asgi-ratelimit/0.4.0/asgi_ratelimit-0.4.0-py3-none-any.whl

---
+++ packages/asgi-ratelimit/asgi_ratelimit-0.4.0-py3-none-any.whl/ratelimit/auths/jwt.py
@@ -0,0 +1,57 @@
+from typing import Callable, Awaitable, Tuple, List, Union, TypeVar
+
+import jwt
+
+try:
+    from cryptography.hazmat.primitives.asymmetric.rsa import (
+        RSAPrivateKey,
+        RSAPublicKey,
+    )
+except ImportError:  # pragma: no cover
+    RSAPublicKey = TypeVar("RSAPublicKey")
+    RSAPrivateKey = TypeVar("RSAPrivateKey")
+
+from ..types import Scope
+from . import EmptyInformation
+
+
+def create_jwt_auth(
+    key: Union[bytes, str, RSAPublicKey, RSAPrivateKey],
+    algorithms: Union[List[str], str],
+) -> Callable[[Scope], Awaitable[Tuple[str, str]]]:
+    """
+    create jwt authentication function
+
+    * key: for algorithm secret key
+    * algorithms: Possible algorithms in https://pyjwt.readthedocs.io/en/latest/algorithms.html#digital-signature-algorithms
+    """
+
+    async def jwt_auth(scope: Scope) -> Tuple[str, str]:
+        """
+        About jwt header, read this link:
+        https://stackoverflow.com/questions/33265812/best-http-authorization-header-type-for-jwt
+        """
+        for name, value in scope["headers"]:  # type: bytes, bytes
+            if name == b"authorization":
+                authorization = value.decode("utf8")
+                break
+        else:
+            authorization = None
+
+        if not authorization:
+            raise EmptyInformation(scope)
+
+        token_type, json_web_token = authorization.split(" ")
+
+        assert (
+            token_type == "Bearer"
+        ), "Authorization header must be `Bearer` type. Like: `Bearer LONG_JWT`"
+
+        data = jwt.decode(json_web_token, key, algorithms=algorithms)
+
+        try:
+            return data["user"], data.get("group", "default")
+        except KeyError:
+            raise EmptyInformation(scope)
+
+    return jwt_auth

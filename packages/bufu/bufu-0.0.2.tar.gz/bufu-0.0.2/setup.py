909a493c9ba005d9c12404d707fbeeaac6d19667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:16 +0000

    Add package bufu/0.0.2/bufu-0.0.2.tar.gz

---
+++ packages/bufu/bufu-0.0.2.tar.gz/bufu-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='bufu',
+    version='0.0.2',
+    author='indigo13love',
+    author_email='indigo13love@gmail.com',
+    description='Minimal file upload for Snowflake internal stage',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license_files='LICENSE',
+    url='https://github.com/indigo13love/bufu',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+    ],
+    entry_points = {
+        'console_scripts': ['bufu = bufu.bufu:main']
+    },
+    python_requires='>=3.6,<3.9',
+)

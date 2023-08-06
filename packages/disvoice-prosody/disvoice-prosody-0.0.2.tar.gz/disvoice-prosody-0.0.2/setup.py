abb4c875b9371d1eab3ff9e921aa20ad12a39ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:52 +0000

    Add package disvoice-prosody/0.0.2/disvoice-prosody-0.0.2.tar.gz

---
+++ packages/disvoice-prosody/disvoice-prosody-0.0.2.tar.gz/disvoice-prosody-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="disvoice-prosody",
+    version="0.0.2",
+    author="Lurein Perera",
+    author_email="lureinperera@gmail.com",
+    description="A pip installable version of the prosody function from  jcvazquezc's DisVoice library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lurein/DisVoice",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

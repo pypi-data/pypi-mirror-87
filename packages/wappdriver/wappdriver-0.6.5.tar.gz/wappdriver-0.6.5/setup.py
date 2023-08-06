8bd7e8ad08ea10b4f7e424ebe463d526be54bb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:58 +0000

    Add package wappdriver/0.6.5/wappdriver-0.6.5.tar.gz

---
+++ packages/wappdriver/wappdriver-0.6.5.tar.gz/wappdriver-0.6.5/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import re
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('wappdriver/__init__.py', 'r').read(),
+    re.M).group(1)
+
+setuptools.setup(
+    name="wappdriver",
+    version=version,
+    license='MIT',
+    author="Aahnik Daw",
+    author_email="meet.aahnik@gmail.com",
+    description="A package that automates sending messages through WhatsApp Web ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aahnik/wappdriver",
+    install_requires=['selenium', 'pyyaml', 'requests', 'pyfiglet', 'verlat'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['wappdriver=wappdriver.cli:main'],
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

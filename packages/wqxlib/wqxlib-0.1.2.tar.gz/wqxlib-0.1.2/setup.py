f2e1ddaa9f46c7774f069fd5bf4ed1d0519f95b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package wqxlib/0.1.2/wqxlib-0.1.2.tar.gz

---
+++ packages/wqxlib/wqxlib-0.1.2.tar.gz/wqxlib-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="wqxlib",
+  version="0.1.2",
+  author="Jon Musselwhite",
+  author_email="JMusselwhite@wvstateu.edu",
+  description="A package for interacting with the EPA's WQX service",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/FlippingBinary/wqxlib/tree/main/wqxlib-python",
+  packages=[],
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Hydrology",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+  python_requires='>=3.6',
+)

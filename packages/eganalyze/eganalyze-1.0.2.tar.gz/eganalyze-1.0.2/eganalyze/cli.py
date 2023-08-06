4e89429aae6369d6c8272c5183ec7471b877d9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:50 +0000

    Add package eganalyze/1.0.2/eganalyze-1.0.2.tar.gz

---
+++ packages/eganalyze/eganalyze-1.0.2.tar.gz/eganalyze-1.0.2/eganalyze/cli.py
@@ -0,0 +1,29 @@
+import click
+import pandas as pd
+from eganalyze import __version__
+from eganalyze.lib import EgData
+
+
+@click.group()
+@click.version_option(
+    __version__, '--version', message="%(prog)s, version %(version)s"
+)
+def main():
+    pass
+
+
+@main.command()
+@click.argument('input', type=click.Path(exists=True))
+def analyze(input):
+
+    data = EgData(pd.read_csv(input))
+
+    click.echo(
+        'Mean interest rate: {0:.4f}%'.format(data.mean_interest_rate)
+    )
+    click.echo(
+        'Outstanding mean interest rate: {0:.4f}%'.format(data.outstanding_mean_interest_rate)
+    )
+    click.echo(
+        'Outstanding weighted mean interest rate: {0:.4f}%'.format(data.outstanding_weighted_mean_interest_rate)
+    )

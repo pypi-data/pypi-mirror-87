dc6bda4dadff7dadd252967bbffebc224e658192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:39 +0000

    Add package pytest-sanity/0.0.5/pytest_sanity-0.0.5.tar.gz

---
+++ packages/pytest-sanity/pytest_sanity-0.0.5.tar.gz/pytest_sanity-0.0.5/pytest_sanity/plugin.py
@@ -0,0 +1,23 @@
+from .item_selector import ItemSelector
+
+
+OPT_NAME = '--sanity-group-size'
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('sanity')
+    group.addoption(
+        OPT_NAME,
+        type=int,
+        help='number of test to run before ending the session successfuly',
+        default=None,
+    )
+
+
+def pytest_collection_modifyitems(session, config, items):
+    number_of_test_to_collect = config.getoption(OPT_NAME)
+    sanity_items = ItemSelector(config, items, number_of_test_to_collect).get()
+
+    if sanity_items:
+        del items[:]
+        items.extend(sanity_items)
---
+++ packages/pytest-sanity/pytest_sanity-0.0.5.tar.gz/pytest_sanity-0.0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='pytest_sanity',
+    packages=['pytest_sanity'],
+    entry_points={'pytest11': ['hailo_sanity=pytest_sanity.plugin']},
+    version='0.0.5',
+)

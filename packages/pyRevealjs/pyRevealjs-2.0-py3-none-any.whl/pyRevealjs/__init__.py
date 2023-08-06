ed49ed9ebb4d33130fd1d1ed36a52b7c11d57b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:45 +0000

    Add package pyRevealjs/2.0/pyRevealjs-2.0-py3-none-any.whl

---
+++ packages/pyRevealjs/pyRevealjs-2.0-py3-none-any.whl/pyRevealjs/__init__.py
@@ -0,0 +1,5 @@
+from pyRevealjs.presentation_settings import PresentationSettings
+from pyRevealjs.presentation import Presentation
+from pyRevealjs.slide import Slide
+from pyRevealjs.slide_generator import SlideGenerator
+from pyRevealjs.slide_catalog import SlideCatalog

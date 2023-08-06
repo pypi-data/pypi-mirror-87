aae0aab7799343896eecef459506000f22026a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:57 +0000

    Add package NlpToolkit-NGram-Cy/1.0.5/NlpToolkit-NGram-Cy-1.0.5.tar.gz

---
+++ packages/NlpToolkit-NGram-Cy/NlpToolkit-NGram-Cy-1.0.5.tar.gz/NlpToolkit-NGram-Cy-1.0.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(["NGram/*.pyx"],
+                          compiler_directives={'language_level': "3"}),
+    name='NlpToolkit-NGram-Cy',
+    version='1.0.5',
+    packages=['NGram'],
+    package_data={'NGram': ['*.pxd', '*.pyx', '*.c', '*.py']},
+    url='https://github.com/olcaytaner/NGram-Cy',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='NGram library',
+    install_requires=['NlpToolkit-DataStructure-Cy', 'NlpToolkit-Sampling-Cy']
+)

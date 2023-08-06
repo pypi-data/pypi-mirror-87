e9c5b1e66ab12af3812d7491a8f39acff60be454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package affiliatelinkconverter/1.0.2/affiliatelinkconverter-1.0.2.tar.gz

---
+++ packages/affiliatelinkconverter/affiliatelinkconverter-1.0.2.tar.gz/affiliatelinkconverter-1.0.2/affiliate_link_converter/__version__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#
+# affiliatelinkconverter - Affiliate link converter made simple.
+#
+# Manoj Sitapara
+# manojsitapara@gmail.com
+#
+# License: Build Amazing Things (Unlicense)
+#
+
+__title__ = 'AffiliateLinkConverter'
+__version__ = '1.0.0'
+__license__ = 'Unlicense'
+__author__ = 'Manoj Sitapara'
+__contact__ = 'manojsitapara@gmail.com'
+__copyright__ = 'Copyright 2021 Manoj Sitapara'
+__description__ = 'Affiliate link converter made simple.'
---
+++ packages/affiliatelinkconverter/affiliatelinkconverter-1.0.2.tar.gz/affiliatelinkconverter-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='affiliatelinkconverter',
+    version='1.0.2',
+    description='affiliate link converter package',
+    url='',
+    author='Manoj Sitapara',
+    author_email='manojsitapara@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='affiliatelinkconverter',
+    packages=find_packages(),
+    install_requires=[
+        'furl>=2.1.0',
+        'unshortenit>=0.4.0'
+
+    ]
+)

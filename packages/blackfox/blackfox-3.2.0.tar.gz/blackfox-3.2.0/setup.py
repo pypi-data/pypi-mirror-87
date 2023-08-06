88636f998c17af18f4d3fb2daf91c36eeececb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:14 +0000

    Add package blackfox/3.2.0/blackfox-3.2.0.tar.gz

---
+++ packages/blackfox/blackfox-3.2.0.tar.gz/blackfox-3.2.0/setup.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+"""
+    BlackFox
+
+    BlackFox client
+
+    OpenAPI spec version: v1
+
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "blackfox"
+VERSION = "3.2.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "blackfox-restapi >= 3.2.0"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="BlackFox client",
+    author="Tomislav Mrdja",
+    author_email="",
+    url="https://github.com/tmrdja/BlackFoxPython",
+    keywords=["BlackFox"],
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test.*", "test"]),
+    include_package_data=True,
+    long_description="""\
+        BlackFox python client
+    """
+)

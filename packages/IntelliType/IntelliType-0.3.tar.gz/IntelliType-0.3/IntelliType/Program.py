f0d6a55e2421d3ad7f47479e5515c8e770805e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:27 +0000

    Add package IntelliType/0.3/IntelliType-0.3.tar.gz

---
+++ packages/IntelliType/IntelliType-0.3.tar.gz/IntelliType-0.3/IntelliType/Program.py
@@ -0,0 +1,22 @@
+import sys
+import subprocess
+import shlex
+
+class Program:
+
+    def __init__(self, command: str):
+        self.command = command
+        self.prepare_command()
+        self.pid = -1
+    def execute(self):
+        is_windows = sys.platform.startswith('win')
+        if is_windows:
+            DETACHED_PROCESS = 0x00000008
+            self.pid = subprocess.Popen(shlex.split(self.command),creationflags=DETACHED_PROCESS)
+        else:
+            self.pid = subprocess.Popen(shlex.split(self.command))
+
+
+
+    def prepare_command(self):
+        return

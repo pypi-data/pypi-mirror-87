756148dafd1794fff174e197b29271efad5de60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package prettyqt/0.141.0/PrettyQt-0.141.0.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.141.0.tar.gz/PrettyQt-0.141.0/prettyqt/custom_widgets/booldicttoolbutton.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from typing import Dict, Optional
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets, gui
+
+
+class BoolDictToolButton(widgets.ToolButton):
+    def __init__(
+        self,
+        title: str,
+        icon: gui.icon.IconType = None,
+        dct: Dict[str, str] = None,
+        parent: Optional[QtWidgets.QWidget] = None,
+    ):
+        super().__init__(parent=parent)
+        self.set_text(title)
+        self.set_icon(icon)
+        self.setMenu(widgets.Menu())
+        self.set_popup_mode("instant")
+        if dct:
+            self.set_dict(dct)
+
+    def __getitem__(self, key: str) -> bool:
+        menu = self.menu()
+        return menu[key].isChecked()
+
+    def __setitem__(self, key: str, value: bool):
+        menu = self.menu()
+        menu[key].setChecked(value)
+
+    def set_dict(self, dct):
+        menu = self.menu()
+        for k, v in dct.items():
+            action = widgets.Action()
+            action.set_text(v)
+            action.setCheckable(True)
+            action.set_id(k)
+            menu.add(action)
+
+    def as_dict(self):
+        return {act.id: act.isChecked() for act in self.menu()}
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    dct = dict(a="test", b="test2")
+    w = BoolDictToolButton("Title", None, dct)
+    w.show()
+    app.main_loop()

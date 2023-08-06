a9176a1899e1a5fee4d34b9c8deab3585e36e4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:15 +0000

    Add package rescape-region/0.1.84/rescape-region-0.1.84.tar.gz

---
+++ packages/rescape-region/rescape-region-0.1.84.tar.gz/rescape-region-0.1.84/rescape_region/middleware/debug_middleware.py
@@ -0,0 +1,14 @@
+from promise import is_thenable
+from rescape_region.schema_models.schema import dump_errors
+
+# This does't work. Never called
+class DebugMiddleware(object):
+    def on_error(self, error):
+        dump_errors(dict(errors=[error]))
+
+    def resolve(self, next, root, info, **args):
+        result = next(root, info, **args)
+        if is_thenable(result):
+            result.catch(self.on_error)
+
+        return result

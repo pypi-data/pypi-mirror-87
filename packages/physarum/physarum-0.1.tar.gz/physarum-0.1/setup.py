7cbb08729b2ed4540ab65b662e01e1413843cda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:56 +0000

    Add package physarum/0.1/physarum-0.1.tar.gz

---
+++ packages/physarum/physarum-0.1.tar.gz/physarum-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='physarum',
+      version='0.1',
+      description='Simulate interacting physarum populations.',
+      url='https://github.com/MNoichl/physarum',
+      author='Maximilian Noichl',
+      author_email='noichlmax@hotmail.co.uk',
+      license='MIT',
+      packages=['physarum'],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+      zip_safe=False)

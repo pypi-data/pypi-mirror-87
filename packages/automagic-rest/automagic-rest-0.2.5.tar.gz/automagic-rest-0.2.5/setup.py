dcee661f0333e18ed18d5aa811f57ab07bc1f384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:27 +0000

    Add package automagic-rest/0.2.5/automagic-rest-0.2.5.tar.gz

---
+++ packages/automagic-rest/automagic-rest-0.2.5.tar.gz/automagic-rest-0.2.5/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="automagic-rest",
+    description="Automagic REST: Django REST Framework PostgreSQL Builder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Timothy Allen",
+    author_email="tallen@wharton.upenn.edu",
+    url="https://github.com/wharton/automagic-rest",
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=[
+        "djangorestframework>=3.9",
+        "djangorestframework-filters==1.0.0.dev2",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)

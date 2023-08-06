205487536e7e714af40ab15c022dc8691a947110
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:19 +0000

    Add package suddenly/0.1.1/suddenly-0.1.1.tar.gz

---
+++ packages/suddenly/suddenly-0.1.1.tar.gz/suddenly-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['suddenly']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['suddenly = suddenly.suddenly:main']}
+
+setup_kwargs = {
+    'name': 'suddenly',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'yassu',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

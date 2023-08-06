91a0c0ededebb243c5736b9556c46fa7ca6badb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:01 +0000

    Add package isbnlib-loc/1.0.1/isbnlib_loc-1.0.1-py2.py3-none-any.whl

---
+++ packages/isbnlib-loc/isbnlib_loc-1.0.1-py2.py3-none-any.whl/isbnlib_loc/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._loc import query
+
+__version__ = '1.0.1'
+
+__plugin_author__ = 'xlcnd'
+__plugin_description__ = 'Metadata for books from Library of Congress'
+__plugin_homepage__ = 'github.com/xlcnd/isbnlib-loc'
+__plugin_service_language__ = 'en'
+__plugin_service_name__ = 'loc'
+__plugin_type__ = 'metadata'

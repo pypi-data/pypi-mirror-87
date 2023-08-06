f68bc34a1dc5e066994461a38d133eb4e274da63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package kangaroo-sockets/0.1/kangaroo-sockets-0.1.tar.gz

---
+++ packages/kangaroo-sockets/kangaroo-sockets-0.1.tar.gz/kangaroo-sockets-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kangaroo-sockets",
+    version="0.1",
+    author="Leonardo Lima",
+    author_email="leonardoaugusto287@gmail.com",
+    description="User-friendly lib for sockets in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leozz37/kangaroo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)

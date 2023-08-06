342092662d90b85f03b194b2a78618425c3a3bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package dataclassy/0.6.2/dataclassy-0.6.2.tar.gz

---
+++ packages/dataclassy/dataclassy-0.6.2.tar.gz/dataclassy-0.6.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='dataclassy',
+    version='0.6.2',
+
+    author='biqqles',
+    author_email='biqqles@protonmail.com',
+    url='https://github.com/biqqles/dataclassy',
+
+    description='A reimplementation of data classes in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    packages=['dataclassy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+    ],
+    python_requires='>=3.6',
+)

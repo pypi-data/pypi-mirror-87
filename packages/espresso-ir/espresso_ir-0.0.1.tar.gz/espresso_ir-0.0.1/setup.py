cbb251b8678302724ec8efb102175eef9a333591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:01 +0000

    Add package espresso-ir/0.0.1/espresso_ir-0.0.1.tar.gz

---
+++ packages/espresso-ir/espresso_ir-0.0.1.tar.gz/espresso_ir-0.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='espresso_ir',
+    version='0.0.1',
+    author='Terrizmo',
+    author_email='terrizmo@tbc.tbc',    
+    description='IR tool for aquiring memory images from windows EC2 instances on AWS',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/terrizmo/espresso_ir",
+    #package_dir={'': 'espresso_ir'},
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points='''
+        [console_scripts]
+        espresso_ir=espresso_ir.cli:cli
+        '''
+)

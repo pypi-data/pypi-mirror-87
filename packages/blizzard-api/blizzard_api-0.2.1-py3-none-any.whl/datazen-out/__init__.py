325db61075d45bc703cc4efbae5831e3cc47ae44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:24 +0000

    Add package blizzard-api/0.2.1/blizzard_api-0.2.1-py3-none-any.whl

---
+++ packages/blizzard-api/blizzard_api-0.2.1-py3-none-any.whl/datazen-out/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+vbinds - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "blizzard-api"
+VERSION = "0.2.0"
+DESCRIPTION = "Interact with Blizard's game-data API."
+
---
+++ packages/blizzard-api/blizzard_api-0.2.1-py3-none-any.whl/vbinds/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+vbinds - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "blizzard-api"
+VERSION = "0.2.1"
+DESCRIPTION = "Interact with Blizard's game-data API."

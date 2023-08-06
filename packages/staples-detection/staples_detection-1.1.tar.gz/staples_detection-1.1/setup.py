4aaa4c7b045f7d7ea56f27e87bbd5985d0e294be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:35 +0000

    Add package staples-detection/1.1/staples_detection-1.1.tar.gz

---
+++ packages/staples-detection/staples_detection-1.1.tar.gz/staples_detection-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='staples_detection',
+    version='1.1',
+    packages=['staples_detection'],
+    url='https://github.com/mmunar97',
+    license='mit',
+    author='marcmunar',
+    author_email='marc.munar@uib.es',
+    description='Algorithm for staple detection and mask generation',
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-image",
+        "opencv-python"
+    ]
+)

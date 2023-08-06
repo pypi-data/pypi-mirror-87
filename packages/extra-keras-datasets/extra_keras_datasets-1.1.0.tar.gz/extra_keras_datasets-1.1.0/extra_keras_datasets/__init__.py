0f8cbd234d6d8455675505ded5c34bfce4d9eabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:10 +0000

    Add package extra-keras-datasets/1.1.0/extra_keras_datasets-1.1.0.tar.gz

---
+++ packages/extra-keras-datasets/extra_keras_datasets-1.1.0.tar.gz/extra_keras_datasets-1.1.0/extra_keras_datasets/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+__version__ = 'v1.1.0'
+
+from . import emnist
+from . import kmnist
+from . import svhn
+from . import stl10
+from . import iris
+from . import wine_quality
+
+__all__ = ['emnist', 'kmnist', 'svhn', 'stl10', 'iris', 'wine_quality']

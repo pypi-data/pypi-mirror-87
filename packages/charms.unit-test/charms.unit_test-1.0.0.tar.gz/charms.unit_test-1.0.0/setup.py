48a9526a74869f9d3442f24b84cee8f45b936e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package charms.unit-test/1.0.0/charms.unit_test-1.0.0.tar.gz

---
+++ packages/charms.unit-test/charms.unit_test-1.0.0.tar.gz/charms.unit_test-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+SETUP = {
+    'name': "charms.unit_test",
+    'version': '1.0.0',
+    'author': "Cory Johns",
+    'author_email': "johnsca@gmail.com",
+    'url': "https://github.com/juju-solutions/charms.unit_test",
+    'packages': [
+        "charms",
+    ],
+    'install_requires': [
+    ],
+    'license': "Apache License 2.0",
+    'long_description_content_type': 'text/markdown',
+    'long_description': open('README.md').read(),
+    'description': 'Helpers for unit-testing reactive-style Juju Charms',
+}
+
+
+if __name__ == '__main__':
+    setup(**SETUP)

0a1f37ac5c55ee0e9cda448fe2b740c81f27ecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package pretix-ldap/0.0.5/pretix_ldap-0.0.5-py3-none-any.whl

---
+++ packages/pretix-ldap/pretix_ldap-0.0.5-py3-none-any.whl/pretix_ldap/__init__.py
@@ -0,0 +1,22 @@
+from .ldap_connector import LDAPAuthBackend # noqa
+from django.utils.translation import ugettext_lazy
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
+
+
+class PluginApp(PluginConfig):
+    name = 'pretix_ldap'
+    verbose_name = 'pretix LDAP'
+
+    class PretixPluginMeta:
+        name = ugettext_lazy('pretix LDAP')
+        author = 'sohalt'
+        description = ugettext_lazy('LDAP authentication backend for pretix')
+        visible = True
+        version = '0.0.5'
+        compatibility = "pretix>=3.3.0"
+
+
+default_app_config = 'pretix_ldap.PluginApp'

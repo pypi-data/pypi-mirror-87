898e85cfb019a2e109d87d4dacd6b38ed6a951e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:55 +0000

    Add package sequana/0.9.5/sequana-0.9.5.tar.gz

---
+++ packages/sequana/sequana-0.9.5.tar.gz/sequana-0.9.5/examples/plot_bam.py
@@ -0,0 +1,22 @@
+"""
+BAM module example
+====================
+
+Plot histogram of MAPQ values contained in a BAM file
+
+"""
+
+#################################################
+# first import the relevant modules
+from sequana import BAM, sequana_data
+
+
+#####################################################
+# Get a data set (BAM file) for testing
+from sequana import BAM, sequana_data
+datatest = sequana_data('test.bam', "testing")
+
+##########################################################################
+# Use :class:`sequana.bamtools.BAM` class to plot the MAPQ historgram 
+b = BAM(datatest)
+b.plot_bar_mapq()

ab449d5da00608b42fc6966832c5e5b0472ab49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:38 +0000

    Add package kokos/1.2.2/kokos-1.2.2.tar.gz

---
+++ packages/kokos/kokos-1.2.2.tar.gz/kokos-1.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kokos',
+    description='KoKoS helper tool',
+    author='Vyacheslav Boben',
+    version='1.2.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests',
+        'configparser',
+        'colorama',
+        'tqdm',
+        'beautifulsoup4',
+    ],
+    entry_points='''
+        [console_scripts]
+        kks=kks.cli:cli
+    ''',
+)

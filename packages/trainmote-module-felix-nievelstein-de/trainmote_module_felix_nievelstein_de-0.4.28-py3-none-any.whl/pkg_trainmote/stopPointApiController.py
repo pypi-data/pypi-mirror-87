81960c3b5cca4363686446884a643e13eef291dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:27 +0000

    Add package trainmote-module-felix-nievelstein-de/0.4.28/trainmote_module_felix_nievelstein_de-0.4.28-py3-none-any.whl

---
+++ packages/trainmote-module-felix-nievelstein-de/trainmote_module_felix_nievelstein_de-0.4.28-py3-none-any.whl/pkg_trainmote/stopPointApiController.py
@@ -0,0 +1,72 @@
+from flask import Blueprint
+from flask import request
+from flask import abort
+from flask import Response
+from . import baseAPI
+from . import gpioservice
+from .validator import Validator
+import json
+from .databaseControllerModule import DatabaseController
+
+
+stopPointApi = Blueprint('stopPointApi', __name__)
+##
+# Endpoint StopPoint
+##
+
+@stopPointApi.route('/trainmote/api/v1/control/stoppoint/<stop_id>', methods=["PATCH"])
+def setStop(stop_id: str):
+    if stop_id is None:
+        abort(400)
+    try:
+        return gpioservice.setStop(stop_id), 200, baseAPI.defaultHeader()
+    except ValueError as e:
+        return json.dumps({"error": str(e)}), 400, baseAPI.defaultHeader()
+
+
+@stopPointApi.route('/trainmote/api/v1/stoppoint/<stop_id>', methods=["PATCH"])
+def updateStop(stop_id: str):
+    mJson = request.get_json()
+    if mJson is not None:
+        abort(501)
+    else:
+        abort(400)
+
+
+@stopPointApi.route('/trainmote/api/v1/stoppoint/<stop_id>', methods=["DELETE"])
+def deleteStop(stop_id: str):
+    if stop_id is None:
+        abort(400)
+    DatabaseController().deleteStopModel(int(stop_id)), 205, baseAPI.defaultHeader()
+    return 'ok'
+
+
+@stopPointApi.route('/trainmote/api/v1/stoppoint', methods=["POST"])
+def addStop():
+    mJson = request.get_json()
+    if mJson is not None:
+        if Validator().validateDict(mJson, "stop_scheme") is False:
+            abort(400)
+
+        config = DatabaseController().getConfig()
+        if config is not None and config.containsPin(mJson["id"]):
+            return json.dumps({"error": "Pin is already in use as power relais"}), 409, baseAPI.defaultHeader()
+
+        try:
+            return gpioservice.createStop(mJson), 201, baseAPI.defaultHeader()
+        except ValueError as e:
+            return json.dumps({"error": str(e)}), 400, baseAPI.defaultHeader()
+    else:
+        abort(400)
+
+
+@stopPointApi.route('/trainmote/api/v1/stoppoint/all', methods=["GET"])
+def getAllStops():
+    return Response(gpioservice.getAllStopPoints(), mimetype="application/json"), 200, baseAPI.defaultHeader()
+
+
+@stopPointApi.route('/trainmote/api/v1/stoppoint/<stop_id>', methods=["GET"])
+def stop(stop_id: str):
+    if stop_id is None:
+        abort(400)
+    return gpioservice.getStop(stop_id), 200, baseAPI.defaultHeader()

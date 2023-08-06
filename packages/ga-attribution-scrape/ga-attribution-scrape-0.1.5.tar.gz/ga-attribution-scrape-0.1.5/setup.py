1fbf2c31de1686d8246ca5ecfa6dc8667fa6c2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:19 +0000

    Add package ga-attribution-scrape/0.1.5/ga-attribution-scrape-0.1.5.tar.gz

---
+++ packages/ga-attribution-scrape/ga-attribution-scrape-0.1.5.tar.gz/ga-attribution-scrape-0.1.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ga-attribution-scrape",
+    version="0.1.5",
+    author="Lewis Bryan",
+    author_email="lewis.a.bryan@googlemail.com",
+    description="Scrapes attribution data from GAs Model Comparison Tool through JS Network and sends to Bigquery.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lewisaustinbryan/ga-attribution-scrape",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'google-auth',
+        'google-api-python-client',
+        'google-cloud-bigquery',
+        'oauth2client',
+        'pandas',
+        'tqdm'
+      ],
+    python_requires='>=3.6',
+)

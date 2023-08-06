6b672c900ba290b543aabe06076f8460adc2fa44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:00 +0000

    Add package opensoundscape/0.4.6/opensoundscape-0.4.6-py3-none-any.whl

---
+++ packages/opensoundscape/opensoundscape-0.4.6-py3-none-any.whl/opensoundscape/audio.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python3
+""" audio.py: Utilities for dealing with audio files
+"""
+
+import librosa
+import soundfile
+import numpy as np
+import pandas as pd
+import warnings
+from math import ceil
+
+
+class OpsoLoadAudioInputError(Exception):
+    """ Custom exception indicating we can't load input
+    """
+
+    pass
+
+
+class OpsoLoadAudioInputTooLong(Exception):
+    """ Custom exception indicating length of audio is too long
+    """
+
+    pass
+
+
+class Audio:
+    """ Container for audio samples
+
+    Initializing an `Audio` object directly requires the specification of the
+    sample rate. Use `Audio.from_file` or `Audio.from_bytesio` with
+    `sample_rate=None` to use a native sampling rate.
+
+    Arguments:
+        samples (np.array):     The audio samples
+        sample_rate (integer):  The sampling rate for the audio samples
+        resample_type (str):    The resampling method to use [default: "kaiser_fast"]
+        max_duration (None or integer): The maximum duration allowed for the audio file [default: None]
+
+    Returns:
+        An initialized `Audio` object
+    """
+
+    __slots__ = ("samples", "sample_rate", "resample_type", "max_duration")
+
+    def __init__(
+        self, samples, sample_rate, resample_type="kaiser_fast", max_duration=None
+    ):
+        # Do not move these lines; it will break Pytorch training
+        self.samples = samples
+        self.sample_rate = sample_rate
+        self.resample_type = resample_type
+        self.max_duration = max_duration
+
+        samples_error = None
+        if not isinstance(self.samples, np.ndarray):
+            samples_error = (
+                "Initializing an Audio object requires samples to be a numpy array"
+            )
+
+        try:
+            self.sample_rate = int(self.sample_rate)
+        except ValueError:
+            sample_rate_error = (
+                "Initializing an Audio object requires the audio samples' sampling rate"
+            )
+            if samples_error:
+                raise ValueError(
+                    f"Audio initialization failed with:\n{samples_error}\n{sample_rate_error}"
+                )
+            raise ValueError(f"Audio initialization failed with:\n{sample_rate_error}")
+
+        if samples_error:
+            raise ValueError(f"Audio initialization failed with:\n{samples_error}")
+
+    @classmethod
+    def from_file(
+        cls, path, sample_rate=None, resample_type="kaiser_fast", max_duration=None
+    ):
+        """ Load audio from files
+
+        Deal with the various possible input types to load an audio
+        file and generate a spectrogram
+
+        Args:
+            path (str, Path): path to an audio file
+            sample_rate (int, None): resample audio with value and resample_type,
+                if None use source sample_rate (default: None)
+            resample_type: method used to resample_type (default: kaiser_fast)
+            max_duration: the maximum length of an input file,
+                None is no maximum (default: None)
+
+        Returns:
+            Audio: attributes samples and sample_rate
+        """
+
+        if max_duration:
+            if librosa.get_duration(filename=path) > max_duration:
+                raise OpsoLoadAudioInputTooLong()
+
+        warnings.filterwarnings("ignore")
+        samples, sr = librosa.load(
+            path, sr=sample_rate, res_type=resample_type, mono=True
+        )
+        warnings.resetwarnings()
+
+        return cls(samples, sr, resample_type=resample_type, max_duration=max_duration)
+
+    @classmethod
+    def from_bytesio(
+        cls, bytesio, sample_rate=None, max_duration=None, resample_type="kaiser_fast"
+    ):
+        """ Read from bytesio object
+
+        Read an Audio object from a BytesIO object. This is primarily used for
+        passing Audio over HTTP.
+
+        TODO:
+            Describe how to initialize an Audio file as a BytesIO object
+
+        Arguments:
+            bytesio: Contents of WAV file as BytesIO
+            sample_rate: The final sampling rate of Audio object [default: None]
+            max_duration: The maximum duration of the audio file [default: None]
+            resample_type: The librosa method to do resampling [default: "kaiser_fast"]
+
+        Returns:
+            An initialized Audio object
+        """
+        samples, sr = soundfile.read(bytesio)
+        if sample_rate:
+            samples = librosa.resample(samples, sr, sample_rate, res_type=resample_type)
+            sr = sample_rate
+
+        return cls(samples, sr, resample_type=resample_type, max_duration=max_duration)
+
+    def __repr__(self):
+        return f"<Audio(samples={self.samples.shape}, sample_rate={self.sample_rate})>"
+
+    def trim(self, start_time, end_time):
+        """ trim Audio object in time
+
+        Args:
+            start_time: time in seconds for start of extracted clip
+            end_time: time in seconds for end of extracted clip
+        Returns:
+            a new Audio object containing samples from start_time to end_time
+        """
+        start_sample = self.time_to_sample(start_time)
+        end_sample = self.time_to_sample(end_time)
+        samples_trimmed = self.samples[start_sample:end_sample]
+        return Audio(
+            samples_trimmed,
+            self.sample_rate,
+            resample_type=self.resample_type,
+            max_duration=self.max_duration,
+        )
+
+    def extend(self, length):
+        """ Extend audio file by looping it
+
+        Args:
+            length: the final length in seconds of the extended file
+        Returns:
+            a new Audio object of the desired length
+        """
+
+        total_samples_needed = round(length * self.sample_rate)
+        samples_extended = np.resize(self.samples, total_samples_needed)
+        return Audio(
+            samples_extended,
+            self.sample_rate,
+            resample_type=self.resample_type,
+            max_duration=self.max_duration,
+        )
+
+    def time_to_sample(self, time):
+        """ Given a time, convert it to the corresponding sample
+
+        Args:
+            time: The time to multiply with the sample_rate
+        Returns:
+            sample: The rounded sample
+        """
+        return int(time * self.sample_rate)
+
+    def bandpass(self, low_f, high_f, order):
+        """ bandpass audio signal frequencies
+
+        uses a phase-preserving algorithm (scipy.signal's butter and solfiltfilt)
+
+        Args:
+            low_f: low frequency cutoff (-3 dB)  in Hz of bandpass filter
+            high_f: high frequency cutoff (-3 dB)  in Hz of bandpass filter
+            order: butterworth filter order (integer) ~= steepness of cutoff
+
+        """
+        from opensoundscape.audio_tools import bandpass_filter
+
+        if low_f <= 0:
+            raise ValueError("low_f must be greater than zero")
+
+        if high_f >= self.sample_rate / 2:
+            raise ValueError("high_f must be less than sample_rate/2")
+
+        filtered_samples = bandpass_filter(
+            self.samples, low_f, high_f, self.sample_rate, order=order
+        )
+        return Audio(
+            filtered_samples,
+            self.sample_rate,
+            resample_type=self.resample_type,
+            max_duration=self.max_duration,
+        )
+
+    # can act on an audio file and be moved into Audio class
+    def spectrum(self):
+        """create frequency spectrum from an Audio object using fft
+
+        Args:
+            self
+
+        Returns:
+            fft, frequencies
+        """
+        from scipy.fftpack import fft
+        from scipy.fft import fftfreq
+
+        # Compute the fft (fast fourier transform) of the selected clip
+        N = len(self.samples)
+        T = 1 / self.sample_rate
+        fft = fft(self.samples)
+        freq = fftfreq(N, d=T)  # the frequencies corresponding to fft bins
+
+        # remove negative frequencies and scale magnitude by 2.0/N:
+        fft = 2.0 / N * fft[0 : int(N / 2)]
+        frequencies = freq[0 : int(N / 2)]
+        fft = np.abs(fft)
+
+        return fft, frequencies
+
+    def save(self, path):
+        """save Audio to file
+
+        Args:
+            path: destination for output
+        """
+        from soundfile import write
+
+        write(path, self.samples, self.sample_rate)
+
+    def duration(self):
+        """ Return duration of Audio
+
+        Output:
+            duration (float): The duration of the Audio
+        """
+
+        return len(self.samples) / self.sample_rate
+
+    def split(self, clip_duration, clip_overlap=0, final_clip=None):
+        """ Split Audio into clips
+
+        The Audio object is split into clips of a specified duration and overlap
+
+        Arguments:
+            clip_duration:  The duration in seconds of the clips
+            clip_overlap:   The overlap of the clips in seconds [default: 0]
+            final_clip:     Possible options (any other input will ignore the final clip entirely),
+                                - "remainder":          Include the remainder of the Audio
+                                                            (clip will not have clip_duration length)
+                                - "full":               Increase the overlap to yield a clip with clip_duration
+                                - "extend":             Similar to remainder but extend the clip to clip_duration
+        Results:
+            A list of dictionaries with keys: ["audio", "begin_time", "end_time"]
+        """
+
+        duration = self.duration()
+        if clip_duration > duration:
+            if final_clip == "remainder":
+                return_clip = Audio(
+                    self.samples,
+                    self.sample_rate,
+                    resample_type=self.resample_type,
+                    max_duration=self.max_duration,
+                )
+                return [
+                    {
+                        "clip": return_clip,
+                        "clip_duration": return_clip.duration(),
+                        "begin_time": 0,
+                        "end_time": duration,
+                    }
+                ]
+            elif final_clip in ["full", "extend"]:
+                return_clip = self.extend(clip_duration)
+                return [
+                    {
+                        "clip": return_clip,
+                        "clip_duration": return_clip.duration(),
+                        "begin_time": 0,
+                        "end_time": duration,
+                    }
+                ]
+            else:
+                warnings.warn(
+                    f"Given Audio object with duration of `{duration}` seconds and `clip_duration={clip_duration}` but `final_clip={final_clip}` produces no clips. Returning empty list."
+                )
+                return []
+
+        num_clips = ceil((duration - clip_overlap) / (clip_duration - clip_overlap))
+        to_return = [None] * num_clips
+        for idx in range(num_clips):
+            if idx == num_clips - 1:
+                if final_clip in ["remainder", "extend"]:
+                    begin_time = clip_duration * idx - clip_overlap * idx
+                    end_time = duration
+                elif final_clip == "full":
+                    begin_time = int(duration - clip_duration)
+                    end_time = duration
+                else:
+                    begin_time = clip_duration * idx - clip_overlap * idx
+                    end_time = begin_time + clip_duration
+                    if end_time > duration:
+                        return to_return[:-1]
+            else:
+                begin_time = clip_duration * idx - clip_overlap * idx
+                end_time = begin_time + clip_duration
+
+            audio_clip = self.trim(begin_time, end_time)
+            if final_clip == "extend":
+                audio_clip = audio_clip.extend(clip_duration)
+            to_return[idx] = {
+                "clip": audio_clip,
+                "clip_duration": audio_clip.duration(),
+                "begin_time": begin_time,
+                "end_time": end_time,
+            }
+
+        return to_return
+
+
+def split_and_save(
+    audio,
+    destination,
+    prefix,
+    clip_duration,
+    clip_overlap=0,
+    final_clip=None,
+    dry_run=False,
+):
+    """ Split audio into clips and save them to a folder
+
+    Arguments:
+        audio:          The input Audio to split
+        destination:    A folder to write clips to
+        prefix:         A name to prepend to the written clips
+        clip_duration:  The duration of each clip in seconds
+        clip_overlap:   The overlap of each clip in seconds [default: 0]
+        final_clip:     Possible options (any other input will ignore the final clip entirely) [default: None]
+                            - "remainder":          Include the remainder of the Audio
+                                                        (clip will not have clip_duration length)
+                            - "full":               Increase the overlap to yield a clip with clip_duration
+                            - "extend":             Similar to remainder but extend the clip to clip_duration
+        dry_run:        If True, skip writing audio and just return clip DataFrame [default: False]
+    
+    Returns:
+        pandas.DataFrame containing begin and end times for each clip from the source audio
+    """
+
+    clips = audio.split(
+        clip_duration=clip_duration, clip_overlap=clip_overlap, final_clip=final_clip
+    )
+    for clip in clips:
+        clip_name = (
+            f"{destination}/{prefix}_{clip['begin_time']}s_{clip['end_time']}s.wav"
+        )
+        if not dry_run:
+            clip["clip"].save(clip_name)
+
+    # Convert [{k: v}] -> {k: [v]}
+    return pd.DataFrame(
+        {key: [clip[key] for clip in clips] for key in clips[0].keys() if key != "clip"}
+    )
---
+++ packages/opensoundscape/opensoundscape-0.4.6-py3-none-any.whl/opensoundscape/spectrogram.py
@@ -0,0 +1,370 @@
+#!/usr/bin/env python3
+""" spectrogram.py: Utilities for dealing with spectrograms
+"""
+
+from scipy import signal
+import numpy as np
+from opensoundscape.audio import Audio
+from opensoundscape.helpers import min_max_scale, linear_scale
+import warnings
+
+
+class Spectrogram:
+    """ Immutable spectrogram container
+    """
+
+    __slots__ = ("frequencies", "times", "spectrogram", "decibel_limits")
+
+    def __init__(self, spectrogram, frequencies, times):
+        if not isinstance(spectrogram, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.spectrogram should be a np.ndarray [shape=(n, m)]. Got {spectrogram.__class__}"
+            )
+        if not isinstance(frequencies, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.frequencies should be an np.ndarray [shape=(n,)]. Got {frequencies.__class__}"
+            )
+        if not isinstance(times, np.ndarray):
+            raise TypeError(
+                f"Spectrogram.times should be an np.ndarray [shape=(m,)]. Got {times.__class__}"
+            )
+
+        if spectrogram.ndim != 2:
+            raise TypeError(
+                f"spectrogram should be a np.ndarray [shape=(n, m)]. Got {spectrogram.shape}"
+            )
+        if frequencies.ndim != 1:
+            raise TypeError(
+                f"frequencies should be an np.ndarray [shape=(n,)]. Got {frequencies.shape}"
+            )
+        if times.ndim != 1:
+            raise TypeError(
+                f"times should be an np.ndarray [shape=(m,)]. Got {times.shape}"
+            )
+
+        if spectrogram.shape != (frequencies.shape[0], times.shape[0]):
+            raise TypeError(
+                f"Dimension mismatch, spectrogram.shape: {spectrogram.shape}, frequencies.shape: {frequencies.shape}, times.shape: {times.shape}"
+            )
+
+        super(Spectrogram, self).__setattr__("frequencies", frequencies)
+        super(Spectrogram, self).__setattr__("times", times)
+        super(Spectrogram, self).__setattr__("spectrogram", spectrogram)
+        super(Spectrogram, self).__setattr__("decibel_limits", (-100, -20))
+
+    @classmethod
+    def from_audio(
+        cls,
+        audio,
+        window_type="hann",
+        window_samples=512,
+        overlap_samples=256,
+        decibel_limits=(-100, -20),
+    ):
+        """
+        create a Spectrogram object from an Audio object
+
+        Args:
+            window_type="hann": see scipy.signal.spectrogram docs for description of window parameter
+            window_samples=512: number of audio samples per spectrogram window (pixel)
+            overlap_samples=256: number of samples shared by consecutive windows
+            decibel_limits = (-100,-20) : limit the dB values to (min,max) (lower values set to min, higher values set to max)
+
+        Returns:
+            opensoundscape.spectrogram.Spectrogram object
+        """
+        if not isinstance(audio, Audio):
+            raise TypeError("Class method expects Audio class as input")
+
+        frequencies, times, spectrogram = signal.spectrogram(
+            audio.samples,
+            audio.sample_rate,
+            window=window_type,
+            nperseg=window_samples,
+            noverlap=overlap_samples,
+            scaling="spectrum",
+        )
+
+        # convert to decibels
+        # -> avoid RuntimeWarning by setting negative values to -np.inf (mapped to min_db later)
+        spectrogram = 10 * np.log10(
+            spectrogram, where=spectrogram > 0, out=np.full(spectrogram.shape, -np.inf)
+        )
+
+        # limit the decibel range (-100 to -20 dB by default)
+        # values below lower limit set to lower limit, values above upper limit set to uper limit
+        min_db, max_db = decibel_limits
+        spectrogram[spectrogram > max_db] = max_db
+        spectrogram[spectrogram < min_db] = min_db
+
+        new_obj = cls(spectrogram, frequencies, times)
+        super(Spectrogram, new_obj).__setattr__("decibel_limits", decibel_limits)
+        return new_obj
+
+    @classmethod
+    def from_file(file,):
+        """
+        create a Spectrogram object from a file
+
+        Args:
+            file: path of image to load
+
+        Returns:
+            opensoundscape.spectrogram.Spectrogram object
+        """
+
+        raise NotImplementedError(
+            "Loading Spectrograms from images is not implemented yet"
+        )
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Spectrogram's cannot be modified")
+
+    def __repr__(self):
+        return f"<Spectrogram(spectrogram={self.spectrogram.shape}, frequencies={self.frequencies.shape}, times={self.times.shape})>"
+
+    def min_max_scale(self, feature_range=(0, 1)):
+        """
+
+        Linearly rescale spectrogram values to a range of values using
+        in_range as minimum and maximum
+
+        Args:
+            feature_range: tuple of (low,high) values for output
+
+        Returns:
+            Spectrogram object with values rescaled to feature_range
+        """
+
+        if len(feature_range) != 2:
+            raise AttributeError(
+                "Error: `feature_range` doesn't look like a 2-element tuple?"
+            )
+        if feature_range[1] < feature_range[0]:
+            raise AttributeError("Error: `feature_range` isn't increasing?")
+
+        return Spectrogram(
+            min_max_scale(self.spectrogram, feature_range=feature_range),
+            self.frequencies,
+            self.times,
+        )
+
+    def linear_scale(self, feature_range=(0, 1)):
+        """
+
+        Linearly rescale spectrogram values to a range of values
+        using in_range as decibel_limits
+
+        Args:
+            feature_range: tuple of (low,high) values for output
+
+        Returns:
+            Spectrogram object with values rescaled to feature_range
+        """
+
+        if len(feature_range) != 2:
+            raise AttributeError(
+                "Error: `feature_range` doesn't look like a 2-element tuple?"
+            )
+        if feature_range[1] < feature_range[0]:
+            raise AttributeError("Error: `feature_range` isn't increasing?")
+
+        return Spectrogram(
+            linear_scale(
+                self.spectrogram, in_range=self.decibel_limits, out_range=feature_range
+            ),
+            self.frequencies,
+            self.times,
+        )
+
+    def limit_db_range(self, min_db=-100, max_db=-20):
+        """ Limit the decibel values of the spectrogram to range from min_db to max_db
+
+            values less than min_db are set to min_db
+            values greater than max_db are set to max_db
+
+            similar to Audacity's gain and range parameters
+
+            Args:
+                min_db: values lower than this are set to this
+                max_db: values higher than this are set to this
+            Returns:
+                Spectrogram object with db range applied
+        """
+        _spec = self.spectrogram
+
+        _spec[_spec > max_db] = max_db
+        _spec[_spec < min_db] = min_db
+
+        return Spectrogram(_spec, self.frequencies, self.times)
+
+    def bandpass(self, min_f, max_f):
+        """ extract a frequency band from a spectrogram
+
+        crops the 2-d array of the spectrograms to the desired frequency range
+
+        Args:
+            min_f: low frequency in Hz for bandpass
+            high_f: high frequency in Hz for bandpass
+
+        Returns:
+            bandpassed spectrogram object
+
+        """
+
+        # find indices of the frequencies in spec_freq closest to min_f and max_f
+        lowest_index = np.abs(self.frequencies - min_f).argmin()
+        highest_index = np.abs(self.frequencies - max_f).argmin()
+
+        # take slices of the spectrogram and spec_freq that fall within desired range
+        return Spectrogram(
+            self.spectrogram[lowest_index : highest_index + 1, :],
+            self.frequencies[lowest_index : highest_index + 1],
+            self.times,
+        )
+
+    def trim(self, start_time, end_time):
+        """ extract a time segment from a spectrogram
+
+        Args:
+            start_time: in seconds
+            end_time: in seconds
+
+        Returns:
+            spectrogram object from extracted time segment
+
+        """
+
+        # find indices of the times in self.times closest to min_t and max_t
+        lowest_index = np.abs(self.times - start_time).argmin()
+        highest_index = np.abs(self.times - end_time).argmin()
+
+        # take slices of the spectrogram and spec_freq that fall within desired range
+        return Spectrogram(
+            self.spectrogram[:, lowest_index : highest_index + 1],
+            self.frequencies,
+            self.times[lowest_index : highest_index + 1],
+        )
+
+    def plot(self, inline=True, fname=None, show_colorbar=False):
+        """Plot the spectrogram with matplotlib.pyplot
+
+        Args:
+            inline=True:
+            fname=None: specify a string path to save the plot to (ending in .png/.pdf)
+            show_colorbar: include image legend colorbar from pyplot
+        """
+        from matplotlib import pyplot as plt
+
+        plt.pcolormesh(
+            self.times, self.frequencies, self.spectrogram, shading="auto", cmap="Greys"
+        )
+        plt.xlabel("time (sec)")
+        plt.ylabel("frequency (Hz)")
+        if show_colorbar:
+            plt.colorbar()
+
+        # if fname is not None, save to file path fname
+        if fname:
+            plt.savefig(fname)
+
+        # if not saving to file, check if a matplotlib backend is available
+        if inline:
+            import os
+
+            if os.environ.get("MPLBACKEND") is None:
+                warnings.warn("MPLBACKEND is 'None' in os.environ. Skipping plot.")
+            else:
+                plt.show()
+
+    def amplitude(self, freq_range=None):
+        """create an amplitude vs time signal from spectrogram
+
+        by summing pixels in the vertical dimension
+
+        Args
+            freq_range=None: sum Spectrogrm only in this range of [low, high] frequencies in Hz
+            (if None, all frequencies are summed)
+
+        Returns:
+            a time-series array of the vertical sum of spectrogram value
+
+        """
+        if freq_range is None:
+            return np.sum(self.spectrogram, 0)
+        else:
+            return np.sum(self.bandpass(freq_range[0], freq_range[1]).spectrogram, 0)
+
+    def net_amplitude(
+        self, signal_band, reject_bands=None
+    ):  # used to be called "net_power_signal" which is misleading (not power)
+        """create amplitude signal in signal_band and subtract amplitude from reject_bands
+
+        rescale the signal and reject bands by dividing by their bandwidths in Hz
+        (amplitude of each reject_band is divided by the total bandwidth of all reject_bands.
+        amplitude of signal_band is divided by badwidth of signal_band. )
+
+        Args:
+            signal_band: [low,high] frequency range in Hz (positive contribution)
+            reject band: list of [low,high] frequency ranges in Hz (negative contribution)
+
+        return: time-series array of net amplitude """
+
+        # find the amplitude signal for the desired frequency band
+        signal_band_amplitude = self.amplitude(signal_band)
+
+        signal_band_bandwidth = signal_band[1] - signal_band[0]
+
+        # rescale amplitude by 1 / size of frequency band in Hz ("amplitude per unit Hz" ~= color on a spectrogram)
+        net_amplitude = signal_band_amplitude / signal_band_bandwidth
+
+        # then subtract the energy in the the reject_bands from the signal_band_amplitude to get net_amplitude
+        if not (reject_bands is None):
+            # we sum up the sizes of the rejection bands (to not overweight signal_band)
+            reject_bands = np.array(reject_bands)
+            reject_bands_total_bandwidth = sum(reject_bands[:, 1] - reject_bands[:, 0])
+
+            # subtract reject_band_amplitude
+            for reject_band in reject_bands:
+                reject_band_amplitude = self.amplitude(reject_band)
+                net_amplitude = net_amplitude - (
+                    reject_band_amplitude / reject_bands_total_bandwidth
+                )
+
+            # negative signal shouldn't be kept, because it means reject was stronger than signal. Zero it:
+            net_amplitude = [max(0, s) for s in net_amplitude]
+
+        return net_amplitude
+
+    #     def save(self,destination):
+    #         with open(destination,'wb') as file:
+    #             pickle.dump(self,file)
+
+    def to_image(self, shape=None, mode="RGB", spec_range=[-100, -20]):
+        """
+        create a Pillow Image from spectrogram
+        linearly rescales values from db_range (default [-100, -20]) to [255,0]
+        (ie, -20 db is loudest -> black, -100 db is quietest -> white)
+
+        Args:
+            destination: a file path (string)
+            shape=None: tuple of image dimensions, eg (224,224)
+            mode="RGB": RGB for 3-channel color or "L" for 1-channel grayscale
+            spec_range=[-100,-20]: the lowest and highest possible values in the spectrogram
+
+        Returns:
+            Pillow Image object
+        """
+        from PIL import Image
+
+        # rescale spec_range to [255, 0]
+        array = linear_scale(self.spectrogram, in_range=spec_range, out_range=(255, 0))
+
+        # create and save pillow Image
+        # we pass the array upside-down to create right-side-up image
+        image = Image.fromarray(array[::-1, :])
+        image = image.convert(mode)
+        if shape is not None:
+            image = image.resize(shape)
+
+        return image

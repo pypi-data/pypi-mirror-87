d53f97dc1f2f5e2c1612baa47f6d0790e015ccb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:44 +0000

    Add package stnm/0.0.5/stnm-0.0.5.tar.gz

---
+++ packages/stnm/stnm-0.0.5.tar.gz/stnm-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import sys
+
+from setuptools import setup
+
+assert sys.version_info[0] == 3 and sys.version_info[1] >= 5, "Requires Python 3.5 or newer"
+
+setup(
+    name="stnm",
+    version="0.0.5",
+    description="Stacks blockchain node process manager",
+    long_description="Stacks blockchain node process manager. See https://github.com/talhasch/stnm for mode details.",
+    py_modules=["stnm"],
+    install_requires=[
+        "psutil==5.7.3",
+        "toml==0.10.2",
+        "tornado==6.1",
+        "Flask==1.1.2"
+    ],
+    entry_points={
+        "console_scripts": [
+            "stnm=stnm.run:main"
+        ]
+    })
---
+++ packages/stnm/stnm-0.0.5.tar.gz/stnm-0.0.5/stnm/web/app.py
@@ -0,0 +1,55 @@
+import json
+import subprocess
+from typing import Dict
+
+from flask import Flask, jsonify, request, abort
+
+from stnm.helper.config import get_config_parsed
+
+app = Flask(__name__)
+
+
+def communicate(cmd: str, arg: str = "") -> Dict:
+    process = subprocess.Popen(["stnm", cmd, arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    resp = process.communicate()[0].decode("utf-8")
+    return json.loads(resp)
+
+
+@app.route("/api", methods=["GET"])
+def api_index():
+    return jsonify({"hello": "world"})
+
+
+@app.route("/api/status", methods=["GET"])
+def api_status():
+    return jsonify(communicate("status"))
+
+
+@app.route("/api/start", methods=["POST"])
+def api_start():
+    return jsonify(communicate("start"))
+
+
+@app.route("/api/stop", methods=["POST"])
+def api_stop():
+    return jsonify(communicate("stop"))
+
+
+@app.route("/api/config", methods=["GET"])
+def api_config_get():
+    return jsonify(get_config_parsed())
+
+
+@app.route("/api/config", methods=["POST"])
+def api_config_post():
+    data = request.json
+
+    if type(data) == dict and "input" in data and type(data["input"]) == str and len(data["input"]) > 0:
+        return jsonify(communicate("config", data["input"]))
+
+    abort(400)
+
+
+@app.route("/ui", methods=["GET"])
+def ui_index():
+    return "coming soon..."

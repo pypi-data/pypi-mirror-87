773a1712b00d63eea4189277d71300fa740b0d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:15 +0000

    Add package shp2postgis/0.2.7/shp2postgis-0.2.7-py3-none-any.whl

---
+++ packages/shp2postgis/shp2postgis-0.2.7-py3-none-any.whl/shp2postgis/Shp2Postgis.py
@@ -0,0 +1,61 @@
+## Módulo instalado
+from shp2postgis.Data2Sql import Data2Sql
+from shp2postgis.Util import *
+
+## Local no projeto
+# from Data2Sql import Data2Sql
+# from Util import *
+
+class Shp2Postgis:
+    """
+    # dictInput: dictionary as {"layerName": "path/shapeFileName", ....}
+    """
+    def __init__(self, dictInput, outputPath="./", schema="public", encoding="latin1", verbose=False, log=False, columnsToLower=False):
+        self.dictInput = dictInput
+        if outputPath == None:
+            self.outputPath = "./"
+        else:
+            if outputPath[-1] != '/':
+                outputPath = outputPath + '/'
+            self.outputPath = outputPath
+        if schema == None:
+            self.schema = "public"
+        else:
+            self.schema = schema
+        if encoding == None:
+            self.encoding = "latin1"
+        else:
+            self.encoding = encoding
+        if verbose == None:
+            self.verbose = False
+        else:
+            self.verbose = verbose
+        if log == None:
+            self.log = False
+        else:
+            self.log = log
+        if columnsToLower == None:
+            self.columnsToLower = False
+        else:
+            self.columnsToLower = columnsToLower
+
+    def run(self):
+        if type(self.dictInput) != dict:
+            print('dicionário de input inválido')
+            exit(1)
+        for layerName in self.dictInput:
+            shapeFileName = self.dictInput[layerName]
+            if self.verbose:
+                print("convertendo arquivo {file} para {layer}.sql".format(file=shapeFileName, layer=layerName))
+            try:
+                f = open(shapeFileName + ".shp","r")
+                f.close()
+            except Exception as e:
+                print(shapeFileName + ".shp não existe")
+                if self.verbose:
+                    print(e)
+                continue
+            shapeReader = Data2Sql(tableName=layerName, schema=self.schema, encoding=self.encoding, file=shapeFileName + ".shp")
+            shapeReader.writeSqlFile(fileName=self.outputPath + layerName, columnsToLower=self.columnsToLower)
+        if self.verbose:
+            print("processo terminado!")
---
+++ packages/shp2postgis/shp2postgis-0.2.7-py3-none-any.whl/shp2postgis/Util.py
@@ -0,0 +1,71 @@
+import datetime
+
+def getVersion():
+    return "0.2.7"
+
+"""
+# *args: multiple arguments, writable things or lists of writable things
+"""
+def listWriter(*args, fileName="file", fileExtension="txt", separator="-", commentChar="--", mode='wt'):
+    separationString = ""
+    if separator != None and separator != "":
+        separationString = "\n" + commentChar + " "
+        for value in range(10):
+            separationString += separator
+    f = open(fileName + "." + fileExtension, mode)
+    index = 0
+    for value in args:
+        if type(value) is list:
+            for item in value:
+                f.write(str(item))
+                f.write(str(separationString))
+        else:
+            f.write(value)
+            f.write(separationString)
+        index += 1
+    f.close()
+
+def readListFile(fileName, commentChar):
+    try:
+        f = open(fileName, "r")
+    except:
+        return False
+    lines = []
+    for x in f:
+        txt = ""
+        try:
+            if x.index(commentChar) != 0:
+                txt = x.rstrip("\n")
+                lines.append(txt)
+        except Exception as e:
+            txt = x.rstrip("\n")
+            lines.append(txt)
+    f.close()
+    return lines
+
+def readDictFile(fileName, commentChar="#", separationChar="="):
+    try:
+        f = open(fileName, "r")
+    except:
+        return False
+    lines = {}
+    for x in f:
+        txt = None
+        try:
+            if x.index(commentChar) != 0:
+                txt = x.rstrip("\n")
+        except Exception as e:
+            txt = x.rstrip("\n")
+        if txt != None:
+            sep = txt.split(separationChar, 1)
+            if len(sep) > 1:
+                lines[sep[0]] = sep[1]
+            else:
+                lines[sep[0]] = sep[0]
+    f.close()
+    return lines
+
+def log(string, path):
+    f = open(path, 'at')
+    f.write("(" + str(datetime.datetime.now()) + "): " + string + "\n")
+    f.close()

90ce72904c797efaa54ee6912af40860ac345253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:11 +0000

    Add package pytik/0.2.1/pytik-0.2.1-py3-none-any.whl

---
+++ packages/pytik/pytik-0.2.1-py3-none-any.whl/pytik/extract.py
@@ -0,0 +1,45 @@
+import json
+import urllib.parse
+import re
+
+
+def video_id(url):
+    """Extract the ``video_id`` from a TikTok url.
+    This function supports the following patterns:
+    - :samp:`https://www.tiktok.com/@{user}/video/{video_id}}`
+    :param str url:
+        A TikTok url containing a video id.
+    :rtype: str
+    :returns:
+        TikTok video id.
+    """
+    return re.findall("\d+", url)[0]
+
+
+def description(json):
+    return json["props"]["pageProps"]["itemInfo"]["itemStruct"]["desc"]
+
+
+def user(json):
+    return json["props"]["pageProps"]["itemInfo"]["itemStruct"]["author"]["uniqueId"]
+
+
+def nickname(json):
+    return json["props"]["pageProps"]["itemInfo"]["itemStruct"]["author"]["nickname"]
+
+
+def song(json):
+    return json["props"]["pageProps"]["itemInfo"]["itemStruct"]["music"]["title"]
+
+
+def song_author(json):
+    return json["props"]["pageProps"]["itemInfo"]["itemStruct"]["music"]["authorName"]
+
+
+def get_json(html):
+    pattern = r"(?<={\"props\":)(.*?)(?=</script>)"
+    regex = re.compile(pattern)
+    result = regex.search(html)
+    span = result.span()
+    full_obj = '{"props":' + html[span[0] : span[1]]
+    return json.loads(full_obj)

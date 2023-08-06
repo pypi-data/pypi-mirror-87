cd956122dbee43b660ef0eb7b00c37d7c3c9aa33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:14 +0000

    Add package RbxAPI/0.19.3/RbxAPI-0.19.3.tar.gz

---
+++ packages/RbxAPI/RbxAPI-0.19.3.tar.gz/RbxAPI-0.19.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='RbxAPI',
+      version='0.19.3',
+      description='A Pythonic and efficient Roblox API wrapper.',
+      author='PythonicIconic',
+      author_email='coatedalmond@protonmail.com',
+      url='https://github.com/PythonicIconic/RbxAPI',
+      packages=['RbxAPI'],
+      requires=['requests'])

ff10ae870df1b02a93be1d26f4d6d1468a0a7106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:22 +0000

    Add package jedit2/1.0/jedit2-1.0.tar.gz

---
+++ packages/jedit2/jedit2-1.0.tar.gz/jedit2-1.0/jedit2/__init__.py
@@ -0,0 +1,1 @@
+from jedit import *
---
+++ packages/jedit2/jedit2-1.0.tar.gz/jedit2-1.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+	name = "jedit2",
+	packages = ["jedit2"],
+	version = "1.0",
+	license = "MIT",
+	description = "A module to make it easier to use JSON files.",
+	author = "Bella Clark",
+	url = "https://github.com/Egggggg/jedit2",
+	download_url = "",
+	keywords = ["JSON", "data", "storage", "load"],
+	classifiers = [
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Natural Language :: English",
+		"Operating System :: OS Independent",
+		"Topic :: Software Development",
+		"Topic :: Utilities",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9"
+	]
+)

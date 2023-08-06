ef62453d389235783f09ba54eacb127a969c5c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:09 +0000

    Add package opennem/3.3.0a10/opennem-3.3.0a10.tar.gz

---
+++ packages/opennem/opennem-3.3.0a10.tar.gz/opennem-3.3.0a10/opennem/spiders/wem/__init__.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+import scrapy
+
+from opennem.pipelines.files import LinkExtract
+from opennem.spiders.dirlisting import DirlistingSpider
+
+
+def get_date_component(format_str):
+    return datetime.now().strftime(format_str)
+
+
+class WemCurrentSpider(scrapy.Spider):
+
+    start_url = None
+
+    url_params = {
+        "day": get_date_component("%d"),
+        "month": get_date_component("%m"),
+        "year": get_date_component("%Y"),
+    }
+
+    def start_requests(self):
+
+        cache_bust = datetime.now().strftime("%Y%M%d%H%M%S%f")
+
+        if self.start_url:
+            request_url = self.start_url.format(**self.url_params)
+            request_url += "?{}".format(cache_bust)
+
+            yield scrapy.Request(request_url)
+
+    def parse(self, response):
+        yield {"content": response.text}
+
+
+class WemHistoricSpider(DirlistingSpider):
+    allowed_domains = ["wa.nemweb.com.au"]
+    pipelines = set([LinkExtract])
+
+    skip = 1
+
+    # Archives tend to contain large zips of embedded zips so throttle
+    # to limit memory use
+    custom_settings = {
+        "CONCURRENT_REQUESTS": 4,
+        "CONCURRENT_ITEMS": 8,
+    }

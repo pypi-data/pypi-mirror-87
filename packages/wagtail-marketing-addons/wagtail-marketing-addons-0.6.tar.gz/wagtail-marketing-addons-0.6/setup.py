1c118287fe557493984b5690770bb90b289f2fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:28 +0000

    Add package wagtail-marketing-addons/0.6/wagtail-marketing-addons-0.6.tar.gz

---
+++ packages/wagtail-marketing-addons/wagtail-marketing-addons-0.6.tar.gz/wagtail-marketing-addons-0.6/setup.py
@@ -0,0 +1,69 @@
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+PROJECT_DIR = os.path.dirname(__file__)
+
+sys.path.append(os.path.join(PROJECT_DIR, 'src'))
+from wagtail_marketing import get_version  # noqa isort:skip
+
+docs_require = [
+    'mkdocs',
+    'pymdown-extensions'
+]
+
+tests_require = [
+    # Required for test and coverage
+    'pytest',
+    'pytest-cov',
+    'pytest-django',
+    'pytest-pythonpath',
+    'coverage',
+    'factory-boy',
+    'psycopg2-binary>=2.5.4',
+    'tox',
+
+    # Linting
+    'flake8',
+    'isort',
+]
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='wagtail-marketing-addons',
+    version=get_version().replace(' ', '-'),
+    description='A Wagtail add-on for supporting marketeer\'s in daily activities',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Lukkien BV',
+    author_email='support@lukkien.com',
+    url='https://www.lukkien.com/',
+    extras_require={
+        'test': tests_require,
+        'doc': docs_require,
+    },
+    tests_require=tests_require,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 2',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ]
+)
---
+++ packages/wagtail-marketing-addons/wagtail-marketing-addons-0.6.tar.gz/wagtail-marketing-addons-0.6/src/wagtail_marketing/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class WagtailMarketingConfig(AppConfig):
+    label = 'wagtail_marketing'
+    name = 'wagtail_marketing'
+    verbose_name = _("Wagtail Marketing")
+
+    def ready(self):
+        if not self.apps.is_installed('wagtail.contrib.modeladmin'):
+            raise RuntimeError(
+                'You need to register wagtail.contrib.modeladmin in your INSTALLED_APPS'
+                'in order for wagtail-marketing-addons to work'
+            )

b5c4521a7c35dceb64dabbbf0b18f121e8e94ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package BGD-probability/0.1/BGD_probability-0.1.tar.gz

---
+++ packages/BGD-probability/BGD_probability-0.1.tar.gz/BGD_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BGD_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['BGD_probability'],
+      author = 'Nima Ghasemi',
+      author_email = 'nima.ghasemi@gmail.com',
+    zip_safe=False)

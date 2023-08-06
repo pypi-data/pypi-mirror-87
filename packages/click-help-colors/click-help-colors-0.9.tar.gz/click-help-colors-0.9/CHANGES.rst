5b0556b0dfaf30e5566c00fbc30a40141f1b2137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:41 +0000

    Add package click-help-colors/0.9/click-help-colors-0.9.tar.gz

---
+++ packages/click-help-colors/click-help-colors-0.9.tar.gz/click-help-colors-0.9/CHANGES.rst
@@ -0,0 +1,18 @@
+[0.9]
+-------------------------------
+- Add support for Click 8
+- Drop support for Python 2 and 3.5.
+
+[0.8.1]
+-------------------------------
+- Changed required version of Click to >=7.0,<8
+
+[0.8]
+-------------------------------
+- Add missing files to sdist
+
+[0.7]
+-------------------------------
+- Add support for NO_COLOR <https://no-color.org/>
+- Add support for MultiCommands
+- Add support for version_option
---
+++ packages/click-help-colors/click-help-colors-0.9.tar.gz/click-help-colors-0.9/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import io
+import re
+from setuptools import setup
+
+
+with io.open("README.rst", "rt", encoding="utf8") as f:
+    readme = f.read()
+
+
+with io.open("click_help_colors/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r"__version__ = '(.*?)'", f.read()).group(1)
+
+
+setup(
+    name='click-help-colors',
+    version=version,
+    packages=['click_help_colors'],
+    description='Colorization of help messages in Click',
+    long_description=readme,
+    url='https://github.com/r-m-n/click-help-colors',
+    keywords=['click'],
+    license='MIT',
+    install_requires=[
+        'click>=7.0,<9'
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    }
+)

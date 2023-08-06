a3e27e2fa11c28bb19a08af2338f9b066320661c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:42 +0000

    Add package dsnd-han-probability/0.5/dsnd_han_probability-0.5.tar.gz

---
+++ packages/dsnd-han-probability/dsnd_han_probability-0.5.tar.gz/dsnd_han_probability-0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_han_probability',
+      version='0.5',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_han_probability'],
+      author= 'Jing Han Ng',
+      zip_safe=False)

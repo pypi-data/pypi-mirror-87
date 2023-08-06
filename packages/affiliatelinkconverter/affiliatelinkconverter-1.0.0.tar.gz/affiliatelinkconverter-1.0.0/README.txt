34ac0eb6d7304c14827b8ea59f987b1d69b3400f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package affiliatelinkconverter/1.0.0/affiliatelinkconverter-1.0.0.tar.gz

---
+++ packages/affiliatelinkconverter/affiliatelinkconverter-1.0.0.tar.gz/affiliatelinkconverter-1.0.0/README.txt
@@ -0,0 +1,1 @@
+This is affiliate link converter package
---
+++ packages/affiliatelinkconverter/affiliatelinkconverter-1.0.0.tar.gz/affiliatelinkconverter-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='affiliatelinkconverter',
+    version='1.0.0',
+    description='affiliate link converter package',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Manoj Sitapara',
+    author_email='manojsitapara@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='affiliatelinkconverter',
+    packages=find_packages(),
+    install_requires=[
+        'furl>=2.1.0',
+        'unshortenit>=0.4.0'
+
+    ]
+)

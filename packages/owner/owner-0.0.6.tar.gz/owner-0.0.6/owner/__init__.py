709339d89d0ae08175a7d89fb63a71ff1401ba9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package owner/0.0.6/owner-0.0.6.tar.gz

---
+++ packages/owner/owner-0.0.6.tar.gz/owner-0.0.6/owner/__init__.py
@@ -0,0 +1,4 @@
+from owner.util import bytes_to_sha256, src_to_bytes, int_to_bytes, bytes_to_int, decode_with
+from owner.base import HeadWeaver, UnWovenBaseHeaderWeave, weave, unweave
+
+assert unweave(weave(b'bob and alice')) == b'bob and alice'

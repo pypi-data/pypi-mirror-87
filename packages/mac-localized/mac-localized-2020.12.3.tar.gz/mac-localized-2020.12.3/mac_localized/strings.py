46cd90a73725bb3d40844ee52a34fefcc962e22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:09 +0000

    Add package mac-localized/2020.12.3/mac-localized-2020.12.3.tar.gz

---
+++ packages/mac-localized/mac-localized-2020.12.3.tar.gz/mac-localized-2020.12.3/mac_localized/strings.py
@@ -0,0 +1,36 @@
+__all__ = ['find', 'load', 'update']
+
+
+import os
+
+"""
+Folder.localized/.localized/de.strings:
+"Folder" = "Translation";
+"""
+
+
+def find(path):
+    """return a list with `.strings` files"""
+    result = []
+    for f in os.listdir(path):
+        name, ext = os.path.splitext(f)
+        if ext == ".strings":
+            fullpath = os.path.join(path, f)
+            result.append(fullpath)
+    return result
+
+
+def load(path):
+    """return dictionary with keys as languages and translations with values"""
+    for s in filter(lambda s: " = " in s, open(path).read().splitlines()):
+        key = s.split(" = ")[0].replace('"', '')
+        value = s.split(" = ")[1].replace('"', '').replace(';', '').rstrip()
+        return key, value
+
+
+def update(path, key, value):
+    """update translation"""
+    string = '"%s" = "%s";\n' % (key, value)
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    open(path, "w").write(string)

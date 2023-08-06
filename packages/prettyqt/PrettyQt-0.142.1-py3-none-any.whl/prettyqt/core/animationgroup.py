494c93fe8d1f2a182bfb9856eb75277d576e3544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:02 +0000

    Add package prettyqt/0.142.1/PrettyQt-0.142.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.142.1-py3-none-any.whl/prettyqt/core/animationgroup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+
+from qtpy import QtCore
+
+from prettyqt import core
+
+QtCore.QAnimationGroup.__bases__ = (core.AbstractAnimation,)
+
+
+class AnimationGroup(QtCore.QAnimationGroup):
+    def __getitem__(self, index: Union[int, slice]) -> QtCore.QAbstractAnimation:
+        if isinstance(index, int):
+            if index < 0:
+                index = len(self) + index
+            return self.animationAt(index)
+        else:
+            anims = [self.animationAt(i) for i in range(len(self))]
+            return anims[index]
+
+    def __setitem__(self, index: int, value: QtCore.QAbstractAnimation):
+        old = self.animationAt(index)
+        self.removeAnimation(old)
+        self.insertAnimation(index, value)
+
+    def __len__(self):
+        return self.animationCount()
+
+    def __delitem__(self, index: int):
+        animation = self[index]
+        self.removeAnimation(animation)
+
+    def __add__(self, other: QtCore.QAbstractAnimation):
+        self.addAnimation(other)
+        return self
+
+    def add_property_animation(
+        self, obj: QtCore.QObject, attribute: str
+    ) -> core.PropertyAnimation:
+        anim = core.PropertyAnimation(obj, attribute.encode())
+        self.addAnimation(anim)
+        return anim
---
+++ packages/prettyqt/PrettyQt-0.142.1-py3-none-any.whl/prettyqt/widgets/widgetaction.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets
+
+
+QtWidgets.QWidgetAction.__bases__ = (widgets.Action,)
+
+
+class WidgetAction(QtWidgets.QWidgetAction):
+    def __init__(self, parent=None):  # QtCore.QObject
+        super().__init__(parent)
+
+    #     self.set_text(text)
+    #     self.set_icon(icon)
+    #     self.set_shortcut(shortcut)
+    #     self.set_tooltip(tooltip)

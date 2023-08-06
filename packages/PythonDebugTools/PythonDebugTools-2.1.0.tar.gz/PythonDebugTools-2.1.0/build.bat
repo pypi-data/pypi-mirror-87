6ae41bdaa1659bee8e6ea1e471da96631402828f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:20 +0000

    Add package PythonDebugTools/2.1.0/PythonDebugTools-2.1.0.tar.gz

---
+++ packages/PythonDebugTools/PythonDebugTools-2.1.0.tar.gz/PythonDebugTools-2.1.0/build.bat
@@ -0,0 +1,7 @@
+rmdir /Q /S dist
+mkdir dist
+
+python setup.py build sdist bdist_wheel
+
+python -m twine upload dist/*
+
---
+++ packages/PythonDebugTools/PythonDebugTools-2.1.0.tar.gz/PythonDebugTools-2.1.0/src/examples.py
@@ -0,0 +1,90 @@
+import json
+import os
+import time
+import sys
+
+import tkinter as tk
+from PythonDebugTools import *
+
+
+
+
+if __name__ == '__main__':
+    _pp = Printer.Default()
+    print('_pp is pp', _pp is pp)
+    Printer.Set(_pp)
+    print('_pp is pp', _pp is pp)
+
+    class test(object):
+        @Debug()
+        def pp_run(self, *args, **kwargs):
+            return args, kwargs
+
+        @Debug()
+        def run(self, *args, **kwargs):
+            return args, kwargs
+
+        @DebugTkinterEvent()
+        def tk_run(self, event: tk.Event):
+            return None
+
+        @CheckTime()
+        def timed(self, delay: int, *args, **kwargs):
+            time.sleep(delay)
+            return args, kwargs
+
+        @CheckTimeWithSignature()
+        def timed_sig(self, delay: int, *args, **kwargs):
+            time.sleep(delay)
+            return args, kwargs
+
+        @StackTrace()
+        def stack(self, *args, **kwargs):
+            return args, kwargs
+
+        @StackTrace()
+        def stack_sig(self, *args, **kwargs):
+            return args, kwargs
+
+
+        @chain()
+        def chain_root(self, *args, **kwargs):
+            return self.sub1(*args, **kwargs)
+
+        @sub()
+        def sub1(self, *args, **kwargs):
+            return self.sub2(*args, **kwargs)
+
+        @sub()
+        def sub2(self, *args, **kwargs):
+            return 'chain.sub.end', args, kwargs
+
+
+
+    t = test()
+
+    t.run()
+    t.pp_run()
+    t.timed(1)
+    t.timed_sig(1)
+
+    evt = tk.Event()
+    evt.widget = None
+    evt.x = None
+    evt.y = None
+    t.tk_run(evt)
+
+
+    t.stack(1, 2, 3, test=True, print=False)
+    t.stack_sig(1, 2, 3, test=True, print=True)
+
+    t.chain_root()
+
+
+
+    path = os.path.abspath(sys.argv[1])
+
+    with open(path, 'r') as f:
+        d = json.load(f)
+
+    # Print(getPPrintStr(d))

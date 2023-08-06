dedf696402407992c9581a208e1afaaeddaba400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:36 +0000

    Add package powerprofile/0.5.0/powerprofile-0.5.0.tar.gz

---
+++ packages/powerprofile/powerprofile-0.5.0.tar.gz/powerprofile-0.5.0/powerprofile/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class PowerProfileIncompleteCurve(ValueError):
+    pass
+
+
+class PowerProfileDuplicatedTimes(ValueError):
+    pass
+
+
+class PowerProfileNotImplemented(NotImplementedError):
+    pass
+
+
+class PowerProfileIncompatible(ValueError):
+    pass

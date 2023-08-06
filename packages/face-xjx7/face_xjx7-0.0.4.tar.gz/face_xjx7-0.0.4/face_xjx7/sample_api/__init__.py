c282bebf7b7c67d74f1440ded035bd3a4d75ecd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:55 +0000

    Add package face-xjx7/0.0.4/face_xjx7-0.0.4.tar.gz

---
+++ packages/face-xjx7/face_xjx7-0.0.4.tar.gz/face_xjx7-0.0.4/face_xjx7/sample_api/__init__.py
@@ -0,0 +1,6 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+#--------------------------------------------------------------------------
+from sample_api.temp import *
+from sample_api.test_image import *

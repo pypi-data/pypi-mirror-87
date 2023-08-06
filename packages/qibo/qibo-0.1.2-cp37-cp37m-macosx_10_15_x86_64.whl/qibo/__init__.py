94380727df5a2b6b2ccacf7521f7756813655065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:20 +0000

    Add package qibo/0.1.2/qibo-0.1.2-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/qibo/qibo-0.1.2-cp37-cp37m-macosx_10_15_x86_64.whl/qibo/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.1.2"
+from qibo.config import set_precision, set_backend, set_device, set_threads, get_threads, get_backend, get_precision, get_device, matrices, K
+from qibo import callbacks
+from qibo import evolution
+from qibo import models
+from qibo import gates
+from qibo import hamiltonians
+from qibo import optimizers
+from qibo import solvers

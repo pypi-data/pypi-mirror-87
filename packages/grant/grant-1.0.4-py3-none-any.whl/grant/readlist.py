dfb665566d9044ea6bb002f8122f30570655e5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:07 +0000

    Add package grant/1.0.4/grant-1.0.4-py3-none-any.whl

---
+++ packages/grant/grant-1.0.4-py3-none-any.whl/grant/readlist.py
@@ -0,0 +1,96 @@
+# -*- coding:utf-8 -*-
+"""
+作者：jiangshuo
+日期：2020年11月27日
+"""
+import user_grant.example.grant as grant
+import xlrd
+import sys
+import importlib
+import re
+importlib.reload(sys)
+
+
+def name_judge(row_name, col_name):
+    for d in range(0, len(row_name)):
+        if row_name[d] == col_name:
+            col = d
+            return col
+
+
+def extract_character(k_lie_value):
+    # 以“、”为分界点对字符串按照用户进行切片
+    ss = k_lie_value.split('、')
+    # 提取各切片中的用户名（数字、英文）
+    privilege_list = []
+    for m in ss:
+        character = re.sub(u"([^\u0030-\u0039\u0041-\u005a\u0061-\u007a])", "", m)
+        privilege_list.append(character)
+    print(privilege_list)
+
+    return privilege_list
+
+
+def write(sql_grant1):
+    f = open('E:/test.txt', 'a+')
+    f.write(sql_grant1)
+    f.close()
+    return sql_grant1
+
+
+def read_list():
+    # 获取workbook中所有的sheet
+    sheets = source.sheets()
+    sheet_names = source.sheet_names()
+    # 循环遍历所有sheet
+    print("一共有%s个sheet" % len(sheets))
+    privilege = 'select'
+    port = '1521'
+    for i in range(len(sheets)):   # 有几个工作表
+        table = source.sheet_by_index(i)   # 通过索引获取表格
+        sheet_name = sheet_names[i]   # 第i个sheet表的名称
+        print("第%s个sheet表: %s" % ((i+1), sheet_name))       # 第几个sheet
+        row_name = table.row_values(0)   # 获取第一行内容
+        a = name_judge(row_name, '用户名')
+        elie_value = table.col_values(a)   # 获取第i个表用户名
+        b = name_judge(row_name, '用户权限说明 ')
+        k_lie_value = table.col_values(b)   # 获取第i个表用户权限
+        # c = name_judge(row_name, 'IP地址')
+        # ip_value = table.col_values(c)   # 获取第i个表ip地址
+        # ips = ip_value[1]
+        # d = name_judge(row_name, '实例名')
+        # sid_value = table.col_values(d)   # 获取第i个表sid
+        # sids = sid_value[1]
+        ips = '10.7.137.75'
+        sids = 'JS19'
+        characters = extract_character(k_lie_value[1])   # 用户权限字段提取
+        h_lie_value = characters
+        for j in range(1, len(elie_value)):
+            e_lie = elie_value[j]
+            print(e_lie)
+            for k in range(len(h_lie_value)):
+                h_lie = h_lie_value[k]
+                test_oracle = grant.TestOracle(user, password, ips, port, sids)
+                sql_grant = test_oracle.sql_grant_character('SYS', privilege, e_lie)
+                # sql_grant = test_oracle.sql_grant_character(h_lie, privilege, e_lie)
+                # print(sql_grant.rs)
+                for jj in sql_grant:
+                    print(jj[0])
+                    txt = write(jj[0]+'\n')
+                # return sql_grant
+
+
+if __name__ == '__main__':
+    path_name = input('请输入Excel表格所在的路径和名称：')
+    path_name = r"C:\Users\jiangshuo\Desktop\客服中心申请数据库实名账户.xls"
+    source = xlrd.open_workbook(r"%s" % path_name)
+    user = input('请输入管理员用户名：')
+    password = input('请输入管理员用户密码：')
+
+    # path_name = r"C:\Users\jiangshuo\Desktop\客服中心申请数据库实名账户.xls"
+    # source = xlrd.open_workbook(r"%s" % path_name)
+    # user = 'SYS'
+    # password = 'jiangshuo6'
+    # 运行主函数
+    json_data = read_list()
+

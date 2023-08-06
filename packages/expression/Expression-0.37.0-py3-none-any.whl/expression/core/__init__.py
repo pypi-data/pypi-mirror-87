281ed378b8f84df295f7310d5358e14d11b98670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package expression/0.37.0/Expression-0.37.0-py3-none-any.whl

---
+++ packages/expression/Expression-0.37.0-py3-none-any.whl/expression/core/__init__.py
@@ -0,0 +1,52 @@
+"""
+Core abstractions such as pipes, options and results.
+"""
+
+from . import aiotools, option, result
+from .builder import Builder
+from .compose import compose
+from .curry import curried
+from .error import EffectError, failwith
+from .fn import TailCall, TailCallResult, tailrec, tailrec_async
+from .mailbox import AsyncReplyChannel, MailboxProcessor
+from .match import Matchable, Matcher, Pattern, match
+from .misc import flip, fst, identity, snd
+from .option import Nothing, Nothing_, Option, Some
+from .pipe import pipe, pipe2, pipe3
+from .result import Error, Ok, Result, Try
+
+__all__ = [
+    "aiotools",
+    "AsyncReplyChannel",
+    "Builder",
+    "compose",
+    "curried",
+    "EffectError",
+    "Error",
+    "failwith",
+    "flip",
+    "fst",
+    "identity",
+    "MailboxProcessor",
+    "match",
+    "Matcher",
+    "Matchable",
+    "Nothing",
+    "Nothing_",
+    "Ok",
+    "Option",
+    "option",
+    "Pattern",
+    "pipe",
+    "pipe2",
+    "pipe3",
+    "result",
+    "Result",
+    "snd",
+    "Some",
+    "TailCall",
+    "TailCallResult",
+    "tailrec",
+    "tailrec_async",
+    "Try",
+]

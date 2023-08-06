e05e3f2019f10dbc2d8dcd616923a4e6ef8b9075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:03 +0000

    Add package aiotfm/1.4.0b3/aiotfm-1.4.0b3-py3-none-any.whl

---
+++ packages/aiotfm/aiotfm-1.4.0b3-py3-none-any.whl/aiotfm/connection.py
@@ -0,0 +1,104 @@
+import asyncio
+
+
+class TFMProtocol(asyncio.Protocol):
+	def __init__(self, conn):
+		self.buffer = bytearray()
+		self.client = conn.client
+		self.connection = conn
+		self.length = 0
+
+	def data_received(self, data):
+		self.buffer.extend(data)
+
+		while len(self.buffer) > self.length:
+			if self.length == 0:
+				for i in range(5):
+					byte = self.buffer.pop(0)
+					self.length |= (byte & 127) << (i * 7)
+
+					if not byte & 0x80:
+						break
+				else:
+					raise Exception("wtf") # pragma: no cover
+
+			if len(self.buffer) >= self.length:
+				self.client.data_received(self.buffer[:self.length], self.connection)
+				del self.buffer[:self.length]
+				self.length = 0
+
+	def connection_made(self, transport):
+		# :desc: Called when a connection has been successfully made with the server.
+		# :param connection: :class:`Connection` the connection that has been made.
+		self.connection.open = True
+		self.client.dispatch('connection_made', self.connection)
+
+	def connection_lost(self, exc):
+		self.connection.open = False
+
+		if exc is not None:
+			# :desc: Called when a connection has been lost due to an error.
+			# :param connection: :class:`Connection` the connection that has been lost.
+			# :param exception: :class:`Exception` the error which occurred.
+			self.client.dispatch('connection_error', self.connection, exc)
+
+		if self.connection.name == "main":
+			if self.client.auto_restart:
+				self.client.loop.create_task(self.client.restart_soon())
+			else:
+				self.client.close()
+
+
+class Connection:
+	"""Represents the connection between the client and the host."""
+	PROTOCOL = TFMProtocol
+
+	def __init__(self, name, client, loop):
+		self.name = name
+		self.client = client
+		self.loop = loop
+
+		self.address = None
+		self.protocol = None
+		self.transport = None
+
+		self.fingerprint = 0
+		self.open = False
+
+	def _factory(self):
+		return Connection.PROTOCOL(self)
+
+	async def connect(self, host, port):
+		"""|coro|
+		Connect the client to the host:port
+		"""
+		self.address = (host, port)
+		self.transport, self.protocol = await self.loop.create_connection(self._factory, host, port)
+
+	async def send(self, packet, cipher=False):
+		"""|coro|
+		Send a packet to the socket
+
+		:param packet: :class:`aiotfm.Packet` the packet to send.
+		:param cipher: :class:`bool` whether or not the packet should be ciphered before sending it.
+		"""
+		if not self.open:
+			return
+
+		if not self.client.bot_role and cipher:
+			packet.xor_cipher(self.client.keys.msg, self.fingerprint)
+
+		self.transport.write(packet.export(self.fingerprint))
+		self.fingerprint = (self.fingerprint + 1) % 100
+
+	def close(self):
+		"""Closes the connection."""
+		self.open = False
+		if self.transport is not None and not self.transport.is_closing():
+			self.transport.write_eof()
+			self.transport.close()
+
+	def abort(self):
+		"""Abort the connection."""
+		self.transport.abort()
+		self.close()
---
+++ packages/aiotfm/aiotfm-1.4.0b3-py3-none-any.whl/aiotfm/utils/get_keys.py
@@ -0,0 +1,54 @@
+import aiohttp
+
+from aiotfm import __version__
+from aiotfm.errors import EndpointError, InternalError, MaintenanceError
+
+
+class Keys:
+	"""Represents the keys used by the client to communicate to the server."""
+	def __init__(self, **keys):
+		self.auth = keys.pop('auth_key', 0)
+		self.connection = keys.pop('connection_key', '')
+		self.identification = keys.pop('identification_keys', [])
+		self.msg = [k & 0xff for k in keys.pop('msg_keys', [])]
+		self.packet = keys.pop('packet_keys', [])
+		self.version = keys.pop('version', 0)
+		self.server_ip = keys.pop('ip', '51.75.130.180')
+		self.kwargs = keys
+
+
+async def get_keys(tfm_id, token):
+	"""|coro|
+	Fetch the keys required to log into the game.
+
+	:param tfm_id: :class:`int` your Transformice user id.
+	:param token: :class:`str` your api token.
+	"""
+	url = 'https://api.tocuto.tk/get_transformice_keys.php'
+	params = {'tfmid': tfm_id, 'token': token}
+	headers = {"User-Agent": f"Mozilla/5.0 aiotfm/{__version__}"}
+
+	async with aiohttp.ClientSession() as session:
+		async with session.get(url, params=params, headers=headers) as resp:
+			data = await resp.json()
+
+	success = data.pop('success', False)
+	internal_error = data.pop('internal_error', True)
+	internal_error_step = data.pop('internal_error_step', 0)
+	error = data.pop('error', None)
+
+	if success: # pragma: no cover
+		if not internal_error:
+			keys = Keys(**data)
+			if len(keys.packet) > 0 and len(keys.identification) > 0 and len(keys.msg) > 0:
+				return keys
+
+			raise EndpointError('Something goes wrong: A key is empty ! {}'.format(data))
+
+		if internal_error_step == 2:
+			raise MaintenanceError('The game might be in maintenance mode.')
+
+		message = 'An internal error occur: {}'.format(internal_error_step)
+		raise InternalError(message)
+
+	raise EndpointError("Can't get the keys. Error info: {}".format(error))

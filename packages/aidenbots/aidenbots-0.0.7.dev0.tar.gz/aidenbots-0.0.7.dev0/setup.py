e618dde2e0b6055f30446260f18c94c1e010cbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:13 +0000

    Add package aidenbots/0.0.7.dev0/aidenbots-0.0.7.dev0.tar.gz

---
+++ packages/aidenbots/aidenbots-0.0.7.dev0.tar.gz/aidenbots-0.0.7.dev0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="aidenbots",
+    version="0.0.7-dev",
+    author="Brian Glenn St Hilaire",
+    author_email="hilaire@bcm.edu",
+    include_package_data=True,
+    description="Opentrons automated protocols developed in the Aiden Lab",
+    url="https://github.com/aidenlab/ot2-bots",
+    packages=setuptools.find_packages(exclude=["aidenbots.bin", "aidenbots.protocols"]),
+    package_data={'': ['sounds/Bb_A_Bb_pause.mp3']},
+    install_requires=[
+        'opentrons >= 3.21.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'aidenbots_check = aidenbots.check:main',
+        ],
+    },
+    python_requires='>=3.7',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

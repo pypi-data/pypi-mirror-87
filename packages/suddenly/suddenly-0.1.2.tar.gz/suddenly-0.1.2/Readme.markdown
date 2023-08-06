40317d0989453272ff3127c5ca570d1eb3b0a7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:20 +0000

    Add package suddenly/0.1.2/suddenly-0.1.2.tar.gz

---
+++ packages/suddenly/suddenly-0.1.2.tar.gz/suddenly-0.1.2/Readme.markdown
@@ -0,0 +1,20 @@
+suddenly
+================================================================================
+![image](https://img.shields.io/pypi/v/suddenly)
+![image](https://img.shields.io/pypi/pyversions/suddenly)
+![image](https://gitlab.com/yassu/suddenly/badges/master/pipeline.svg)
+![image](https://img.shields.io/pypi/l/suddenly)
+
+```
+$ suddenly "突然の死"                                                                                                                      0
+＿人人人人人人＿
+＞　突然の死　＜
+￣Y^Y^Y^Y^Y^Y^￣
+
+```
+
+# How to install
+
+```
+$ pip install suddenly
+```
---
+++ packages/suddenly/suddenly-0.1.2.tar.gz/suddenly-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['suddenly']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['suddenly = suddenly.suddenly:main']}
+
+setup_kwargs = {
+    'name': 'suddenly',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': 'suddenly\n================================================================================\n![image](https://img.shields.io/pypi/v/suddenly)\n![image](https://img.shields.io/pypi/pyversions/suddenly)\n![image](https://gitlab.com/yassu/suddenly/badges/master/pipeline.svg)\n![image](https://img.shields.io/pypi/l/suddenly)\n\n```\n$ suddenly "突然の死"                                                                                                                      0\n＿人人人人人人＿\n＞\u3000突然の死\u3000＜\n￣Y^Y^Y^Y^Y^Y^￣\n\n```\n\n# How to install\n\n```\n$ pip install suddenly\n```\n',
+    'author': 'yassu',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

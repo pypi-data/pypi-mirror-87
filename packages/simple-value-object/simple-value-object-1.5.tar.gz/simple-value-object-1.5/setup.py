d8a4782c2f629d1584084033461618622947fd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:30 +0000

    Add package simple-value-object/1.5/simple-value-object-1.5.tar.gz

---
+++ packages/simple-value-object/simple-value-object-1.5.tar.gz/simple-value-object-1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from simple_value_object import get_version
+
+
+setup(
+    name='simple-value-object',
+    version=get_version(),
+    license='MIT/X11',
+    author='Quique Porta',
+    author_email='quiqueporta@gmail.com',
+    description='A simple mixin for create Value Objects',
+    long_description=open('README.rst').read(),
+    url='https://github.com/quiqueporta/value-object',
+    download_url='https://github.com/quiqueporta/simple-value-object/releases',
+    keywords=['python', 'ddd'],
+    packages=['simple_value_object'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+)

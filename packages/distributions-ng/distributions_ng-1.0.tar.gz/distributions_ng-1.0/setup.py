e894c78c0ebc4e046e973602a97f92b2de505fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:41 +0000

    Add package distributions-ng/1.0/distributions_ng-1.0.tar.gz

---
+++ packages/distributions-ng/distributions_ng-1.0.tar.gz/distributions_ng-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_ng',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributions_ng'],
+      author = 'Nitin Raj',
+      author_email = 'nitingupta180@gmail.com',
+      zip_safe=False)

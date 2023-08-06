a0729aa185831d5631f4816461136b62bf6ec2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:24 +0000

    Add package git-id/1.1.2/git-id-1.1.2.tar.gz

---
+++ packages/git-id/git-id-1.1.2.tar.gz/git-id-1.1.2/git_id/__init__.py
@@ -0,0 +1,12 @@
+from git_id.profile import *
+from git_id.core import main
+
+__copyright__ = 'Copyright (C) 2020 Yuichiro Smith'
+__version__ = '1.1.2'
+__license__ = 'MIT License'
+__author__ = 'Yuichiro Smith'
+__author_email__ = 'contact@yu-smith.com'
+__url__ = 'http://github.com/yu-ichiro/git-id'
+
+if __name__ == "__main__":
+    main()

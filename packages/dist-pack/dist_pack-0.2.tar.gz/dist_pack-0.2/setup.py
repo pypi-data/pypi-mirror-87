0fe08227c40e30d334ec1fff6ebb8419ff3104a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:25 +0000

    Add package dist-pack/0.2/dist_pack-0.2.tar.gz

---
+++ packages/dist-pack/dist_pack-0.2.tar.gz/dist_pack-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_pack',
+      version='0.2',
+      description='Gaussian and binomial distributions',
+      author='Fernando de la Torre',
+      author_email='fernandodelatorre4@gmail.com',
+      packages=['dist_pack'],
+      zip_safe=False)

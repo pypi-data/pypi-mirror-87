33d60a51cb62805188a91b196f2c6aa664c0fabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:03 +0000

    Add package ConsLoadingBar/1.2.4/ConsLoadingBar-1.2.4.tar.gz

---
+++ packages/ConsLoadingBar/ConsLoadingBar-1.2.4.tar.gz/ConsLoadingBar-1.2.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ConsLoadingBar",
+    version="1.2.4",
+    author="flamechain",
+    author_email="rwc@thezulus.com",
+    description="Easy to make progress bars",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = [
+        "termcolor >= 1.1.0",
+    ],
+)
+
+# delete files from dist
+# update version number
+# run: python3 setup.py sdist bdist_wheel
+# run: twine upload dist/*
+# push to git
+
+# https://pypi.org/project/ConsLoadingBar/1.2.3/

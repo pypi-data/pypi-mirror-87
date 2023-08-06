893200ed1cd39cb47933ed63c1a014f4b1adb378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package pyEX/0.2.9/pyEX-0.2.9-py2.py3-none-any.whl

---
+++ packages/pyEX/pyEX-0.2.9-py2.py3-none-any.whl/pyEX/marketdata/fx.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+from .sse import _runSSE, _runSSEAsync
+
+
+class FXSSE(Enum):
+    FOREX = "forex"
+
+    @staticmethod
+    def options():
+        return list(map(lambda c: c.value, FXSSE))
+
+
+def fxSSE(symbols=None, on_data=None, token="", version=""):
+    """This endpoint streams real-time foreign currency exchange rates.
+
+    https://iexcloud.io/docs/api/#forex-currencies
+
+    Args:
+        symbols (str): Tickers to request
+        on_data (function): Callback on data
+        token (str): Access token
+        version (str): API version
+
+    """
+    return _runSSE("forex", symbols, on_data, token, version)
+
+
+async def fxSSEAsync(symbols=None, token="", version=""):
+    """This endpoint streams real-time foreign currency exchange rates.
+
+    https://iexcloud.io/docs/api/#forex-currencies
+
+    Args:
+        symbols (str): Tickers to request
+        token (str): Access token
+        version (str): API version
+    """
+    async for item in _runSSEAsync("forex", symbols, token, version):
+        yield item
---
+++ packages/pyEX/pyEX-0.2.9-py2.py3-none-any.whl/pyEX/refdata/__init__.py
@@ -0,0 +1,45 @@
+from .symbols import (
+    symbols,
+    iexSymbols,
+    mutualFundSymbols,
+    otcSymbols,
+    internationalSymbols,
+    fxSymbols,
+    optionsSymbols,
+    cryptoSymbols,
+    symbolsDF,
+    iexSymbolsDF,
+    mutualFundSymbolsDF,
+    otcSymbolsDF,
+    internationalSymbolsDF,
+    fxSymbolsDF,
+    optionsSymbolsDF,
+    cryptoSymbolsDF,
+    symbolsList,
+    iexSymbolsList,
+    mutualFundSymbolsList,
+    otcSymbolsList,
+    internationalSymbolsList,
+    fxSymbolsList,
+    optionsSymbolsList,
+    cryptoSymbolsList,
+)
+from .refdata import (
+    corporateActions,
+    corporateActionsDF,
+    dividends as refDividends,
+    dividendsDF as refDividendsDF,
+    nextDayExtDate,
+    nextDayExtDateDF,
+    directory,
+    directoryDF,
+)
+from .calendar import calendar, calendarDF, holidays, holidaysDF
+from .figi import figi, figiDF
+from .exchanges import (
+    exchanges,
+    exchangesDF,
+    internationalExchanges,
+    internationalExchangesDF,
+)
+from .sectors import sectors, sectorsDF, tags, tagsDF

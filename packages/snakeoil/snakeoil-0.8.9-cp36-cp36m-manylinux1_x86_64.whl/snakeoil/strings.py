4a48584b55654834cc656d04fe59330fb1a6ec0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:52 +0000

    Add package snakeoil/0.8.9/snakeoil-0.8.9-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/snakeoil/snakeoil-0.8.9-cp36-cp36m-manylinux1_x86_64.whl/snakeoil/strings.py
@@ -0,0 +1,43 @@
+"""String related methods."""
+
+from .demandload import demand_compile_regexp
+
+demand_compile_regexp('_whitespace_regex', r'^(?P<indent>\s+)')
+
+
+def pluralism(obj, none=None, singular='', plural='s'):
+    """Return singular or plural suffix depending on object's length or value."""
+    # default to plural for empty objects, e.g. there are 0 repos
+    if none is None:
+        none = plural
+
+    try:
+        value = len(obj)
+    except TypeError:
+        # value is probably an int
+        value = obj
+
+    if value == 0:
+        return none
+    elif value == 1:
+        return singular
+    return plural
+
+
+def doc_dedent(s):
+    """Support dedenting docstrings with initial line having no indentation."""
+    try:
+        l = s.split('\n')
+    except AttributeError:
+        raise TypeError(f'{s!r} is not a string')
+    if l:
+        # find first line with an indent if one exists
+        for line in l:
+            indent = _whitespace_regex.match(line)
+            if indent is not None:
+                indent = indent.group('indent')
+                break
+        else:
+            indent = ''
+    len_i = len(indent)
+    return '\n'.join(x[len_i:] if x.startswith(indent) else x for x in l)

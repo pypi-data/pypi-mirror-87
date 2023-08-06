3360c627395a7d7c309b57f1d1d51a462e608524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:26 +0000

    Add package shopwareapi/0.1.8/shopwareapi-0.1.8-py3-none-any.whl

---
+++ packages/shopwareapi/shopwareapi-0.1.8-py3-none-any.whl/shopwareapi/api/product/product_manufacturer_model.py
@@ -0,0 +1,13 @@
+import uuid
+
+from shopwareapi import fields
+from shopwareapi.core.model import Model
+
+
+class ProductManufacturerModel(Model):
+    id = fields.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = fields.CharField()
+
+    class Meta:
+        api_endpoint = "product-manufacturer"
+        api_type = "product_manufacturer"

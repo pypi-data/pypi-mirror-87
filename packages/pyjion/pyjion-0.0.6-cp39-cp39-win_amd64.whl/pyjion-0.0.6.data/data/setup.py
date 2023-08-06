e244345a732561b4abcfd51bb0ce3925cbf4035f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:06 +0000

    Add package pyjion/0.0.6/pyjion-0.0.6-cp39-cp39-win_amd64.whl

---
+++ packages/pyjion/pyjion-0.0.6-cp39-cp39-win_amd64.whl/pyjion-0.0.6.data/data/setup.py
@@ -0,0 +1,43 @@
+from skbuild import setup
+from packaging.version import LegacyVersion
+from skbuild.exceptions import SKBuildError
+from skbuild.cmaker import get_cmake_version
+
+# Add CMake as a build requirement if cmake is not installed or is too low a version
+setup_requires = []
+try:
+    if LegacyVersion(get_cmake_version()) < LegacyVersion("3.2"):
+        setup_requires.append('cmake')
+except SKBuildError:
+    setup_requires.append('cmake')
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("CHANGELOG.md", "r") as fh:
+    long_description += "\n\n" + fh.read()
+
+setup(
+    name='pyjion',
+    version='0.0.6',
+    description='A JIT compiler wrapper for CPython',
+    author='Anthony Shaw and Microsoft',
+    author_email='anthonyshaw@apache.org',
+    url='https://github.com/tonybaloney/Pyjion',
+    license='MIT',
+    packages=['pyjion'],
+    package_dir={'': 'src'},
+    setup_requires=setup_requires,
+    python_requires='>=3.9',
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+    # Uncomment and reinstall for super-verbose tracing
+    # cmake_args=['-DDUMP_TRACES=1']
+)

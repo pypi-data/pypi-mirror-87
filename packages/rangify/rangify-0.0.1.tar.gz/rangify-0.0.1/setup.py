909e7960482a1d8e2002ce31f91550ae6d28effe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:06 +0000

    Add package rangify/0.0.1/rangify-0.0.1.tar.gz

---
+++ packages/rangify/rangify-0.0.1.tar.gz/rangify-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+classifiers = [
+  'Intended Audience :: Education',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3.9',
+  'Programming Language :: Python :: 3.8',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='rangify',
+    version='0.0.1',
+    description='cisco interface rangifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Nijat Aliyev',
+    author_email='nicataliyev@gmail.com',
+    classifiers=classifiers,
+    py_modules=['rangify', 'helpers'],
+    package_dir={'': 'rangify'},
+    install_requires=['deepdiff']
+)

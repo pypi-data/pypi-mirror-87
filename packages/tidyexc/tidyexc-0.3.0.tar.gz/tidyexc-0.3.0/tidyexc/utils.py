58b47d2604dbbdba8632abc1ae614db46f6f2293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package tidyexc/0.3.0/tidyexc-0.3.0.tar.gz

---
+++ packages/tidyexc/tidyexc-0.3.0.tar.gz/tidyexc-0.3.0/tidyexc/utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+class list_iadd(list):
+
+    def __iadd__(self, message):
+        self.append(message)
+        return self
+
+def property_iadd(getter):
+
+    def setter(self, x):
+        if x is not getter(self):
+            raise AttributeError("can't set attribute")
+
+    return property(getter, setter)
+
+def eval_template(template, data):
+    try:
+        if callable(template):
+            return template(data)
+        else:
+            return template.format(**data)
+    except Exception as err:
+        return str(err)
+
+

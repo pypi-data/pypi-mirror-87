c4c6c9bfd0f8236c2c8c270e57511804105b9b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:00 +0000

    Add package FPLI-Minimum-Commutes/10/FPLI_Minimum_Commutes-10-py2-none-any.whl

---
+++ packages/FPLI-Minimum-Commutes/FPLI_Minimum_Commutes-10-py2-none-any.whl/FPLI_Minimum_Commutes/main.py
@@ -0,0 +1,10 @@
+import minimum_commute_calculator
+import config  # local API key
+
+
+def main():
+    minimum_commute_calculator.commute_calculator(download_msid=True)
+
+
+if __name__ == '__main__':
+    main()

664c9c9937cd61bc2c0fc6992e5a07bb79a610b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:17 +0000

    Add package Moustache-fusion.beta/0.0.2.post163120/Moustache-fusion.beta-0.0.2.post163120.tar.gz

---
+++ packages/Moustache-fusion.beta/Moustache-fusion.beta-0.0.2.post163120.tar.gz/Moustache-fusion.beta-0.0.2.post163120/moustache_fusion/exceptions.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=400, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+# @todo: CommandException
---
+++ packages/Moustache-fusion.beta/Moustache-fusion.beta-0.0.2.post163120.tar.gz/Moustache-fusion.beta-0.0.2.post163120/moustache_fusion/skittlespy.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from itertools import count, product
+from string import ascii_uppercase
+import tempfile
+
+from moustache_fusion.logger import logger
+from moustache_fusion.exceptions import InvalidUsage
+
+PDFTK_PATH = '/usr/bin/pdftk'
+PDFINFO_PATH = '/usr/bin/pdfinfo'
+PDFGREP_PATH = '/usr/bin/pdfgrep'
+
+
+# alphabet de lettre (A,B, ..., AB, AC)
+def multiletters(seq):
+    for n in count(1):
+        for s in product(seq, repeat=n):
+            yield ''.join(s)
+
+
+letter = multiletters(ascii_uppercase)
+
+
+def set_annexes_length(annexes):
+    for annexe in annexes:
+        logger().debug("exec %s %s" % (PDFINFO_PATH, annexe['path']))
+        output = subprocess.run([PDFINFO_PATH, annexe['path']], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if output.returncode != 0:
+            cmd = '%s %s' % (PDFINFO_PATH, annexe['path'])
+            msgstr = 'Error trying to get page count for annexe "%s" using command %s (error code %d, output was: %s)'
+            raise RuntimeError(msgstr % (annexe['name'], cmd, output.returncode, output.stderr.decode('utf-8')))
+        output_array = output.stdout.decode('utf-8').split()
+        pageinfo = output_array.index("Pages:")
+        v = output_array[pageinfo + 1]
+        if not v.isdigit():
+            msgstr = 'Could not read page count for annexe "%s" using command %s %s (output was: %s)'
+            raise RuntimeError(msgstr % (annexe['name'], PDFINFO_PATH, annexe['path'], v))
+        annexe['length'] = int(v)
+        logger().debug("%s pages:%d" % (annexe['path'], annexe['length']))
+
+
+# find only one + cache file speed +++
+# pdfgrep -n --cache "challenge" principal.pdf
+# find pattern page
+# TODO test if not null
+def set_annexe_pattern_pages(annexes, document_path):
+    new_annexes = []
+    for annex in annexes:
+        # ubuntu 16.04 ships with pdfgrep 1.4 which does not support '--cache' parameter
+        # cmd = '{0} -n --cache "{1}" {2}'.format(PDFGREP_PATH, annex['pattern'], documentPath)
+        cmd = '{0} -n --cache "{1}" {2}'.format(PDFGREP_PATH, annex['pattern'], document_path)
+        logger().debug("exec %s" % cmd)
+        res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        stdout_content, stderr_content = res.communicate()
+        if res.returncode != 0:
+            if not stderr_content:
+                msgstr = 'Error code %d returned by command for annexe "%s": %s'
+                stderr_content = msgstr % (res.returncode, annex['name'], cmd)
+            raise RuntimeError(stderr_content)
+
+        stdout_content = stdout_content.split()
+        for line in stdout_content:
+            v = line.split(":")[0]
+            if not v.isdigit():
+                msgstr = 'Could not read start page for annexe "%s" using command %s (output was: %s)'
+                raise RuntimeError(msgstr % (annex['name'], cmd, line))
+            annex['alias'] = next(letter)
+            annex['startPage'] = int(v)
+            new_annexes.append(dict(annex))
+            logger().debug("%s alias=%s start page=%d" % (annex['name'], annex['alias'], annex['startPage']))
+    return new_annexes
+
+
+# create and execute script to replace annexes
+# cmd = "/usr/bin/pdftk A=main.pdf B=annexe.pdf cat A1-10 B A12-end output generated.pdf"
+# TODO test if not null
+def replace_annexes(docs, outputfile):
+    # generate aliases (/usr/bin/pdftk A=main.pdf B=annexe.pdf)
+    # sort Annexes by position
+    docs['annexes'].sort(key=lambda ann: ann['startPage'])
+
+    docs['general']['alias'] = next(letter)
+    cmd = "{0} {1}={2} ".format(PDFTK_PATH, docs['general']['alias'], docs['general']['path'])
+    logger().debug(docs)
+    for annex in docs['annexes']:
+        annex['alias'] = next(letter)
+        # cmd += annex['alias'] + "=" + annex['path'] + " "
+        cmd += "{0}={1} ".format(annex['alias'], annex['path'])
+
+    # (cat A1-10 B A12-end output generated.pdf)
+    cmd += "cat "
+    general_doc_position = 1
+
+    for annex in docs['annexes']:
+        if general_doc_position <= annex['startPage'] - 1:
+            cmd += "{0}{1}-{2} {3} ".format(
+                docs['general']['alias'],
+                general_doc_position,
+                annex['startPage'] - 1,
+                annex['alias']
+            )
+        else:
+            cmd += "{0} ".format(annex['alias'])
+
+        general_doc_position = annex['startPage'] + annex['length']
+
+    cmd += "{0}{1}-end output {2}".format(docs['general']['alias'], general_doc_position, outputfile)
+
+    logger().debug(cmd)
+    res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    stdout_content, stderr_content = res.communicate()
+    if res.returncode != 0:
+        msgstr = 'Error while replacing annexes using command %s (error code %d, output was: %s)'
+        raise RuntimeError(msgstr % (cmd, res.returncode, stderr_content))
+
+
+def add_page_numbering_to_annexes(docs):
+    for annexe in docs['annexes']:
+        start = annexe['startPage']
+        try:
+            existing_pdf = PdfFileReader(open(annexe['path'], "rb"))
+        except Exception as exc:
+            msgstr = 'Error while opening annexe "%s" (%s)'
+            raise RuntimeError(msgstr % (annexe['name'], exc))
+        output = PdfFileWriter()
+        end = start + annexe['length']
+        increment = 0
+        while start < end:
+            packet = io.BytesIO()
+            # @todo: parameters
+            le_can = canvas.Canvas(packet, pagesize=A4)
+            le_can.setFont('Helvetica', 10)
+            le_can.drawString(200, 20, "{0}".format(start))
+            le_can.save()
+
+            # move to the beginning of the buffer
+            packet.seek(0)
+            new_pdf = PdfFileReader(packet)
+
+            logger().debug("%s cur=%d [%d-%d]" % (annexe['path'], increment, start, end))
+            page = existing_pdf.getPage(increment)
+            page.mergePage(new_pdf.getPage(0))
+            output.addPage(page)
+            increment += 1
+            start += 1
+
+        # finally, write "output" to a real file
+        new_annexe_name = tempfile.NamedTemporaryFile(suffix='.pdf', delete=False).name
+        output_stream = open(new_annexe_name, 'wb')
+        output.write(output_stream)
+        output_stream.close()
+        logger().debug("%s to %s" % (annexe['path'], new_annexe_name))
+        annexe['path'] = new_annexe_name
+
+
+def check_patterns(data: dict) -> None:
+    """
+    Check that all patterns are unique or raise an exception.
+    """
+    found = {}
+    for pdf in data['annexes']:
+        if pdf['pattern'] in found:
+            msgstr = 'Non unique pattern "%s" found for file "%s" (already found for file "%s")'
+            raise InvalidUsage(msgstr % (pdf['pattern'], pdf['name'], found[pdf['pattern']]))
+        found[pdf['pattern']] = pdf['name']
+
+
+# @todo: temp_dir parameter, return file path
+def skittles(data: dict, outputfile: str) -> None:
+    logger().debug("skittles starts")
+
+    check_patterns(data)
+
+    set_annexes_length(data['annexes'])
+    # logger().debug('************** %s' % data)
+    data['annexes'] = set_annexe_pattern_pages(data['annexes'], data['general']['path'])
+    # logger().debug('************** %s' % data)
+
+    try:
+        with_page_numbering = data["options"]["with_annexes_pages_numbered"]
+    except KeyError:
+        with_page_numbering = False
+
+    if with_page_numbering:
+        add_page_numbering_to_annexes(data)
+    else:
+        logger().debug("skip add_page_numbering_to_annexes")
+    replace_annexes(data, outputfile)
+
+    logger().debug("file created: %s" % outputfile)

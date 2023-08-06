930082f9aca279e7dff2f80f5e02b6d398b4639d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:47 +0000

    Add package cloudshell-shell-automation-tests/0.14.2/cloudshell_shell_automation_tests-0.14.2-py3-none-any.whl

---
+++ packages/cloudshell-shell-automation-tests/cloudshell_shell_automation_tests-0.14.2-py3-none-any.whl/shell_tests/errors.py
@@ -0,0 +1,18 @@
+class BaseAutomationException(Exception):
+    """Base Exception."""
+
+
+class ResourceIsNotAliveError(BaseAutomationException):
+    """Resource that needed for tests is not alive."""
+
+
+class CSIsNotAliveError(ResourceIsNotAliveError):
+    """Can't connect to CS."""
+
+
+class DeploymentResourceNotFoundError(BaseAutomationException):
+    """Could not find a deployment resource."""
+
+
+class CreationReservationError(BaseAutomationException):
+    """Error with creating a reservation."""

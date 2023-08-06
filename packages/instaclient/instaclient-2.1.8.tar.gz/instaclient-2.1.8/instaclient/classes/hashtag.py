5eaf33a9ce7d9df2ac94102464d5e788bb3940f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:06 +0000

    Add package instaclient/2.1.8/instaclient-2.1.8.tar.gz

---
+++ packages/instaclient/instaclient-2.1.8.tar.gz/instaclient-2.1.8/instaclient/classes/hashtag.py
@@ -0,0 +1,20 @@
+from instaclient.errors.common import InvalidInstaRequestError
+from instaclient.client.urls import GraphUrls
+from instaclient.classes import InstaBaseObject, BasePost, BaseProfile
+import requests
+
+class Hashtag(InstaBaseObject):
+    def __init__(self, id:str, viewer:int or BaseProfile, name:str, count:int, posts_data:str or dict, loaded_posts:list=None):
+        super().__init__(id, viewer, InstaBaseObject.GRAPH_HASHTAG)
+        self.name = name
+        self.count = count
+        self.posts_data = posts_data
+        self.loaded_posts = loaded_posts
+
+    def __repr__(self) -> str:
+        return f'Hashtag<{self.name}: {self.count}>'
+
+    """ def load_posts(self, count:int, types:list=None):
+        print() """
+
+

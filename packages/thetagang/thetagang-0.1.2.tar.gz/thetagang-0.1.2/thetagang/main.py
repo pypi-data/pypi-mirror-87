b3bdbc60e01f0596605129ab7ce7051c361dd2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:39 +0000

    Add package thetagang/0.1.2/thetagang-0.1.2.tar.gz

---
+++ packages/thetagang/thetagang-0.1.2.tar.gz/thetagang-0.1.2/thetagang/main.py
@@ -0,0 +1,30 @@
+import logging
+
+import click
+import click_log
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
+
+
+CONTEXT_SETTINGS = dict(
+    help_option_names=["-h", "--help"], auto_envvar_prefix="THETAGANG"
+)
+
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click_log.simple_verbosity_option(logger)
+@click.option(
+    "--config", help="Path to toml config", required=True, default="thetagang.toml"
+)
+def cli(config):
+    """ThetaGang is an IBKR bot for collecting money.
+
+    You can configure this tool by supplying a toml configuration file.
+    There's a sample config on GitHub, here:
+    https://github.com/brndnmtthws/thetagang/blob/main/thetagang.toml
+    """
+
+    from .thetagang import start
+
+    start(config)

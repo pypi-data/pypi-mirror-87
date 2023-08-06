7befd28daf31fb5af5eb95fc084dd13ac02b799c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:34 +0000

    Add package cirq/0.10.0.dev20201203210947/cirq-0.10.0.dev20201203210947-py3-none-any.whl

---
+++ packages/cirq/cirq-0.10.0.dev20201203210947-py3-none-any.whl/cirq/work/__init__.py
@@ -0,0 +1,21 @@
+from cirq.work.collector import (
+    CircuitSampleJob,
+    Collector,
+)
+from cirq.work.pauli_sum_collector import (
+    PauliSumCollector,
+)
+from cirq.work.observable_settings import (
+    InitObsSetting,
+    _MeasurementSpec,
+    observables_to_settings,
+)
+from cirq.work.observable_grouping import (
+    group_settings_greedy,
+)
+from cirq.work.sampler import (
+    Sampler,
+)
+from cirq.work.zeros_sampler import (
+    ZerosSampler,
+)

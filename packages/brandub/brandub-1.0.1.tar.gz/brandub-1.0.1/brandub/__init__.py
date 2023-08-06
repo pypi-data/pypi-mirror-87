c415bbad5ce2c53f361d903e2261d6e54f337531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:23 +0000

    Add package brandub/1.0.1/brandub-1.0.1.tar.gz

---
+++ packages/brandub/brandub-1.0.1.tar.gz/brandub-1.0.1/brandub/__init__.py
@@ -0,0 +1,5 @@
+from .board import Board
+from .brandubgame import BrandubGame
+from .gamestate import GameState
+from .gamehistory import GameHistory
+from .rawboard import RawBoard
---
+++ packages/brandub/brandub-1.0.1.tar.gz/brandub-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+# make the README into the long description
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="brandub",
+    version="1.0.1",
+    description="play brandub",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/kevinnowland/brandub",
+    author="Kevin Nowland",
+    license="MIT",
+    packages=["brandub"],
+    install_requires = [
+        'ipython',
+        'numpy',
+    ],
+    scripts=['bin/brandub'],
+    zip_safe=False
+)

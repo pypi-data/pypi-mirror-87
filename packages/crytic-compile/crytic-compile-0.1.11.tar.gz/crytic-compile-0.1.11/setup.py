1027cee28d3d220acb8794621cc06e5d9b9330e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:09 +0000

    Add package crytic-compile/0.1.11/crytic-compile-0.1.11.tar.gz

---
+++ packages/crytic-compile/crytic-compile-0.1.11.tar.gz/crytic-compile-0.1.11/setup.py
@@ -0,0 +1,19 @@
+"""
+CryticCompile package installation
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="crytic-compile",
+    description="Util to facilitate smart contracts compilation.",
+    url="https://github.com/crytic/crytic-compile",
+    author="Trail of Bits",
+    version="0.1.11",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["pysha3>=1.0.2"],
+    license="AGPL-3.0",
+    long_description=open("README.md", encoding="utf8").read(),
+    package_data={"crytic_compile": ["py.typed"]},
+    entry_points={"console_scripts": ["crytic-compile = crytic_compile.__main__:main"]},
+)

9381e79662fd5f1debfe7728a76a4a48f79c7735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:10 +0000

    Add package bro-pkg/2.5.0/bro_pkg-2.5.0-py2.py3-none-any.whl

---
+++ packages/bro-pkg/bro_pkg-2.5.0-py2.py3-none-any.whl/bropkg/_util.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import sys
+
+from zeekpkg._util import *
+
+print("Warning: the 'bropkg._util' module is deprecated use 'zeekpkg._util' instead.",
+      file=sys.stderr)
---
+++ packages/bro-pkg/bro_pkg-2.5.0-py2.py3-none-any.whl/bropkg/source.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+import sys
+
+from zeekpkg.source import *
+
+print("Warning: the 'bropkg.source' module is deprecated use 'zeekpkg.source' instead.",
+      file=sys.stderr)

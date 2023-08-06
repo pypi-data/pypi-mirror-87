aa336c0ee6c53a87a11c8a48ba09604c9177c949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:23 +0000

    Add package transmitter/0.2.8/transmitter-0.2.8.tar.gz

---
+++ packages/transmitter/transmitter-0.2.8.tar.gz/transmitter-0.2.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='transmitter',
+    version='0.2.8',
+    packages=[
+        'transmitter',
+        'transmitter.pricefeed',
+        'transmitter.pricefeed.adapters',
+    ],
+    url='http://github.com/emre/transmitter',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='STEEM Witness updates made easy.',
+    entry_points={
+        'console_scripts': [
+            'transmitter = transmitter.main:main',
+        ],
+    },
+    install_requires=["beem==0.24.19", "requests", "numpy"]
+)

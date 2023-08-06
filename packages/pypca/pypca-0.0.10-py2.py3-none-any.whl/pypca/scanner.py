2e1d198a116888b7bf0850ae80fe1e328218252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:31 +0000

    Add package pypca/0.0.10/pypca-0.0.10-py2.py3-none-any.whl

---
+++ packages/pypca/pypca-0.0.10-py2.py3-none-any.whl/pypca/scanner.py
@@ -0,0 +1,18 @@
+
+# import serial
+
+# timeout = 10
+# port = '/dev/cu.usbserial-1440'
+# devices = {}
+# port = port
+# baud = 57600
+# timeout = timeout
+# _serial = serial.Serial(timeout=timeout)
+# _serial.port = port
+# _serial.baudrate = baud
+# _serial.timeout = timeout
+# _serial.open()
+# _serial.flushInput()
+# _serial.flushOutput()
+
+# for i in range(0,999999999):

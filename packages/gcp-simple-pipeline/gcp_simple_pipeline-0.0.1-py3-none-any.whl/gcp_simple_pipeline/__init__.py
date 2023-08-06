afbed3ef3270f10fe3d505ae31e8c89690797993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package gcp-simple-pipeline/0.0.1/gcp_simple_pipeline-0.0.1-py3-none-any.whl

---
+++ packages/gcp-simple-pipeline/gcp_simple_pipeline-0.0.1-py3-none-any.whl/gcp_simple_pipeline/__init__.py
@@ -0,0 +1,17 @@
+"""GCP simple pipeline
+
+This package contains the modules for deploying a simple pipeline on GCP.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the CI/CD.
+
+Package contents ..
+
+    >>> import gcp_simple_pipeline
+    >>> help(gcp_simple_pipeline)
+
+# license MIT
+# support https://github.com/bilardi/gcp-simple-pipeline/issues
+"""
+__version__ = '0.0.1'
+__author__ = 'Alessandra Bilardi'

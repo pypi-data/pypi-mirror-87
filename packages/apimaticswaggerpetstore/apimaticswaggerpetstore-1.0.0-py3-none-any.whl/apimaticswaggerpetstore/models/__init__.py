2d78795b88664cedec75c92e6c47f89cfa5bcee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:02 +0000

    Add package apimaticswaggerpetstore/1.0.0/apimaticswaggerpetstore-1.0.0-py3-none-any.whl

---
+++ packages/apimaticswaggerpetstore/apimaticswaggerpetstore-1.0.0-py3-none-any.whl/apimaticswaggerpetstore/models/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'api_response',
+    'category',
+    'pet',
+    'tag',
+    'order',
+    'user',
+    'status_enum',
+    'status_1_enum',
+    'status_2_enum',
+    'o_auth_scope_enum',
+]

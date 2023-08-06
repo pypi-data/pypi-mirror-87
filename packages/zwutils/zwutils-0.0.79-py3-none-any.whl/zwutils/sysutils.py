84c656218f5f09771baaa8ef7045d8fa0ea4b3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:56 +0000

    Add package zwutils/0.0.79/zwutils-0.0.79-py3-none-any.whl

---
+++ packages/zwutils/zwutils-0.0.79-py3-none-any.whl/zwutils/sysutils.py
@@ -0,0 +1,39 @@
+import os
+import re
+import psutil
+import subprocess
+from .fileutils import writefile
+
+def write_pidfile(dir='.'):
+    pid = os.getpid()
+    pth = os.path.join(dir, '%s.pid'%pid)
+    writefile(pth, pid)
+    return pth
+
+def pids_by_name(nm=None):
+    rtn = []
+    for proc in psutil.process_iter(['pid', 'name']):
+        pinfo = proc.info
+        pname = pinfo['name']
+        if nm:
+            _regex = re.compile(nm)
+            iscontained = bool(_regex.search(pname))
+            if iscontained:
+                rtn.append(pinfo)
+                continue
+        else:
+            rtn.append(pinfo)
+    rtn = sorted(rtn, key=lambda o: o['name'])
+    return rtn
+
+def run_shell(cmd, *args):
+    cmds = [cmd]
+    cmds.extend(list(args))
+    r = subprocess.run(cmds, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return r
+
+def get_sys_usage():
+    return {
+        'mem': psutil.virtual_memory().percent,
+        'cpu': psutil.cpu_percent(None),
+    }

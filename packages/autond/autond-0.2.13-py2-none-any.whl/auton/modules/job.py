017dc117178189d5ba0b495019b9b7d3d0d3fca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:34 +0000

    Add package autond/0.2.13/autond-0.2.13-py2-none-any.whl

---
+++ packages/autond/autond-0.2.13-py2-none-any.whl/auton/modules/job.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2019 fjord-technologies
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""auton.modules.job"""
+
+import copy
+import logging
+import re
+
+from dwho.classes.modules import DWhoModuleBase, MODULES
+from httpdis.ext.httpdis_json import HttpReqErrJson
+from sonicprobe.libs import xys
+from sonicprobe.libs.moresynchro import RWLock
+
+# pylint: disable=unused-import
+from auton.classes.plugins import (AutonEPTObject,
+                                   EPTS_SYNC,
+                                   STATUS_NEW,
+                                   STATUS_PROCESSING,
+                                   STATUS_COMPLETE)
+
+LOG = logging.getLogger('auton.modules.job')
+xys.add_regex('job.envname', re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]{0,63}$').match)
+
+
+class JobModule(DWhoModuleBase):
+    MODULE_NAME = 'job'
+
+    LOCK        = RWLock()
+
+    # pylint: disable=attribute-defined-outside-init
+    def safe_init(self, options):
+        self.objs         = {}
+        self.lock_timeout = self.config['general']['lock_timeout']
+
+    @staticmethod
+    def _get_ept_sync(endpoint):
+        if endpoint not in EPTS_SYNC:
+            raise HttpReqErrJson(404, "unable to find endpoint: %r" % endpoint)
+
+        return EPTS_SYNC[endpoint]
+
+    @staticmethod
+    def _get_uid(endpoint, xid):
+        return "%s:%s" % (endpoint, xid)
+
+    def _get_obj(self, endpoint, xid):
+        uid      = self._get_uid(endpoint, xid)
+
+        if uid not in self.objs:
+            raise HttpReqErrJson(404, "unable to find object with uid: %r" % uid)
+
+        return self.objs[uid]
+
+    def _clear_obj(self, endpoint, xid):
+        uid = self._get_uid(endpoint, xid)
+
+        if uid in self.objs:
+            del self.objs[uid]
+
+    def _push_epts_sync(self, endpoint, xid, method, request):
+        ept_sync = self._get_ept_sync(endpoint)
+        uid      = self._get_uid(endpoint, xid)
+
+        if uid in self.objs:
+            raise HttpReqErrJson(415, "uid already exists: %r" % uid)
+
+        obj      = AutonEPTObject(ept_sync.name,
+                                  uid,
+                                  endpoint,
+                                  method,
+                                  request)
+        self.objs[uid] = obj
+        ept_sync.qput(obj)
+
+        return obj
+
+    @staticmethod
+    def _build_result(obj):
+        r = {'code':        200,
+             'uid':         obj.get_uid(),
+             'status':      obj.get_status(),
+             'return_code': obj.get_return_code(),
+             'started_at':  obj.get_started_at(),
+             'stream':      obj.get_last_result(),
+             'ended_at':    obj.get_ended_at()}
+
+        if obj.has_error():
+            r['code']   = 400
+            r['errors'] = obj.get_errors()
+
+        return r
+
+    RUN_QSCHEMA = xys.load("""
+    endpoint: !!str
+    id: !!str
+    """)
+
+    RUN_PSCHEMA = xys.load("""
+    env*:
+      !~~regex? (0,64) job.envname: !!str
+    envfiles*: !~~seqlen(0,64) [ !!str ]
+    args*: !~~seqlen(0,64) [ !!str ]
+    argfiles*: !~~seqlen(0,64)
+      - arg: !!str
+        content: !!str
+        filename: !!str
+    """)
+
+    def job_run(self, request):
+        params  = request.query_params() or {}
+        payload = request.payload_params() or {}
+
+        if not isinstance(params, dict):
+            raise HttpReqErrJson(400, "invalid arguments type")
+
+        if not xys.validate(params, self.RUN_QSCHEMA):
+            raise HttpReqErrJson(415, "invalid arguments for command")
+
+        if not isinstance(payload, dict):
+            raise HttpReqErrJson(400, "invalid arguments type")
+
+        if not xys.validate(payload, self.RUN_PSCHEMA):
+            raise HttpReqErrJson(415, "invalid arguments for command")
+
+        if not self.LOCK.acquire_write(self.lock_timeout):
+            raise HttpReqErrJson(503, "unable to take LOCK for writing after %s seconds" % self.lock_timeout)
+
+        try:
+            obj = self._push_epts_sync(params['endpoint'],
+                                       params['id'],
+                                       'run',
+                                       copy.copy(request))
+
+            return self._build_result(obj)
+        except HttpReqErrJson:
+            raise
+        except Exception as e:
+            LOG.exception(e)
+            raise HttpReqErrJson(503, repr(e))
+        finally:
+            self.LOCK.release()
+
+
+    STATUS_QSCHEMA = xys.load("""
+    endpoint: !!str
+    id: !!str
+    """)
+
+    def job_status(self, request):
+        params = request.query_params()
+
+        if not isinstance(params, dict):
+            raise HttpReqErrJson(400, "invalid arguments type")
+
+        if not xys.validate(params, self.STATUS_QSCHEMA):
+            raise HttpReqErrJson(415, "invalid arguments for command")
+
+        obj = self._get_obj(params['endpoint'], params['id'])
+
+        if not self.LOCK.acquire_read(self.lock_timeout):
+            raise HttpReqErrJson(503, "unable to take LOCK for reading after %s seconds" % self.lock_timeout)
+
+        try:
+            return self._build_result(obj)
+        except HttpReqErrJson:
+            raise
+        except Exception as e:
+            LOG.exception(e)
+            raise HttpReqErrJson(503, repr(e))
+        finally:
+            if obj.get_status() == STATUS_COMPLETE:
+                self._clear_obj(params['endpoint'], params['id'])
+            self.LOCK.release()
+
+
+if __name__ != "__main__":
+    def _start():
+        MODULES.register(JobModule())
+    _start()
---
+++ packages/autond/autond-0.2.13-py2-none-any.whl/auton/plugins/subproc.py
@@ -0,0 +1,348 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018-2019 fjord-technologies
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""auton.plugins.subproc"""
+
+import copy
+import logging
+import os
+import shutil
+import subprocess
+import threading
+import time
+import tempfile
+
+import six
+try:
+    from six.moves import cStringIO as StringIO
+except ImportError:
+    from six import StringIO
+
+from dotenv.main import dotenv_values
+
+from sonicprobe import helpers
+from auton.classes.exceptions import (AutonConfigurationError,
+                                      AutonTargetFailed,
+                                      AutonTargetTimeout)
+from auton.classes.plugins import AutonPlugBase, PLUGINS
+
+LOG = logging.getLogger('auton.plugins.subproc')
+
+
+class AutonSubProcPlugin(AutonPlugBase):
+    PLUGIN_NAME = 'subproc'
+
+    def __init__(self, name):
+        AutonPlugBase.__init__(self, name)
+        self._dirs_to_delete = []
+        self._killed = False
+
+    def at_stop(self):
+        self._killed = True
+
+    def _proc_stdout(self, obj, proc, texit):
+        stopped = False
+        while not self._killed and not stopped:
+            try:
+                for x in iter(proc.stdout.readline, b''):
+                    if x != '':
+                        obj.add_result(x)
+            except Exception as e:
+                obj.add_error(repr(e))
+                LOG.exception(e)
+                break
+            finally:
+                if texit.is_set():
+                    stopped = True
+
+    def _proc_stderr(self, obj, proc, texit):
+        stopped = False
+        while not self._killed and not stopped:
+            try:
+                for x in iter(proc.stderr.readline, b''):
+                    if x != '':
+                        obj.add_error(x)
+            except Exception as e:
+                obj.add_error(repr(e))
+                LOG.exception(e)
+                break
+            finally:
+                if texit.is_set():
+                    stopped = True
+
+    def _mk_args(self, args, cargs, pargs, ovars):
+        r = copy.copy(args)
+
+        if cargs:
+            if not isinstance(cargs, list):
+                LOG.error("invalid configuration args for target: %r", self.target.name)
+                return None
+
+            for x in cargs:
+                if not isinstance(x, six.string_types):
+                    LOG.error("invalid configuration argument %r for target: %r", x, self.target.name)
+                    return None
+
+                if '{' in x and '}' in x:
+                    x = x.format(**ovars)
+                r.append(x)
+
+        if pargs:
+            if not isinstance(pargs, list):
+                LOG.error("invalid payload args for target: %r", self.target.name)
+                return None
+
+            for x in pargs:
+                if not isinstance(x, six.string_types):
+                    LOG.error("invalid payload argument %r for target: %r", x, self.target.name)
+                    return None
+
+                if '{' in x and '}' in x:
+                    x = x.format(**ovars)
+                r.append(x)
+
+        return r
+
+    def _mk_argfiles(self, args, cargfiles, pargfiles):
+        r = copy.copy(args)
+
+        if cargfiles:
+            if not isinstance(cargfiles, list):
+                LOG.error("invalid configuration argfiles for target: %r", self.target.name)
+                return None
+
+            for cargfile in cargfiles:
+                if not isinstance(cargfile, dict):
+                    LOG.error("invalid type in configuration argfiles for target: %r", self.target.name)
+                    return None
+
+                if not cargfile.get('arg'):
+                    LOG.error("missing arg in configuration argfiles for target: %r", self.target.name)
+                    return None
+
+                if not cargfile.get('filepath'):
+                    LOG.error("missing filepath in configuration argfiles for target: %r", self.target.name)
+                    return None
+
+                if not os.path.isfile(cargfile['filepath']):
+                    LOG.error("invalid filepath in configuration argfiles for target: %r", self.target.name)
+                    return None
+
+                if cargfile['arg'].endswith('@'):
+                    if len(cargfile['arg']) == 1:
+                        LOG.error("invalid arg %r in configuration argfiles for target: %r",
+                                  cargfile['arg'],
+                                  self.target.name)
+                        return None
+                    r.extend([cargfile['arg'][:-1], "@%s" % cargfile['filepath']])
+                else:
+                    r.extend([cargfile['arg'], cargfile['filepath']])
+
+        if pargfiles:
+            if not isinstance(pargfiles, list):
+                LOG.error("invalid payload argfiles for target: %r", self.target.name)
+                return None
+
+            tmpdir = tempfile.mkdtemp(prefix = '.auton.')
+            self._dirs_to_delete.append(tmpdir)
+
+            for pargfile in pargfiles:
+                if pargfile['filename'] == '':
+                    with tempfile.NamedTemporaryFile(dir = tmpdir, delete = False) as tmpfile:
+                        tmpfile.close()
+                    filepath = tmpfile.name
+                else:
+                    filepath = os.path.join(tmpdir, pargfile['filename'])
+                helpers.base64_decode_file(StringIO(pargfile['content']),
+                                           filepath)
+                if pargfile['arg'].endswith('@'):
+                    if len(pargfile['arg']) == 1:
+                        LOG.error("invalid arg %r in payload argfiles for target: %r",
+                                  pargfile['arg'],
+                                  self.target.name)
+                        return None
+                    r.extend([pargfile['arg'][:-1], "@%s" % filepath])
+                else:
+                    r.extend([pargfile['arg'], filepath])
+
+        return r
+
+    def _load_envfile(self, envfiles):
+        r = {}
+
+        if not isinstance(envfiles, list):
+            LOG.error("invalid payload envfiles for target: %r", self.target.name)
+            return r
+
+        for envfile in envfiles:
+            try:
+                r.update(dotenv_values(envfile))
+            except Exception as e:
+                LOG.warning("unable to load envfile: %r, error: %r", envfile, e)
+
+        return r
+
+    def _mk_env(self, cenvfiles, penvfiles, cenv, penv, ovars):
+        r   = {}
+        env = []
+
+        if penvfiles:
+            if not isinstance(penvfiles, list):
+                LOG.warning("invalid payload envfiles for target: %r", self.target.name)
+                return r
+
+            for key, val in six.iteritems(self._load_envfile(penvfiles)):
+                env.append({key: val})
+
+        if cenvfiles:
+            if not isinstance(cenvfiles, list):
+                LOG.warning("invalid configuration envfiles for target: %r", self.target.name)
+                return r
+
+            for key, val in six.iteritems(self._load_envfile(cenvfiles)):
+                env.append({key: val})
+
+        if cenv:
+            if isinstance(cenv, dict):
+                for key, val in six.iteritems(cenv):
+                    env.append({key: val})
+            elif not isinstance(cenv, list):
+                LOG.warning("invalid configuration env for target: %r", self.target.name)
+                return r
+            else:
+                env.extend(cenv)
+
+        if penv:
+            if not isinstance(penv, dict):
+                LOG.warning("invalid payload env for target: %r", self.target.name)
+                return r
+
+            r = penv.copy()
+
+        self._build_params_dict('env', env, penv, ovars, r)
+
+        return r
+
+    def safe_init(self):
+        AutonPlugBase.safe_init(self)
+
+        if not self.target.config.get('prog'):
+            raise AutonConfigurationError("missing prog keyword for target: %r" % self.target.name)
+
+    def do_run(self, obj):
+        cfg       = self.target.config
+        payload   = obj.get_request().payload_params()
+        ovars     = obj.get_vars()
+        pargs     = None
+        pargfiles = None
+        args      = [cfg['prog']]
+        penvfiles = []
+        penv      = {}
+
+        if isinstance(payload, dict) and payload.get('args'):
+            if cfg.get('disallow-args'):
+                LOG.warning("args from payload isn't allowed for target: %r", self.target.name)
+            else:
+                pargs = copy.copy(payload['args'])
+
+        args    = self._mk_args(args, cfg.get('args'), pargs, ovars)
+        if not args:
+            raise AutonTargetFailed("invalid args for command on target: %r" % self.target.name)
+
+        if isinstance(payload, dict) and payload.get('argfiles'):
+            if cfg.get('disallow-argfiles'):
+                LOG.warning("argfiles from payload isn't allowed for target: %r", self.target.name)
+            else:
+                pargfiles = copy.copy(payload['argfiles'])
+
+        args    = self._mk_argfiles(args, cfg.get('argfiles'), pargfiles)
+        if not args:
+            raise AutonTargetFailed("invalid argfiles for command on target: %r" % self.target.name)
+
+        if isinstance(payload, dict) and payload.get('envfiles'):
+            if cfg.get('disallow-envfiles'):
+                LOG.warning("envfile from payload isn't allowed for target: %r", self.target.name)
+            else:
+                penvfiles = payload['envfiles']
+
+        if isinstance(payload, dict) and payload.get('env'):
+            if cfg.get('disallow-env'):
+                LOG.warning("env from payload isn't allowed for target: %r", self.target.name)
+            else:
+                penv.update(copy.copy(payload['env']))
+
+        env     = self._mk_env(cfg.get('envfiles'), penvfiles, cfg.get('env'), penv, ovars)
+        if not env:
+            env = {}
+
+        if cfg.get('search_paths'):
+            if not isinstance(cfg['search_paths'], list):
+                LOG.warning("invalid search_paths for target: %r", self.target.name)
+            else:
+                env['PATH'] = os.path.pathsep.join(cfg['search_paths'])
+
+        env     = self._set_default_env(env, ovars)
+
+        bargs   = self._get_become(cfg.get('become'))
+
+        texit   = threading.Event()
+        proc    = None
+
+        LOG.debug("cmd line: %r", bargs + args)
+
+        try:
+            proc  = subprocess.Popen(bargs + args,
+                                     stdout = subprocess.PIPE,
+                                     stderr = subprocess.PIPE,
+                                     env    = env,
+                                     cwd    = cfg.get('workdir'))
+
+            to    = threading.Thread(target=self._proc_stdout,
+                                     args=(obj, proc, texit))
+            to.daemon = True
+            to.start()
+
+            te    = threading.Thread(target=self._proc_stderr,
+                                     args=(obj, proc, texit))
+            te.daemon = True
+            te.start()
+
+            start = time.time()
+
+            while True:
+                if proc.poll() is not None:
+                    break
+
+                if start + cfg['timeout'] <= time.time():
+                    raise AutonTargetTimeout("timeout on target: %r" % self.target.name)
+
+            if proc.returncode:
+                raise subprocess.CalledProcessError(proc.returncode, args[0])
+        except (AutonTargetFailed, AutonTargetTimeout):
+            raise
+        except subprocess.CalledProcessError as e:
+            raise AutonTargetFailed("error on target: %r. exception: %s"
+                                    % (self.target.name, e), code = e.returncode)
+        except Exception as e:
+            raise AutonTargetFailed("error on target: %r. exception: %r"
+                                    % (self.target.name, e))
+        finally:
+            texit.set()
+
+        try:
+            if proc and proc.returncode is None:
+                proc.terminate()
+        except OSError:
+            pass
+
+    def do_terminate(self):
+        while self._dirs_to_delete:
+            tdir = self._dirs_to_delete.pop()
+            if os.path.isdir(tdir):
+                shutil.rmtree(tdir, True)
+
+
+if __name__ != "__main__":
+    def _start():
+        PLUGINS.register(AutonSubProcPlugin)
+    _start()

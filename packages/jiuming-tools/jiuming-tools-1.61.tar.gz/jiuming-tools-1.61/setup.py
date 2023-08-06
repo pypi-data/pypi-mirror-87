edc5a70d682957ac9bb549edd775a5521a4fa1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:36 +0000

    Add package jiuming-tools/1.61/jiuming-tools-1.61.tar.gz

---
+++ packages/jiuming-tools/jiuming-tools-1.61.tar.gz/jiuming-tools-1.61/setup.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+from src.config import global_config
+
+setup(
+    name='jiuming-tools',
+    # 版本 如果需要发布更新 需要调整版本号
+    version=global_config.version,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='linux',
+    # 需要的依赖
+    install_requires=[
+        'Click==7.0',
+        'psutil==5.6.7',
+        'PyYAML==5.2',
+        'gitpython==3.1.1',
+        'bcrypt==3.1.7',
+        'speedtest-cli',
+        'pexpect',
+        'scapy'
+    ],
+    # 命令行入口
+    entry_points='''
+        [console_scripts]
+        m=src.ming:cli
+    ''',
+    author='ming',
+    license='MIT',
+    url='https://github.com/xujiuming/jiuming-tools',
+    description='个人常用功能'
+)

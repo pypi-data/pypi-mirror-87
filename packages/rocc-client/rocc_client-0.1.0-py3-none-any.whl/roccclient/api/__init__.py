3199b9ae0c68f5a24c9402d264119e64bf32d583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package rocc-client/0.1.0/rocc_client-0.1.0-py3-none-any.whl

---
+++ packages/rocc-client/rocc_client-0.1.0-py3-none-any.whl/roccclient/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from roccclient.api.challenge_api import ChallengeApi
+from roccclient.api.user_api import UserApi

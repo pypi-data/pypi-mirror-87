7c07fe4d42131f722f90c0ce7cd55c52a34d2d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:15 +0000

    Add package nornir-pyez/0.0.7/nornir_pyez-0.0.7-py3-none-any.whl

---
+++ packages/nornir-pyez/nornir_pyez-0.0.7-py3-none-any.whl/nornir_pyez/plugins/connections/__init__.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict, Optional
+
+from jnpr.junos import Device
+
+from nornir.core.configuration import Config
+
+CONNECTION_NAME = "pyez"
+
+
+class Pyez:
+    def open(
+        self,
+        hostname: Optional[str],
+        username: Optional[str],
+        password: Optional[str],
+        port: Optional[int],
+        platform: Optional[str],
+        extras: Optional[Dict[str, Any]] = None,
+        configuration: Optional[Config] = None,
+    ) -> None:
+        extras = extras or {}
+        if not port:
+            port = 830
+        parameters: Dict[str, Any] = {
+            "host": hostname,
+            "user": username,
+            "password": password,
+            "port": port,
+            "optional_args": {},
+        }
+
+        connection = Device(**parameters)
+
+        connection.open()
+        self.connection = connection
+
+    def close(self) -> None:
+        self.connection.close()

5fd5b523033a26eaf1ad52a8f29abea2c2089bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package graviti/0.3.4/graviti-0.3.4-py3-none-any.whl

---
+++ packages/graviti/graviti-0.3.4-py3-none-any.whl/graviti/opendataset/Flower17/Flower17.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020 Graviti. All Rights Reserved.
+#
+# pylint: disable=invalid-name
+
+"""This file define the 17 Category Flower Dataloader"""
+
+import os
+
+from ...dataset import Data, Dataset
+from ...label import Classification, LabelType
+from .._utility import glob
+
+DATASET_NAME = "Flower17"
+CATEFORIES = [
+    "Daffodil",
+    "Snowdrop",
+    "Lily Valley",
+    "Bluebell",
+    "Crocus",
+    "Iris",
+    "Tigerlily",
+    "Tulip",
+    "Fritillary",
+    "Sunflower",
+    "Daisy",
+    "Colts Foot",
+    "Dandelalion",
+    "Cowslip",
+    "Buttercup",
+    "Windflower",
+    "Pansy",
+]
+
+
+def Flower17(path: str) -> Dataset:
+    """Load the 17 Category Flower Dataset to TensorBay
+
+    :param path: the root directory of the dataset
+    The file structure should be like:
+    <path>
+        jpg/
+            image_0001.jpg
+            ...
+
+    :return: a loaded dataset
+    """
+    root_path = os.path.abspath(os.path.expanduser(path))
+
+    image_paths = glob(os.path.join(root_path, "jpg", "*.jpg"))
+
+    dataset = Dataset(DATASET_NAME)
+    dataset.load_label_tables(os.path.join(os.path.dirname(__file__), "labeltables.json"))
+    segment = dataset.create_segment()
+
+    for image_path in image_paths:
+        data = Data(os.path.join(root_path, image_path))
+        data.register_label(LabelType.CLASSIFICATION)
+
+        # There are 80 images for each category
+        index = int(image_path[6:11]) - 1
+        data.append_label(Classification(category=CATEFORIES[index // 80]))
+        segment.append(data)
+
+    return dataset

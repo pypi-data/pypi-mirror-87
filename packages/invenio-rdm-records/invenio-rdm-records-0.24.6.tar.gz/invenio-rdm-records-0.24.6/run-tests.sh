c367bdb7a19943b554049393953fa7b7863fd052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:58 +0000

    Add package invenio-rdm-records/0.24.6/invenio-rdm-records-0.24.6.tar.gz

---
+++ packages/invenio-rdm-records/invenio-rdm-records-0.24.6.tar.gz/invenio-rdm-records-0.24.6/run-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019-2020 CERN.
+# Copyright (C) 2019-2020 Northwestern University.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+docker-services-cli --verbose up es postgresql redis
+python -m pytest
+tests_exit_code=$?
+docker-services-cli down
+exit "$tests_exit_code"

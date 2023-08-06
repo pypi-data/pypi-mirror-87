cf14009183d3ba78dc2b47fe61460bbf8e68b36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:59 +0000

    Add package mygraphtools/1.2.0/mygraphtools-1.2.0.tar.gz

---
+++ packages/mygraphtools/mygraphtools-1.2.0.tar.gz/mygraphtools-1.2.0/mygraphtools/__init__.py
@@ -0,0 +1,1 @@
+from .mygraphtools import Graph
---
+++ packages/mygraphtools/mygraphtools-1.2.0.tar.gz/mygraphtools-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='mygraphtools',
+    version='1.2.0',
+    author='Gabriel Souto',
+    author_email='ga.vs_@outlook.com',
+    packages=['mygraphtools'],
+    description='Uma biblioteca para trabalhar com grafos de maneira simples',
+    license='MIT',
+    keywords='grafos graphs',
+    url='https://github.com/gabrielsoutoabreu/mygraphtools',
+    project_urls={
+        'Código fonte': 'https://github.com/gabrielsoutoabreu/mygraphtools',
+        'Download': 'https://github.com/gabrielsoutoabreu/mygraphtools/archive/main.zip'
+    }
+)

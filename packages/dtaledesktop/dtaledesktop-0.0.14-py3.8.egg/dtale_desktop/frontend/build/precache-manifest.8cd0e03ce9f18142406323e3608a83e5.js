bac8d8b3d7a6985b39bd89de2026e9dc9b44933d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:45 +0000

    Add package dtaledesktop/0.0.14/dtaledesktop-0.0.14-py3.8.egg

---
+++ packages/dtaledesktop/dtaledesktop-0.0.14-py3.8.egg/dtale_desktop/frontend/build/precache-manifest.8cd0e03ce9f18142406323e3608a83e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e885f5daace793d18ef6d6cd863aab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd4a6c52708c8295fd0",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "e983baf6486b8e582978",
+    "url": "/static/css/main.ce00ed80.chunk.css"
+  },
+  {
+    "revision": "7fd4a6c52708c8295fd0",
+    "url": "/static/js/2.e81b7b11.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e81b7b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e983baf6486b8e582978",
+    "url": "/static/js/main.7c460e57.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ packages/dtaledesktop/dtaledesktop-0.0.14-py3.8.egg/dtale_desktop/frontend/build/precache-manifest.c167e02fa93bbdc363d1fd30ee13e527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825fafe9c84f8b1736f8bacfbad3c963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ef2057a9c83de4dabb",
+    "url": "/static/css/2.13b92dd1.chunk.css"
+  },
+  {
+    "revision": "8ff50ece2bd9ffea1212",
+    "url": "/static/css/main.7968f145.chunk.css"
+  },
+  {
+    "revision": "b1ef2057a9c83de4dabb",
+    "url": "/static/js/2.1e1e7ec5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1e1e7ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff50ece2bd9ffea1212",
+    "url": "/static/js/main.1191e43e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);

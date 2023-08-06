2e3842f885021f3c56a56f05a4e1766055059996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:49 +0000

    Add package data-downloader/0.2.0/data-downloader-0.2.0.tar.gz

---
+++ packages/data-downloader/data-downloader-0.2.0.tar.gz/data-downloader-0.2.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-downloader",
+    version="0.2.0",
+    author="fanchegyan",
+    author_email="fanchy14@lzu.edu.cn",
+    description="Make downloading scientific data much easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fanchengyan/data-downloader",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'httpx',
+        'tqdm',
+        'setuptools',
+        'beautifulsoup4',
+        'nest_asyncio'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

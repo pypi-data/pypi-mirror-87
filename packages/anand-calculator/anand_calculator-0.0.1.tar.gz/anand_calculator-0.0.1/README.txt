464a405ab71c13752bb91af2fc03d1f0de62869c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package anand-calculator/0.0.1/anand_calculator-0.0.1.tar.gz

---
+++ packages/anand-calculator/anand_calculator-0.0.1.tar.gz/anand_calculator-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a vert simple calullaor which performs calcularot functions
---
+++ packages/anand-calculator/anand_calculator-0.0.1.tar.gz/anand_calculator-0.0.1/calculator/__init__.py
@@ -0,0 +1,13 @@
+def add_numbers(num1,num2):
+    return num1+num2
+
+def sub_numbers(num1,num2):
+    return num1-num2
+
+def mul_numbers(num1,num2):
+    return num1*num2
+
+def div_numbers(num1,num2):
+    return num1/num2
+
+print(add_numbers(5,6))

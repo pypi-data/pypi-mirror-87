c496ae2947487399d4e2db8f75e74edb78e9690f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:43 +0000

    Add package 32blit/0.1.3/32blit-0.1.3-py3-none-any.whl

---
+++ packages/32blit/32blit-0.1.3-py3-none-any.whl/ttblit/__init__.py
@@ -0,0 +1,60 @@
+
+__version__ = '0.1.3'
+
+import argparse
+import logging
+import pathlib
+import sys
+
+from .asset import font, image, map, raw
+from .tool import cmake, flasher, metadata, packer
+
+
+def exception_handler(exception_type, exception, traceback):
+    sys.stderr.write(f"{type(exception).__name__}: {exception}\n")
+    sys.stderr.flush()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true', help='Enable exception traces')
+    parser.add_argument('-v', '--verbosity', action='count', default=0, help='Output verbosity')
+    subparsers = parser.add_subparsers(dest='command', help='Commands')
+
+    if len(sys.argv) == 2:
+        file = pathlib.Path(sys.argv[1])
+        if file.is_file() and file.exists():
+            f = flasher.Flasher(subparsers)
+            f.run_save({'file': file, 'directory': '/'})
+            return
+
+    tools = {}
+
+    tools[image.ImageAsset.command] = image.ImageAsset(subparsers)
+    tools[font.FontAsset.command] = font.FontAsset(subparsers)
+    tools[map.MapAsset.command] = map.MapAsset(subparsers)
+    tools[raw.RawAsset.command] = raw.RawAsset(subparsers)
+
+    # Add the non-asset tools
+    tools[packer.Packer.command] = packer.Packer(subparsers)
+    tools[cmake.CMake.command] = cmake.CMake(subparsers)
+    tools[flasher.Flasher.command] = flasher.Flasher(subparsers)
+    tools[metadata.Metadata.command] = metadata.Metadata(subparsers)
+
+    args = parser.parse_args()
+
+    log_format = '%(levelname)s: %(message)s'
+
+    log_verbosity = min(args.verbosity, 3)
+    log_level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG][log_verbosity]
+    logging.basicConfig(level=log_level, format=log_format)
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG, format=log_format)
+    else:
+        sys.excepthook = exception_handler
+
+    if args.command is None:
+        parser.print_help()
+    else:
+        sys.exit(tools[args.command].run(args))

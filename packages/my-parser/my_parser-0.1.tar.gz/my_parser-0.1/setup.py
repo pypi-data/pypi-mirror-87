5236b206fdb18bfbde5a49e14a9b8efdccd6b45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:18 +0000

    Add package my-parser/0.1/my_parser-0.1.tar.gz

---
+++ packages/my-parser/my_parser-0.1.tar.gz/my_parser-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='my_parser',
+	version='0.1',
+	author='aksenov',
+	description='This is module for parse some data from urls',
+	long_description=open('README.md').read(),
+	packages=find_packages()
+)

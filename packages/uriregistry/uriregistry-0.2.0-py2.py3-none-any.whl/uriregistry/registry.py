7160b8936478fccf2d116037f3d021f3f07fd50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package uriregistry/0.2.0/uriregistry-0.2.0-py2.py3-none-any.whl

---
+++ packages/uriregistry/uriregistry-0.2.0-py2.py3-none-any.whl/uriregistry/registry.py
@@ -0,0 +1,69 @@
+from zope.interface import Interface
+
+from .models import Application
+from .models import UriTemplate
+
+
+class IUriRegistry(Interface):
+    pass
+
+
+class UriRegistry:
+    """
+    Central registry that tracks uris and the applications they are being used in.
+    """
+
+    def __init__(self, applications=[], uris=[]):
+        self.applications = [Application(app['uri'], app['name'], app['service_url']) for app in applications]
+        self.uris = [
+            UriTemplate(
+                u['match_uri'],
+                [app for app in self.applications if app.uri in u['applications']]
+            ) for u in uris
+        ]
+
+    def get_applications(self, uri):
+        """
+        Get all applications that might have a reference to this URI.
+
+        :param string uri: Uri for which the applications need to be found.
+        """
+        applications = []
+        for u in self.uris:
+            if u.matches(uri):
+                applications.extend(u.applications)
+        applications = list(set(applications))
+        return applications
+
+
+def _build_uri_registry(registry, registryconfig):
+    """
+    :param pyramid.registry.Registry registry: Pyramid registry
+    :param dict registryconfig: UriRegistry config in dict form.
+    :rtype: :class:`uriregistry.registry.UriRegistry`
+    """
+    uri_registry = registry.queryUtility(IUriRegistry)
+    if uri_registry is not None:
+        return uri_registry
+
+    uri_registry = UriRegistry(
+        registryconfig['applications'],
+        registryconfig['uri_templates']
+    )
+
+    registry.registerUtility(uri_registry, IUriRegistry)
+    return registry.queryUtility(IUriRegistry)
+
+
+def get_uri_registry(registry):
+    """
+    Get the :class:`uriregistry.registry.UriRegistry` attached to this pyramid
+    application.
+
+    :rtype: :class:`uriregistry.registry.UriRegistry`
+    """
+    # Argument might be a config or request
+    regis = getattr(registry, 'registry', None)
+    if regis is None:
+        regis = registry
+    return regis.queryUtility(IUriRegistry)
---
+++ packages/uriregistry/uriregistry-0.2.0-py2.py3-none-any.whl/uriregistry/utils.py
@@ -0,0 +1,23 @@
+import logging
+import requests
+
+from pyramid_urireferencer.models import ApplicationResponse
+
+log = logging.getLogger(__name__)
+
+
+def query_application(app, uri):
+    """
+    Checks if a certain app has references to a URI.
+
+    :param uriregistry.models.Application: The application to be evaluated
+    :param uri: The uri that has to be checked
+    :rtype pyramid_urireferencer.models.ApplicationResponse:
+    """
+    try:
+        r = requests.get(app.service_url, params={'uri': uri})
+        a = ApplicationResponse.load_from_json(r.json())
+        return a
+    except Exception as e:
+        log.error(f'Could not check if uri {uri} is known to app {app.title} (uri: {app.uri})')
+        return ApplicationResponse(app.title, app.uri, app.service_url, False, None, None, None)

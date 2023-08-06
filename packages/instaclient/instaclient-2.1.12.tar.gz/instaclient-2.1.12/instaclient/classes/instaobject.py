6919578a10d7c092aa8f09621ed8f197ddf706d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:57 +0000

    Add package instaclient/2.1.12/instaclient-2.1.12.tar.gz

---
+++ packages/instaclient/instaclient-2.1.12.tar.gz/instaclient-2.1.12/instaclient/classes/instaobject.py
@@ -0,0 +1,38 @@
+import abc
+
+class InstaBaseObject(abc.ABC):
+    GRAPH_IMAGE = 'GraphImage'
+    GRAPH_VIDEO = 'GraphVideo'
+    GRAPH_SIDECAR = 'GraphSidecar'
+    GRAPH_PROFILE = 'GRAPH_PROFILE'
+    GRAPH_HASHTAG = 'GRAPH_HASHTAG'
+
+    GRAPH_FOLLOW = 'GraphFollowAggregatedStory'
+    GRAPH_LIKE = 'GraphLikeAggregatedStory'
+    GRAPTH_TAGGED = 'GraphUserTaggedStory'
+    GRAPH_MENTION = 'GraphMentionStory'
+    GRAPH_COMMENT = 'GraphCommentMediaStory'
+
+    def __init__(self, id:str, viewer, type:str):
+        self.id = id
+        self.viewer = viewer
+        self.type = type
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, InstaBaseObject):
+            if o.get_id() == self.id:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def get_id(self):
+        return self.id
+
+    def get_viewer(self):
+        return self.viewer
+
+    def get_type(self):
+        return self.type
+

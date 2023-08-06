5d987099f5bd173e3fad01e698ff36c7a84cce28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:57 +0000

    Add package gdalos/0.47.0/gdalos-0.47.0-py3-none-any.whl

---
+++ packages/gdalos/gdalos-0.47.0-py3-none-any.whl/gdalos/viewshed/viewshed_params.py
@@ -0,0 +1,162 @@
+from typing import Sequence
+from osgeo import gdal
+
+from gdalos.calc import dict_util
+from gdalos.viewshed.radio_params import RadioParams
+
+st_seen = 5
+st_seenbut = 4
+st_hidbut = 3
+st_hidden = 2
+st_nodtm = 1
+st_nodata = 0  # out of range value
+
+viewshed_visible = st_seen
+viewshed_thresh = st_hidbut
+viewshed_invisible = st_hidden
+viewshed_out_of_range = st_nodata
+viewshed_ndv = st_nodata
+viewshed_comb_ndv = 255
+viewshed_comb_multi_val = 254
+
+atmospheric_refraction_coeff = 1/7
+
+
+class ViewshedParams(object):
+    __slots__ = ['max_r', 'min_r', 'min_r_shave', 'max_r_slant',
+                 'ox', 'oy', 'oz', 'tz', 'omsl', 'tmsl',
+                 'azimuth', 'h_aperture', 'elevation', 'v_aperture',
+                 'vv', 'iv', 'ov', 'ndv',
+                 'refraction_coeff', 'mode', 'radio_parameters']
+
+    def __init__(self):
+        self.min_r = 0
+        self.max_r = None
+
+        self.min_r_shave = False
+        self.max_r_slant = True
+
+        self.ox = None
+        self.oy = None
+        self.oz = None
+        self.tz = None
+        self.omsl = False  # observer MSL
+        self.tmsl = False  # target MSL
+
+        self.azimuth = 0
+        self.h_aperture = 360
+        self.elevation = 0
+        self.v_aperture = 180
+
+        self.vv = viewshed_visible
+        self.iv = viewshed_invisible
+        self.ov = viewshed_out_of_range
+        self.ndv = viewshed_ndv
+
+        self.refraction_coeff = atmospheric_refraction_coeff
+        self.mode = 2
+        self.radio_parameters = None
+
+    def get_calc_module(self):
+        return -1 if not self.is_radio() else -2
+
+    def is_omni_h(self):
+        return not self.h_aperture or abs(self.h_aperture - 360) < 0.0001
+
+    @property
+    def oxy(self):
+        return self.ox, self.oy
+
+    @oxy.setter
+    def oxy(self, oxy):
+        self.ox, self.oy = oxy
+
+    def get_as_gdal_params(self):
+        vp_params = \
+            'max_r', 'ox', 'oy', 'oz', 'tz', \
+            'vv', 'iv', 'ov', 'ndv', 'mode'
+
+        gdal_params = \
+            'maxDistance', 'observerX', 'observerY', 'observerHeight', 'targetHeight', \
+            'visibleVal', 'invisibleVal', 'outOfRangeVal', 'noDataVal', 'mode'
+        d = {k1: getattr(self, k0) for k0, k1 in
+             zip(vp_params, gdal_params)}
+        d['dfCurvCoeff'] = 1 - self.refraction_coeff
+        return d
+
+    def is_radio(self):
+        return self.radio_parameters is not None
+
+    def is_calc_oz(self):
+        return self.oz is None
+
+    def is_calc_tz(self):
+        return self.tz is None
+
+    def get_result_dt(self):
+        return gdal.GDT_Int16 if self.is_radio() or self.is_calc_oz() or self.is_calc_tz() else gdal.GDT_Byte
+
+    def get_radio_as_talos_params(self):
+        return self.radio_parameters.get_dict()
+
+    def get_as_talos_params(self):
+        vp_params = \
+            'ox', 'oy', 'oz', 'max_r', 'min_r', 'min_r_shave', 'max_r_slant', 'tz', \
+            'omsl', 'tmsl', 'azimuth', 'h_aperture', 'elevation', 'v_aperture'
+
+        talos_params = \
+            'ox', 'oy', 'oz', 'MaxRange', 'MinRange', 'MinRangeShave', 'SlantRange', 'tz', \
+            'ObsMSL', 'TarMSL', 'Direction', 'Aperture', 'Elevation', 'ElevationAperture'
+        d = {k1: getattr(self, k0) for k0, k1 in
+             zip(vp_params, talos_params)}
+
+        slack_dummy_height = -1000
+        if d['oz'] is None or d['tz'] is None:
+            if self.is_radio():
+                raise Exception('You have to specify oz and tz for radio calc')
+            if d['oz'] is None:
+                d['oz'] = slack_dummy_height
+                if d['tz'] is None:
+                    raise Exception('You have to specify at least one of oz or tz')
+            else:
+                d['tz'] = slack_dummy_height
+
+        d['result_dt'] = self.get_result_dt()
+        return d
+
+    def update(self, d: dict):
+        for k, v in d.items():
+            setattr(self, k, v)
+
+    @staticmethod
+    def get_list_from_lists_dict(d: dict, key_map=None) -> Sequence['ViewshedParams']:
+        radio_d = d['radio_parameters']
+        d['radio_parameters'] = None
+        vp_array = dict_util.get_list_from_lists_dict(d, ViewshedParams(), key_map=key_map)
+        if radio_d is not None:
+            radio_array = dict_util.get_list_from_lists_dict(radio_d, RadioParams(), key_map=key_map)
+            r = None
+            for i, v in enumerate(vp_array):
+                if i < len(radio_array):
+                    r = radio_array[i]
+                v.radio_parameters = r
+
+        return vp_array
+
+
+# gdal_viewshed_params_short = \
+#     'max_r', 'ox', 'oy', 'oz', 'tz', \
+#     'vv', 'iv', 'ov', 'ndv', 'mode',
+#
+# gdal_viewshed_params_full = \
+#     'maxDistance', 'observerX', 'observerY', 'observerHeight', 'targetHeight', \
+#     'visibleVal', 'invisibleVal', 'outOfRangeVal', 'noDataVal', 'mode'
+# gdal_viewshed_keymap = dict(zip(gdal_viewshed_params_short, gdal_viewshed_params_full))
+
+viewshed_defaults = dict(vv=viewshed_visible,
+                         iv=viewshed_invisible,
+                         ov=viewshed_out_of_range,
+                         ndv=viewshed_ndv,
+                         )
+
+# gdal_viewshed_defaults = dict_util.replace_keys(viewshed_defaults,  gdal_viewshed_keymap)

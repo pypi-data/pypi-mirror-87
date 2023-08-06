0ac84ddd2f12d35ad4b8e4ed41ad8c6f3bac49a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:39 +0000

    Add package hns-tr069-sdk/0.2/hns_tr069_sdk-0.2.tar.gz

---
+++ packages/hns-tr069-sdk/hns_tr069_sdk-0.2.tar.gz/hns_tr069_sdk-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hns_tr069_sdk',
+    version='0.2',
+    packages=find_packages(),
+    url='https://gitlab.com/horsebridge/hns_tr069_sdk.git',
+    license='MIT',
+    author='Nitin Sidhu',
+    author_email='nitin.sidhu23@gmail.com',
+    description='TR069 server sdk',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    python_requires='~=3.6',
+    install_requires=['requests>=2,<3']
+)
+

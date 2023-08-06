038ff8e0f3105582093d784f83a890159c0a7de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:48 +0000

    Add package aiplat-acu-asr/2.0.6/aiplat-acu-asr-2.0.6.tar.gz

---
+++ packages/aiplat-acu-asr/aiplat-acu-asr-2.0.6.tar.gz/aiplat-acu-asr-2.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+# here = os.path.abspath(os.path.dirname(__file__))
+# changes = open(os.path.join(here, 'CHANGES.md')).read()
+# with open('CHANGES.md') as f:
+#     changes = f.read()
+
+setup(
+    name="aiplat-acu-asr",
+    version="2.0.6",
+    description="asr grpc client",
+    long_description="[https://github.com/baidubce/pie/tree/master/audio-streaming-client-python]" +
+                     "(https://github.com/baidubce/pie/tree/master/audio-streaming-client-python)",
+    long_description_content_type='text/markdown',
+    author="Baidu",
+    url="https://github.com/baidubce/pie/tree/master/audio-streaming-client-python-sdk",
+    author_email="1908131339@qq.com",
+    packages=find_packages(),
+    license="Apache License",
+    python_requires=">=2.7",
+    install_requires=["protobuf", "grpcio", 'threadpool', 'pyaudio','pyjwt'],
+    keywords=['baidu', 'asr', 'speech'],
+)

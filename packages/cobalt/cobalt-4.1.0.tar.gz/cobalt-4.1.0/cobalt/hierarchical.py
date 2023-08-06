4e58a04ba745c77827b1ff20ebd603e2d6646d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:38 +0000

    Add package cobalt/4.1.0/cobalt-4.1.0.tar.gz

---
+++ packages/cobalt/cobalt-4.1.0.tar.gz/cobalt-4.1.0/cobalt/hierarchical.py
@@ -0,0 +1,217 @@
+from iso8601 import parse_date
+
+from .akn import StructuredDocument, datestring
+
+
+class HierarchicalStructure(StructuredDocument):
+    structure_type = "hierarchicalStructure"
+    main_content_tag = "body"
+
+
+class Act(HierarchicalStructure):
+    """
+    An Akoma Ntoso Act document.
+    """
+    document_type = "act"
+
+    @classmethod
+    def empty_document_content(cls, E):
+        return E('body',
+                 E('section',
+                   E('content',
+                     E('p')),
+                   eId="section_1")
+                 )
+
+    @classmethod
+    def empty_document_attrs(cls):
+        attrs = super().empty_document_attrs()
+        attrs['contains'] = 'originalVersion'
+        return attrs
+
+    @property
+    def publication_name(self):
+        """ Name of the publication in which this act was published.
+        """
+        pub = self.get_element('meta.publication')
+        return pub.get('name') if pub is not None else None
+
+    @publication_name.setter
+    def publication_name(self, value):
+        value = value or ""
+        pub = self.ensure_element('meta.publication', after=self.meta.identification)
+        pub.set('name', value)
+        pub.set('showAs', value)
+
+    @property
+    def publication_date(self):
+        """ Date of publication as a `datetime.date` object.
+        """
+        pub = self.get_element('meta.publication')
+        if pub is not None and pub.get('date'):
+            return parse_date(pub.get('date')).date()
+        return None
+
+    @publication_date.setter
+    def publication_date(self, value):
+        self.ensure_element('meta.publication', after=self.meta.identification)\
+            .set('date', datestring(value))
+
+    @property
+    def publication_number(self):
+        """ Sequence number of the publication.
+        """
+        pub = self.get_element('meta.publication')
+        return pub.get('number') if pub is not None else None
+
+    @publication_number.setter
+    def publication_number(self, value):
+        self.ensure_element('meta.publication', after=self.meta.identification)\
+            .set('number', value or "")
+
+    @property
+    def amendments(self):
+        amendments = []
+
+        for e in self.meta.iterfind(f'.//{{{self.namespace}}}lifecycle/{{{self.namespace}}}eventRef[@type="amendment"]'):
+            date = parse_date(e.get('date')).date()
+            event = AmendmentEvent(date=date)
+            amendments.append(event)
+
+            eid = e.get('source')[1:]
+            source = self.meta.findall(f'.//{{{self.namespace}}}references/{{{self.namespace}}}passiveRef[@eId="{eid}"]')
+            if source:
+                event.amending_title = source[0].get('showAs')
+                event.amending_uri = source[0].get('href')
+
+        amendments.sort(key=lambda a: a.date)
+        return amendments
+
+    @amendments.setter
+    def amendments(self, value):
+        # delete existing entries
+        for e in self.meta.iterfind(f'.//{{{self.namespace}}}lifecycle/{{{self.namespace}}}eventRef[@type="amendment"]'):
+            # delete the passive ref elements
+            eid = e.get('source')[1:]
+            for node in self.meta.iterfind(f'.//{{{self.namespace}}}references/{{{self.namespace}}}passiveRef[@eId="{eid}"]'):
+                node.getparent().remove(node)
+
+            # delete the event
+            e.getparent().remove(e)
+
+        if not value:
+            # no amendments
+            self.act.set('contains', 'originalVersion')
+        else:
+            self.act.set('contains', 'singleVersion')
+            lifecycle = self._ensure_lifecycle()
+            references = self.ensure_element('meta.references', after=lifecycle)
+            if not references.get('source'):
+                references.set('source', '#' + self.source[1])
+
+            for i, event in enumerate(value):
+                date = datestring(event.date)
+                ref = f'amendment-{i}-source'
+
+                # create the lifecycle element
+                node = self.make_element('eventRef')
+                node.set('eId', 'amendment-' + date)
+                node.set('date', date)
+                node.set('type', 'amendment')
+                node.set('source', '#' + ref)
+                lifecycle.append(node)
+
+                # create the passive ref
+                node = self.make_element('passiveRef')
+                node.set('eId', ref)
+                node.set('href', event.amending_uri)
+                node.set('showAs', event.amending_title)
+                references.append(node)
+
+    @property
+    def repeal(self):
+        e = self.meta.find(f'.//{{{self.namespace}}}lifecycle/{{{self.namespace}}}eventRef[@type="repeal"]')
+        if e is not None:
+            date = parse_date(e.get('date')).date()
+            event = RepealEvent(date=date)
+
+            id = e.get('source')[1:]
+            source = self.meta.findall(f'.//{{{self.namespace}}}references/{{{self.namespace}}}passiveRef[@eId="{id}"]')
+            if source:
+                event.repealing_title = source[0].get('showAs')
+                event.repealing_uri = source[0].get('href')
+            return event
+
+    @repeal.setter
+    def repeal(self, value):
+        # delete existing entries
+        for e in self.meta.iterfind(f'.//{{{self.namespace}}}lifecycle/{{{self.namespace}}}eventRef[@type="repeal"]'):
+            # delete the passive ref elements
+            id = e.get('source')[1:]
+            for node in self.meta.iterfind(f'.//{{{self.namespace}}}references/{{{self.namespace}}}passiveRef[@eId="{id}"]'):
+                node.getparent().remove(node)
+
+            # delete the event
+            e.getparent().remove(e)
+
+        if value:
+            lifecycle = self._ensure_lifecycle()
+            references = self.ensure_element('meta.references', after=lifecycle)
+
+            date = datestring(value.date)
+            ref = 'repeal-source'
+
+            # create the lifecycle element
+            node = self.make_element('eventRef')
+            node.set('eId', 'repeal-' + date)
+            node.set('date', date)
+            node.set('type', 'repeal')
+            node.set('source', '#' + ref)
+            lifecycle.append(node)
+
+            # create the passive ref
+            node = self.make_element('passiveRef')
+            node.set('eId', ref)
+            node.set('href', value.repealing_uri)
+            node.set('showAs', value.repealing_title)
+            references.append(node)
+        else:
+            try:
+                if len(list(self.meta.lifecycle.iterchildren())) == 0:
+                    # remove empty lifecycle
+                    self.meta.remove(self.meta.lifecycle)
+            except AttributeError:
+                pass
+
+
+class AmendmentEvent(object):
+    """ An event that amended a document.
+
+    :ivar date: :class:`datetime.date` date of the event
+    :ivar amending_title: String title of the amending document
+    :ivar amending_uri: String form of the FRBR URI of the amending document
+    """
+    def __init__(self, date=None, amending_title=None, amending_uri=None):
+        self.date = date
+        self.amending_title = amending_title
+        self.amending_uri = amending_uri
+
+
+class RepealEvent(object):
+    """ An event that repealed a document.
+
+    :ivar date: :class:`datetime.date` date of the event
+    :ivar repealing_title: String title of the repealing document
+    :ivar repealing_uri: String form of the FRBR URI of the repealing document
+    """
+    def __init__(self, date=None, repealing_title=None, repealing_uri=None):
+        self.date = date
+        self.repealing_title = repealing_title
+        self.repealing_uri = repealing_uri
+
+
+class Bill(HierarchicalStructure):
+    """
+    An Akoma Ntoso Bill document.
+    """
+    document_type = "bill"
---
+++ packages/cobalt/cobalt-4.1.0.tar.gz/cobalt-4.1.0/cobalt/uri.py
@@ -0,0 +1,224 @@
+import re
+
+FRBR_URI_RE = re.compile(r"""^(/(?P<prefix>akn))?            # optional 'akn' prefix
+                              /(?P<country>[a-z]{2})         # country
+                              (-(?P<locality>[^/]+))?        # locality code
+                              /(?P<doctype>[^/]+)            # document type
+                              (/(?P<subtype>[^0-9][^/]*))?   # subtype (optional, cannot start with a number)
+                              (/(?P<actor>[^0-9][^/]*))?     # actor (optional), cannot start with a number
+                              /(?P<date>[0-9]{4}(-[0-9]{2}(-[0-9]{2})?)?)  # date
+                              /(?P<number>[^/]+)             # number
+                              (/
+                               (                             # either a work component or expression details
+                                (                              # optional expression details
+                                  (?P<language>[a-z]{3})                    # language (eg. eng)
+                                  (?P<expression_date>[@:][^/]*)?           # expression date (eg. @ or @2012-12-22 or :2012-12-22)
+                                  (/!?                                      # optional expression component
+                                                                            # the ! is optional for backwards compatibility but won't be optional
+                                                                            # in a future version
+                                    (?P<expression_component>[^/]+?)?       # expression component (eg. !main or !schedule1)
+                                    (/(?P<expression_subcomponent>[^.]+))?  # expression subcomponent (eg. chapter/1 or section/20)
+                                  )?                                        #
+                                  (\.(?P<format>[a-z0-9]+))?                # format (eg. .xml, .akn, .html, .pdf)
+                                )|                                          #
+                                !?(?P<work_component>[^/]+)     # work component
+                              ))?$""", re.X)
+
+
+class FrbrUri(object):
+    """
+    An object for working with
+    `Akoma Ntoso 3.0 FRBR URIs <https://docs.oasis-open.org/legaldocml/akn-nc/v1.0/os/akn-nc-v1.0-os.html>`_ (IRIs).
+
+    URIs can be parsed from a plain string using :meth:`parse` or they can be
+    constructed directly. URIs can be manipulated in-place once constructed,
+    which is useful for turning a work-level URI into an expression or
+    manifestation URI.
+
+    URIs can be transformed back to the string form of work, expression and
+    manifestation URIs using the :meth:`work_uri`, :meth:`expression_uri` and
+    :meth:`manifestation_uri` methods.
+
+    Example::
+
+        >>> uri = FrbrUri.parse('/akn/za-jhb/act/by-law/2003/public-health/eng:2015-01-01/!main/part/A.xml')
+        >>> uri.prefix
+        'akn'
+        >>> uri.country
+        'za'
+        >>> uri.locality
+        'jhb'
+        >>> uri.doctype
+        'act'
+        >>> uri.subtype
+        'by-law'
+        >>> uri.date
+        '2003'
+        >>> uri.number
+        'public-health'
+        >>> uri.language
+        'eng'
+        >>> uri.expression_date
+        ':2015-01-01'
+        >>> uri.expression_component
+        'main'
+        >>> uri.expression_subcomponent
+        'part/A'
+        >>> uri.format
+        'xml'
+        >>> uri.work_uri()
+        '/za-jhb/act/by-law/2003/public-health'
+        >>> uri.expression_uri()
+        '/za-jhb/act/by-law/2003/public-health/eng:2015-01-01/main/part/A'
+        >>> uri.manifestation_uri()
+        '/za-jhb/act/by-law/2003/public-health/eng:2015-01-01/main/part/A.xml'
+
+    :ivar prefix: optional `akn` prefix
+    :ivar country: two letter country code
+    :ivar locality: locality within the country, may be None
+    :ivar doctype: type of document (eg. ``act``)
+    :ivar subtype: document subtype, may be None
+    :ivar actor: emenating actor, may be None
+    :ivar date: document data (str), YYYY[-MM[-DD]]
+    :ivar number: document number (str)
+    :ivar work_component: name of the work component, may be None
+    :ivar language: three-letter expression language code, may be None
+    :ivar expression_date: expression date (str), [@:]YYYY[-MM[-DD]], may be None
+    :ivar expression_component: name of the expression component, may be None
+    :ivar expression_subcomponent: name of the expression subcomponent, may be None
+    :ivar format: format extension, may be None
+    """
+
+    default_language = 'eng'
+
+    def __init__(self, country, locality, doctype, subtype, actor, date, number,
+                 work_component=None, language=None, expression_date=None, expression_component=None,
+                 expression_subcomponent=None, format=None, prefix="akn"):
+        self.prefix = prefix
+        self.country = country
+        self.locality = locality
+        self.doctype = doctype
+        self.subtype = subtype
+        self.actor = actor
+        self.date = date
+        self.number = number
+        self.work_component = work_component
+
+        self.language = language or self.default_language
+        self.expression_date = expression_date
+        self.expression_component = expression_component
+        self.expression_subcomponent = expression_subcomponent
+        self.format = format
+
+    def clone(self):
+        """ Return a copy of this FrbrUri object.
+        """
+        return FrbrUri(
+            prefix=self.prefix,
+            country=self.country,
+            locality=self.locality,
+            doctype=self.doctype,
+            subtype=self.subtype,
+            actor=self.actor,
+            date=self.date,
+            number=self.number,
+            work_component=self.work_component,
+            language=self.language,
+            expression_date=self.expression_date,
+            expression_component=self.expression_component,
+            expression_subcomponent=self.expression_subcomponent,
+            format=self.format,
+        )
+
+    def uri(self):
+        """ String form of the work URI, excluding the work component, if any. """
+        return self.work_uri(work_component=False)
+
+    def work_uri(self, work_component=True):
+        """ String form of the work URI. """
+        country = self.country
+        parts = ['']
+        if self.locality:
+            country = country + "-" + self.locality
+
+        if self.prefix:
+            parts.append(self.prefix)
+
+        parts += [country, self.doctype]
+
+        if self.subtype:
+            parts.append(self.subtype)
+            if self.actor:
+                parts.append(self.actor)
+
+        parts += [self.date, self.number]
+
+        if work_component and self.work_component:
+            parts += ['!' + self.work_component]
+
+        return '/'.join(parts)
+
+    def expression_uri(self, work_component=True):
+        """ String form of the expression URI. """
+        if not self.language:
+            raise ValueError("Expression URI requires a language.")
+
+        uri = self.work_uri(work_component=False) + "/" + self.language
+
+        if self.expression_date is not None:
+            uri = uri + self.expression_date
+
+        # expression component is preferred over a work component
+        if self.expression_component:
+            uri = uri + "/!" + self.expression_component
+            if self.expression_subcomponent:
+                uri = uri + "/" + self.expression_subcomponent
+
+        # if we have a work component, use it
+        elif work_component and self.work_component:
+            uri = uri + "/!" + self.work_component
+
+        return uri
+
+    def manifestation_uri(self, work_component=True):
+        """ String form of the manifestation URI. """
+        uri = self.expression_uri(work_component)
+        if self.format:
+            uri = uri + "." + self.format
+        return uri
+
+    def __str__(self):
+        if self.format:
+            return self.manifestation_uri()
+        if self.expression_date or self.expression_component:
+            return self.expression_uri()
+        return self.work_uri()
+
+    def __repr__(self):
+        return f'<FrbrUri({self})>'
+
+    @classmethod
+    def parse(cls, s):
+        """ Parse a string into an FrbrUri instance.
+
+        :raises ValueError: if parsing fails
+        """
+        s = s.rstrip('/')
+        match = FRBR_URI_RE.match(s)
+        if match:
+            return cls(**match.groupdict())
+        else:
+            raise ValueError("Invalid FRBR URI: %s" % s)
+
+    @property
+    def year(self):
+        """ The year, derived from :data:`date`. Read-only. """
+        return self.date.split("-", 1)[0]
+
+    @property
+    def place(self):
+        """ Full place code, including both country and locality (if present).
+        """
+        if self.locality:
+            return self.country + "-" + self.locality
+        return self.country

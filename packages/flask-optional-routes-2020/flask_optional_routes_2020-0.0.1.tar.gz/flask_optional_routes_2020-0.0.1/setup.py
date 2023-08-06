810bfc61ac6c929fdabd5f7ff30c6f43248039da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package flask-optional-routes-2020/0.0.1/flask_optional_routes_2020-0.0.1.tar.gz

---
+++ packages/flask-optional-routes-2020/flask_optional_routes_2020-0.0.1.tar.gz/flask_optional_routes_2020-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='flask_optional_routes_2020',
+    version='0.0.1',
+    description='flask optional routes helps for multiple navigation options',
+    url='https://www.lamed-oti2.ml',
+    author='kobi shutzi',
+    author_email='josh@edublocks.org',
+    license='MIT',
+)

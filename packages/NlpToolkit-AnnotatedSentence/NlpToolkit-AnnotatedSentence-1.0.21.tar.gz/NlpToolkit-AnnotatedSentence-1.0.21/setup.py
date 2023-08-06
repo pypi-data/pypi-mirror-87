51f603b9e303d59b7e9ae4b258d921355078ded9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:56 +0000

    Add package NlpToolkit-AnnotatedSentence/1.0.21/NlpToolkit-AnnotatedSentence-1.0.21.tar.gz

---
+++ packages/NlpToolkit-AnnotatedSentence/NlpToolkit-AnnotatedSentence-1.0.21.tar.gz/NlpToolkit-AnnotatedSentence-1.0.21/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-AnnotatedSentence',
+    version='1.0.21',
+    packages=['AnnotatedSentence', 'AnnotatedSentence.AutoProcessor', 'AnnotatedSentence.AutoProcessor.AutoNER',
+              'AnnotatedSentence.AutoProcessor.AutoArgument', 'AnnotatedSentence.AutoProcessor.AutoSemantic',
+              'AnnotatedSentence.AutoProcessor.AutoPredicate', 'AnnotatedSentence.AutoProcessor.AutoDisambiguation'],
+    url='https://github.com/olcaytaner/AnnotatedSentence-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcaytaner@isikun.edu.tr',
+    description='Annotated Sentence Processing Library',
+    install_requires=['NlpToolkit-WordNet', 'NlpToolkit-NamedEntityRecognition', 'NlpToolkit-PropBank', 'NlpToolkit-DependencyParser']
+)

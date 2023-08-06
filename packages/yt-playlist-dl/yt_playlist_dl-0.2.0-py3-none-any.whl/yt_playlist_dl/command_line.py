a111314650154cc8f71fde912086f9e625023ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:27 +0000

    Add package yt-playlist-dl/0.2.0/yt_playlist_dl-0.2.0-py3-none-any.whl

---
+++ packages/yt-playlist-dl/yt_playlist_dl-0.2.0-py3-none-any.whl/yt_playlist_dl/command_line.py
@@ -0,0 +1,20 @@
+"""CLI entrypoint for the playlist downloader"""
+import argparse
+
+import yt_playlist_dl
+
+
+def main() -> None:
+    """CLI entry point"""
+    parser = argparse.ArgumentParser(
+        description='Download youtube playlists'
+    )
+
+    parser.add_argument(
+        'urls',
+        nargs='+',
+        help='URL(s) of the youtube playlist'
+    )
+
+    args = parser.parse_args()
+    yt_playlist_dl.download_playlists(args.urls)

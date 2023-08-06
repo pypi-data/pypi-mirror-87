13a18389cee6ebdae30196370a1a082836cd884b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:05 +0000

    Add package PySide6/6.0.0a1.dev1606913934/PySide6-6.0.0a1.dev1606913934-6.0.0-cp36.cp37.cp38.cp39-none-win_amd64.whl

---
+++ packages/PySide6/PySide6-6.0.0a1.dev1606913934-6.0.0-cp36.cp37.cp38.cp39-none-win_amd64.whl/PySide6/_config.py
@@ -0,0 +1,16 @@
+built_modules = list(name for name in
+    "Core;Gui;Widgets;PrintSupport;Sql;Network;Test;Concurrent;Xml;Help;OpenGL;OpenGLFunctions;OpenGLWidgets;Qml;Quick;QuickControls2;QuickWidgets;Svg;SvgWidgets;UiTools"
+    .split(";"))
+
+shiboken_library_soversion = str(6.0)
+pyside_library_soversion = str(6.0)
+
+version = "6.0.0a1.dev1606911839"
+version_info = (6, 0, 0, "a", "1")
+
+__build_date__ = '2020-12-02T12:29:30+00:00'
+
+
+
+__setup_py_package_timestamp__ = '1606911839'
+__setup_py_package_version__ = '6.0.0a1.dev1606911839'

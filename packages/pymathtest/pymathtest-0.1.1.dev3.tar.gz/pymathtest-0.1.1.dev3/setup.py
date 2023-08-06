6ecd4e7f8dbee073e9698f52f4867de47e672609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:20 +0000

    Add package pymathtest/0.1.1.dev3/pymathtest-0.1.1.dev3.tar.gz

---
+++ packages/pymathtest/pymathtest-0.1.1.dev3.tar.gz/pymathtest-0.1.1.dev3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -- coding: utf-8 --
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pymathtest',
+    version='0.1.1.dev3',
+    description='A tool that can randomly generates simple add and subtract questions.',
+    long_description=long_description,
+    url='https://github.com/whattobuild/py-mathtest',
+    author='Telon Chyi',
+    author_email='laoqizhuzhanghaoyong02@outlook.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.4',
+    keywords='maths question python',
+)

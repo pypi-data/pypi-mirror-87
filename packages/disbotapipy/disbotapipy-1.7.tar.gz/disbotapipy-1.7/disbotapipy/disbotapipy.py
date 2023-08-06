c7051aa9ac9cdeca710e2a817179a6fcf0c6a645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:45 +0000

    Add package disbotapipy/1.7/disbotapipy-1.7.tar.gz

---
+++ packages/disbotapipy/disbotapipy-1.7.tar.gz/disbotapipy-1.7/disbotapipy/disbotapipy.py
@@ -0,0 +1,13 @@
+import requests
+
+class disbot():
+
+    def updateStats(self, clientid, token, serverCount):
+        if(serverCount < 0):
+            print("A server count must be sent by the bot")
+        else:
+            sendURL = f'https://disbot.top/api/v1/botupdate/{clientid}'
+            body = {'serverCount': serverCount}
+            header = {'authorization': token}
+            success = requests.post(sendURL, data = body, headers=header)
+            print(f"{success} We did it")

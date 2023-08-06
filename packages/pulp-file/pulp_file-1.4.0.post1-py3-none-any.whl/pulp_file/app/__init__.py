c9869fc0c495511d97561ca7f860d5060232a1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:30 +0000

    Add package pulp-file/1.4.0.post1/pulp_file-1.4.0.post1-py3-none-any.whl

---
+++ packages/pulp-file/pulp_file-1.4.0.post1-py3-none-any.whl/pulp_file/app/__init__.py
@@ -0,0 +1,10 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpFilePluginAppConfig(PulpPluginAppConfig):
+    """
+    Entry point for pulp_file plugin.
+    """
+
+    name = "pulp_file.app"
+    label = "file"

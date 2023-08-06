aad5b3c376a9c729475292840096122b89d50d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package jnt-django-graphene-toolbox/0.1.23/jnt-django-graphene-toolbox-0.1.23.tar.gz

---
+++ packages/jnt-django-graphene-toolbox/jnt-django-graphene-toolbox-0.1.23.tar.gz/jnt-django-graphene-toolbox-0.1.23/src/jnt_django_graphene_toolbox/converters/forms.py
@@ -0,0 +1,36 @@
+import graphene
+from graphene_django.forms.converter import (
+    convert_form_field,
+    convert_form_field_to_string,
+)
+from jnt_django_toolbox.forms.fields import EnumChoiceField
+
+from jnt_django_graphene_toolbox.converters.registry import get_registered_enum
+from jnt_django_graphene_toolbox.filters.integers_array import (
+    IntegersArrayField,
+)
+from jnt_django_graphene_toolbox.filters.strings_array import StringsArrayField
+
+
+@convert_form_field.register(IntegersArrayField)
+def convert_integers_array_field(field):
+    """Convert form field."""
+    return graphene.List(graphene.ID)
+
+
+@convert_form_field.register(StringsArrayField)
+def convert_strings_array_field(field):
+    """Convert form field."""
+    return graphene.List(graphene.String)
+
+
+@convert_form_field.register(EnumChoiceField)
+def convert_choice_field(field):
+    """Convert form field."""
+    registered = get_registered_enum(field.enum)
+    if registered:
+        return registered._meta.class_type(  # noqa: WPS437
+            required=field.required,
+        )
+
+    return convert_form_field_to_string(field)
---
+++ packages/jnt-django-graphene-toolbox/jnt-django-graphene-toolbox-0.1.23.tar.gz/jnt-django-graphene-toolbox-0.1.23/src/jnt_django_graphene_toolbox/filters/__init__.py
@@ -0,0 +1,4 @@
+from .integer import IntegerFilter
+from .integers_array import IntegersArrayFilter
+from .ordering import BaseCollectionOrderingFilter, OrderingFilter
+from .search import SearchFilter

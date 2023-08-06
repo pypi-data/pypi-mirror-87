413db1959baeba5d17403b77cfe6f65cfe8f677a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:38 +0000

    Add package django-bread/1.0.1/django_bread-1.0.1-py3-none-any.whl

---
+++ packages/django-bread/django_bread-1.0.1-py3-none-any.whl/bread/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BreadTestModel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=10)),
+            ],
+            options={
+                'ordering': ['name'],
+                'permissions': [('browse_breadtestmodel', 'can browse BreadTestModel')],
+            },
+            bases=(models.Model,),
+        ),
+    ]

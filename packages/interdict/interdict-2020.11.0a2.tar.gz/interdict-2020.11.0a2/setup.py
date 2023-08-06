e92b4ece10471b22e786d7bf18ca5ec046a122ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package interdict/2020.11.0a2/interdict-2020.11.0a2.tar.gz

---
+++ packages/interdict/interdict-2020.11.0a2.tar.gz/interdict-2020.11.0a2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['interdict']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flatten-dict']
+
+setup_kwargs = {
+    'name': 'interdict',
+    'version': '2020.11.0a2',
+    'description': 'Dictionary filter',
+    'long_description': "# interdict\nPython dictionary filter\n\n# Installation\n\n`pip install interdict`\n\n# Examples\n\n## Default (inclusion False)\n\n```python\n>>> from interdict import filter_obj\n>>> obj = {'name': 'shane', 'job': {'salary': 1000000, 'role': 'master of universe'}}\n>>> filter = {'name': True, 'job': {'role': True}}\n>>> filter_obj(obj, filter)\n{'name': 'shane', 'job': {'role': 'master of universe'}}\n```\n\n## Inclusive (a.k.a. TMI)\n\n```python\n>>> filter_obj(obj, filter, default=True)\n{'name': 'shane', 'job': {'salary': 1000000, 'role': 'master of universe'}}\n```\n\n## Inclusive with better filter\n\n```python\n>>> filter = {'job': {'salary': False}}\n>>> filter_obj(obj, filter, default=True)\n{'name': 'shane', 'job': {'role': 'master of universe'}}\n```\n",
+    'author': 'Shane R. Spencer',
+    'author_email': '305301+whardier@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/whardier/interdict',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

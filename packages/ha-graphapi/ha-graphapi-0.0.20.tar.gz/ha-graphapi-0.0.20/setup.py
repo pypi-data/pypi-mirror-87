83730299e31fe0fd6b94117c9bd9f4f3bc706b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package ha-graphapi/0.0.20/ha-graphapi-0.0.20.tar.gz

---
+++ packages/ha-graphapi/ha-graphapi-0.0.20.tar.gz/ha-graphapi-0.0.20/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+from setuptools import find_packages, setup
+
+PACKAGES = [f"hagraph.{p}" for p in find_packages(where="hagraph")]
+
+# This call to setup() does all the work
+setup(
+    name="ha-graphapi",
+    version="0.0.20",
+    description="For use with Home Assistant to query Microsoft's Graph API",
+    author="Jamie Weston",
+    license="MIT",
+    url="https://github.com/jlweston/ha-graphapi/",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=PACKAGES,
+    install_requires=[
+        "aiohttp",
+        "appdirs",
+        "ms_cv",
+        "pydantic",
+    ]
+)

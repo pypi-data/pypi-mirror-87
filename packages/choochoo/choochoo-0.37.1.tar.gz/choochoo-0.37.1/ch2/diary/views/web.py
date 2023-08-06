6ec434764bf199e2be2cc785c7b92a45acc27f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:03 +0000

    Add package choochoo/0.37.1/choochoo-0.37.1.tar.gz

---
+++ packages/choochoo/choochoo-0.37.1.tar.gz/choochoo-0.37.1/ch2/diary/views/web.py
@@ -0,0 +1,22 @@
+
+from ..model import DB
+from ...sql import StatisticJournal, Source
+
+
+def str_or_none(x):
+    return x is None or isinstance(x, str)
+
+
+def rewrite_db(model):
+    if isinstance(model, list):
+        return [rewrite_db(m) for m in model]
+    else:
+        if DB in model:
+            db = model[DB]
+            if hasattr(db, 'id'):
+                model[DB] = db.id
+            elif isinstance(db, str):  # images have urls
+                model[DB] = db
+            else:  # links have tuples
+                model[DB] = [x if str_or_none(x) else x.id for x in db]
+        return model
---
+++ packages/choochoo/choochoo-0.37.1.tar.gz/choochoo-0.37.1/ch2/pipeline/calculate/cluster.py
@@ -0,0 +1,26 @@
+from ch2.data.cluster import hulls_from_last_activity, sectors_from_hulls
+from ch2.pipeline.calculate.utils import ProcessCalculator, RerunWhenNewActivitiesMixin
+from ch2.sql import Timestamp
+from ch2.sql.database import connect_config
+from ch2.sql.types import short_cls
+
+
+class ClusterCalculator(RerunWhenNewActivitiesMixin, ProcessCalculator):
+
+    def __init__(self, *args, excess=0.1, radius_km=200, **kargs):
+        super().__init__(*args, excess=excess, **kargs)
+        self.__radius_km = radius_km
+
+    def _run_one(self, missed):
+        with self._config.db.session_context() as s:
+            with Timestamp(owner=self.owner_out).on_success(s):
+                sector_group = hulls_from_last_activity(s, self.__radius_km)
+                if sector_group:
+                    sectors_from_hulls(s, sector_group)
+
+
+if __name__ == '__main__':
+    from ch2.pipeline.calculate.sector import SectorCalculator
+    config = connect_config(['-v5'])
+    # ClusterCalculator(config, owner_in=short_cls(ActivityReader)).run()
+    SectorCalculator(config, owner_in=short_cls(ClusterCalculator)).run()

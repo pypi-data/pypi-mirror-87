eafd04f1f6ffc6dd7aca58adc086a0b3ab3e0219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:06 +0000

    Add package duckietown-challenges-daffy/6.2.10/duckietown-challenges-daffy-6.2.10.tar.gz

---
+++ packages/duckietown-challenges-daffy/duckietown-challenges-daffy-6.2.10.tar.gz/duckietown-challenges-daffy-6.2.10/src/duckietown_challenges/types.py
@@ -0,0 +1,15 @@
+from typing import NewType
+
+
+RPath = NewType("RPath", str)
+JobStatusString = NewType("JobStatusString", str)
+StepName = NewType("StepName", str)
+ChallengeName = NewType("ChallengeName", str)
+JobID = NewType("JobID", int)
+EvaluatorID = NewType("EvaluatorID", int)
+EvaluationFeatureID = NewType("EvaluationFeatureID", int)
+UserID = NewType("UserID", int)
+SubmissionID = NewType("SubmissionID", int)
+ChallengeID = NewType("ChallengeID", int)
+ChallengeStepID = NewType("ChallengeStepID", int)
+ServiceName = NewType("ServiceName", str)

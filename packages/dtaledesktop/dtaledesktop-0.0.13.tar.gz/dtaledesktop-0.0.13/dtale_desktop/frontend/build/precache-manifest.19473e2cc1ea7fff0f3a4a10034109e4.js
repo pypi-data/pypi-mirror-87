18bbe23528c4712f8269ea8b33c625bb521bba29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:38 +0000

    Add package dtaledesktop/0.0.13/dtaledesktop-0.0.13.tar.gz

---
+++ packages/dtaledesktop/dtaledesktop-0.0.13.tar.gz/dtaledesktop-0.0.13/dtale_desktop/frontend/build/precache-manifest.19473e2cc1ea7fff0f3a4a10034109e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a6d75a4b8f009e57cd0a840a173715",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ad78dd2e7d842a9b53",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae6f58027deb1531a021",
+    "url": "/static/js/2.9e8dd5df.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.9e8dd5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ad78dd2e7d842a9b53",
+    "url": "/static/js/main.7de39f9f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);

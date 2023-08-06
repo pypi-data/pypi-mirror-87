e5e91ad316e79307d5fd2218d763f82292056c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:04 +0000

    Add package goodenough/0.0.1/goodenough-0.0.1.tar.gz

---
+++ packages/goodenough/goodenough-0.0.1.tar.gz/goodenough-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="goodenough",
+    version="0.0.1",
+    description="Lib to pick the \"best\" item from a random sample",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcgurevitx/goodenough",
+    author="Marc Gurevitx",
+    author_email="marcgurevitx@gmail.com",
+    license="The Unlicense",
+    py_modules=["goodenough"],
+    extras_require={
+        "web": "aiohttp[speedups]",
+    },
+)

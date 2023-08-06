5ede59755f747f32f30dc4adac077f54754bf47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:35 +0000

    Add package RackioOPC-UA/0.1/RackioOPC_UA-0.1-py3-none-any.whl

---
+++ packages/RackioOPC-UA/RackioOPC_UA-0.1-py3-none-any.whl/rackio_opcua/device.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""rackio_opcua/device.py
+
+This module implements the device class for RackioOPCUA.
+"""
+from rackio import TagEngine
+
+from .updater import TagUpdater
+
+
+class Device:
+
+    def __init__(self, name, folder, idx):
+
+        self.name = name
+        self.folder = folder
+        self.idx = idx
+
+        self._device = folder.add_object(idx, name)
+
+        self.mappings = list()
+
+    def define_mapping(self, tag, mode, period=0.25):
+
+        dev = self._device
+        idx = self.idx
+
+        engine = TagEngine()
+
+        value = engine.read_tag(tag)
+
+        opc_var = dev.add_variable(idx, tag, value)
+
+        mapping = TagUpdater(opc_var, tag, mode, period)
+        self.mappings.append(mapping)
+
+    def get_mappings(self):
+
+        return self.mappings
+
+    
---
+++ packages/RackioOPC-UA/RackioOPC_UA-0.1-py3-none-any.whl/rackio_opcua/folder.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""rackio_opcua/folder.py
+
+This module implements the folder class for RackioOPCUA.
+"""
+
+from .updater import TagUpdater
+from .device import Device
+
+class Folder:
+
+    def __init__(self, name, server, idx):
+
+        self.name = name
+        self.server = server
+        self.idx = idx
+
+        self._folder = server.nodes.objects.add_folder(idx, name)
+
+        self.devices = list()
+
+    def define_device(self, name):
+
+        folder = self._folder
+        idx = self.idx
+
+        device = Device(name, folder, idx)
+
+        self.devices.append(device)
+
+        return device
+
+    def get_mappings(self):
+
+        result = list()
+
+        for device in self.devices:
+
+            result += device.get_mappings()
+
+        return result
+    

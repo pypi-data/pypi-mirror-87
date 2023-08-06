803cf66ecdcda0281f3bf3177c78fbc6b26c78a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:05 +0000

    Add package ckwrap/0.1.5/ckwrap-0.1.5.tar.gz

---
+++ packages/ckwrap/ckwrap-0.1.5.tar.gz/ckwrap-0.1.5/README.md
@@ -0,0 +1,13 @@
+# ckwrap
+
+Python wrapper for the R package Ckmeans.1d.dp (https://cran.r-project.org/package=Ckmeans.1d.dp).
+Ckmeans.1d.dp produces fast, optimal, and reproducible weighted univariate clustering by dynamic programming.
+
+Based off of https://github.com/rocketrip/ckmeans/.
+
+
+## Installation
+
+To install via pip:
+
+```pip install ckwrap```
---
+++ packages/ckwrap/ckwrap-0.1.5.tar.gz/ckwrap-0.1.5/setup.py
@@ -0,0 +1,44 @@
+from Cython.Build import cythonize
+from setuptools import setup, Extension
+import numpy as np
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+sources = [
+    "ckwrap/_ckwrap.pyx",
+    "Ckmeans.1d.dp/src/dynamic_prog.cpp",
+    "Ckmeans.1d.dp/src/EWL2_dynamic_prog.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_log_linear.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_quadratic.cpp",
+    "Ckmeans.1d.dp/src/EWL2_fill_SMAWK.cpp",
+    "Ckmeans.1d.dp/src/fill_log_linear.cpp",
+    "Ckmeans.1d.dp/src/fill_quadratic.cpp",
+    "Ckmeans.1d.dp/src/fill_SMAWK.cpp",
+    "Ckmeans.1d.dp/src/select_levels.cpp",
+    "Ckmeans.1d.dp/src/weighted_select_levels.cpp",
+]
+
+ckwrap = Extension(
+    name="_ckwrap",
+    sources=sources,
+    language="c++",
+    include_dirs=["Ckmeans.1d.dp/src", np.get_include()],
+    extra_compile_args=["-std=c++11", "-g0"],
+)
+
+setup(
+    name="ckwrap",
+    version="0.1.5",
+    description="Python wrapper for Ckmeans.1d.dp, 4.3.3.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="djdt",
+    license="LGPL",
+    url="https://github.com/djdt/pewlib",
+    packages=["ckwrap"],
+    ext_modules=cythonize(ckwrap),
+    install_requires=["numpy", "Cython"],
+    tests_require=["pytest", "scipy"],
+)

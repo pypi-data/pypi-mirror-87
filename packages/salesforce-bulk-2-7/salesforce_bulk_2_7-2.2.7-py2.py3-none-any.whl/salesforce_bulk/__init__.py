ae54197a6bd00f3e8b135a549712045d61b67148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:03 +0000

    Add package salesforce-bulk-2-7/2.2.7/salesforce_bulk_2_7-2.2.7-py2.py3-none-any.whl

---
+++ packages/salesforce-bulk-2-7/salesforce_bulk_2_7-2.2.7-py2.py3-none-any.whl/salesforce_bulk/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .salesforce-bulk-2-7 import SalesforceBulk, BulkApiError, UploadResult
+from .csv_adapter import CsvDictsAdapter
+from .__version__ import __version__

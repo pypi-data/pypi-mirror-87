84b5b62cfc6f517e1c2704591b795bcb4a0acb9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:48 +0000

    Add package django-migrations-diff/2.0.5/django-migrations-diff-2.0.5.tar.gz

---
+++ packages/django-migrations-diff/django-migrations-diff-2.0.5.tar.gz/django-migrations-diff-2.0.5/django_migrations_diff/help_text.py
@@ -0,0 +1,21 @@
+HELP = """
+<g>mdiff</g> is a CLI to compare Django migrations between two snapshots.
+
+  Find more information at: https://github.com/deniskrumko/django-migrations-diff
+
+<g>Basic Commands</g>
+
+  mdiff <b><name></b>\t\t\tCreate snapshot
+  mdiff <b><name_1></b> <b><name_2></b>\tCompare two snapshots
+
+  mdiff <g>list</g>\t\t\tList of all snapshots
+
+  mdiff <g>rm all</g></b>\t\t\tRemove all snapshots
+  mdiff <g>rm</g> <b><name_1></b> <b><name_2></b>\tRemove specific snapshots
+
+<g>Flags</g>
+
+  --number\t\t\tGet only number of diffent migrations
+  --width <b><N></b>\t\t\tSet max characters in line (<y>default: 120</y>)
+
+""".rstrip() # noqa

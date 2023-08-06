9b2e5f960958bbac9bae0e4c7fdc48380eb37c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package mac-app-factory/2020.12.3/mac-app-factory-2020.12.3.tar.gz

---
+++ packages/mac-app-factory/mac-app-factory-2020.12.3.tar.gz/mac-app-factory-2020.12.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='mac-app-factory',
+    version='2020.12.3',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages(),
+    scripts=['scripts/mac-app-factory']
+)

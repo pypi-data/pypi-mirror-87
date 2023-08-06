a001abfa89671f3fe5ce342d0a9537d8ddd9c8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:34 +0000

    Add package aeolus/0.4.7/aeolus-0.4.7.tar.gz

---
+++ packages/aeolus/aeolus-0.4.7.tar.gz/aeolus-0.4.7/src/aeolus/calc/meta.py
@@ -0,0 +1,43 @@
+"""Metadata-related functionality."""
+import functools
+from collections.abc import Iterable
+
+import cf_units
+
+import iris
+
+
+def update_metadata(name=None, units=None):
+    """Update metadata of a cube returned by a function."""
+
+    def update_name_units(cube):
+        """Update name and convert units."""
+        if isinstance(name, str):
+            cube.rename(name)
+        if isinstance(units, (str, cf_units.Unit)):
+            cube.convert_units(units)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Call the decorated function
+            out = func(*args, **kwargs)
+            if isinstance(out, iris.cube.Cube):
+                update_name_units(out)
+            elif isinstance(out, Iterable):
+                [update_name_units(cube) for cube in out]
+            return out
+
+        return wrapper
+
+    return decorator
+
+
+def copy_doc(original):
+    """Copy docstring from another function."""
+
+    def wrapper(func):
+        func.__doc__ = original.__doc__
+        return func
+
+    return wrapper

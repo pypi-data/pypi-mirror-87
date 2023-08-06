94738a21bb07858b807c625f01bb7c47f7f1f2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:16 +0000

    Add package accepts/2020.12.3/accepts-2020.12.3.tar.gz

---
+++ packages/accepts/accepts-2020.12.3.tar.gz/accepts-2020.12.3/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='accepts',
+    version='2020.12.3',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages()
+)

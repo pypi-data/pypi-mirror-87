922656ab14f0106dca807fde13aef511b7b0de6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package modian/0.3.2/modian-0.3.2.tar.gz

---
+++ packages/modian/modian-0.3.2.tar.gz/modian-0.3.2/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+NAME = "modian"
+
+setup(name=NAME,
+    version='0.3.2',
+    author = "Loïc Paulevé",
+    author_email = "loic.pauleve@labri.fr",
+    description = "Simple python library for Boolean networks",
+    long_description = "For teaching purpose",
+    install_requires = [
+        "jupyter",
+        "pydotplus"
+    ],
+    py_modules = ["modian_setup"],
+    license="CeCILL",
+    packages = [NAME],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    keywords="boolean networks, model checing, teaching",
+)
+

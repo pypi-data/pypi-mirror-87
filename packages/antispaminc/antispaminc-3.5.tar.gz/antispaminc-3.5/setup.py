265e3939a0fab0d3eaed353cc2ba36ea3cd18fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:32 +0000

    Add package antispaminc/3.5/antispaminc-3.5.tar.gz

---
+++ packages/antispaminc/antispaminc-3.5.tar.gz/antispaminc-3.5/setup.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+#
+#                    GNU GENERAL PUBLIC LICENSE
+#                      Version 3, 29 June 2007
+
+# Copyright (C) 2020 AntiSpam, Inc. <http://antispaminc.tk/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="antispaminc",
+    version="3.5",
+    author="Midhun KM",
+    url="http://antispaminc.tk",
+    author_email="dev@antispaminc.tk",
+    description="A Python Wrapper For Antispam Inc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["requests >= 2.22.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

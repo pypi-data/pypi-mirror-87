27ea9e215f2e3c8dc46fd0273bee228a59721e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:20 +0000

    Add package aiohttp-prometheus-client/0.1.0/aiohttp-prometheus-client-0.1.0.tar.gz

---
+++ packages/aiohttp-prometheus-client/aiohttp-prometheus-client-0.1.0.tar.gz/aiohttp-prometheus-client-0.1.0/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+tests_require = [
+    'pytest==5.2.2',
+    'pytest-aiohttp==0.3.0',
+    'nose',
+    'coverage',
+    'flake8',
+]
+
+setup(
+    name='aiohttp-prometheus-client',
+    version='0.1.0',
+    description="HTTP metrics for a aiohttp application",
+    long_description=open('README.rst').read(),
+    keywords=['prometheus', 'aiohttp'],
+    author='Globo.com, Rail Yakup',
+    author_email='backstage@corp.globo.com, Rail1996@mail.ru',
+    url='https://github.com/RailYakup/aiohttp-prometheus.git',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    packages=find_packages(
+        exclude=(
+            'tests',
+        ),
+    ),
+    include_package_data=True,
+    install_requires=[
+        'aiohttp',
+        'prometheus-client',
+    ],
+    extras_require={
+        'tests': tests_require,
+    },
+)

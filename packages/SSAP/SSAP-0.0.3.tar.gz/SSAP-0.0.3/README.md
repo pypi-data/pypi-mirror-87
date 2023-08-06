a50a93df3d3fd7ffd19514db7f12d0da4f4bfc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:38 +0000

    Add package SSAP/0.0.3/SSAP-0.0.3.tar.gz

---
+++ packages/SSAP/SSAP-0.0.3.tar.gz/SSAP-0.0.3/README.md
@@ -0,0 +1,4 @@
+# SSAP
+
+Welcome to SSAP [Self Service Analytics Platform] Community! 
+In this community, we provide several common function modules to support business & technical data analytics scenarios. The function will be kept updating with the specific analytics scenario and analytics use cases. Please stay tuned😑.
---
+++ packages/SSAP/SSAP-0.0.3.tar.gz/SSAP-0.0.3/SSAP/SSAP.py
@@ -0,0 +1,6 @@
+def info():
+    print('Welcome to SSAP [Self Service Analytics Platform] Community! '
+          'In this community, we provide several common analytics function modules to support business & technical data analytics scenarios. '
+          'The function will be kept updating with the specific analytics scenario and analytics use cases.')
+
+

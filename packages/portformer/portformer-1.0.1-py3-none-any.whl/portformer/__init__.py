452a0aa6fea9dd1dc99589a5a0572014bfb5c659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:55 +0000

    Add package portformer/1.0.1/portformer-1.0.1-py3-none-any.whl

---
+++ packages/portformer/portformer-1.0.1-py3-none-any.whl/portformer/__init__.py
@@ -0,0 +1,22 @@
+"""
+Portformer API and Portfolio analysis tools for the modern investor
+"""
+
+__version__ = "1.0.1"
+__short_description__ = "Portfolios. Made Better."
+__license__ = "Astrocyte Research, Inc"
+__author__ = "Sean Kruzel, Astrocyte Research Inc."
+__author_email__ = "skruzel@portformer.com"
+__maintainer__ = "Sean Kruzel"
+__maintainer_email__ = "support@portformer.com"
+__github_username__ = "closedLoop"
+
+# Library Functions
+__all__ = ["load_config", "BreakpointAPI"]
+
+from .breakpoint.api import BreakpointAPI
+
+# Environmental variables
+from .config import load_config
+
+_api = "v1"

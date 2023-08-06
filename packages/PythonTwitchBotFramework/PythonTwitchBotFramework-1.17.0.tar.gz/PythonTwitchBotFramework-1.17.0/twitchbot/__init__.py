b46aff8cd02151ad730d0408ee47f6ea5a1e5c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:44 +0000

    Add package PythonTwitchBotFramework/1.17.0/PythonTwitchBotFramework-1.17.0.tar.gz

---
+++ packages/PythonTwitchBotFramework/PythonTwitchBotFramework-1.17.0.tar.gz/PythonTwitchBotFramework-1.17.0/twitchbot/__init__.py
@@ -0,0 +1,49 @@
+from .arena import *
+from .channel import *
+from twitchbot.api.chatters import *
+from .colors import *
+from .command import *
+from .config import *
+from .enums import *
+from .irc import *
+from .message import *
+from .permission import *
+from .ratelimit import *
+from .regex import *
+from .util import *
+from .database import *
+from .bots import *
+from .api import *
+from .events import *
+from .loyalty_ticker import *
+from .disabled_commands import *
+from .duel import *
+from .command_server import start_command_server
+from .modloader import *
+from .disabled_mods import *
+from .data import *
+from .exceptions import *
+from .shared import *
+from .replywaiter import *
+from .command_whitelist import *
+from .poll import *
+from .event_util import *
+from .extra_configs import *
+from .pubsub import *
+from . import builtin_commands
+from . import builtin_mods
+
+BOT_VERSION = (1, 17, 0)  # keep in sync with version in setup.py:11
+
+import os
+
+load_commands_from_directory(os.path.join(__path__[0], 'builtin_commands'))
+
+load_mods_from_directory(os.path.join(__path__[0], 'builtin_mods'))
+load_mods_from_directory(os.path.abspath(cfg.mods_folder))
+
+ensure_mods_folder_exists()
+ensure_commands_folder_exists()
+
+load_commands_from_directory(os.path.abspath(cfg.commands_folder))
+start_command_server()

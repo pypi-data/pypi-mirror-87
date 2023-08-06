71b1570840da050bbea278b41991e73b4d35e9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:13 +0000

    Add package nhc2-coco/1.3.3/nhc2-coco-1.3.3.tar.gz

---
+++ packages/nhc2-coco/nhc2-coco-1.3.3.tar.gz/nhc2-coco-1.3.3/nhc2_coco/coco_device_class.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class CoCoDeviceClass(Enum):
+    SWITCHES = 'switches'
+    LIGHTS = 'lights'
+    SHUTTERS = 'shutters'
+    FANS = 'fans'
+    SWITCHED_FANS = 'switched-fans'

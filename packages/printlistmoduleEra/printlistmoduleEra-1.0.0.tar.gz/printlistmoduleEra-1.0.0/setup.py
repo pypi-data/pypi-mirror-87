f144fe8ce3f006f1e7c0dac85de3e3d827af2a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:38 +0000

    Add package printlistmoduleEra/1.0.0/printlistmoduleEra-1.0.0.tar.gz

---
+++ packages/printlistmoduleEra/printlistmoduleEra-1.0.0.tar.gz/printlistmoduleEra-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 16:15:54 2020
+
+@author: user
+"""
+
+from distutils.core import setup
+
+setup(name = "printlistmoduleEra" , version="1.0.0", description="an API to print\
+      a list recursively", author="Era", author_email="era.tiwari@ignitarium.com",\
+      py_modules=["printlistmoduleEra"])

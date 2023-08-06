e61db40a71518c16e4effc86762e2de8d62f2616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:47 +0000

    Add package pyinfra/1.3.dev2/pyinfra-1.3.dev2.tar.gz

---
+++ packages/pyinfra/pyinfra-1.3.dev2.tar.gz/pyinfra-1.3.dev2/pyinfra/facts/git.py
@@ -0,0 +1,34 @@
+from __future__ import unicode_literals
+
+from pyinfra.api.facts import FactBase
+
+
+class GitBranch(FactBase):
+    requires_command = 'git'
+
+    @staticmethod
+    def command(repo):
+        return 'cd {0} && git rev-parse --abbrev-ref HEAD'.format(repo)
+
+
+class GitConfig(FactBase):
+    default = dict
+
+    requires_command = 'git'
+
+    @staticmethod
+    def command(repo=None):
+        if repo is None:
+            return 'git config --global -l'
+
+        return 'cd {0} && git config --local -l'.format(repo)
+
+    @staticmethod
+    def process(output):
+        items = {}
+
+        for line in output:
+            key, value = line.split('=', 1)
+            items[key] = value
+
+        return items

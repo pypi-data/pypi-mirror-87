818a0bf59a831ee654b4da802cf974be491f956b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:22 +0000

    Add package datacatalog-tag-exporter/0.3.2/datacatalog_tag_exporter-0.3.2-py2.py3-none-any.whl

---
+++ packages/datacatalog-tag-exporter/datacatalog_tag_exporter-0.3.2-py2.py3-none-any.whl/datacatalog_tag_exporter/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for datacatalog_tag_exporter."""
+
+from .datacatalog_tag_exporter_cli import main  # noqa: F401
+
+__author__ = """Marcelo Miranda"""
+__email__ = 'mesmacosta@gmail.com'
+__version__ = '0.3.1'
+
+__all__ = ('main')

1cbfed16f7fbdf7424bb3b5da788be2bc17497fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:18 +0000

    Add package py3dep/0.2.0/py3dep-0.2.0.tar.gz

---
+++ packages/py3dep/py3dep-0.2.0.tar.gz/py3dep-0.2.0/HISTORY.rst
@@ -0,0 +1,44 @@
+=======
+History
+=======
+
+0.2.0 (2020-12-06)
+------------------
+
+- Add support for multipolygon.
+- Remove the ``fill_hole`` argument.
+- Add a new function to get elevations for a list of coordinates called ``elevation_bycoords``.
+- Refactor ``elevation_bygrid`` function for increasing readability and performance.
+
+0.1.7 (2020-08-18)
+------------------
+
+- Added a rename operation to ``get_map`` to automatically rename the variables to a
+  more sensible one.
+- Replaced ``simplejson`` with ``orjson`` to speed-up JSON operations.
+
+0.1.6 (2020-08-11)
+------------------
+
+- Add a new function, ``show_versions``, for getting versions of the installed dependecies
+  which is useful for debugging and reporting.
+- Fix typos in the docs and improved the README.
+- Improve testing and coverage.
+
+0.1.5 (2020-08-03)
+------------------
+
+- Fixed the geometry CRS issue
+- Improved the documentation
+
+0.1.4 (2020-07-23)
+------------------
+
+- Refactor ``get_map`` to use ``pygeoutils`` package.
+- Change the versioning method to ``setuptools_scm``.
+- Polish README and add installation from ``conda-forge``.
+
+0.1.0 (2020-07-19)
+------------------
+
+- First release on PyPI.
---
+++ packages/py3dep/py3dep-0.2.0.tar.gz/py3dep-0.2.0/tests/test_py3dep.py
@@ -0,0 +1,72 @@
+import io
+
+import numpy as np
+import pytest
+from pygeoogc import MatchCRS
+from shapely.geometry import Polygon
+
+import py3dep
+
+DEF_CRS = "epsg:4326"
+ALT_CRS = "epsg:3857"
+
+
+@pytest.fixture
+def geometry():
+    return Polygon(
+        [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+    )
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_getmap(geometry):
+    lyr = ["DEM", "Slope Degrees"]
+    ds = py3dep.get_map(lyr, geometry.bounds, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
+    dem_10 = py3dep.get_map(lyr[0], geometry, 10, geo_crs=DEF_CRS, crs=ALT_CRS)
+    dem_1e3 = py3dep.get_map(lyr[0], geometry, 1e3, geo_crs=DEF_CRS, crs=ALT_CRS)
+    assert (
+        sorted(ds.keys()) == ["elevation", "slope_degrees"]
+        and abs(dem_10.mean().item() - dem_1e3.mean().item()) < 7e-2
+    )
+
+
+def test_loc():
+    elev = py3dep.elevation_byloc((-7766049.664788851, 5691929.739021257), ALT_CRS)
+    assert abs(elev - 356.59) < 1e-3
+
+
+@pytest.mark.flaky(max_runs=3)
+def test_deg2mpm(geometry):
+    slope = py3dep.get_map("Slope Degrees", geometry, 1e3)
+    slope = py3dep.deg2mpm(slope)
+    assert abs(slope.mean().item() - 0.05) < 1e-3
+
+
+def test_grid(geometry):
+    geo_crs = DEF_CRS
+    crs = "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +ellps=WGS84 +units=km +no_defs"
+    geom = MatchCRS.geometry(geometry, geo_crs, crs)
+    xmin, ymin, xmax, ymax = geom.bounds
+    res = 1
+    gx = np.arange(xmin, xmax, res)
+    gy = np.arange(ymin, ymax, res)
+    elev = py3dep.elevation_bygrid(gx, gy, crs, res * 1e3)
+    assert abs(elev.mean().item() - 295.763) < 1e-3
+
+
+def test_coords(geometry):
+    geo_crs = DEF_CRS
+    crs = "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +ellps=WGS84 +units=km +no_defs"
+    geom = MatchCRS.geometry(geometry, geo_crs, crs)
+    xmin, ymin, xmax, ymax = geom.bounds
+    res = 1
+    gx = np.arange(xmin, xmax, res)
+    gy = np.arange(ymin, ymax, res)
+    elev = py3dep.elevation_bycoords(list(zip(gx, gy)), crs, res * 1e3)
+    assert abs(elev.mean() - 282.006) < 1e-3
+
+
+def test_show_versions():
+    f = io.StringIO()
+    py3dep.show_versions(file=f)
+    assert "INSTALLED VERSIONS" in f.getvalue()

f9f8344ba4dd8c83eb921937022fe4a1e6f2eb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:00 +0000

    Add package xblock-utils/2.1.2/xblock-utils-2.1.2.tar.gz

---
+++ packages/xblock-utils/xblock-utils-2.1.2.tar.gz/xblock-utils-2.1.2/setup.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2014-2015 edX
+#
+# This software's license gives you freedom; you can copy, convey,
+# propagate, redistribute and/or modify this program under the terms of
+# the GNU Affero General Public License (AGPL) as published by the Free
+# Software Foundation (FSF), either version 3 of the License, or (at your
+# option) any later version of the AGPL published by the FSF.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in a file in the toplevel directory called
+# "AGPLv3". If not, see <http://www.gnu.org/licenses/>.
+#
+"""Set up for xblock-utils"""
+
+import os
+import os.path
+import sys
+from setuptools import setup
+
+
+def package_data(pkg, root_list):
+    """Generic function to find package_data for `pkg` under `root`."""
+    data = []
+    for root in root_list:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line.strip())
+        )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+') or
+        line.startswith('-c')
+    )
+
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+VERSION = '2.1.2'
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a v%s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+setup(
+    name='xblock-utils',
+    version=VERSION,
+    description='Various utilities for XBlocks',
+    long_description=README,
+    author='edX',
+    author_email='oscm@edx.org',
+    packages=[
+        'xblockutils',
+    ],
+    install_requires=load_requirements('requirements/base.in'),
+    package_data=package_data("xblockutils", ["public", "templates", "templatetags"]),
+    url='https://github.com/edx/xblock-utils',
+    classifiers=[
+        'Framework :: Django :: 2.2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+
+)

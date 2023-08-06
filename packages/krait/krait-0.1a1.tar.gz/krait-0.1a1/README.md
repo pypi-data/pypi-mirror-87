3a00cc9b785ff8c089a59c5635109326f976ddc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:19 +0000

    Add package krait/0.1a1/krait-0.1a1.tar.gz

---
+++ packages/krait/krait-0.1a1.tar.gz/krait-0.1a1/README.md
@@ -0,0 +1,11 @@
+# Krait – CLI for setting up new python projects
+
+![Build status badge](https://img.shields.io/github/workflow/status/taliamax/krait/build)
+
+
+## Extra documentation
+The following links might be helpful:
+- [Click Quickstart](https://click.palletsprojects.com/en/7.x/quickstart/)
+- [Pytest docs](https://docs.pytest.org/en/stable/contents.html)
+- [Pytest fixtures](https://docs.pytest.org/en/stable/fixture.html)
+- [Parametrizing tests](https://docs.pytest.org/en/stable/example/parametrize.html)
---
+++ packages/krait/krait-0.1a1.tar.gz/krait-0.1a1/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='krait',
+    description='A Python CLI tool to create new python projects.',
+    author='Natalia Maximo',
+    author_email='iam@natalia.dev',
+    packages=find_packages(
+        'src'
+    ),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    version='0.1a1',
+    install_requires=[
+        'click', 'jinja2'
+    ],
+    entry_points={
+        'console_scripts': ['krait = krait.cli:cli'],
+        'krait.cli_frameworks': [
+            'click = krait.lib.plugins.cli_frameworks:ClickFramework',
+            'none = krait.lib.plugins.cli_frameworks:NoCliFramework',
+        ],
+        'krait.linters': [
+            'flake8 = krait.lib.plugins.linters:Flake8',
+            'none = krait.lib.plugins.linters:NoLinter',
+        ],
+        'krait.type_checkers': [
+            'mypy = krait.lib.plugins.type_checkers:MyPy',
+            'none = krait.lib.plugins.type_checkers:NoTypeChecker',
+        ],
+        'krait.test_frameworks': [
+            'pytest = krait.lib.plugins.test_frameworks:Pytest',
+            'none = krait.lib.plugins.test_frameworks:NoTesting',
+        ],
+        'krait.automations': [
+            'gha = krait.lib.plugins.automations:GithubActions',
+            'none = krait.lib.plugins.automations:NoAutomation',
+        ],
+        'krait.helplinks': [
+            'krait = krait.lib.plugins.help_links:KraitHelpLinks',
+        ]
+    },
+)

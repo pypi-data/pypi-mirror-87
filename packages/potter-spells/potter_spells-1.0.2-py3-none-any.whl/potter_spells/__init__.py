69f0aec196bb5fa91f166b104a40517f175dc023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:02 +0000

    Add package potter-spells/1.0.2/potter_spells-1.0.2-py3-none-any.whl

---
+++ packages/potter-spells/potter_spells-1.0.2-py3-none-any.whl/potter_spells/__init__.py
@@ -0,0 +1,11 @@
+from .find import find
+from .all_data import all_data
+from .all_by_type import all_by_type
+from .all_enchantment_names import all_enchantment_names
+from .all_spell_names import all_spell_names
+from .enchants_by_type import enchants_by_type
+from .spells_by_type import spells_by_type
+
+__name__ = 'potter_spells'
+__version__ = '1.0.2'
+__author__ = 'Vibhu Agarwal'

00713a61edbabf8e54c48fa0fe290331bc7ff9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:47 +0000

    Add package my-3rdAppPipInstallable-TestScript/0.1/my-3rdAppPipInstallable-TestScript-0.1.tar.gz

---
+++ packages/my-3rdAppPipInstallable-TestScript/my-3rdAppPipInstallable-TestScript-0.1.tar.gz/my-3rdAppPipInstallable-TestScript-0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  5 09:41:59 2020
+
+@author: user
+"""
+
+from setuptools import setup
+
+setup(
+      name = "my-3rdAppPipInstallable-TestScript",
+      version = "0.1",
+      author = "PtiPas",
+      description = "A test script",
+      scripts = ["ndAppPip"]
+)

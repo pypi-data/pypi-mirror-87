8e7653b00060414ca1c1bfa19e7e247d6ec10680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:20 +0000

    Add package onhold-ding/0.1.0/onhold_ding-0.1.0-py2.py3-none-any.whl

---
+++ packages/onhold-ding/onhold_ding-0.1.0-py2.py3-none-any.whl/ding/after.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from pathlib import Path
+
+from onhold.base import dumb_pipe, using_path, is_pipeline
+from play_sounds import play_after, bell_after, DEFAULT_SOUND
+import click
+
+
+ENV_VAR = 'DING'
+
+
+def run(file: Optional[Path]):
+  with play_after(file):
+    if not is_pipeline():
+      return
+
+    dumb_pipe()
+
+
+@click.command(help="Play specified sound after job is complete.")
+@click.option('-s', '--sound_path', required=False,
+  type=click.Path(exists=True), help="Path to sound to play.")
+@click.option('-b', '--bell', required=False,
+  default=False, show_default=True, is_flag=True,
+  type=click.BOOL, help="Ring the terminal bell, as well.")
+@click.option('-w', '--warn', required=False,
+  is_flag=True, default=False, help="Show warnings.")
+def cmd(sound_path: Optional[str], bell: bool, warn: bool):
+  with bell_after(bell), \
+       using_path(sound_path, warn, DEFAULT_SOUND, ENV_VAR) as path:
+    run(path)
+
+
+if __name__ == "__main__":
+  cmd()

137e84f6b6f0e4a78100f58d9835b12423a44f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:10 +0000

    Add package zephyrus-sc2-parser/0.2.30/zephyrus-sc2-parser-0.2.30.tar.gz

---
+++ packages/zephyrus-sc2-parser/zephyrus-sc2-parser-0.2.30.tar.gz/zephyrus-sc2-parser-0.2.30/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zephyrus-sc2-parser",
+    version="0.2.30",
+    author="Luke Holroyd",
+    author_email="hello@zephyrus.gg",
+    description="Parser for SC2 replay files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZephyrBlu/zephyrus-sc2-parser",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'mpyq',
+        'pytz',
+        # 'sc2_simulator',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+    ],
+    python_requires='>=3.6',
+)

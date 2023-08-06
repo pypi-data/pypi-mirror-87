a6a0dd19b12403df77eae1a8d5e1eea83a3a2c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package env-canada/0.3.0/env_canada-0.3.0.tar.gz

---
+++ packages/env-canada/env_canada-0.3.0.tar.gz/env_canada-0.3.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="env_canada",
+    version="0.3.0",
+    author="Michael Davie",
+    author_email="michael.davie@gmail.com",
+    description="A package to access meteorological data from Environment Canada",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michaeldavie/env_canada",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "aiohttp",
+        "geopy",
+        "imageio",
+        "Pillow",
+        "python-dateutil",
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

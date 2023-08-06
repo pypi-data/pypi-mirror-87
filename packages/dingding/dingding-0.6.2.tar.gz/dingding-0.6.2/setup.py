a21e3319e5ac229559c4eb2afff4940d792cc8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:53 +0000

    Add package dingding/0.6.2/dingding-0.6.2.tar.gz

---
+++ packages/dingding/dingding-0.6.2.tar.gz/dingding-0.6.2/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dingding",
+    version="0.6.2",
+    author="Twotiger",
+    author_email="dgl0518@gmail.com",
+    description="ding ding robot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Twotiger/dingding",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

627675ee9d35b1c5bc1a17b54986e10235c41d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package quantconnect-stubs/9931/quantconnect_stubs-9931-py2-none-any.whl

---
+++ packages/quantconnect-stubs/quantconnect_stubs-9931-py2-none-any.whl/QuantConnect/Lean/Launcher/__init__.pyi
@@ -0,0 +1,22 @@
+import typing
+
+import QuantConnect.Lean.Launcher
+import System
+
+
+class Program(System.Object):
+    """This class has no documentation."""
+
+    @staticmethod
+    def Main(args: typing.List[str]) -> None:
+        ...
+
+    @staticmethod
+    def ExitKeyPress(sender: System.Object, args: System.ConsoleCancelEventArgs) -> None:
+        ...
+
+    @staticmethod
+    def Exit() -> None:
+        ...
+
+

02993a779ec766e9acf41a4f24b719399987a708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:18 +0000

    Add package caipirinha-cmdtools/0.1.0/caipirinha_cmdtools-0.1.0.tar.gz

---
+++ packages/caipirinha-cmdtools/caipirinha_cmdtools-0.1.0.tar.gz/caipirinha_cmdtools-0.1.0/caipirinha_cmdtools/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Caipirinha Command Tools."""
+
+__author__ = """Ralph Brecheisen"""
+__email__ = 'ralph.brecheisen@gmail.com'
+__version__ = '0.1.0'
+
+from .castorclient.castorclient import CastorClient

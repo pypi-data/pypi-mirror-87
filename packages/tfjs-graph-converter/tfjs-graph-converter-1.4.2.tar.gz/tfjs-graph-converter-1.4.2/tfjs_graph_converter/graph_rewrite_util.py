e63b943c4dcecb25ed63e85ec002b162e22fca9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:59 +0000

    Add package tfjs-graph-converter/1.4.2/tfjs-graph-converter-1.4.2.tar.gz

---
+++ packages/tfjs-graph-converter/tfjs-graph-converter-1.4.2.tar.gz/tfjs-graph-converter-1.4.2/tfjs_graph_converter/graph_rewrite_util.py
@@ -0,0 +1,373 @@
+# SPDX-License-Identifier: MIT
+# Copyright © 2020 Patrick Levin
+"""Utility functions for rewriting TensorFow Graphs"""
+
+from typing import Any, Callable, Dict, List, Optional, Text, Tuple, Union
+
+from tensorflow import as_dtype, cast as tensor_cast, Tensor as TfTensor
+from tensorflow.core.framework.attr_value_pb2 import AttrValue
+from tensorflow.core.framework.graph_pb2 import GraphDef
+from tensorflow.core.framework.node_def_pb2 import NodeDef
+from tensorflow.core.framework.op_def_pb2 import OpDef
+from tensorflow.core.framework.tensor_pb2 import TensorProto
+from tensorflow.core.framework.tensor_shape_pb2 import TensorShapeProto
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import op_def_registry
+
+from numpy import ndarray as Tensor
+
+# common type definitions
+NodeList = List[NodeDef]
+NameOrNode = Union[Text, NodeDef]
+NameToNode = Dict[Text, NodeDef]
+InputList = List[NameOrNode]
+Inputs = Union[NameOrNode, InputList]
+WeightTransform = Callable[[Tensor], Tensor]
+WeightModifiers = Dict[Text, WeightTransform]
+NodeTransform = Callable[[NodeDef, NameToNode, WeightModifiers], NodeList]
+TensorDict = Dict[str, TfTensor]
+
+
+def get_op_def(op_name: Text) -> Optional[OpDef]:
+    """
+    Get the definition for a native TF operation.
+    This is useful for checking whether an operation is supported or
+    to get all valid inputs and attributes.
+
+    Args:
+        op_name: Name of the native TF operation (e.g. "AddV2")
+
+    Returns:
+        Protobuf object containing the operation definition
+        `None` is returned, if the operation is not registered with TF
+    """
+    return op_def_registry.get(op_name)
+
+
+def make_op_node(op_name: Text, inputs: Inputs, name: Text = None,
+                 dtype: Any = None) -> NodeDef:
+    """
+    Create a TF graph node given the operation, input, and a name.
+    The resulting node definition won't include any operation-specific
+    attributes. It returns a valid node for most operations, though.
+
+    Args:
+        op_name: Native TF operation name (e.g. "MatMul")
+        inputs: Input node, node name, or list of inputs nodes or node names
+        name: Node name in the graph, must be unique and defaults to the
+              operation name
+        dtype: Optional data type of the operation (default: float32)
+
+    Returns:
+        TF graph node definition for the given operation, inputs, and name
+    """
+    # convert scalar input into list
+    input_list: list = [inputs] if not isinstance(inputs, list) else inputs
+    # convert list items to strings
+    for i, item in enumerate(input_list):
+        if hasattr(item, 'name'):
+            input_list[i] = item.name
+    # generate node defintion
+    if dtype is None:
+        dtype = dtypes.float32.as_datatype_enum
+    elif hasattr(dtype, 'as_datatype_enum'):
+        dtype = dtype.as_datatype_enum
+    else:
+        dtype = dtypes.as_dtype(dtype).as_datatype_enum
+
+    node_def = NodeDef(op=op_name, name=name or op_name,
+                       attr={'T': AttrValue(type=dtype)})
+    node_def.input.extend(input_list)
+    return node_def
+
+
+def make_const_node(data: Tensor, name: str = None) -> NodeDef:
+    """
+    Create a TF graph node containing a constant value.
+    The resulting node is equivalent to using `tf.constant` on the
+    default graph.
+
+    Args:
+        data: Numpy-array containing the data, shape, and datatype
+        name: Optional name of the node
+
+    Returns:
+        Graph node for adding to a TF Graph instance
+    """
+    dtype = as_dtype(data.dtype).as_datatype_enum
+    tensor_content = data.tobytes()
+    tensor_dim = [TensorShapeProto.Dim(size=size) for size in data.shape]
+    tensor_shape = TensorShapeProto(dim=tensor_dim)
+    tensor_proto = TensorProto(tensor_content=tensor_content,
+                               tensor_shape=tensor_shape,
+                               dtype=dtype)
+    node_def = NodeDef(op='Const', name=name or 'Const',
+                       attr={
+                           'value': AttrValue(tensor=tensor_proto),
+                           'dtype': AttrValue(type=dtype)
+                        })
+    return node_def
+
+
+def copy_op_attrs(source: NodeDef, target: NodeDef) -> NodeDef:
+    """
+    Copy valid node attributes from one node to another.
+    Only attributes supported by the target node's operation will be copied.
+    This is useful when splitting fused operations to retain the attributes
+    of the original, non-fused operation in the isolated target node.
+
+    Existing attributes will be overridden.
+
+    Args:
+        source: Graph node containing attributes to copy
+        target: Graph node to copy the attributes to
+
+    Returns:
+        The updated target node.
+    """
+    op_def = get_op_def(target.op)
+    if op_def is None:
+        raise ValueError(f'Node {target.name}: unknown op name {target.op}')
+    attrs_to_copy = set(attr.name for attr in op_def.attr)
+    for key in source.attr:
+        if key in attrs_to_copy:
+            target.attr[key].CopyFrom(source.attr[key])
+    return target
+
+
+def update_graph_def(input_graph_def: GraphDef,
+                     nodes_to_remap: Dict[Text, List[NodeDef]],
+                     inputs_to_replace: Dict[Text, Text]) -> GraphDef:
+    """
+    Update a TF graph_def by replacing nodes and node inputs.
+    There will be no consistency check in this function.
+    Callers have to make sure the given remappings and input replacements
+    result in a valid graph.
+
+    Args:
+        input_graph_def: TF graph_def with nodes or node inputs to replace
+        nodes_to_remap: `dict` that maps node names to a list of replacement
+            nodes. Nodes whose name map to an empty list, will be
+            removed from the returned graph.
+            Nodes that are not in the input graph_def but have an
+            entry in the remap dict, will be ignored.
+        inputs_to_replace: `dict` that maps node names to replacement names.
+            Nodes that have been removed need to be replaced in all referenced
+            graph nodes. This mapping can be used to make sure this happens.
+
+    Returns:
+        An updated copy of the input graph_def. The original inputs remains
+        unchanged.
+    """
+    result_graph_def = GraphDef()
+    for node in input_graph_def.node:
+        if node.name in nodes_to_remap:
+            nodes_to_insert = nodes_to_remap[node.name]
+            if nodes_to_insert and len(nodes_to_insert) > 0:
+                result_graph_def.node.extend(nodes_to_insert)
+            continue
+        new_node = NodeDef()
+        new_node.CopyFrom(node)
+        for i, input_node in enumerate(new_node.input):
+            if input_node in inputs_to_replace:
+                new_node.input[i] = inputs_to_replace[input_node]
+        result_graph_def.node.extend([new_node])
+    result_graph_def.versions.CopyFrom(input_graph_def.versions)
+    return result_graph_def
+
+
+def get_input_node_map(input_graph_def: GraphDef) -> NameToNode:
+    """
+    Return a mapping from node names to node_def instances from a given
+    graph_def.
+    The result can be used to check whether a node name is referenced in
+    the graph or to quickly lookup the node_def given a node name.
+
+    Args:
+        input_graph_def: TF graph_def containing the nodes to generate the
+                         mapping from.
+
+    Returns:
+        `dict` that maps node names to the corresponding node_def instances.
+    """
+    input_node_map = dict()
+    for node in input_graph_def.node:
+        if node.name not in input_node_map:
+            input_node_map[node.name] = node
+        else:
+            raise ValueError(f'Duplicate node name: {node.name}')
+    return input_node_map
+
+
+def replace_matching_nodes(input_graph_def: GraphDef,
+                           predicate: Callable[[NodeDef], bool],
+                           transform: NodeTransform
+                           ) -> Tuple[GraphDef, WeightModifiers]:
+    """
+    Replace all nodes that match a given predicate using the provided
+    transformation function and return the new graph.
+    The transformation function can also register a function to modify
+    existing node weights or variables.
+
+    Args:
+        input_graph_def: TF graph_def to traverse and possibly modify
+        predicate: a callable that takes a node_def and returns `True`
+            if the node should be transformed
+        transform: a callable that receives a graph node, a node map
+            containing the names of all current graph nodes, and a
+            `dict` that can be used to add node weight modifiers.
+            The function is expected to return a list of nodes that
+            replace the given node.
+            The first node of this list is expected to receive at least
+            one input of the replaced node.
+            The last node in the returned list is expected to replace all
+            references to the original node.
+
+    Returns:
+        Updated copy of the input graph with matching nodes replaced by
+        the output of the transform function.
+    """
+    input_node_map = get_input_node_map(input_graph_def)
+    nodes_to_remap = {}
+    inputs_to_remap = {}
+    weight_modifiers: WeightModifiers = {}
+    for node in input_graph_def.node:
+        if predicate(node):
+            # the name of a replaced node becomes available for use right away
+            del input_node_map[node.name]
+            new_nodes = transform(node, input_node_map, weight_modifiers)
+            nodes_to_remap[node.name] = new_nodes
+            if new_nodes and len(new_nodes) > 0:
+                # by convention, the output of the last node in the returned
+                # sub-graph replaces the output of the original node
+                inputs_to_remap[node.name] = new_nodes[-1].name
+                # we need to update the input node map to avoid duplicate names
+                for new_node in new_nodes:
+                    input_node_map[new_node.name] = new_node
+    output_graph_def = update_graph_def(input_graph_def, nodes_to_remap,
+                                        inputs_to_remap)
+    return output_graph_def, weight_modifiers
+
+
+def generate_name_from(base_name: Text,
+                       input_node_map: NameToNode,
+                       suffix: Optional[Text] = None) -> Text:
+    """
+    The tfjs converter generates names separated by forward slashes ('/').
+    We can get the root name of an operation node by splitting off the last
+    part (optionally with an added suffix):
+
+    prefix/model_name/node_name/layer_name -> prefix/model_name/node_name
+
+    The resulting name is then made unique with repespect to the given node
+    mapping.
+
+    Args:
+        base_name: Node name to generate the new name from
+        input_node_map: `dict` containing the names of all nodes in the graph
+        suffix: optional suffix that will replace the removed part of the
+                original name
+
+    Returns:
+        Root name of the given node name; guaranteed to be unique within the
+        graph.
+    """
+    base_name = '/'.join(base_name.split('/')[:-1]) or base_name
+    if suffix:
+        base_name = base_name + '/' + suffix
+    target_name = base_name
+    count = 0
+    while target_name in input_node_map:
+        count += 1
+        target_name = f'{base_name}_{count}'
+    return target_name
+
+
+def is_fused_op(node: NodeDef, op_name: Text, activation: Text) -> bool:
+    """
+    Return whether a node represents a fused TF operation.
+
+    Args:
+        node: Node defintion
+        op_name: Fused operation name (e.g. 'MatMul')
+        activation: Name of the fused activation function (e.g. 'Relu')
+
+    Returns:
+        `True`, iff the node is a fused operation with the given activation
+    """
+    if node.op == f'_Fused{op_name}' and 'fused_ops' in node.attr:
+        fused_ops = node.attr['fused_ops'].list.s
+        return (len(fused_ops) == 2
+                and fused_ops[0] in (b'BiasAdd', b'BiasAddV1')
+                and fused_ops[1] == activation)
+    return False
+
+
+def is_fused_conv2d(node: NodeDef, activation: Text) -> bool:
+    """Return whether a node is a fused conv2d operation with given activation
+    """
+    return is_fused_op(node, 'Conv2D', activation)
+
+
+def is_fused_matmul(node: NodeDef, activation: Text) -> bool:
+    """Return whether a node is a fused matmul operation with given activation
+    """
+    return is_fused_op(node, 'MatMul', activation)
+
+
+def is_fused_depthwise(node: NodeDef):
+    """Return whether a node is a fused DepthwiseConv2DNative with bias and
+       optional activation
+    """
+    if node.op == 'FusedDepthwiseConv2dNative' and 'fused_ops' in node.attr:
+        fused_ops = node.attr['fused_ops'].list.s
+        return (len(fused_ops) in (1, 2)
+                and fused_ops[0] in (b'BiasAdd', b'BiasAddV1'))
+    else:
+        return False
+
+
+def validate_supported_ops(input_graph_def: GraphDef) -> None:
+    """
+    Iterate through all graph nodes and validate operation names.
+
+    Args:
+        input_graph_def: Input graph to validate
+
+    Raises:
+        ValueError: the graph contains an unsupported operation
+    """
+    for node in input_graph_def.node:
+        if not get_op_def(node.op):
+            raise ValueError(f'Node {node.name}: unsupported op {node.op}')
+        if 'fused_ops' in node.attr:
+            # check all fused operations as well
+            fused_ops = list(node.attr['fused_ops'].list.s)
+            unsupported_ops = [op for op in fused_ops if not get_op_def(op)]
+            if any(unsupported_ops):
+                raise ValueError(f'Node {node.name}: unsupported fused op '
+                                 f'{unsupported_ops[0]}')
+
+
+def harmonize_dtypes(graph_def: GraphDef, weights: TensorDict) -> TensorDict:
+    """Iterate through the weight dictionary and ensure matching dtypes
+    between tensor data and graph nodes
+
+    Args:
+        graph_def: GraphDef proto containing the network layout
+        weights: Dictionary that maps node names to tf tensor data
+
+    Returns:
+        Updated weight dictionary
+    """
+    node_by_name = get_input_node_map(graph_def)
+    for key, value in weights.items():
+        if key in node_by_name:                 # must refer to existing node
+            node = node_by_name[key]
+            if 'dtype' in node.attr:            # must have 'dtype' attribute
+                node_type = as_dtype(node.attr['dtype'].type)
+                tensor_type = as_dtype(value.dtype)
+                if node_type != tensor_type:    # must have difference in type
+                    weights[key] = tensor_cast(value, node_type)
+    return weights
---
+++ packages/tfjs-graph-converter/tfjs-graph-converter-1.4.2.tar.gz/tfjs-graph-converter-1.4.2/tfjs_graph_converter/util.py
@@ -0,0 +1,254 @@
+# SPDX-License-Identifier: MIT
+# Copyright © 2020 Patrick Levin
+"""Utility functions for working with TensorFlow Graphs"""
+from __future__ import absolute_import
+
+from collections import defaultdict, namedtuple
+from typing import List, Optional, Union
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.core.framework.graph_pb2 import GraphDef
+from tensorflow.core.framework.node_def_pb2 import NodeDef
+from tensorflow.core.protobuf.meta_graph_pb2 import SignatureDef
+from tensorflow.core.protobuf.meta_graph_pb2 import TensorInfo
+
+import tfjs_graph_converter.common as c
+import tfjs_graph_converter.graph_rewrite_util as rewrite
+
+_DTYPE_MAP: List[Optional[type]] = [
+    None,
+    np.float32,
+    np.float64,
+    np.int32,
+    np.uint8,
+    np.int16,
+    np.int8,
+    None,
+    np.complex64,
+    np.int64,
+    np.bool
+]
+
+NodeInfo = namedtuple('NodeInfo', 'name shape dtype tensor')
+
+
+def _is_op_node(node: NodeDef) -> bool:
+    return node.op not in (c.TFJS_NODE_CONST_KEY, c.TFJS_NODE_PLACEHOLDER_KEY)
+
+
+def _op_nodes(graph_def: GraphDef) -> List[NodeDef]:
+    return [node for node in graph_def.node if _is_op_node(node)]
+
+
+def _map_type(type_id: int) -> Optional[type]:
+    if type_id < 0 or type_id > len(_DTYPE_MAP):
+        raise ValueError(f'Unsupported data type: {type_id}')
+    np_type = _DTYPE_MAP[type_id]
+    return np_type
+
+
+def _get_shape(node: NodeDef) -> List[int]:
+    def shape(attr): return attr.shape.dim
+    def size(dim): return dim.size if dim.size > 0 else None
+    # accessing non-existing keys would CREATE the key in mapfields!
+    if c.TFJS_ATTR_SHAPE_KEY in node.attr:
+        return [size(dim) for dim in shape(node.attr[c.TFJS_ATTR_SHAPE_KEY])]
+    else:
+        return []
+
+
+def _dtype(node: NodeDef) -> Optional[type]:
+    # accessing non-existing keys would CREATE the key in mapfields!
+    if c.TFJS_ATTR_DTYPE_KEY in node.attr:
+        return _map_type(node.attr[c.TFJS_ATTR_DTYPE_KEY].type)
+    elif 'T' in node.attr:
+        return _map_type(node.attr['T'].type)
+    else:
+        return None
+
+
+def _node_info(node: NodeDef) -> NodeInfo:
+    return NodeInfo(name=node.name, shape=_get_shape(node), dtype=_dtype(node),
+                    tensor=node.name + ':0')
+
+
+def _build_tensor_info(graph: tf.Graph, info: NodeInfo) -> TensorInfo:
+    if info is not None:
+        tensor = graph.get_tensor_by_name(info.tensor)
+        return TensorInfo(
+            dtype=tf.dtypes.as_dtype(tensor.dtype).as_datatype_enum,
+            tensor_shape=tf.TensorShape(tensor.shape).as_proto(),
+            name=info.tensor)
+    else:
+        return None
+
+
+def get_input_nodes(graph: Union[tf.Graph, GraphDef]) -> List[NodeInfo]:
+    """
+    Return information about a graph's inputs.
+
+    Args:
+        graph: Graph or GraphDef object
+
+    Returns:
+        List of NodeInfo tuples holding name, shape, and type of the input
+    """
+    if isinstance(graph, tf.Graph):
+        graph_def = graph.as_graph_def()
+    else:
+        graph_def = graph
+
+    def is_input(node): return node.op == c.TFJS_NODE_PLACEHOLDER_KEY
+    return [_node_info(node) for node in graph_def.node if is_input(node)]
+
+
+def get_output_nodes(graph: Union[tf.Graph, GraphDef]) -> List[NodeInfo]:
+    """
+    Return information about a graph's outputs.
+
+    Args:
+        graph: Graph or GraphDef object
+
+    Returns:
+        List of NodeInfo tuples holding name, shape, and type of the input;
+        shape will be left empty
+    """
+    # normalise input
+    if isinstance(graph, tf.Graph):
+        graph_def = graph.as_graph_def()
+    else:
+        graph_def = graph
+    # visit graph nodes and test for references
+    # assumption: all referenced nodes are declared *before* use
+    ops = _op_nodes(graph_def)
+    outputs = []
+    for i, node in enumerate(ops):
+        has_ref = False
+        for test in ops[i+1:]:
+            if node.name in test.input:
+                has_ref = True
+                break
+        if not has_ref:
+            outputs.append(_node_info(node))
+    return outputs
+
+
+def get_input_tensors(graph: Union[tf.Graph, GraphDef]) -> List[str]:
+    """
+    Return the names of the graph's input tensors.
+
+    Args:
+        graph: Graph or GraphDef object
+
+    Returns:
+        List of tensor names
+    """
+    return [node.tensor for node in get_input_nodes(graph)]
+
+
+def get_output_tensors(graph: Union[tf.Graph, GraphDef]) -> List[str]:
+    """
+    Return the names of the graph's output tensors.
+
+    Args:
+        graph: Graph or GraphDef object
+
+    Returns:
+        List of tensor names
+    """
+    return [node.tensor for node in get_output_nodes(graph)]
+
+
+def infer_signature(graph: tf.Graph) -> Optional[SignatureDef]:
+    """
+    Return the graph's signature (inputs and outputs) as a TF SignatureDef.
+
+    Args:
+        graph: Graph object
+
+    Returns:
+        TF SignatureDef from Graph; None, iff signature couldn't be inferred
+    """
+    inputs = {info.name: _build_tensor_info(graph, info)
+              for info in get_input_nodes(graph)}
+    outputs = {info.name: _build_tensor_info(graph, info)
+               for info in get_output_nodes(graph)}
+    if all(tensor_info is not None for tensor_info in outputs.values()):
+        # only valid if we could infer all output shapes
+        return SignatureDef(inputs=inputs, outputs=outputs,
+                            method_name=tf.saved_model.PREDICT_METHOD_NAME)
+    else:
+        return None
+
+
+def rename_input_nodes(graph_def: GraphDef, name_mapping: dict) -> GraphDef:
+    """Rename input nodes - the update is performed in-place.
+
+        Args:
+            graph_def: GraphDef proto containing the model
+            name_mapping: dict that maps input names to new names
+
+        Returns:
+            Updated GraphDef proto - same as input since the update is
+            performed in-place.
+    """
+    _validate_mapping(graph_def, get_input_nodes(graph_def), name_mapping)
+    renamed_nodes = name_mapping.keys()
+    for node in graph_def.node:
+        if node.name in renamed_nodes:
+            node.name = name_mapping[node.name]
+        else:
+            for idx, name in enumerate(node.input):
+                if name in renamed_nodes:
+                    node.input[idx] = name_mapping[name]
+    return graph_def
+
+
+def rename_output_nodes(graph_def: GraphDef, name_mapping: dict) -> GraphDef:
+    """Rename output nodes - the update is performed in-place.
+
+        Args:
+            graph_def: GraphDef proto containing the model
+            name_mapping: dict that maps output names to new names
+
+        Returns:
+            Updated GraphDef proto - same as input since the update is
+            performed in-place.
+    """
+    output_nodes = get_output_nodes(graph_def)
+    _validate_mapping(graph_def, output_nodes, name_mapping)
+    nodes_to_rename = name_mapping.keys()
+    target_nodes = filter(lambda n: n.name in nodes_to_rename, graph_def.node)
+    for node in target_nodes:
+        new_name = name_mapping[node.name]
+        if node.op == 'Identity':
+            # dummy nodes can just be renamed
+            node.name = new_name
+        else:
+            # append an identity node that takes the original output as input
+            identity = rewrite.make_op_node('Identity', node, name=new_name,
+                                            dtype=_dtype(node))
+            graph_def.node.append(identity)
+    return graph_def
+
+
+def _validate_mapping(graph_def: GraphDef, valid_nodes: List[NodeInfo],
+                      mapping: dict):
+    valid_names = set(info.name for info in valid_nodes)
+    existing_nodes = set(node.name for node in graph_def.node)
+    inverted_mapping: dict = defaultdict(list)
+    for old_name, new_name in mapping.items():
+        if old_name not in valid_names:
+            raise ValueError(f'"{old_name}" is not a valid node name. '
+                             f'Valid names are {str(valid_names)[1:-1]}')
+        if new_name in existing_nodes:
+            raise ValueError(f'"{old_name}" cannot be renamed to "{new_name}":'
+                             ' the name is already in use.')
+        already_mapped = inverted_mapping[new_name]
+        if len(already_mapped) > 0:
+            mapped_to = already_mapped[0]
+            raise ValueError(f'"{old_name}" cannot be renamed to "{new_name}":'
+                             f' the name is already mapped to "{mapped_to}"')
+        else:
+            already_mapped.append(old_name)

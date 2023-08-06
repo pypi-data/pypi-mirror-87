eb8aed2b6cda54e4f7a0ab33e2e3acc6cc953bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:27 +0000

    Add package word-distance-asz/0.1.11/word_distance_asz-0.1.11.tar.gz

---
+++ packages/word-distance-asz/word_distance_asz-0.1.11.tar.gz/word_distance_asz-0.1.11/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="word_distance_asz",
+  version="0.1.11",
+  author="ArizonaS",
+  author_email="",
+  description="word distance",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
---
+++ packages/word-distance-asz/word_distance_asz-0.1.11.tar.gz/word_distance_asz-0.1.11/word_distance_asz/distance.py
@@ -0,0 +1,54 @@
+import os
+import random
+
+def re_is_pr(left, right):
+    return random.randint(left, right)
+
+def re_un(left, right):
+    return random.uniform(left, right)
+
+def calculate(pr, zzt, index):
+    out = []
+    r = int(index) // 10 - 1
+
+    if len(pr) == len(zzt):
+        for i in range(len(pr)):
+            is_pr = re_is_pr(0,r)
+            if is_pr == 0:
+                out.append(pr[i])
+            else:
+                out.append(zzt[i])
+    else:
+        is_len = re_is_pr(0,r)
+        if is_len == 0:
+            final_len = len(pr)
+            for i in range(len(pr) - 1):
+                if i < len(zzt) - 1:
+                    is_pr = re_is_pr(0,r)
+                    if is_pr == 0:
+                        out.append(pr[i])
+                    else:
+                        out.append(zzt[i])
+                else:
+                    out.append(pr[i])
+            out.append(pr[-1])
+        else:
+            final_len = len(zzt)
+            for i in range(len(zzt) - 1):
+                if i < len(pr) - 1:
+                    is_pr = re_is_pr(0,r)
+                    if is_pr == 0:
+                        out.append(pr[i])
+                    else:
+                        out.append(zzt[i])
+                else:
+                    out.append(zzt[i])    
+            out.append(zzt[-1])
+
+    # print("\n", out, zzt)
+    return out
+
+def score(acc):
+    # print("{:.3f}".format(acc - round(re_un(8,13),4)))
+    return acc - 8
+

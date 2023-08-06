3ee1cf2edbe3b1a697e9ccfe4f7fc0718ac28635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:36 +0000

    Add package nornir-pyez/0.0.8/nornir_pyez-0.0.8-py3-none-any.whl

---
+++ packages/nornir-pyez/nornir_pyez-0.0.8-py3-none-any.whl/nornir_pyez/plugins/tasks/__init__.py
@@ -0,0 +1,19 @@
+from .pyez_facts import pyez_facts
+from .pyez_config import pyez_config
+from .pyez_get_config import pyez_get_config
+from .pyez_commit import pyez_commit
+from .pyez_diff import pyez_diff
+from .pyez_int_terse import pyez_int_terse
+from .pyez_route_info import pyez_route_info
+from .pyez_rpc import pyez_rpc
+
+__all__ = (
+    "pyez_facts",
+    "pyez_config",
+    "pyez_get_config",
+    "pyez_diff",
+    "pyez_commit",
+    "pyez_int_terse",
+    "pyez_route_info",
+    "pyez_rpc",
+)

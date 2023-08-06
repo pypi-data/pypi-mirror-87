a6e32b0813b05baf48b6c837b21982ef96a8f7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:10 +0000

    Add package relay-sdk/0.1.4/relay_sdk-0.1.4-py3-none-any.whl

---
+++ packages/relay-sdk/relay_sdk-0.1.4-py3-none-any.whl/relay_sdk/outputs.py
@@ -0,0 +1,35 @@
+"Outputs allow a step to write data that will be available for later steps"
+import json
+import logging
+from typing import Any
+from urllib.parse import quote
+
+from requests import Session
+
+from .util import JSONEncoder
+
+logger = logging.getLogger(__name__)
+
+
+class Outputs:
+    """Use Outputs to write keys to the metadata service
+        that later steps can retrieve"""
+    def __init__(self, client: Session) -> None:
+        self._client = client
+
+    def set(self, name: str, value: Any) -> None:
+        """Set writes a value of a given name to the service
+
+        Args:
+            name: a string containing the key name to write
+            value: the value of the key to be written. Can
+                be any data type that can serialize to JSON
+        """
+        r = self._client.put(
+            'http+api://api/outputs/{0}'.format(quote(name)),
+            data=json.dumps(value, cls=JSONEncoder),
+            headers={'content-type': 'application/json'},
+        )
+        r.raise_for_status()
+
+        logger.info('Set output %s', repr(name))

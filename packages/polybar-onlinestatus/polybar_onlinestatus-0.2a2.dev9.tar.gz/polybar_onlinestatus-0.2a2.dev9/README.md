331b9cff75a3d6a41d90544e63034d1222fd9636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:24 +0000

    Add package polybar-onlinestatus/0.2a2.dev9/polybar_onlinestatus-0.2a2.dev9.tar.gz

---
+++ packages/polybar-onlinestatus/polybar_onlinestatus-0.2a2.dev9.tar.gz/polybar_onlinestatus-0.2a2.dev9/README.md
@@ -0,0 +1,23 @@
+# polybar-onlinestatus
+polybar module that checks if you are online
+
+To install use `pip install polybar-onlinestatus`
+
+And use module below:
+
+```
+[module/onlinestatus]
+type = custom/script
+exec = polybar-onlinestatus
+tail = true
+label = %{T4} %output% %{T-}
+format-underline = #07BDEE
+```
+
+```font-3 = Hack Nerd Font:style=Bold:size=12```
+
+You can use any font you want, but make sure icon is big enough. I recommend Nerd Hack but it's up to you.
+
+For more options use ```polybar-onlinestatus --help``` in your terminal and assign options in ```exec = polybar-onlinestatus``` line.
+
+Icon used by default is ``, if you are unable to see it - install nerd font from nerdfonts.com, replace it with your own or use --boolean-mode.
---
+++ packages/polybar-onlinestatus/polybar_onlinestatus-0.2a2.dev9.tar.gz/polybar_onlinestatus-0.2a2.dev9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+from polybar_onlinestatus import __version__ as version
+
+# META DATA
+__author__ = "sTiKyt"
+__version__ = version
+__package_name__ = "polybar_onlinestatus"
+__description__ = "Polybar module to check if you are online"
+__python_requires__ = ">=3.6.*"
+__url__ = "https://github.com/sTiKyt/polybar-onlinestatus"
+
+with open("README.md", "r") as readme:
+    __long_description__ = readme.read()
+
+setup(
+    name=__package_name__,
+    version=__version__,
+    author=__author__,
+    description=__description__,
+    python_requires=__python_requires__,
+    url=__url__,
+    long_description=__long_description__,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=("tests",)),
+    entry_points={
+        "console_scripts": [
+            'polybar-onlinestatus = polybar_onlinestatus.polybar_onlinestatus:main'
+        ]
+    }
+
+)

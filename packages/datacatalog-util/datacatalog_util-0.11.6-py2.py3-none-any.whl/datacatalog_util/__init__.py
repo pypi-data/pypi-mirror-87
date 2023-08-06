6446acdd413cb229b82e3655270a2a009ac3fe67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package datacatalog-util/0.11.6/datacatalog_util-0.11.6-py2.py3-none-any.whl

---
+++ packages/datacatalog-util/datacatalog_util-0.11.6-py2.py3-none-any.whl/datacatalog_util/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for datacatalog_util."""
+
+from .datacatalog_util_cli import main  # noqa: F401
+
+__author__ = """Marcelo Miranda"""
+__email__ = 'mesmacosta@gmail.com'
+__version__ = '0.11.4'
+
+__all__ = ('main')

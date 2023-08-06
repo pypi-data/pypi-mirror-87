d5923f30de911da0cb931b03be704cc8b8a6560b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:40 +0000

    Add package pyraider/1.0.9/pyraider-1.0.9.tar.gz

---
+++ packages/pyraider/pyraider-1.0.9.tar.gz/pyraider-1.0.9/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='pyraider',
+    version='1.0.9',
+    packages=setuptools.find_packages(),    
+    author='Tilak Thimmappa',
+    author_email='tilaknayarmelpal@gmail.com',
+    description="Using PyRaider You can scan installed dependencies known security vulnerabilities. It uses publicly known exploits, vulnerabilities database.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points ={ 
+        'console_scripts': [ 
+            'pyraider = pyraider.cli:main'
+        ] 
+    }, 
+    url="https://github.com/raidersource/pyraider",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+    keywords = ['SCA', 'pyraider', 'Source Composition Analysis', 'vulnerability scanner'],   # Keywords that define your package best
+    install_requires=[
+        'docopt',
+        'beautifultable',
+        'colored',
+        'json2html',
+        'pickle5',
+    ],
+)

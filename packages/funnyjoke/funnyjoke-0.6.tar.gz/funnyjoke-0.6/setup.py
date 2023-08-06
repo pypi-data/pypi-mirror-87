bdcd130e2c68b8a880da1f3e445f65ae44860003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:00 +0000

    Add package funnyjoke/0.6/funnyjoke-0.6.tar.gz

---
+++ packages/funnyjoke/funnyjoke-0.6.tar.gz/funnyjoke-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension, find_packages
+
+import os.path
+import distutils.extension
+
+setup(name='funnyjoke',
+      version='0.6',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      license='MIT',
+      #packages=['funnyjoke'],
+	  packages=find_packages(),
+	  scripts=['tell.py'],
+	  ext_modules = [Extension('funnyjoke.fastjoke', ['src/fastjoke.c'])],
+      zip_safe=False)
---
+++ packages/funnyjoke/funnyjoke-0.6.tar.gz/funnyjoke-0.6/tell.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+__requires__= 'funnyjoke==0.4'
+import pkg_resources
+pkg_resources.require("funnyjoke==0.4")
+import funnyjoke
+funnyjoke.tell()
+
+import funnyjoke.fastjoke
+funnyjoke.fastjoke.tell()

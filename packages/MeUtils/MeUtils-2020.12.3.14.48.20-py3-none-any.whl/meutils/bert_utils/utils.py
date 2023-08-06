4be1f8d17b0df8f5131a860d4c7bf267341024a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:05 +0000

    Add package MeUtils/2020.12.3.14.48.20/MeUtils-2020.12.3.14.48.20-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2020.12.3.14.48.20-py3-none-any.whl/meutils/bert_utils/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : utils
+# @Time         : 2020/11/23 3:41 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+

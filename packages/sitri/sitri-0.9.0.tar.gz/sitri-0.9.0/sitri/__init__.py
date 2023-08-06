d8e38a2848d1e69e98c30548d16e2cc3f5f514ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:14 +0000

    Add package sitri/0.9.0/sitri-0.9.0.tar.gz

---
+++ packages/sitri/sitri-0.9.0.tar.gz/sitri-0.9.0/sitri/__init__.py
@@ -0,0 +1,9 @@
+"""
+.. module:: __init__
+   :synopsis: General Sitri class
+.. moduleauthor:: Aleksander Lavrov <github.com/egnod>
+"""
+
+from sitri.configurator import SitriProviderConfigurator
+
+Sitri = SitriProviderConfigurator

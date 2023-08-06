62e3fb7b554b38d4e8676793f4de7046023981cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:50 +0000

    Add package onednn-devel-cpu-tbb/2021.1.1/onednn_devel_cpu_tbb-2021.1.1-py2.py3-none-macosx_10_15_x86_64.whl

---
+++ packages/onednn-devel-cpu-tbb/onednn_devel_cpu_tbb-2021.1.1-py2.py3-none-macosx_10_15_x86_64.whl/onednn_devel_cpu_tbb-2021.1.1.data/data/lib/cmake/dnnl/dnnl-targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "DNNL::dnnl" for configuration "release"
+set_property(TARGET DNNL::dnnl APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(DNNL::dnnl PROPERTIES
+  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "TBB::tbb"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libdnnl.2.0.dylib"
+  IMPORTED_SONAME_RELEASE "libdnnl.2.dylib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS DNNL::dnnl )
+list(APPEND _IMPORT_CHECK_FILES_FOR_DNNL::dnnl "${_IMPORT_PREFIX}/lib/libdnnl.2.0.dylib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)

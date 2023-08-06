6666e011011e350ab212b808b14b5c643f8700fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:30 +0000

    Add package kuber/1.13.0/kuber-1.13.0-py3-none-any.whl

---
+++ packages/kuber/kuber-1.13.0-py3-none-any.whl/kuber/v1_17/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label="v1.17",
+    version="v1.17.14",
+    major="1",
+    minor="17",
+    patch="14",
+    pre_release="",
+    build="",
+    created_at=_datetime.datetime(2020, 12, 8),
+    commit_sha="f238f5142728be4033c37aa0ad69bf806090beae",
+)
---
+++ packages/kuber/kuber-1.13.0-py3-none-any.whl/kuber/v1_18/__init__.py
@@ -0,0 +1,18 @@
+import datetime as _datetime
+
+from kuber import versioning
+
+# Information about the kubernetes API library version associated with this
+# package and kuber metadata around its creation and labeling within the
+# kuber library.
+KUBERNETES_VERSION = versioning.KubernetesVersion(
+    label="v1.18",
+    version="v1.18.12",
+    major="1",
+    minor="18",
+    patch="12",
+    pre_release="",
+    build="",
+    created_at=_datetime.datetime(2020, 12, 8),
+    commit_sha="7cd5e9086de8ae25d6a1514d0c87bac67ca4a481",
+)

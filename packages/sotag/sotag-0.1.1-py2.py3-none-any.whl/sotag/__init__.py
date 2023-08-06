9b7cdaec40c5a2cb35e8fb4882ef4585d456fe13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:42 +0000

    Add package sotag/0.1.1/sotag-0.1.1-py2.py3-none-any.whl

---
+++ packages/sotag/sotag-0.1.1-py2.py3-none-any.whl/sotag/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Stack Overflow Tag."""
+
+__author__ = """Software Engineering Laboratory of Fudan University"""
+__email__ = 'lmwtclmwtc@outlook.com'
+__version__ = '0.1.1'

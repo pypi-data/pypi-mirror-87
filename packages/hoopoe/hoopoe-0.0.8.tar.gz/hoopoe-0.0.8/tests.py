4f063ae83dc03908e04cd517857729163c2211be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:12 +0000

    Add package hoopoe/0.0.8/hoopoe-0.0.8.tar.gz

---
+++ packages/hoopoe/hoopoe-0.0.8.tar.gz/hoopoe-0.0.8/tests.py
@@ -0,0 +1,21 @@
+import hoopoe
+import pytest
+
+def test_us_state_name_abbr_to_full_name():
+    assert hoopoe.enrich("ca", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "california"
+    assert hoopoe.enrich("wa", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "washington"
+    assert hoopoe.enrich("NY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
+    assert hoopoe.enrich("nY", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full") == "new york"
+    with pytest.raises(KeyError) as excinfo:
+        hoopoe.enrich("n", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        assert "KeyError" in str(excinfo.value)
+    with pytest.raises(KeyError) as excinfo:
+        hoopoe.enrich(" ", source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        assert "KeyError" in str(excinfo.value)
+    with pytest.raises(ValueError) as excinfo:
+        hoopoe.enrich(source_data_type="us_state_name_abbr", target_data_type="us_state_name_full")
+        assert "ValueError" in str(excinfo.value)
+    
+
+
+# print(test_us_state_name_abbr_to_full_name())

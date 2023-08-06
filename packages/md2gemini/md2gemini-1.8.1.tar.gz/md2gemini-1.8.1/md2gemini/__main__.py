e23153ea66379915f102efa7e1e049a934558d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:00 +0000

    Add package md2gemini/1.8.1/md2gemini-1.8.1.tar.gz

---
+++ packages/md2gemini/md2gemini-1.8.1.tar.gz/md2gemini-1.8.1/md2gemini/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import md2gemini
+
+if __name__ == "__main__":
+    md2gemini.main()
---
+++ packages/md2gemini/md2gemini-1.8.1.tar.gz/md2gemini-1.8.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+from md2gemini import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="md2gemini",  # Replace with your own username
+    version=__version__,
+    author="makeworld",
+    author_email="example@example.com",
+    description="Convert Markdown to the Gemini text format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/makeworld-the-better-one/md2gemini",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Markup",
+    ],
+    python_requires=">=3.5",
+    install_requires=[
+        "mistune>=2.0.0a4,<3",
+        "cjkwrap",
+        "wcwidth",
+    ],
+    entry_points={"console_scripts": ["md2gemini = md2gemini:main"]},
+)

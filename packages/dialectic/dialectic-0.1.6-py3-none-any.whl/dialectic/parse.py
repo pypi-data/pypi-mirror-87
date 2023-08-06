5ce0d96c16bbffa16277637a455079a0741cff8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:10 +0000

    Add package dialectic/0.1.6/dialectic-0.1.6-py3-none-any.whl

---
+++ packages/dialectic/dialectic-0.1.6-py3-none-any.whl/dialectic/parse.py
@@ -0,0 +1,37 @@
+from .sentence import *
+
+
+def parse(sentence_list):
+    input_list = sentence_list.copy()
+    parsed_list = []
+    while input_list:
+        sentence = input_list.pop()
+
+        if (type(sentence) is Atomic) or (type(sentence) is Invert):
+            parsed_list.append(sentence)
+
+        elif type(sentence) is Conjunction:
+            input_list.append(sentence.lchild)
+            input_list.append(sentence.rchild)
+
+        elif type(sentence) is Disjunction:
+            if (~sentence.lchild).validate(input_list + parsed_list):
+                input_list.append(sentence.rchild)
+            elif (~sentence.rchild).validate(input_list + parsed_list):
+                input_list.append(sentence.lchild)
+
+        elif type(sentence) is Implication:
+            if sentence.lchild.validate(input_list + parsed_list):
+                input_list.append(sentence.rchild)
+
+        elif type(sentence) is Equality:
+            if sentence.lchild.validate(input_list + parsed_list):
+                input_list.append(sentence.rchild)
+            elif sentence.rchild.validate(input_list + parsed_list):
+                input_list.append(sentence.lchild)
+            elif (~sentence.lchild).validate(input_list + parsed_list):
+                input_list.append(~sentence.rchild)
+            elif (~sentence.rchild).validate(input_list + parsed_list):
+                input_list.append(~sentence.lchild)
+
+    return set(parsed_list)

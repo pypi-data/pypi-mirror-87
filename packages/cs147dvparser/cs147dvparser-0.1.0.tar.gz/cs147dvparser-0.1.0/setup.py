b85929aafd8c4d650f210169ae1840da241e25ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:51 +0000

    Add package cs147dvparser/0.1.0/cs147dvparser-0.1.0.tar.gz

---
+++ packages/cs147dvparser/cs147dvparser-0.1.0.tar.gz/cs147dvparser-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md",'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cs147dvparser",
+    version="0.1.0",
+    author="Richard DeAmicis, Jordan Conragan",
+    author_email="rtdeamicis@gmail.com, jordanvonpordan@gmail.com",
+    description="converts CS147DV assembly language instructions into hexadecimal code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://rdeamici.github.io/CS147DVParser/",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts":['cs147DVParser = AssemblyParser.AssemblyParser:main']
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Java",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls = {
+        'Source': 'https://github.com/rdeamici/CS147DVParser/tree/master/CS147DVPyParser'   
+    }
+)

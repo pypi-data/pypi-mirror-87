4ef3ecd643a48297a3f760e58653f83197121632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package splitgraph-pipelinewise-target-postgres/2.1.0/splitgraph-pipelinewise-target-postgres-2.1.0.tar.gz

---
+++ packages/splitgraph-pipelinewise-target-postgres/splitgraph-pipelinewise-target-postgres-2.1.0.tar.gz/splitgraph-pipelinewise-target-postgres-2.1.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as f:
+      long_description = f.read()
+
+# Rename the package so that we can upload the fork to PyPI
+setup(name="splitgraph-pipelinewise-target-postgres",
+      version="2.1.0",
+      description="Singer.io target for loading data to PostgreSQL - PipelineWise compatible",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author="TransferWise",
+      url='https://github.com/transferwise/pipelinewise-target-postgres',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=["target_postgres"],
+      install_requires=[
+          'psycopg2-binary>=2.8.5',
+          'inflection>=0.3.1',
+          'joblib>=0.13.2'
+      ],
+      extras_require={
+          "test": [
+              'nose==1.3.7',
+              'mock==3.0.5',
+              'pylint==2.4.4',
+              'nose-cov==1.6'
+            ]
+      },
+      entry_points="""
+          [console_scripts]
+          target-postgres=target_postgres:main
+      """,
+      packages=["target_postgres"],
+      package_data = {},
+      include_package_data=True,
+)

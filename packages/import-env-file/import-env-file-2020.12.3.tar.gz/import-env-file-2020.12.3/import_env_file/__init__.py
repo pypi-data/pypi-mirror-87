6d1ba9808de09e6ed6179b712bf6500dfea3c866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:56 +0000

    Add package import-env-file/2020.12.3/import-env-file-2020.12.3.tar.gz

---
+++ packages/import-env-file/import-env-file-2020.12.3.tar.gz/import-env-file-2020.12.3/import_env_file/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+
+
+def parse(line):
+    """parse line and return a dictionary with variable value"""
+    if line.lstrip().startswith('#'):
+        return {}
+    if not line.lstrip():
+        return {}
+    """find the second occurence of a quote mark:"""
+    if line.find("export=") == 0:
+        line = line.replace("export=", "")
+    quote_delimit = max(line.find('\'', line.find('\'') + 1),
+                        line.find('"', line.rfind('"')) + 1)
+    """find first comment mark after second quote mark"""
+    if '#' in line:
+        line = line[:line.find('#', quote_delimit)]
+    key, value = map(lambda x: x.strip().strip('\'').strip('"'),line.split('=', 1))
+    return {key: value}
+
+path=".env"
+lines = open(path).read().splitlines()
+for line_no,line in enumerate(lines,0):
+    try:
+        os.environ.update(parse(line))
+    except ValueError:
+        raise ValueError(".env:%s\n%s" % (line_no,line))

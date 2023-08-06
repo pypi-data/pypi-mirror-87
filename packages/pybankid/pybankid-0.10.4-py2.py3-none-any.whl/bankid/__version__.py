d526be0a53ad1d273a9647b3dec63d66639c2cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:41 +0000

    Add package pybankid/0.10.4/pybankid-0.10.4-py2.py3-none-any.whl

---
+++ packages/pybankid/pybankid-0.10.4-py2.py3-none-any.whl/bankid/__version__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Version info
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+__version__ = "0.10.4"
+version = __version__  # backwards compatibility name

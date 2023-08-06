f3cd35cc96878eedd036f48c81091e1b51901023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:58 +0000

    Add package seqrecord-expanded/0.2.11/seqrecord_expanded-0.2.11-py3-none-any.whl

---
+++ packages/seqrecord-expanded/seqrecord_expanded-0.2.11-py3-none-any.whl/seqrecord_expanded/__init__.py
@@ -0,0 +1,11 @@
+#! coding: utf-8
+"""
+seqrecord-expanded - Biopython's SeqRecord class, but expanded with additional methods:
+degenerate seqs, codon positions based on reading frames, etc.
+"""
+from .seqrecord import SeqRecordExpanded
+
+
+__version__ = '0.2.10'
+__author__ = 'Carlos Peña <mycalesis@gmail.com>'
+__all__ = []

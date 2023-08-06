454a85b4566813463fe1857bf9c22c4a049cf1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:21 +0000

    Add package django-errors/0.3b10/django_errors-0.3b10.tar.gz

---
+++ packages/django-errors/django_errors-0.3b10.tar.gz/django_errors-0.3b10/django_errors/urls.py
@@ -0,0 +1,28 @@
+"""
+    Django Errors URL Pattern List.
+"""
+
+# pylint: disable=W0401, W0614, E1120
+
+from django.conf.urls import url
+from django.views.decorators.cache import never_cache
+from . import views
+
+urlpatterns = [
+    url(r'^403', views.custom_403, name="403"),
+    url(r'^404', views.custom_404, name="404"),
+    url(r'^400', never_cache(views.custom_400), name="400"),
+    url(r'^500', never_cache(views.custom_500), name="500"),
+]
+#
+# handler400 = views.custom_400
+# """ Handle 400 error """
+#
+# handler403 = views.custom_403
+# """ Handle 403 error """
+#
+# handler404 = views.custom_404
+# """ Handle 404 error """
+#
+# handler500 = views.custom_500
+# """ Handle 500 error """
---
+++ packages/django-errors/django_errors-0.3b10.tar.gz/django_errors-0.3b10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+try:
+    readme = open('README.md').read()
+except:
+    readme = u"Wrapper Views for common errors"
+
+from django_errors import __version__ as version
+
+setup(
+    name="django_errors",
+    version=version,
+    url='https://github.com/DLRSP/django-errors',
+    license='MIT',
+    description="Wrapper Views for common errors",
+    author='DLRSP',
+    author_email='dlrsp.py@gmail.com',
+    packages=['django_errors', ],
+    long_description=readme,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['django_nose',
+                      'django<3',
+                      'django-errors',
+                      ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)

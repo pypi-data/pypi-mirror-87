879f08edcf6c948e7c673a23358b2e5873007b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package ivy/3.4.2/ivy-3.4.2.tar.gz

---
+++ packages/ivy/ivy-3.4.2.tar.gz/ivy-3.4.2/ivy/ext/ivy_shortcodes.py
@@ -0,0 +1,37 @@
+# ------------------------------------------------------------------------------
+# This extension adds support for shortcodes.
+# ------------------------------------------------------------------------------
+
+import ivy
+import sys
+
+try:
+    import shortcodes
+except ImportError:
+    shortcodes = None
+
+
+# Check the config file for custom settings for the shortcode parser.
+settings = ivy.site.config.get('shortcodes', {})
+
+
+# The shortcodes module is an optional dependency.
+if shortcodes:
+
+    # Initialize a single parser instance.
+    parser = shortcodes.Parser(**settings)
+
+    # Filter each node's content on the 'node_text' filter hook.
+    @ivy.filters.register('node_text')
+    def render(text, node):
+        try:
+            return parser.parse(text, node)
+        except shortcodes.ShortcodeError as err:
+            msg = "Shortcode Error\n"
+            msg += f"  Node: {node}\n"
+            msg += f"  {err.__class__.__name__}: {err}"
+            if (cause := err.__cause__):
+                msg += f"\n  Cause: {cause.__class__.__name__}: {cause}"
+            elif (context := err.__context__):
+                msg += f"\n  Context: {context.__class__.__name__}: {context}"
+            sys.exit(msg)

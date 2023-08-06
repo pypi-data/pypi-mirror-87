0c031bd93eae9c17dcf459fcf64a00ebae11ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:15 +0000

    Add package geemap/0.8.3/geemap-0.8.3-py2.py3-none-any.whl

---
+++ packages/geemap/geemap-0.8.3-py2.py3-none-any.whl/geemap/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for geemap."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.8.3"
+
+from .geemap import *
+
+# from .basemaps import ee_basemaps
+# from .legends import builtin_legends

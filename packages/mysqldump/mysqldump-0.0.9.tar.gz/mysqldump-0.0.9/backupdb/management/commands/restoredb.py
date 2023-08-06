5997b8c1dde0f197c6b85152f402e8c483004ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package mysqldump/0.0.9/mysqldump-0.0.9.tar.gz

---
+++ packages/mysqldump/mysqldump-0.0.9.tar.gz/mysqldump-0.0.9/backupdb/management/commands/restoredb.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """
+    restoredb command to restore data to current or mentioned database(s)
+    """
+    def handle(self, *args, **options):
+        """
+        docstring
+        """
+        print("Sorry!!! This command will be released in next version.")

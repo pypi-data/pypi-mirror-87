00a2e6e3b4a3b880f3fbe4f316430509c7b97187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:06 +0000

    Add package pyrustic/0.0.2/pyrustic-0.0.2-py3-none-any.whl

---
+++ packages/pyrustic/pyrustic-0.0.2-py3-none-any.whl/pyrustic/core.py
@@ -0,0 +1,42 @@
+text = """
+
+This current package isn't `Pyrustic Suite`. This package exists to redirect you to the recommended way to get/install `Pyrustic Suite`.
+
+
+## Overview
+`Pyrustic Suite` is a lightweight software suite to help develop, package, and publish Python desktop applications.
+
+Read more about `Pyrustic Suite` [here](https://github.com/pyrustic/pyrustic#readme).
+
+
+## Installation
+The recommended way to get `Pyrustic Suite` is via `Geet`.
+
+### Install Geet
+`pip install geet`
+
+### Run Geet
+`python3 -m geet [command]`
+
+### Install Pyrustic Suite
+`python3 -m geet pyrustic/pyrustic`
+
+or
+
+`python3 -m geet`
+
+`(geet) pyrustic/pyrustic`
+
+## Geet
+With `Geet`, get, install, and run the latest release of a compatible app published on Github.
+
+`pip install geet`
+
+Read more about `Geet` [here](https://github.com/pyrustic/geet#readme).
+
+
+
+"""
+
+
+print(text)

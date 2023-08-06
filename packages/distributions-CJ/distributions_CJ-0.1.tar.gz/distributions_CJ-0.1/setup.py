5b857ba298b0911b100478cbc9795776785839bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:54 +0000

    Add package distributions-CJ/0.1/distributions_CJ-0.1.tar.gz

---
+++ packages/distributions-CJ/distributions_CJ-0.1.tar.gz/distributions_CJ-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_CJ',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_CJ'],
+      zip_safe=False)

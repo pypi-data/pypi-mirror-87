ce366bd15af01a4c40ec0e9ef780846f9c72518d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:01 +0000

    Add package lino/20.12.0/lino-20.12.0.tar.gz

---
+++ packages/lino/lino-20.12.0.tar.gz/lino-20.12.0/lino/modlib/__init__.py
@@ -0,0 +1,70 @@
+# Copyright 2008-2019 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+
+"""The standard model library included with Lino.
+
+A collection of ready-to-use plugins (Django calls them "apps") for
+Lino applications.
+
+System plugins
+==============
+
+.. autosummary::
+   :toctree:
+
+    users
+    gfks
+    memo
+    system
+
+Utilities
+=========
+
+.. autosummary::
+   :toctree:
+
+    jinja
+    checkdata
+    printing
+    summaries
+
+Front ends
+==========
+
+.. autosummary::
+   :toctree:
+
+    bootstrap3
+    extjs
+
+"Options"
+=========
+
+.. autosummary::
+   :toctree:
+
+    about
+    export_excel
+    tinymce
+    notify
+    restful
+    changes
+    wkhtmltopdf
+    weasyprint
+    dashboard
+
+"Enterprise Resources"
+======================
+
+.. autosummary::
+   :toctree:
+
+    comments
+    importfilters
+    languages
+    office
+    smtpd
+    uploads
+
+
+"""

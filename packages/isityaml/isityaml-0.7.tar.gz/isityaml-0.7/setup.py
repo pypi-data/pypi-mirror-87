fb0a36e700733620767bbcf0877bfaacf9f5ab8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:09 +0000

    Add package isityaml/0.7/isityaml-0.7.tar.gz

---
+++ packages/isityaml/isityaml-0.7.tar.gz/isityaml-0.7/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='isityaml',
+    version='0.7',
+    description='A Django app for checking the correctness of YAML',
+    author='Peter Murphy',
+    author_email='peterkmurphy@gmail.com',
+    url='http://pypi.python.org/pypi/isityaml/',
+    packages=['isityaml'],
+    package_data={
+        'isityaml': [
+            'templates/isityaml/*.html',
+        ],
+    },
+    keywords = 'YAML parse text Django',
+    license='LICENSE.txt',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Other Audience",
+        'License :: OSI Approved :: BSD License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        "Topic :: Education",
+        ],
+    long_description=open('README.rst').read(),
+    install_requires = ["Django >= 1.1.1", "PyYAML >= 3.0"],
+)

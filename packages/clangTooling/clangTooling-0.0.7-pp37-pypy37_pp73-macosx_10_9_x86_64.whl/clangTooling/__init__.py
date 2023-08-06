94e3a7b9ca4ef667e36e29ebcc6a7da01a331d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:07 +0000

    Add package clangTooling/0.0.7/clangTooling-0.0.7-pp37-pypy37_pp73-macosx_10_9_x86_64.whl

---
+++ packages/clangTooling/clangTooling-0.0.7-pp37-pypy37_pp73-macosx_10_9_x86_64.whl/clangTooling/__init__.py
@@ -0,0 +1,14 @@
+'''clangTooling libraries and headers.'''
+
+from .lib import (
+    get_lib_ext,
+    library_dir,
+    library_list,
+    llvm_library_list,
+    clang_library_list,
+)
+from .include import (
+    header_dir,
+    llvm_includes,
+    clang_includes,
+)

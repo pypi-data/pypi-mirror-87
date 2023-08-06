d1583e77826f79b7d3240895cbcd9e0b1dd15790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:37 +0000

    Add package vcfsynonymous/0.1.8/vcfsynonymous-0.1.8.tar.gz

---
+++ packages/vcfsynonymous/vcfsynonymous-0.1.8.tar.gz/vcfsynonymous-0.1.8/setup.py
@@ -0,0 +1,38 @@
+import pathlib
+from setuptools import find_packages, setup
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+setup(
+
+    name='vcfsynonymous',
+    version='0.1.8',    
+    author="Pierre-Edouard GUERIN",    
+    author_email="pierre-edouard.guerin@cefe.cnrs.fr",
+    description="Detect synonymous genetic variants in VCF",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    licence="MIT",
+    url="https://github.com/Grelot/vcfsynonymous",
+    packages=find_packages(include=['vcfsynonymous', 'vcfsynonymous.*']),
+    install_requires=['argparse', 'numpy', 'biopython', 'bcbio-gff', 'pyfaidx', 'gffutils', 'PyVCF'],
+    package_data={'vcfsynonymous': ['vcfsynonymous/tests/data/genome.fasta',
+                                    'vcfsynonymous/tests/data/gff.db',
+                                    'vcfsynonymous/tests/data/genome.gff3'
+                                    'vcfsynonymous/tests/data/sample.vcf']},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "Programming Language :: Python :: 3.6",
+         "Programming Language :: Python :: 3.7",
+         "Programming Language :: Python :: 3.8",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent"
+     ]
+)

33c1c0964a4277d0f458c9e5d01ff11326364b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:22 +0000

    Add package hacs-frontend/202012051441/hacs-frontend-202012051441.tar.gz

---
+++ packages/hacs-frontend/hacs-frontend-202012051441.tar.gz/hacs-frontend-202012051441/hacs_frontend/entrypoint.js
@@ -0,0 +1,9 @@
+
+try {
+  new Function("import('/hacsfiles/frontend/e.a5d24e53.js')")();
+} catch (err) {
+  var el = document.createElement('script');
+  el.src = '/hacsfiles/frontend/e.a5d24e53.js';
+  document.body.appendChild(el);
+}
+  

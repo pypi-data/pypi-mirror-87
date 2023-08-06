825831339caf99795a5cfd24e4ceb08bf74fffac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:25 +0000

    Add package fmodpy/1.1.0/fmodpy-1.1.0.tar.gz

---
+++ packages/fmodpy/fmodpy-1.1.0.tar.gz/fmodpy-1.1.0/fmodpy/development/todo.py
@@ -0,0 +1,15 @@
+# TODO: Vectors as module attributes need to be returned by memory
+#       address instead of being copied.
+
+# TODO: Consider a user-defined "TYPE" that only contains one of the
+#       fundamental types to simply be of that type (no struct).
+# TODO: User defined "TYPE" can only contain fundamental types
+#       (logical, character, integer, real) and other valid user
+#       defined types.
+# TODO: The interface being used by a subroutine-as-an-argument could
+#       be defined in another module. Need to check for that.
+#       Need to disallow "EXTERNAL" subroutines-as-arguments.
+
+
+# TODO: "LOGICAL A" is a valid declaration that is not accepted, need
+#       to look into when the "::" is required.

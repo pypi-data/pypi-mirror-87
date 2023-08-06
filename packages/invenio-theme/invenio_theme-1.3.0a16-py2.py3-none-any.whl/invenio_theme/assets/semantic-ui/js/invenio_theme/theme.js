559ba947d34f2a85a82672784337d729396028a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:19 +0000

    Add package invenio-theme/1.3.0a16/invenio_theme-1.3.0a16-py2.py3-none-any.whl

---
+++ packages/invenio-theme/invenio_theme-1.3.0a16-py2.py3-none-any.whl/invenio_theme/assets/semantic-ui/js/invenio_theme/theme.js
@@ -0,0 +1,23 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2017-2020 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+// eslint-disable-next-line no-unused-vars
+import jquery from "jquery/dist/jquery";
+import "semantic-ui-css/semantic.js";
+import "semantic-ui-less/semantic.less";
+
+// Initialize Semantic UI components
+jquery(".ui.dropdown.icon.button").dropdown();
+
+jquery(".message .close").on("click", function () {
+  jquery(this).closest(".message").transition({
+    animation: "fade",
+    duration: 0,
+    interval: 0,
+  });
+});

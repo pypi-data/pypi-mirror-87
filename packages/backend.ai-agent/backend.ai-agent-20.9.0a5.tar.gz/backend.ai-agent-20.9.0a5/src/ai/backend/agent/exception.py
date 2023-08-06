a935e34cac98b3c6e7afcb08ac40e269b25237c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:35 +0000

    Add package backend.ai-agent/20.9.0a5/backend.ai-agent-20.9.0a5.tar.gz

---
+++ packages/backend.ai-agent/backend.ai-agent-20.9.0a5.tar.gz/backend.ai-agent-20.9.0a5/src/ai/backend/agent/exception.py
@@ -0,0 +1,16 @@
+class InitializationError(Exception):
+    pass
+
+
+class UnsupportedResource(Exception):
+    pass
+
+
+class InsufficientResource(Exception):
+    pass
+
+
+class K8sError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message

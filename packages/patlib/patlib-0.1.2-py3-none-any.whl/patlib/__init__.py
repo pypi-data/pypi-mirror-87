710f121250351ab0d69689cb94030f2bb1f50e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:54 +0000

    Add package patlib/0.1.2/patlib-0.1.2-py3-none-any.whl

---
+++ packages/patlib/patlib-0.1.2-py3-none-any.whl/patlib/__init__.py
@@ -0,0 +1,10 @@
+"""A small, personal, pylab.
+
+Its purpose is to provide shorthands,
+and so should not be used in production code.
+It may be loaded with wildcard, eg:
+    - from pylib.all import *  # executes all submodules
+    - from pylib.math import * # won't execute the other submodules
+"""
+
+__version__ = "0.1.2"

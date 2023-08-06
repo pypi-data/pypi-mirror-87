27d21c3f374445e575b494c81de23f9a4182cb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:54 +0000

    Add package polyfuzz/0.2.2/polyfuzz-0.2.2-py2.py3-none-any.whl

---
+++ packages/polyfuzz/polyfuzz-0.2.2-py2.py3-none-any.whl/polyfuzz/models/_utils.py
@@ -0,0 +1,103 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+from typing import List
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics.pairwise import cosine_similarity as scikit_cosine_similarity
+
+try:
+    from sparse_dot_topn import awesome_cossim_topn
+    _HAVE_SPARSE_DOT = True
+except ImportError:
+    _HAVE_SPARSE_DOT = False
+
+
+def cosine_similarity(from_vector: np.ndarray,
+                      to_vector: np.ndarray,
+                      from_list: List[str],
+                      to_list: List[str],
+                      min_similarity: float = 0.75,
+                      method: str = "sparse") -> pd.DataFrame:
+    """ Calculate similarity between two matrices/vectors and return best matches
+
+    Arguments:
+        from_vector: the matrix or vector representing the embedded strings to map from
+        to_vector: the matrix or vector representing the embedded strings to map to
+        from_list: The list from which you want mappings
+        to_list: The list where you want to map to
+        min_similarity: The minimum similarity between strings, otherwise return 0 similarity
+        method: The method/package for calculating the cosine similarity.
+                Options: "sparse", "sklearn", "knn".
+                Sparse is the fastest and most memory efficient but requires a
+                package that might be difficult to install.
+                Sklearn is a bit slower than sparse and requires significantly more memory as
+                the distance matrix is not sparse
+                Knn uses 1-nearest neighbor to extract the most similar strings
+                it is significantly slower than both methods but requires little memory
+
+    Returns:
+        matches:  The best matches between the lists of strings
+
+
+    Usage:
+
+    Make sure to fill the `to_vector` and `from_vector` with vector representations
+    of `to_list` and `from_list` respectively:
+
+    ```python
+    from polyfuzz.models import extract_best_matches
+    indices, similarity = extract_best_matches(from_vector, to_vector, method="sparse")
+    ```
+    """
+    # Slower but uses less memory
+    if method == "knn":
+
+        if from_list == to_list:
+            knn = NearestNeighbors(n_neighbors=2, n_jobs=-1, metric='cosine').fit(to_vector)
+            distances, indices = knn.kneighbors(from_vector)
+            distances = distances[:, 1]
+            indices = indices[:, 1]
+
+        else:
+            knn = NearestNeighbors(n_neighbors=1, n_jobs=-1, metric='cosine').fit(to_vector)
+            distances, indices = knn.kneighbors(from_vector)
+
+        similarity = [round(1 - distance, 3) for distance in distances.flatten()]
+
+    # Fast, but does has some installation issues
+    elif _HAVE_SPARSE_DOT and method == "sparse":
+        if isinstance(to_vector, np.ndarray):
+            to_vector = csr_matrix(to_vector)
+        if isinstance(from_vector, np.ndarray):
+            from_vector = csr_matrix(from_vector)
+
+        # There is a bug with awesome_cossim_topn that when to_vector and from_vector
+        # have the same shape, setting topn to 1 does not work. Apparently, you need
+        # to it at least to 2 for it to work
+        similarity_matrix = awesome_cossim_topn(from_vector, to_vector.T, 2, min_similarity)
+
+        if from_list == to_list:
+            similarity_matrix = similarity_matrix.tolil()
+            similarity_matrix.setdiag(0.)
+            similarity_matrix = similarity_matrix.tocsr()
+
+        indices = np.array(similarity_matrix.argmax(axis=1).T).flatten()
+        similarity = similarity_matrix.max(axis=1).toarray().T.flatten()
+
+    # Faster than knn and slower than sparse but uses more memory
+    else:
+        similarity_matrix = scikit_cosine_similarity(from_vector, to_vector)
+
+        if from_list == to_list:
+            np.fill_diagonal(similarity_matrix, 0)
+
+        indices = similarity_matrix.argmax(axis=1)
+        similarity = similarity_matrix.max(axis=1)
+
+    # Convert results to df
+    matches = [to_list[idx] for idx in indices.flatten()]
+    matches = pd.DataFrame(np.vstack((from_list, matches, similarity)).T, columns=["From", "To", "Similarity"])
+    matches.Similarity = matches.Similarity.astype(float)
+    matches.loc[matches.Similarity < 0.001, "To"] = None
+
+    return matches
---
+++ packages/polyfuzz/polyfuzz-0.2.2-py2.py3-none-any.whl/polyfuzz/polyfuzz.py
@@ -0,0 +1,326 @@
+import logging
+import pandas as pd
+from typing import List, Mapping, Union, Iterable
+
+from polyfuzz.linkage import single_linkage
+from polyfuzz.utils import check_matches, check_grouped, create_logger
+from polyfuzz.models import TFIDF, RapidFuzz, Embeddings, BaseMatcher
+from polyfuzz.metrics import precision_recall_curve, visualize_precision_recall
+
+logger = create_logger()
+
+
+class PolyFuzz:
+    """
+    PolyFuzz class for Fuzzy string matching, grouping, and evaluation.
+
+    Arguments:
+        method: the method(s) used for matching. For quick selection of models
+                select one of the following: "EditDistance", "TF-IDF" or "Embeddings".
+                If you want more control over the models above, pass
+                in a model from polyfuzz.models. For examples, see
+                usage below.
+        verbose: Changes the verbosity of the model, Set to True if you want
+                 to track the stages of the model.
+
+    Usage:
+
+    For basic, out-of-the-box usage, run the code below. You can replace "TF-IDF"
+    with either "EditDistance"  or "Embeddings" for quick access to these models:
+
+    ```python
+    import polyfuzz as pf
+    model = pf.PolyFuzz("TF-IDF")
+    ```
+
+    If you want more control over the String Matching models, you can load
+    in these models separately:
+
+    ```python
+    tfidf = TFIDF(n_gram_range=(3, 3), min_similarity=0, model_id="TF-IDF-Sklearn")
+    model = pf.PolyFuzz(tfidf)
+    ```
+
+    You can also select multiple models in order to compare performance:
+
+    ```python
+    tfidf = TFIDF(n_gram_range=(3, 3), min_similarity=0, model_id="TF-IDF-Sklearn")
+    edit = EditDistance(n_jobs=-1)
+    model = pf.PolyFuzz([tfidf, edit])
+    ```
+
+    To use embedding models, please use Flair word embeddings:
+
+    ```python
+    from flair.embeddings import WordEmbeddings, TransformerWordEmbeddings
+    fasttext_embedding = WordEmbeddings('news')
+    bert_embedding = TransformerWordEmbeddings('bert-base-multilingual-cased')
+    embedding = Embeddings([fasttext_embedding, bert_embedding ], min_similarity=0.0)
+    model = pf.PolyFuzz(embedding)
+    ```
+    """
+
+    def __init__(self,
+                 method: Union[str,
+                               BaseMatcher,
+                               List[BaseMatcher]] = "TF-IDF",
+                 verbose: bool = False):
+        self.method = method
+        self.matches = None
+
+        # Metrics
+        self.min_precisions = None
+        self.recalls = None
+        self.average_precisions = None
+
+        # Cluster
+        self.clusters = None
+        self.cluster_mappings = None
+        self.grouped_matches = None
+
+        if verbose:
+            logger.setLevel(logging.DEBUG)
+        else:
+            logger.setLevel(logging.WARNING)
+
+    def match(self,
+              from_list: List[str],
+              to_list: List[str]):
+        """ Match the from_list of strings to the to_list of strings with whatever models
+        you have initialized
+
+        Arguments:
+            from_list: The list from which you want mappings
+            to_list: The list where you want to map to
+
+        Updates:
+            self.matches: A dictionary with the matches from all models, can
+                          be accessed with `model.get_all_matches` or
+                          `model.get_match("TF-IDF")`
+
+        Usage:
+
+        After having initialized your models, you can pass through lists of strings:
+
+        ```python
+        import polyfuzz as pf
+        model = pf.PolyFuzz("TF-IDF", model_id="TF-IDF")
+        model.match(from_list = ["string_one", "string_two"],
+                    to_list = ["string_three", "string_four"])
+        ```
+
+        You can access the results matches with `model.get_all_matches` or a specific
+        model with `model.get_match("TF-IDF")` based on their model_id.
+        """
+        # Standard models - quick access
+        if isinstance(self.method, str):
+            if self.method in ["TF-IDF", "TFIDF"]:
+                self.matches = {"TF-IDF": TFIDF(min_similarity=0).match(from_list, to_list)}
+            elif self.method in ["EditDistance", "Edit Distance"]:
+                self.matches = {"EditDistance": RapidFuzz().match(from_list, to_list)}
+            elif self.method in ["Embeddings", "Embedding"]:
+                self.matches = {"Embeddings": Embeddings(min_similarity=0).match(from_list, to_list)}
+            else:
+                raise ValueError("Please instantiate the model with one of the following methods: \n"
+                                 "* 'TF-IDF'\n"
+                                 "* 'EditDistance'\n"
+                                 "* 'Embeddings'\n")
+            logger.info(f"Ran model with model id = {self.method}")
+
+        # Custom models
+        elif isinstance(self.method, BaseMatcher):
+            self.matches = {self.method.model_id: self.method.match(from_list, to_list)}
+            logging.info(f"Ran model with model id = {self.method.model_id}")
+
+        # Multiple custom models
+        elif isinstance(self.method, Iterable):
+            self._update_model_ids()
+            self.matches = {}
+            for model in self.method:
+                self.matches[model.model_id] = model.match(from_list, to_list)
+                logging.info(f"Ran model with model id = {model.model_id}")
+
+        return self
+
+    def visualize_precision_recall(self,
+                                   kde: bool = False,
+                                   save_path: str = None
+                                   ):
+        """ Calculate and visualize precision-recall curves
+
+        A minimum similarity score might be used to identify
+        when a match could be considered to be correct. For example,
+        we can assume that if a similarity score pass 0.95 we are
+        quite confident that the matches are correct. This minimum
+        similarity score can be defined as **precision** since it shows
+        you how precise we believe the matches are at a minimum.
+
+        **Recall** can then be defined as as the percentage of matches
+        found at a certain minimum similarity score. A high recall means
+        that for a certain minimum precision score, we find many matches.
+
+        Arguments:
+            kde: whether to also visualize the kde plot
+            save_path: the path to save the resulting image to
+
+        Usage:
+
+        ```python
+        import polyfuzz as pf
+        model = pf.PolyFuzz("TF-IDF", model_id="TF-IDF")
+        model.match(from_list = ["string_one", "string_two"],
+                    to_list = ["string_three", "string_four"])
+        model.visualize_precision_recall(save_path="results.png")
+        ```
+        """
+        check_matches(self)
+
+        self.min_precisions = {}
+        self.recalls = {}
+        self.average_precisions = {}
+
+        for name, match in self.matches.items():
+            min_precision, recall, average_precision = precision_recall_curve(match)
+            self.min_precisions[name] = min_precision
+            self.recalls[name] = recall
+            self.average_precisions[name] = average_precision
+
+        visualize_precision_recall(self.matches, self.min_precisions, self.recalls, kde, save_path)
+
+    def group(self,
+              model: Union[str, BaseMatcher] = None,
+              link_min_similarity: float = 0.75,
+              group_all_strings: bool = False):
+        """ From the matches, group the `To` matches together using single linkage
+
+         Arguments:
+             model: you can choose one of the models in `polyfuzz.models` to be used as a grouper
+             link_min_similarity: the minimum similarity between strings before they are grouped
+                                  in a single linkage fashion
+             group_all_strings: if you want to compare a list of strings with itself and then cluster
+                                those strings, set this to True. Otherwise, only the strings that
+                                were mapped To are clustered.
+
+         Updates:
+            self.matches: Adds a column `Group` that is the grouped version of the `To` column
+         """
+        check_matches(self)
+        self.clusters = {}
+        self.cluster_mappings = {}
+
+        # Standard models - quick access
+        if isinstance(model, str):
+            if model in ["TF-IDF", "TFIDF"]:
+                model = TFIDF(n_gram_range=(3, 3), min_similarity=link_min_similarity)
+            elif self.method in ["EditDistance", "Edit Distance"]:
+                model = RapidFuzz()
+            elif self.method in ["Embeddings", "Embedding"]:
+                model = Embeddings(min_similarity=link_min_similarity)
+            else:
+                raise ValueError("Please instantiate the model with one of the following methods: \n"
+                                 "* 'TF-IDF'\n"
+                                 "* 'EditDistance'\n"
+                                 "* 'Embeddings'\n"
+                                 "* Or None if you want to automatically use TF-IDF")
+
+        # Use TF-IDF if no model is specified
+        elif not model:
+            model = TFIDF(n_gram_range=(3, 3), min_similarity=link_min_similarity)
+
+        # Group per model
+        for name, match in self.matches.items():
+            self._create_groups(name, model, link_min_similarity, group_all_strings)
+
+    def get_ids(self) -> Union[str, List[str], None]:
+        """ Get all model ids for easier access """
+        check_matches(self)
+
+        if isinstance(self.method, str):
+            return self.method
+        elif isinstance(self.method, Iterable):
+            return [model.model_id for model in self.method]
+        return None
+
+    def get_matches(self, model_id: str = None) -> Union[pd.DataFrame,
+                                                           Mapping[str, pd.DataFrame]]:
+        """ Get the matches from one or more models"""
+        check_matches(self)
+
+        if len(self.matches) == 1:
+            return list(self.matches.values())[0]
+
+        elif len(self.matches) > 1 and model_id:
+            return self.matches[model_id]
+
+        return self.matches
+
+    def get_clusters(self, model_id: str = None) -> Mapping[str, List[str]]:
+        """ Get the groupings/clusters from a single model
+
+        Arguments:
+            model_id: the model id of the model if you have specified multiple models
+
+        """
+        check_matches(self)
+        check_grouped(self)
+
+        if len(self.matches) == 1:
+            return list(self.clusters.values())[0]
+
+        elif len(self.matches) > 1 and model_id:
+            return self.clusters[model_id]
+
+        return self.clusters
+
+    def get_cluster_mappings(self, name: str = None) -> Mapping[str, int]:
+        """ Get the mappings from the `To` column to its respective column """
+        check_matches(self)
+        check_grouped(self)
+
+        if len(self.matches) == 1:
+            return list(self.cluster_mappings.values())[0]
+
+        elif len(self.matches) > 1 and name:
+            return self.cluster_mappings[name]
+
+        return self.cluster_mappings
+
+    def _create_groups(self,
+                       name: str,
+                       model: BaseMatcher,
+                       link_min_similarity: float,
+                       group_all_strings: bool):
+        """ Create groups based on either the To mappings if you compare two different lists of strings, or
+        the From mappings if you compare lists of strings that are equal (set group_all_strings to True)
+        """
+
+        if group_all_strings:
+            strings = list(self.matches[name].From.dropna().unique())
+        else:
+            strings = list(self.matches[name].To.dropna().unique())
+
+        # Create clusters
+        matches = model.match(strings, strings)
+        clusters, cluster_id_map, cluster_name_map = single_linkage(matches, link_min_similarity)
+
+        # Map the `to` list to groups
+        df = self.matches[name]
+        df["Group"] = df['To'].map(cluster_name_map).fillna(df['To'])
+        self.matches[name] = df
+
+        # Track clusters and their ids
+        self.clusters[name] = clusters
+        self.cluster_mappings[name] = cluster_id_map
+
+    def _update_model_ids(self):
+        """ Update model ids such that there is no overlap between ids """
+        # Give models a model_id if it didn't already exist
+        for index, model in enumerate(self.method):
+            if not model.model_id:
+                model.model_id = f"Model {index}"
+
+        # Update duplicate names
+        model_ids = [model.model_id for model in self.method]
+        if len(set(model_ids)) != len(model_ids):
+            for index, model in enumerate(self.method):
+                model.model_id = f"Model {index}"

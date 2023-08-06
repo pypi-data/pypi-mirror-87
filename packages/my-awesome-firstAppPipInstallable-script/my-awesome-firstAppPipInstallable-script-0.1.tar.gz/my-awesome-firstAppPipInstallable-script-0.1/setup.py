116cff89b5a209a45e1b8dbb5ade0d3f71a8f7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:37 +0000

    Add package my-awesome-firstAppPipInstallable-script/0.1/my-awesome-firstAppPipInstallable-script-0.1.tar.gz

---
+++ packages/my-awesome-firstAppPipInstallable-script/my-awesome-firstAppPipInstallable-script-0.1.tar.gz/my-awesome-firstAppPipInstallable-script-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 20:08:55 2020
+
+@author: WildTuan
+"""
+from setuptools import setup
+ 
+setup(
+     name='my-awesome-firstAppPipInstallable-script',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['FirstAppPipInst']                  # The name of your scipt, and also the command you'll be using for calling it
+ )

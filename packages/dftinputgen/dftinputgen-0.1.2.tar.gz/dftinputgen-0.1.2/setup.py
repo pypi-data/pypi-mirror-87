619967cd8d88aa5f3474849205dd367e87f743d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:31 +0000

    Add package dftinputgen/0.1.2/dftinputgen-0.1.2.tar.gz

---
+++ packages/dftinputgen/dftinputgen-0.1.2.tar.gz/dftinputgen-0.1.2/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open(
+    os.path.join(
+        os.path.dirname(__file__), "src", "dftinputgen", "VERSION.txt"
+    )
+) as fr:
+    version = fr.read().strip()
+
+
+setup(
+    name="dftinputgen",
+    version=version,
+    description="Unopinionated library to generate input files for DFT codes",
+    url="https://github.com/CitrineInformatics/dft-input-gen",
+    author="Vinay Hegde",
+    author_email="vhegde@citrine.io",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["six", "numpy", "ase <= 3.17"],
+    entry_points={"console_scripts": ["dftinputgen = dftinputgen.cli:driver"]},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
---
+++ packages/dftinputgen/dftinputgen-0.1.2.tar.gz/dftinputgen-0.1.2/src/dftinputgen/demo/pwx.py
@@ -0,0 +1,101 @@
+"""Demo generating input files for doing a calculation with pw.x."""
+
+import json
+import argparse
+
+from dftinputgen.utils import read_crystal_structure
+from dftinputgen.qe.pwx import PwxInputGenerator
+
+
+def _get_default_parser():
+    description = "Input file generation for pw.x."
+    return argparse.ArgumentParser(description=description)
+
+
+def build_pwx_parser(parser):
+    """Adds pw.x arguments to the input `argparse.ArgumentParser` object."""
+    # Required:
+    crystal_structure = "(REQUIRED) File with the input crystal structure"
+    parser.add_argument(
+        "-i",
+        "--crystal-structure",
+        type=read_crystal_structure,
+        help=crystal_structure,
+        required=True,
+    )
+
+    # Optional:
+    calculation_presets = "Preset group of tags and default values to use"
+    parser.add_argument(
+        "-pre",
+        "--calculation-presets",
+        choices=["scf", "relax", "vc-relax"],
+        default=None,
+        help=calculation_presets,
+    )
+
+    custom_settings_file = "JSON file with custom DFT settings to use"
+    parser.add_argument(
+        "-file",
+        "--custom-settings-file",
+        default=None,
+        help=custom_settings_file,
+    )
+
+    custom_settings_dict = """JSON string with a dictionary of custom DFT
+    settings to use. Example: '{"pseudo_dir": "/path/to/pseudo_dir/"}'"""
+    parser.add_argument(
+        "-dict",
+        "--custom-settings-dict",
+        default="{}",
+        type=json.loads,
+        help=custom_settings_dict,
+    )
+
+    specify_potentials = "Specify a potential for every chemical species?"
+    parser.add_argument(
+        "-pot",
+        "--specify-potentials",
+        type=bool,
+        default=False,
+        help=specify_potentials,
+    )
+
+    write_location = "Directory to write the input file(s) in"
+    parser.add_argument("-loc", "--write-location", help=write_location)
+
+    pwx_input_file = "Name of the pw.x input file"
+    parser.add_argument("-o", "--pwx-input-file", help=pwx_input_file)
+
+
+def generate_pwx_input_files(args):
+    """Write input files for the input crystal structure."""
+    pwig = PwxInputGenerator(
+        crystal_structure=args.crystal_structure,
+        calculation_presets=args.calculation_presets,
+        custom_sett_file=args.custom_settings_file,
+        custom_sett_dict=args.custom_settings_dict,
+        specify_potentials=args.specify_potentials,
+        write_location=args.write_location,
+        pwx_input_file=args.pwx_input_file,
+    )
+    pwig.write_input_files()
+
+
+def run_demo(*sys_args):
+    """End-to-end run of pw.x input file generation."""
+    parser = _get_default_parser()
+    build_pwx_parser(parser)
+    args = parser.parse_args(*sys_args)
+    generate_pwx_input_files(args)
+
+
+if __name__ == "__main__":
+    """
+    When run as a script, this module will generate input files to use with
+    pw.x, for a specified crystal structure, calculation presets, and any
+    custom DFT settings on top of preset defaults.
+
+    For a list of optional arguments, run this script with "-h" argument.
+    """
+    run_demo()  # pragma: no cover

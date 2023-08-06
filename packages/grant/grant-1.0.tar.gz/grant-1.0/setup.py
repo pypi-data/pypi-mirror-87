ebf135fcd56b8daf9f593928deb611bc9bacf9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:49 +0000

    Add package grant/1.0/grant-1.0.tar.gz

---
+++ packages/grant/grant-1.0.tar.gz/grant-1.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+"""
+作者：jiangshuo
+日期：2020年12月01日
+"""
+
+
+from setuptools import find_packages, setup
+setup(
+    name='grant',
+    version='1.0',
+    packages=find_packages(),
+)

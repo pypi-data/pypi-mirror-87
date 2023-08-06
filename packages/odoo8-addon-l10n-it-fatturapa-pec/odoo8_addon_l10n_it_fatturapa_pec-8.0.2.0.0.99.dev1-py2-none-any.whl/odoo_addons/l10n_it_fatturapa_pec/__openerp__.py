192dc447455ccde1083fd5196c5c1131c830200d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:33 +0000

    Add package odoo8-addon-l10n-it-fatturapa-pec/8.0.2.0.0.99.dev1/odoo8_addon_l10n_it_fatturapa_pec-8.0.2.0.0.99.dev1-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-it-fatturapa-pec/odoo8_addon_l10n_it_fatturapa_pec-8.0.2.0.0.99.dev1-py2-none-any.whl/odoo_addons/l10n_it_fatturapa_pec/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Author(s): Andrea Colangelo (andreacolangelo@openforce.it)
+# Copyright 2018 Openforce Srls Unipersonale (www.openforce.it)
+# Copyright 2018-2019 Sergio Corato (https://efatto.it)
+# Copyright 2018-2019 Lorenzo Battistini <https://github.com/eLBati>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Italian Localization - Fattura elettronica - Supporto PEC',
+    'version': '8.0.2.0.0',
+    'category': 'Localization/Italy',
+    'summary': 'Send electronic invoices via PEC',
+    'author': 'Openforce Srls Unipersonale, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-italy',
+    'license': 'AGPL-3',
+    'depends': [
+        'fetchmail',
+        'l10n_it_fatturapa_out',
+        'l10n_it_fatturapa_in',
+        'l10n_it_sdi_channel',
+    ],
+    'data': [
+        'security/groups.xml',
+        'views/account.xml',
+        'views/fatturapa_attachment_out.xml',
+        'wizard/send_pec_view.xml',
+        'views/fetchmail_view.xml',
+        'security/ir.model.access.csv',
+        'views/company_view.xml',
+        'data/fetchmail_data.xml',
+    ],
+    'installable': True
+}
---
+++ packages/odoo8-addon-l10n-it-fatturapa-pec/odoo8_addon_l10n_it_fatturapa_pec-8.0.2.0.0.99.dev1-py2-none-any.whl/odoo_addons/l10n_it_fatturapa_pec/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import account
+from . import fatturapa_attachment_out
+from . import mail_thread
+from . import fetchmail
+from . import company

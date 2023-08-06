66da75f1b04edbf6e4b4e2e2cd3c0e351ebeec27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:21 +0000

    Add package morphocut/0.1.2/morphocut-0.1.2.tar.gz

---
+++ packages/morphocut/morphocut-0.1.2.tar.gz/morphocut-0.1.2/README.rst
@@ -0,0 +1,35 @@
+MorphoCut
+=========
+
+.. image:: https://travis-ci.org/morphocut/morphocut.svg?branch=master
+    :target: https://travis-ci.org/morphocut/morphocut
+
+.. image:: https://codecov.io/gh/morphocut/morphocut/branch/master/graph/badge.svg
+    :alt: Codecov
+    :target: https://codecov.io/gh/morphocut/morphocut
+
+This repository contains the MorphoCut image processing pipeline
+library.
+
+MorphoCut is a pipelined image processing library designed to handle
+large volumes of biological, medical, oceanographic and remote sensing
+image data. It is modular from ground up and comes with "batteries
+included" to provide common processing steps like segmentation and
+feature extraction. It reads and writes many
+`formats <https://morphocut.readthedocs.io/en/stable/formats.html>`__
+like video,
+`Bio-Formats <https://docs.openmicroscopy.org/bio-formats/latest/supported-formats.html>`__
+and `EcoTaxa <https://ecotaxa.obs-vlfr.fr/>`__.
+
+The MorphoCut library is accompanied by a `web application`_ that
+serves as a front end to the library.
+
+For further information, have a look at the `documentation`_.
+
+.. _`web application`: https://github.com/morphocut/morphocut-server
+.. _documentation: https://morphocut.readthedocs.io/en/stable/
+
+License
+-------
+
+This project is licensed under the terms of the MIT license.
---
+++ packages/morphocut/morphocut-0.1.2.tar.gz/morphocut-0.1.2/setup.py
@@ -0,0 +1,49 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+with open("README.rst", "r") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+setup(
+    name="morphocut",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Simon-Martin Schroeder",
+    author_email="martin.schroeder@nerdluecht.de",
+    description="Image processing pipeline",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/morphocut/morphocut",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["numpy", "scikit-image>=0.16.0", "pandas", "tqdm", "scipy"],
+    python_requires=">=3.6",
+    extras_require={
+        "tests": [
+            # Pytest
+            "pytest",
+            "pytest-cov",
+            "timer-cm",
+            # Optional dependencies
+            "parse",
+        ],
+        "docs": [
+            "sphinx ~= 2.2",
+            "sphinx_rtd_theme",
+            "sphinxcontrib-programoutput",
+            "sphinx-autodoc-typehints>=1.10.0",
+        ],
+    },
+    entry_points={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+    ],
+)

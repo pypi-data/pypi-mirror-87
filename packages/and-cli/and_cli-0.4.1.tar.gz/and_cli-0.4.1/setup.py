7e8fc58beab14365fdff550ffd457351c18465f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:46 +0000

    Add package and-cli/0.4.1/and_cli-0.4.1.tar.gz

---
+++ packages/and-cli/and_cli-0.4.1.tar.gz/and_cli-0.4.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['and_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['and = and_cli.commands:_and']}
+
+setup_kwargs = {
+    'name': 'and-cli',
+    'version': '0.4.1',
+    'description': 'Simple CLI for COMP4128',
+    'long_description': None,
+    'author': 'Felix Lonergan',
+    'author_email': 'felix.lonergan@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

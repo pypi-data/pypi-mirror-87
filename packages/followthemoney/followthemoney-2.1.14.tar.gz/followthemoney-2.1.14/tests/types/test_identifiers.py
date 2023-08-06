b9a6a895120f9f6a78e301b18225ee698244b730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:33 +0000

    Add package followthemoney/2.1.14/followthemoney-2.1.14.tar.gz

---
+++ packages/followthemoney/followthemoney-2.1.14.tar.gz/followthemoney-2.1.14/tests/types/test_identifiers.py
@@ -0,0 +1,23 @@
+import unittest
+
+from followthemoney.types import registry
+
+identifiers = registry.identifier
+
+
+class IdentifiersTest(unittest.TestCase):
+    def test_parse(self):
+        self.assertEqual(identifiers.clean("88/9"), "88/9")
+
+    def test_domain_validity(self):
+        self.assertTrue(identifiers.validate("foo@pudo.org"))
+
+    def test_compare(self):
+        comp = identifiers.compare("AS98187", "98187")
+        assert comp > 0, comp
+        comp = identifiers.compare_safe(None, "98187")
+        assert comp == 0, comp
+        comp = identifiers.compare_sets(["AS98187"], ["98187"])
+        assert comp > 0, comp
+        comp = identifiers.compare_sets(["98187"], ["AS98187"])
+        assert comp > 0, comp

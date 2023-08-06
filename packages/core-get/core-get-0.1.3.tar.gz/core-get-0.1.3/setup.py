5706cc3cb556c07a7ffba9473e75eee4a8b6a34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:04 +0000

    Add package core-get/0.1.3/core-get-0.1.3.tar.gz

---
+++ packages/core-get/core-get-0.1.3.tar.gz/core-get-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='core-get',
+      version='0.1.3',
+      description='Client for the core-get package sharing system',
+      url='https://github.com/core-get/core-get',
+      author='Oskar Holstensson',
+      author_email='oskar@holstensson.se',
+      license='MIT',
+      install_requires=[
+            'tomlkit~=0.7.0',
+            'injector~=0.18.3',
+            'appdirs~=1.4.4',
+            'semver~=2.13.0',
+            'requests~=2.25.0',
+      ],
+      packages=find_packages(),
+      zip_safe=False)

10e24cc07bf7d7eef08e51a3e832c160d3173c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package dtaledesktop/0.0.12/dtaledesktop-0.0.12-py3.8.egg

---
+++ packages/dtaledesktop/dtaledesktop-0.0.12-py3.8.egg/dtale_desktop/frontend/build/precache-manifest.467bd085f28788cabfaf932e3275dceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72c97ccb561a6036ec8f3b55fdc10cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58f89e1349437a5787cc",
+    "url": "/static/css/2.13b92dd1.chunk.css"
+  },
+  {
+    "revision": "eacc3afa594de500d4f0",
+    "url": "/static/css/main.7968f145.chunk.css"
+  },
+  {
+    "revision": "58f89e1349437a5787cc",
+    "url": "/static/js/2.0c1a9420.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.0c1a9420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacc3afa594de500d4f0",
+    "url": "/static/js/main.2c4944bd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);

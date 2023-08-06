22834ba8617de974bfaabdda1148a208c8a987a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:36 +0000

    Add package abcd-distributions/0.1/abcd_distributions-0.1.tar.gz

---
+++ packages/abcd-distributions/abcd_distributions-0.1.tar.gz/abcd_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='abcd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['abcd_distributions'],
+      zip_safe=False)

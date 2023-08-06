109b10b26ed141136255acdba1e9b7e8ed618842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:48 +0000

    Add package ud-mixpanel-api/0.2/ud_mixpanel_api-0.2.tar.gz

---
+++ packages/ud-mixpanel-api/ud_mixpanel_api-0.2.tar.gz/ud_mixpanel_api-0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='ud_mixpanel_api',  
+     version='0.2',
+     author="Sachin Dev",
+     author_email="hellosachindev@gmail.com",
+     description="Mixpanel API Package for Python 3",
+     long_description="Mixpanel API Package for Python 3",
+     long_description_content_type="text/markdown",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    python_requires='>=3.6',
+ )

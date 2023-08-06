ab0e19fc00b24480bd1f688c02b833952606001a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:03 +0000

    Add package pyembedded/1.3/pyembedded-1.3.tar.gz

---
+++ packages/pyembedded/pyembedded-1.3.tar.gz/pyembedded-1.3/pyembedded/rfid_module/rfid.py
@@ -0,0 +1,24 @@
+import serial
+
+
+class RFID:
+    def __init__(self, port, baud_rate):
+        self.rfid_serial_port = serial.Serial(port, baud_rate)
+
+    def get_id(self):
+        id_num = []
+        i = 0
+        while True:
+            serial_data = self.rfid_serial_port.read()
+            data = serial_data.decode('utf-8')
+            i = i + 1
+            if i == 12:
+                i = 0
+                ID = "".join(map(str, id_num))
+                return ID
+            else:
+                id_num.append(data)
+
+
+if __name__ == '__main__':
+    RFID(port="COM1", baud_rate=9600)

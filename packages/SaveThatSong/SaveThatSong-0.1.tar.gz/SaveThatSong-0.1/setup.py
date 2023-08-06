1af683afca20dfc4ed3e32e218ecffe3769c3d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:15 +0000

    Add package SaveThatSong/0.1/SaveThatSong-0.1.tar.gz

---
+++ packages/SaveThatSong/SaveThatSong-0.1.tar.gz/SaveThatSong-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+        name='SaveThatSong',
+        version='0.1',
+         classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+                     ],
+        packages=setuptools.find_packages(),
+        url='https://github.com/ArjunxyzSatish/SaveThatSong',
+        author='Arjun Satish'
+    )

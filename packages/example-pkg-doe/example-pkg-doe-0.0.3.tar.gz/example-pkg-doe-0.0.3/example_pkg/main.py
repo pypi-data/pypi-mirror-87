6ffb98dd9fe7ff46b435053aa6b4b0fe105c3157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package example-pkg-doe/0.0.3/example-pkg-doe-0.0.3.tar.gz

---
+++ packages/example-pkg-doe/example-pkg-doe-0.0.3.tar.gz/example-pkg-doe-0.0.3/example_pkg/main.py
@@ -0,0 +1,11 @@
+class MyClass:
+    def __init__(self, name):
+        self.name = name
+        self.called = 0
+
+    def call(self):
+        self.called += 1
+
+    def printy(self):
+        print("Hello " + self.name + " for the " + str(self.called) + " time.")
+        self.called += 1

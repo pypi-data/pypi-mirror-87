0088ff1a39c54d8def3ab34efbd617b47a97d1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:55 +0000

    Add package flasktool/0.3.1/flasktool-0.3.1.tar.gz

---
+++ packages/flasktool/flasktool-0.3.1.tar.gz/flasktool-0.3.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time: 2018-1-23 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+import flasktool
+setup(
+    name="flasktool",
+    version="0.3.1",
+    keywords=("pip","flasktool"),
+    description="test pip upload",
+    long_description="test pip upload",
+    license="MIT Licence",
+
+    url="https://github.com/fengmm521/pipProject",
+    author="zhangtianming",
+    author_email="980152647@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['Flask']
+)

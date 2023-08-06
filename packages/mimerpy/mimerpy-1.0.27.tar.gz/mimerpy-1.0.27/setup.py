f7f1563c4bf679296c44aa6c3f754483b06dd760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:19 +0000

    Add package mimerpy/1.0.27/mimerpy-1.0.27.tar.gz

---
+++ packages/mimerpy/mimerpy-1.0.27.tar.gz/mimerpy-1.0.27/setup.py
@@ -0,0 +1,79 @@
+from setuptools import setup, Extension
+from distutils.core import Extension
+import platform, os
+
+plat = platform.system()
+bits = platform.architecture()[0][0:2]
+
+incDirs = []
+libDirs = []
+libs = ['mimerapi']
+
+if plat == 'Linux':
+    pass
+elif plat == 'Darwin':
+    incDirs = ['/usr/local/include']
+    libDirs = ['/usr/local/lib']
+elif plat == 'Windows':
+    libs = ['mimapi' + bits]
+    if bits == '64':
+        libDirs = [os.getenv('LIB')]
+    elif bits == '32':
+        from winreg import HKEY_LOCAL_MACHINE, KEY_READ, KEY_WOW64_64KEY, ConnectRegistry, OpenKeyEx, QueryValueEx, CloseKey
+        root = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+        mimer_key = OpenKeyEx(root, r"SOFTWARE\Mimer\Mimer SQL\11.0", 0,  KEY_READ | KEY_WOW64_64KEY)
+        path = QueryValueEx(mimer_key, 'PathName')[0]
+        CloseKey(root)
+        libDirs = [path + 'dev\\lib\\x86']
+    else: 
+        raise Exception('Unsupported windows version, have to be 32 or 64 bits: ' + bits)
+else:
+    raise Exception('Unsupported platform: ' + plat)
+
+sources = ["src/mimerapi.c"]
+
+extensions = [
+    Extension('mimerapi',
+              include_dirs = incDirs,
+              library_dirs = libDirs,
+              libraries = libs,
+              sources = sources),
+    ]
+
+setup (
+    name='mimerpy',
+    #use_scm_version = True,
+    version = "1.0.27",
+    setup_requires = ['setuptools_scm'],
+    url='https://www.mimer.com',
+    description='Python database interface for Mimer SQL',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    #long_description_content_type="text/markdown",
+    #download_url='www.developer.mimer.com/python/download',
+    author='Erik Gunne & Magdalena Bostrom',
+    author_email='erik.gunne@mimer.com',
+    maintainer = 'Mimer Information Technology AB',
+    maintainer_email = 'mimerpy@mimer.com',
+    license='MIT',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='Mimer MimerSQL Database SQL PEP249',
+    ext_modules = extensions,
+    packages=['mimerpy'],
+    package_dir={'mimerpy': 'mimerpy', 'mimerpy.tests': 'tests'},
+    python_requires='>=3.5',
+    #install_requires=['Mimer>=11.0']
+    )

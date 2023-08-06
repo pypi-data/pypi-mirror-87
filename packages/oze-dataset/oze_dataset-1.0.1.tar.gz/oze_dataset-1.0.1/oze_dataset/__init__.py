00e89dd75ed98d7bce86817b7f90c6d334e70c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:12 +0000

    Add package oze-dataset/1.0.1/oze_dataset-1.0.1.tar.gz

---
+++ packages/oze-dataset/oze_dataset-1.0.1.tar.gz/oze_dataset-1.0.1/oze_dataset/__init__.py
@@ -0,0 +1,6 @@
+"""
+__init__.py
+"""
+from .utils import npz_check
+from .labels import labels
+from .dataset import OzeEvaluationDataset, OzeNPZDataset, TIME_SERIES_LENGTH
---
+++ packages/oze-dataset/oze_dataset-1.0.1.tar.gz/oze_dataset-1.0.1/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name='oze_dataset',
+    version='1.0.1',
+    description='Downloads oze dataset and creates npz file.',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(exclude=("tests",)),
+    author='Daniel Kaminski de Souza',
+    author_email='daniel@kryptonunite.com',
+    keywords=['Dataset', 'Oze Dataset', 'Time Series'],
+    url='https://github.com/krypton-unite/oze_dataset.git',
+    download_url='https://pypi.org/project/oze-dataset/',
+    install_requires = [
+        'time-series-dataset',
+        'pandas',
+        'lxml',
+        'requests',
+        'tqdm'
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'python-dotenv',
+            'pytest-cov'
+        ],
+        'dev': [
+            'bumpversion',
+            'twine',
+            'wheel'
+        ]
+    }
+)

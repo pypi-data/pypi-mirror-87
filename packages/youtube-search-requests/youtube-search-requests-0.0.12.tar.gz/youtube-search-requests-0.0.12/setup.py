7fcbda3b29c8992a4db1b1095c004fadb43eb02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:39 +0000

    Add package youtube-search-requests/0.0.12/youtube-search-requests-0.0.12.tar.gz

---
+++ packages/youtube-search-requests/youtube-search-requests-0.0.12.tar.gz/youtube-search-requests-0.0.12/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'youtube-search-requests',         
+  packages = ['youtube_search_requests'],   
+  version = '0.0.12',
+  license='MIT',     
+  description = 'Search Youtube videos using python requests',
+  long_description= 'This project still need documentation !!!',
+  author = 'Rahman Yusuf',              
+  author_email = 'danipart4@gmail.com',
+  url = 'https://github.com/trollfist20/youtube-search-requests',  
+  download_url = 'https://github.com/trollfist20/youtube-search-requests/archive/v0.0.12.tar.gz',
+  keywords = ['youtube', 'youtube-search'], 
+  install_requires=[           
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',  
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)

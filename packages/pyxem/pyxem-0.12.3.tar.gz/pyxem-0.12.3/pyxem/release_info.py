332619e8d47a002f4f295256913ff3c422ef21bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:02 +0000

    Add package pyxem/0.12.3/pyxem-0.12.3.tar.gz

---
+++ packages/pyxem/pyxem-0.12.3.tar.gz/pyxem-0.12.3/pyxem/release_info.py
@@ -0,0 +1,37 @@
+name = "pyxem"
+version = "0.12.3"
+author = "Duncan Johnstone, Phillip Crout, Magnus Nord"
+copyright = "Copyright 2016-2020, The pyXem Developers"
+credits = [
+    "Duncan Johnstone",
+    "Phillip Crout",
+    "Magnus Nord",
+    "Joonatan Laulainen",
+    "Simon Høgås",
+    "Eirik Opheim",
+    "Ben Martineau",
+    "Tina Bergh",
+    "Stef Smeets",
+    "Carter Francis",
+    "Eric Prestat",
+    "Andrew Ross",
+    "Sean Collins",
+    "Daen Jannis",
+    "Ida Hjorth",
+    "Tom Furnival",
+    "Endre Jacobsen",
+    "Mohsen Danaie",
+    "Andrew Herzing",
+    "Håkon Wiik Ånes",
+    "Tiarnan Doherty",
+    "Affan Iqbal",
+    "Jedrzej Morzy",
+    "Tomas Ostasevicius",
+    "Rob Tovey",
+    "Matt von Lany",
+    "Tim Poon",
+]
+license = "GPLv3"
+maintainer = "Duncan Johnstone, Phillip Crout, Magnus Nord"
+email = "pyxem.team@gmail.com"
+status = "development"

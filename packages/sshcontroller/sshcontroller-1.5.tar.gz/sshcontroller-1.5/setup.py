0f030fd66d0d16c6ef5514e73f2ad309e003463f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:44 +0000

    Add package sshcontroller/1.5/sshcontroller-1.5.tar.gz

---
+++ packages/sshcontroller/sshcontroller-1.5.tar.gz/sshcontroller-1.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sshcontroller",
+    version="1.5",
+    author="Olivier Roques",
+    author_email="olivier@oroques.dev",
+    description="A package to easily run SSH commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ojroques/python-sshcontroller",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+        "Topic :: Internet",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development",
+    ],
+    python_requires='>=3.6',
+    install_requires=['paramiko'],
+)

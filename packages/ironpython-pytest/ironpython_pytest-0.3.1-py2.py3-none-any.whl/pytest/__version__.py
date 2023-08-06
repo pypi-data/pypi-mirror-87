e8b366509977d6c23714f5a1d5ed7d33a5d72b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package ironpython-pytest/0.3.1/ironpython_pytest-0.3.1-py2.py3-none-any.whl

---
+++ packages/ironpython-pytest/ironpython_pytest-0.3.1-py2.py3-none-any.whl/pytest/__version__.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+__author__ = 'Gramazio Kohler Research'
+__copyright__ = 'Copyright 2020 Gramazio Kohler Research'
+__license__ = 'MIT'
+__email__ = 'gramaziokohler@arch.ethz.ch'
+
+__version__ = '0.3.1'
+
+__all__ = ['__author__', '__email__', '__copyright__', '__license__', '__version__']

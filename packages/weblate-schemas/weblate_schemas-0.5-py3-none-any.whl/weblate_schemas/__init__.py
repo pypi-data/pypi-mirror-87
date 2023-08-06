9539223f7ca11ebcc8956fb43bae9226a245e39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:48 +0000

    Add package weblate-schemas/0.5/weblate_schemas-0.5-py3-none-any.whl

---
+++ packages/weblate-schemas/weblate_schemas-0.5-py3-none-any.whl/weblate_schemas/__init__.py
@@ -0,0 +1,38 @@
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate weblate_schemas
+# <https://github.com/WeblateOrg/weblate_schemas>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Schemas used by Weblate."""
+
+__version__ = "0.5"
+
+import json
+from pathlib import Path
+
+SCHEMA_BASE = Path(__file__).resolve().parent.joinpath("schemas")
+
+
+def get_path(name):
+    """Build filename for a schema."""
+    return SCHEMA_BASE.joinpath(name)
+
+
+def load_schema(name):
+    """Load schema from a disk."""
+    with get_path(name).open("r") as handle:
+        return json.load(handle)
---
+++ packages/weblate-schemas/weblate_schemas-0.5-py3-none-any.whl/weblate_schemas/test_valid.py
@@ -0,0 +1,87 @@
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate weblate_schemas
+# <https://github.com/WeblateOrg/weblate_schemas>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Test schemas are valid."""
+
+from jsonschema import validate
+
+from weblate_schemas import load_schema
+
+
+def test_memory():
+    """Test memory schema being valid."""
+    validate([], load_schema("weblate-memory.schema.json"))
+
+
+def test_memory_newline():
+    """Test memory entry containing newlines."""
+    validate(
+        [
+            {
+                "source": "Error reading config file {filename!r}:\n{error_msg}",
+                "target": "Fehler der Konfigurationsdatei {filename!r}:\n{error_msg}",
+                "source_language": "en",
+                "target_language": "de",
+                "origin": "myproject/mycomponent",
+                "category": 10000004,
+            }
+        ],
+        load_schema("weblate-memory.schema.json"),
+    )
+
+
+def test_userdata():
+    """Test user data schema being valid."""
+    validate(
+        {
+            "basic": {
+                "username": "nijel",
+                "full_name": "Weblate Admin",
+                "email": "michal@cihar.com",
+                "date_joined": "2019-11-18T18:53:54.862Z",
+            },
+            "profile": {
+                "language": "cs",
+                "suggested": 1,
+                "translated": 24,
+                "uploaded": 1,
+                "hide_completed": False,
+                "secondary_in_zen": True,
+                "hide_source_secondary": False,
+                "editor_link": "",
+                "translate_mode": 0,
+                "zen_mode": 0,
+                "special_chars": "\u00a0 ",
+                "dashboard_view": 1,
+                "dashboard_component_list": None,
+                "languages": ["cs", "vi"],
+                "secondary_languages": ["sk"],
+                "watched": ["weblate"],
+            },
+            "auditlog": [
+                {
+                    "address": "127.0.0.1",
+                    "user_agent": "PC / Linux / Firefox 70.0",
+                    "timestamp": "2019-11-18T18:58:30.845Z",
+                    "activity": "login",
+                },
+            ],
+        },
+        load_schema("weblate-userdata.schema.json"),
+    )

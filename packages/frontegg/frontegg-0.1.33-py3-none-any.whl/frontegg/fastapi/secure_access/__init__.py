a17a2e9b804d9e7c49ac636dc66816b81a177a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package frontegg/0.1.33/frontegg-0.1.33-py3-none-any.whl

---
+++ packages/frontegg/frontegg-0.1.33-py3-none-any.whl/frontegg/fastapi/secure_access/__init__.py
@@ -0,0 +1,5 @@
+from .security import FronteggHTTPAuthentication, FronteggSecurity, User
+from .context_provider import context_provider
+from .authentication_middleware import authentication_middleware
+
+__all__ = ('FronteggHTTPAuthentication', 'FronteggSecurity', 'context_provider', 'authentication_middleware', 'User')

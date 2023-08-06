e658966d315ae8ae0aa937bd7431b7f3dcd85cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:20 +0000

    Add package opentims-bruker-bridge/1.0.0/opentims_bruker_bridge-1.0.0.tar.gz

---
+++ packages/opentims-bruker-bridge/opentims_bruker_bridge-1.0.0.tar.gz/opentims_bruker_bridge-1.0.0/opentims_bruker_bridge/__init__.py
@@ -0,0 +1,27 @@
+import os
+import platform
+
+
+_abspath = os.path.abspath(__file__)
+_mod_dir = os.path.dirname(_abspath)
+
+_opj = os.path.join
+
+
+
+_paths = {
+        ("64bit", "ELF") : _opj(_mod_dir, "libtimsdata.so"),
+        ("32bit", "WindowsPE") : _opj(_mod_dir, "win32", "timsdata.dll"),
+        ("64bit", "WindowsPE") : _opj(_mod_dir, "win64", "timsdata.dll"),
+        # Probably CYGWIN:
+        ("32bit", "") : _opj(_mod_dir, "win32", "timsdata.dll"),
+        ("64bit", "") : _opj(_mod_dir, "win64", "timsdata.dll"),
+     }
+
+
+def get_so_paths():
+    try:
+        return [_paths[platform.architecture()]] + list(set(_paths.values()) -  set([_paths[platform.architecture()]]))
+    except KeyError:
+        # unsupported platform: return everything and hope for the best
+        return list(set(_paths.values()))

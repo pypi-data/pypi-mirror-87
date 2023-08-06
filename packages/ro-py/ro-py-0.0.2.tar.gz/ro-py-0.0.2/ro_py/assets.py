b3693db965bee95450f56d919582df9c8d2e420f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package ro-py/0.0.2/ro-py-0.0.2.tar.gz

---
+++ packages/ro-py/ro-py-0.0.2.tar.gz/ro-py-0.0.2/ro_py/assets.py
@@ -0,0 +1,68 @@
+from ro_py import User, Group
+from ro_py.errors import NotLimitedError
+from ro_py.asset_type import asset_types
+import iso8601
+import requests
+
+endpoint = "https://api.roblox.com/"
+
+
+class LimitedResaleData:
+    def __init__(self, resale_data):
+        self.asset_stock = resale_data["assetStock"]
+        self.sales = resale_data["sales"]
+        self.number_remaining = resale_data["numberRemaining"]
+        self.recent_average_price = resale_data["recentAveragePrice"]
+        self.original_price = resale_data["originalPrice"]
+
+
+class Asset:
+    def __init__(self, asset_id):
+        asset_info_req = requests.get(
+            url=endpoint + "marketplace/productinfo",
+            params={
+                "assetId": asset_id
+            }
+        )
+        asset_info = asset_info_req.json()
+        self.target_id = asset_info["TargetId"]
+        self.product_type = asset_info["ProductType"]
+        self.asset_id = asset_info["AssetId"]
+        self.product_id = asset_info["ProductId"]
+        self.name = asset_info["Name"]
+        self.description = asset_info["Description"]
+        self.asset_type_id = asset_info["AssetTypeId"]
+        self.asset_type_name = asset_types[self.asset_type_id]
+        if asset_info["Creator"]["CreatorType"] == "User":
+            self.creator = User(asset_info["Creator"]["Id"])
+        elif asset_info["Creator"]["CreatorType"] == "Group":
+            self.creator = Group(asset_info["Creator"]["CreatorTargetId"])
+        self.created = iso8601.parse_date(asset_info["Created"])
+        self.updated = iso8601.parse_date(asset_info["Updated"])
+        self.price = asset_info["PriceInRobux"]
+        self.is_new = asset_info["IsNew"]
+        self.is_for_sale = asset_info["IsForSale"]
+        self.is_public_domain = asset_info["IsPublicDomain"]
+        self.is_limited = asset_info["IsLimited"]
+        self.is_limited_unique = asset_info["IsLimitedUnique"]
+        self.minimum_membership_level = asset_info["MinimumMembershipLevel"]
+        self.content_rating_type_id = asset_info["ContentRatingTypeId"]
+
+    @property
+    def remaining(self):
+        asset_info_req = requests.get(
+            url=endpoint + "marketplace/productinfo",
+            params={
+                "assetId": self.asset_id
+            }
+        )
+        asset_info = asset_info_req.json()
+        return asset_info["Remaining"]
+
+    @property
+    def limited_resale_data(self):
+        if self.is_limited:
+            resale_data_req = requests.get(f"https://economy.roblox.com/v1/assets/{self.asset_id}/resale-data")
+            return LimitedResaleData(resale_data_req.json())
+        else:
+            raise NotLimitedError("You can only read this information on limited items.")

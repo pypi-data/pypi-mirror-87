55d6b0690869821f2d9d467e988cda679a44ecb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:50 +0000

    Add package sneact/0.1.0/sneact-0.1.0-py3-none-any.whl

---
+++ packages/sneact/sneact-0.1.0-py3-none-any.whl/sneact/__init__.py
@@ -0,0 +1,8 @@
+from sneact._magic_tags import MagicHTMLTag
+from sneact._scopes import _SneactScope
+from sneact._sneact import Sneact
+
+__version__ = "0.1.0"
+
+_ = MagicHTMLTag("\n")
+s = _SneactScope()
---
+++ packages/sneact/sneact-0.1.0-py3-none-any.whl/sneact/form.py
@@ -0,0 +1,7 @@
+from sneact._magic_tags import MagicHTMLTag as _MHT
+
+
+form = _MHT("form")
+form_input = _MHT("input")
+form_text_input = _MHT("textarea")
+form_button = _MHT("button")

73d7018b32cab7272b9a9af4d85baaa735738392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:34 +0000

    Add package molecular/0.1.dev18/molecular-0.1.dev18.tar.gz

---
+++ packages/molecular/molecular-0.1.dev18.tar.gz/molecular-0.1.dev18/molecular/analysis/protein/__init__.py
@@ -0,0 +1,12 @@
+
+from .secondary_structure import *
+from .hydrophobicity import *
+from .r1N import *
+
+__all__ = [
+    'compute_hydrophobic_moment',
+    'HydrophobicityScale',
+    'r1N',
+    'SecondaryStructure',
+    'secondary_structure',
+]

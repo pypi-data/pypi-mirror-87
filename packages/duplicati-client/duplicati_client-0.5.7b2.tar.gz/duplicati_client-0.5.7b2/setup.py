c05495df834870e8a41e8bae99d8d629bfc49233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package duplicati-client/0.5.7b2/duplicati_client-0.5.7b2.tar.gz

---
+++ packages/duplicati-client/duplicati_client-0.5.7b2.tar.gz/duplicati_client-0.5.7b2/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup, find_packages
+HERE = pathlib.Path(__file__).parent
+VERSION = '0.5.7_beta2'
+PACKAGE_NAME = 'duplicati_client'
+AUTHOR = 'Rune Henriksen'
+AUTHOR_EMAIL = 'contact@henriksen.dk'
+URL = 'https://github.com/pectojin/duplicati-client'
+LICENSE = 'LGPL-2.1'
+DESCRIPTION = 'A command-line tool for controlling remote or local Duplicti servers'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+INSTALL_REQUIRES = [
+      'requests',
+      'PyYaml',
+      'python-dateutil'
+]
+ENTRY_POINTS = {
+      'console_scripts': ['duplicati-client=duplicati_client:main']
+}
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      entry_points=ENTRY_POINTS
+      )

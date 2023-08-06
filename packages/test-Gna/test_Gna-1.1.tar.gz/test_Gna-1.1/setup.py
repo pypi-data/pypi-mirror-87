6a3100da0d281ce460848bc5c830d3b3efbce916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:43 +0000

    Add package test-Gna/1.1/test_Gna-1.1.tar.gz

---
+++ packages/test-Gna/test_Gna-1.1.tar.gz/test_Gna-1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="test_Gna",
+    version="1.1",
+    license='MIT',
+    author="Gna",
+    author_email="luckyrookie@khu.ac.kr",
+    description="test.",
+    long_description=open('README.md').read(),
+    url="https://github.com/GyeongahNa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)

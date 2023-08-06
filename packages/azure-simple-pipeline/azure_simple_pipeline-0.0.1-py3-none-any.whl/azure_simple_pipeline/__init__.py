638b7b2fe9e77d2b8578368ce49722d3583b9afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:35 +0000

    Add package azure-simple-pipeline/0.0.1/azure_simple_pipeline-0.0.1-py3-none-any.whl

---
+++ packages/azure-simple-pipeline/azure_simple_pipeline-0.0.1-py3-none-any.whl/azure_simple_pipeline/__init__.py
@@ -0,0 +1,17 @@
+"""Azure simple pipeline
+
+This package contains the modules for deploying a simple pipeline on Azure.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the CI/CD.
+
+Package contents ..
+
+    >>> import azure_simple_pipeline
+    >>> help(azure_simple_pipeline)
+
+# license MIT
+# support https://github.com/bilardi/azure-simple-pipeline/issues
+"""
+__version__ = '0.0.1'
+__author__ = 'Alessandra Bilardi'

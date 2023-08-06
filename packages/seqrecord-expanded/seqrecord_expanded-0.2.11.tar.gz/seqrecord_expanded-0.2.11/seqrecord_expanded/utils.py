e4e23c7450a6540fe5aaa9b80cee02d0a897389d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:59 +0000

    Add package seqrecord-expanded/0.2.11/seqrecord_expanded-0.2.11.tar.gz

---
+++ packages/seqrecord-expanded/seqrecord_expanded-0.2.11.tar.gz/seqrecord_expanded-0.2.11/seqrecord_expanded/utils.py
@@ -0,0 +1,18 @@
+import itertools
+
+import six
+if six.PY2:
+    from six.moves import zip_longest
+else:
+    from itertools import zip_longest
+
+
+def chain_and_flatten(seq1, seq2):
+    """Takes two strings (first and second codon positions) and chains them.
+
+    Returns:
+        (str): String of combining the two seq strings.
+    """
+    my_chain = zip_longest(seq1, seq2)
+    out = [i for i in itertools.chain.from_iterable(my_chain) if i]
+    return ''.join(out)
---
+++ packages/seqrecord-expanded/seqrecord_expanded-0.2.11.tar.gz/seqrecord_expanded-0.2.11/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+import setuptools
+from os.path import join
+from os.path import dirname
+
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setuptools.setup(
+    name="seqrecord_expanded",
+    version="0.2.11",
+    license="BSD",
+    url="https://github.com/carlosp420/seqrecord-expanded",
+
+    author="Carlos Peña",
+    author_email="mycalesis@gmail.com",
+
+    description="Another SeqRecord class with methods: degenerate seqs, codon positions based on reading frames, etc.",
+    long_description='%s' % read('README.rst'),
+
+    packages=['seqrecord_expanded'],
+
+    install_requires=[
+        'biopython==1.78',
+        'degenerate-dna==0.0.9',
+        'six==1.10.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+    test_suite='tests',
+)

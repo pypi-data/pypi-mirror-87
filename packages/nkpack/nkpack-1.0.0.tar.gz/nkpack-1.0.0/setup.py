78459559b6e320b2d583a52fe0905b3adfdc3d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package nkpack/1.0.0/nkpack-1.0.0.tar.gz

---
+++ packages/nkpack/nkpack-1.0.0.tar.gz/nkpack-1.0.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="nkpack", # Replace with your own username
+    version="1.0.0",
+    author="Ravshan S.K.",
+    author_email="rsk@ravshansk.com",
+    description="A library of utilities for NK modeling",
+    url="https://github.com/ravshansk/nkpack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+

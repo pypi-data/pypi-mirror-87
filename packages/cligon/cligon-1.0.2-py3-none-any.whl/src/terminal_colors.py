0ca5bd07ff5236f086a5d6732827c3b718077ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:09 +0000

    Add package cligon/1.0.2/cligon-1.0.2-py3-none-any.whl

---
+++ packages/cligon/cligon-1.0.2-py3-none-any.whl/src/terminal_colors.py
@@ -0,0 +1,11 @@
+"""class for color codes"""
+
+
+class TerminalColors:
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    GREY = "\033[37m"
+    ENDCOLOR = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"

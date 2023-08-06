1a711f67a4361d39d138d7230429b5ba0d790b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:44 +0000

    Add package azure-blob-check/1.0/azure_blob_check-1.0.tar.gz

---
+++ packages/azure-blob-check/azure_blob_check-1.0.tar.gz/azure_blob_check-1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='azure_blob_check',
+    version='1.0',
+    description='azure blob filelist check',
+    author='Kyung Jun Lee',
+    author_email='kyungjunlee.me@gmail.com',
+    url='https://github.com/kyungjunleeme/azure_blob_check',
+    download_url='https://github.com/kyungjunleeme/azure_blob_check/archive/main.zip',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=[],
+    keywords=['azure_blob', 'blob_list'],
+    python_requires='>=3.6',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)

8299f830f070d0ffbc83fe376f4f8f05d1097539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:54 +0000

    Add package github-repos/2020.12.3/github-repos-2020.12.3.tar.gz

---
+++ packages/github-repos/github-repos-2020.12.3.tar.gz/github-repos-2020.12.3/github_repos/__init__.py
@@ -0,0 +1,20 @@
+__all__ = ['repos']
+
+
+import github
+import os
+
+
+def repos(login=None):
+    """return a list of user repos"""
+    fullnames = []
+    TOKEN = os.environ["GITHUB_TOKEN"]
+    g = github.Github(TOKEN)
+    if login:
+        user = g.get_user(login)
+    else:
+        user = g.get_user()
+    for repo in user.get_repos():
+        fullname = "%s/%s" % (repo.owner.login, repo.name)
+        fullnames += [fullname]
+    return fullnames

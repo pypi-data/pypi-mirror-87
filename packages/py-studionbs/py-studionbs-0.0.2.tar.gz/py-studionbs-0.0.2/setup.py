50edc96c3087e4853ba32958d7d1aa7894fc19cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:54 +0000

    Add package py-studionbs/0.0.2/py-studionbs-0.0.2.tar.gz

---
+++ packages/py-studionbs/py-studionbs-0.0.2.tar.gz/py-studionbs-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = 'py-studionbs',
+  packages = ['py_studionbs'],
+  version = '0.0.2',
+  license='MIT',
+  description = 'A personal repository of useful Python functions',
+  author = 'Isaiah Discipulo',
+  author_email = 'ikdiscipulo@gmail.com',
+  url = 'https://github.com/idiscipulo/py-studionbs',
+  download_url = 'https://github.com/idiscipulo/py-studionbs/archive/v_0_0_2.tar.gz',
+  keywords = [],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7'
+  ],
+)

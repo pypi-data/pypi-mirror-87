5b4b782b5763667e7de37a6b44f6f35129c5b819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:22 +0000

    Add package elasticroute/2.0.2/elasticroute-2.0.2.tar.gz

---
+++ packages/elasticroute/elasticroute-2.0.2.tar.gz/elasticroute-2.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="elasticroute",
+    version="2.0.2",
+    author="Detrack",
+    author_email="chester@detack.com",
+    description="Free, intelligent routing for your logistics – now on Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/detrack/elasticroute-python",
+    packages=setuptools.find_packages(exclude=("tests")),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+    ],
+    setup_requires=[
+        "pytest-runner"
+    ],
+    tests_require=[
+        "pytest",
+        "pytest-cov"
+    ]
+)

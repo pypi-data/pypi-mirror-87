8adf2001aac2e8eaf8678c5c151455520cc79401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package graphql-py/0.8.1/graphql-py-0.8.1.tar.gz

---
+++ packages/graphql-py/graphql-py-0.8.1.tar.gz/graphql-py-0.8.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import os
+import graphql
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+packages = ['graphql']
+requires = ['ply>=3.6']
+
+setup(
+    name='graphql-py',
+    version=graphql.__version__,
+    description='graphql-py: Parser for latest GraphQL specification',
+    long_description=open('README.rst').read(),
+    author='Denis Stebunov',
+    author_email='support@ivelum.com',
+    url='https://github.com/ivelum/graphql-py/',
+    packages=packages,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
+del os.environ['PYTHONDONTWRITEBYTECODE']

94107569dfd09a4d74b96200c4fca0ac4539e547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:08 +0000

    Add package arbol/2020.11.5/arbol-2020.11.5.tar.gz

---
+++ packages/arbol/arbol-2020.11.5.tar.gz/arbol-2020.11.5/arbol/demo/demo.py
@@ -0,0 +1,42 @@
+from arbol.arbol import aprint, asection, section, Arbol
+
+# for colors, install the ansicolors package: 'pip install ansicolors',
+# and for windows install the colorama package: 'pip install colorama'
+
+# You can limit the tree depth:
+Arbol.max_depth = 4
+
+# use aprint (=arbol print) instead of the standard print
+aprint('Test')
+
+# You can decorate functions:
+@section('function')
+def fun(x):
+    if x >= 0:
+        with asection('recursive call to f'):
+            aprint(f"f(x)+1={fun(x - 1)}")
+
+# The context manager let's you go down one level in the tree
+with asection('a section'):
+    aprint('a line')
+    aprint('another line')
+    aprint('we are done \n or are we? \n someone gotta check!')
+
+    with asection('a subsection'):
+        aprint('another line')
+        aprint('we are done')
+
+    # works through function calls and the like...
+    fun(2)
+
+    # You can deactivate the elapsed time measurement and printing:
+    Arbol.elapsed_time = False
+    fun(100)
+
+aprint('demo is finished...')
+
+# You can also turn off all output with one switch:
+Arbol.enable_output = False
+aprint('you will not see that')
+
+

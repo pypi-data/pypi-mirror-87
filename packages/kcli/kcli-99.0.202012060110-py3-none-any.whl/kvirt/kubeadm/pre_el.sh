fb526b9b0ea314bb8c7acd77851dca41013ed6d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:01 +0000

    Add package kcli/99.0.202012060110/kcli-99.0.202012060110-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202012060110-py3-none-any.whl/kvirt/kubeadm/pre_el.sh
@@ -0,0 +1,22 @@
+echo """[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0""" >/etc/yum.repos.d/kubernetes.repo
+echo net.bridge.bridge-nf-call-iptables=1 >> /etc/sysctl.d/99-sysctl.conf
+modprobe br_netfilter
+sysctl -p
+setenforce 0
+sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+{% if version != None %}
+VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | grep {{ version }} | tail -1 | awk '{print $2}' | xargs)
+{% else %}
+VERSION=$(dnf --showduplicates list kubectl  | grep kubectl | tail -1 | awk '{print $2}' | xargs)
+{% endif %}
+
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install -y docker-ce --nobest
+systemctl enable --now docker
+dnf -y install -y kubelet-$VERSION kubectl-$VERSION kubeadm-$VERSION git
+systemctl enable --now kubelet

1b4e13304acb90e53c8135a5f606962943374254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:02 +0000

    Add package morph-api-tomcart/0.1.9/morph_api_tomcart-0.1.9.tar.gz

---
+++ packages/morph-api-tomcart/morph_api_tomcart-0.1.9.tar.gz/morph_api_tomcart-0.1.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='morph_api_tomcart',
+    version='0.1.9',
+    packages=['morph_api_tomcart'],
+    url='',
+    license='',
+    install_requires=[
+        'requests',
+        'isodate'
+    ],
+    author='Ricky Nelson',
+    author_email='rnelson@thetomcart.com',
+    description='Morpheus API Functions'
+)

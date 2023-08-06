21163aa478815693f7ab41ffb53fc3340ddf3d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:16 +0000

    Add package pipreqs-update/2020.12.3/pipreqs-update-2020.12.3.tar.gz

---
+++ packages/pipreqs-update/pipreqs-update-2020.12.3.tar.gz/pipreqs-update-2020.12.3/pipreqs_update/__init__.py
@@ -0,0 +1,30 @@
+__all__ = ['update']
+
+
+import requests
+
+
+def getnames(path):
+    names = []
+    for l in open(path).read().splitlines():
+        name = l.split("#")[0].split(">")[0].split("<")[0].split("=")[0]
+        if name:
+            names.append(name)
+    return names
+
+
+def get_latest_version(name):
+    url = "https://pypi.org/pypi/%s/json" % name
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()
+    return data["info"]["version"]
+
+
+def update(path):
+    """update pip requirements file with latest versions"""
+    lines = []
+    for name in getnames(path):
+        version = get_latest_version(name)
+        lines.append('%s==%s' % (name, version))
+    open(path, "w").write("\n".join(lines))

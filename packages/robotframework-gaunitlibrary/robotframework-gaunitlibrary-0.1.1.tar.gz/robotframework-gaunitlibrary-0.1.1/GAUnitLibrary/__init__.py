7da50811d7976baeb73b50ce8189795166db0022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package robotframework-gaunitlibrary/0.1.1/robotframework-gaunitlibrary-0.1.1.tar.gz

---
+++ packages/robotframework-gaunitlibrary/robotframework-gaunitlibrary-0.1.1.tar.gz/robotframework-gaunitlibrary-0.1.1/GAUnitLibrary/__init__.py
@@ -0,0 +1,14 @@
+from .__about__ import __version__
+import gaunit
+
+
+class GAUnitLibrary:
+
+    ROBOT_LIBRARY_VERSION = __version__
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+
+    def check_tracking_from_har(
+        self, test_case: str, tracking_plan: str, har: dict
+    ) -> list:
+        r = gaunit.check_har(test_case, tracking_plan, har=har)
+        return r.checklist_expected

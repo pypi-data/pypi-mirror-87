8dd113a05b60f7f21027b65ad5538aba1c69f0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:28 +0000

    Add package maestral/1.3.0.dev0/maestral-1.3.0.dev0-py3-none-any.whl

---
+++ packages/maestral/maestral-1.3.0.dev0-py3-none-any.whl/maestral/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+
+The following APIs should remain stable for frontends:
+
+* maestral.main.Maestral
+* maestral.constants
+* maestral.daemon
+* maestral.errors
+* maestral.utils.appdirs
+* maestral.utils.autostart
+
+"""
+
+__version__ = "1.3.0.dev0"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/SamSchott/maestral"

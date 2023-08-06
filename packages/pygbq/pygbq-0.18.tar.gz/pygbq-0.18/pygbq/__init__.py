0f10e34c40d64a99f8a8066cfebc830580f7d59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:01 +0000

    Add package pygbq/0.18/pygbq-0.18.tar.gz

---
+++ packages/pygbq/pygbq-0.18.tar.gz/pygbq-0.18/pygbq/__init__.py
@@ -0,0 +1,12 @@
+from .pygbq import (
+    gbq,
+    table,
+    set_dataset,
+    set_service_key_path,
+    MyError,
+    read_jsonl,
+    generate_schema,
+    get_secret,
+    add_secret,
+    update_table_using_temp,
+)

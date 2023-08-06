6daf9e981bef5b1d66ee27101c4060ecad845c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package mestpy/0.3.2/mestpy-0.3.2.tar.gz

---
+++ packages/mestpy/mestpy-0.3.2.tar.gz/mestpy-0.3.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='mestpy',
+      version='0.3.2',
+      description='Gauss-Jordan Elimination in Teaching, Work with docs of main professional educational program',
+      packages=find_packages( include=['mestpy','mestpy.*']),
+      install_requires = [ 
+        'pandas >= 0.23.3', 
+        'NumPy >= 1.14.5', 
+		'doc2pdf',
+		'PyPDF2>=1.26.0',
+		'python-docx'
+        ],
+      extras_require = { 
+        'interactive' :  ['jupyter']
+		},
+      author ='Mestnikov SV',
+      author_email='mestsv@mail.ru',
+      zip_safe=False)

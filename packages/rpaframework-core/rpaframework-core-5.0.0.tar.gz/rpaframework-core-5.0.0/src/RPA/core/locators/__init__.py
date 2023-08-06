0b133471bb7e1961966e786caf35bf749d366558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:48 +0000

    Add package rpaframework-core/5.0.0/rpaframework-core-5.0.0.tar.gz

---
+++ packages/rpaframework-core/rpaframework-core-5.0.0.tar.gz/rpaframework-core-5.0.0/src/RPA/core/locators/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+from dataclasses import fields
+from .containers import (
+    TYPES,
+    Locator,
+    PointLocator,
+    OffsetLocator,
+    RegionLocator,
+    SizeLocator,
+    ImageLocator,
+    OcrLocator,
+    BrowserLocator,
+    # For backwards compatibility:
+    Coordinates,
+    Offset,
+    BrowserDOM,
+    ImageTemplate,
+)
+from .database import LocatorsDatabase, sanitize_name

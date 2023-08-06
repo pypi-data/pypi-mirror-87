72791f2ac682803642c4852102bd02c59b012fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package prettyqt/0.141.0/PrettyQt-0.141.0-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.141.0-py3-none-any.whl/prettyqt/custom_validators/notemptyvalidator.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from prettyqt import gui
+
+
+class NotEmptyValidator(gui.Validator):
+    def validate(self, text: str, pos: int = 0) -> tuple:
+        if text == "":
+            return (self.Intermediate, text, pos)
+        return (self.Acceptable, text, pos)
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    val = NotEmptyValidator()
+    app = widgets.app()
+    widget = widgets.LineEdit("This is a test")
+    widget.setValidator(val)
+    widget.show()
+    app.main_loop()

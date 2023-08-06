6185ae61f5a74f6247d636b7dfd1d3ee7df37f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:38 +0000

    Add package aioshadowsocks/0.1.4/aioshadowsocks-0.1.4-py3-none-any.whl

---
+++ packages/aioshadowsocks/aioshadowsocks-0.1.4-py3-none-any.whl/shadowsocks/proxyman.py
@@ -0,0 +1,75 @@
+import asyncio
+import logging
+from collections import defaultdict
+
+from shadowsocks.core import LocalTCP, LocalUDP
+from shadowsocks.mdb.models import User
+
+
+class ProxyMan:
+    """
+    1. 将model的调用都放在这里
+    2. server相关的东西也放在这里
+    app -> proxyman -> core ->cipherman/model
+    """
+
+    AEAD_METHOD_LIST = [
+        "chacha20-ietf-poly1305",
+        "aes-128-gcm",
+        "aes-256-gcm",
+    ]
+
+    def __init__(self, listen_host):
+        self.loop = asyncio.get_event_loop()
+
+        # {"port":{"tcp":tcp_server,"udp":udp_server}}
+        self.__running_servers__ = defaultdict(dict)
+
+        self.api_endpoint = None
+        self.sync_time = None
+        self.listen_host = listen_host
+
+    def get_server_by_port(self, port):
+        return self.__running_servers__.get(port)
+
+    async def start_ss_server(self):
+        for user in User.select().where(User.enable == True):
+            await self.loop.create_task(self.init_server(user))
+        for user in User.select().where(User.enable == False):
+            self.close_user_server(user)
+
+    async def init_server(self, user: User):
+
+        running_server = self.get_server_by_port(user.port)
+        if running_server:
+            return
+
+        tcp_server = await self.loop.create_server(
+            LocalTCP(user.port), self.listen_host, user.port
+        )
+        udp_server, _ = await self.loop.create_datagram_endpoint(
+            LocalUDP(user.port), (self.listen_host, user.port)
+        )
+        self.__running_servers__[user.port] = {
+            "tcp": tcp_server,
+            "udp": udp_server,
+        }
+        logging.info(
+            "user:{} method:{} password:{} {}:{} 已启动".format(
+                user, user.method, user.password, self.listen_host, user.port
+            )
+        )
+
+    def close_user_server(self, user):
+        running_server = self.get_server_by_port(user.port)
+        if running_server and user.method not in self.AEAD_METHOD_LIST:
+            running_server["tcp"].close()
+            running_server["udp"].close()
+            self.__running_servers__.pop(user.port)
+            logging.info(f"user {user} 已关闭!")
+
+    def close_server(self):
+        for port, server_data in self.__running_servers__.items():
+            server_data["tcp"].close()
+            server_data["udp"].close()
+            logging.info(f"port:{port} 已关闭!")
---
+++ packages/aioshadowsocks/aioshadowsocks-0.1.4-py3-none-any.whl/shadowsocks/utils.py
@@ -0,0 +1,87 @@
+import logging
+import socket
+import struct
+from functools import lru_cache
+
+from bloom_filter import BloomFilter
+
+from shadowsocks import protocol_flag as flag
+
+
+def logging_cahce_info():
+    def wrapper(func):
+        def decorated(*args, **kwargs):
+            logging.debug(f"domain:{args[0]} cache_info: {func.cache_info()}")
+            return func(*args, **kwargs)
+
+        return decorated
+
+    return wrapper
+
+
+@logging_cahce_info()
+@lru_cache(2 ** 8)
+def get_ip_from_domain(domain):
+    try:
+        return socket.gethostbyname(domain.encode())
+    except Exception:
+        logging.warning(f"Failed to query DNS: {domain}")
+        return domain
+
+
+def parse_header(data):
+    # shadowsocks protocol https://shadowsocks.org/en/spec/Protocol.html
+    atype, dst_addr, dst_port, header_length = data[0], None, None, 0
+    if atype == flag.ATYPE_IPV4:
+        if len(data) >= 7:
+            dst_addr = socket.inet_ntop(socket.AF_INET, data[1:5])
+            dst_port = struct.unpack("!H", data[5:7])[0]
+            header_length = 7
+        else:
+            logging.warning("header is too short")
+    elif atype == flag.ATYPE_IPV6:
+        if len(data) >= 19:
+            dst_addr = socket.inet_ntop(socket.AF_INET6, data[1:17])
+            dst_port = struct.unpack("!H", data[17:19])[0]
+            header_length = 19
+        else:
+            logging.warning("header is too short")
+    elif atype == flag.ATYPE_DOMAINNAME:
+        if len(data) > 2:
+            addrlen = data[1]
+            if len(data) >= 4 + addrlen:
+                dst_addr = data[2 : 2 + addrlen]
+                dst_addr = get_ip_from_domain(dst_addr.decode())
+                dst_port = struct.unpack("!H", data[2 + addrlen : addrlen + 4])[0]
+                header_length = 4 + addrlen
+            else:
+                logging.warning("header is too short")
+        else:
+            logging.warning("header is too short")
+    else:
+        logging.warning(f"unknown atype: {atype}")
+
+    return atype, dst_addr, dst_port, header_length
+
+
+class AutoResetBloomFilter:
+
+    MAX_ELEMENTS = 10 ** 6
+    ERROR_RATE = 10 ** -6
+
+    def new_bf(self):
+        self.size = self.MAX_ELEMENTS
+        return BloomFilter(max_elements=self.MAX_ELEMENTS, error_rate=self.ERROR_RATE)
+
+    def __init__(self):
+        self.bf = self.new_bf()
+
+    def add(self, v):
+        if self.size <= 0:
+            logging.warning("bloom filter reset")
+            self.bf = self.new_bf()
+        self.bf.add(v)
+        self.size -= 1
+
+    def __contains__(self, key):
+        return key in self.bf

f8b8effdc727b86965e04ae5bd23564a87d5afb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package topdesk/0.0.8/topdesk-0.0.8.tar.gz

---
+++ packages/topdesk/topdesk-0.0.8.tar.gz/topdesk-0.0.8/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='topdesk',
+    author='Veit Heller',
+    version='0.0.8',
+    license='GPLv3',
+    url='https://gitlab.com/wobcom/topdesk',
+    description='A modern TOPdesk wrapper for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages('.'),
+    install_requires=[
+        "requests>=2.22.0",
+    ]
+)
+

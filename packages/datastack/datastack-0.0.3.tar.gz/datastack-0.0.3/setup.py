f982f6ee00421805e4b3e6009e45e4e775a086aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:10 +0000

    Add package datastack/0.0.3/datastack-0.0.3.tar.gz

---
+++ packages/datastack/datastack-0.0.3.tar.gz/datastack-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='datastack',
+    version='0.0.3',
+    author='Max Luebbering',
+    description="DataStack, a stream based solution for machine learning dataset retrieval and storage",
+    long_description=long_description,
+    url="https://github.com/le1nux/datastack",
+    packages=find_packages(),
+    install_requires=[
+                      "pytest",
+                      "pytest-cov",
+                      "torch",
+                      "torchvision",
+                      "tqdm",
+                      "PyYAML"
+                      ],
+    python_requires=">=3.7"
+)

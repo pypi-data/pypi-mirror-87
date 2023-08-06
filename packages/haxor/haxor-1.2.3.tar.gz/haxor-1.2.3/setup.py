e01af82b42850005c5c29f7678d53640d54cdc53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:36 +0000

    Add package haxor/1.2.3/haxor-1.2.3.tar.gz

---
+++ packages/haxor/haxor-1.2.3.tar.gz/haxor-1.2.3/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md', encoding='utf-8').read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = '1.2.3'
+
+setup(
+    name='haxor',
+    version=version,
+    install_requires=requirements,
+    author='Avinash Sajjanshetty',
+    author_email='a@sajjanshetty.com',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='tests',
+    url='https://github.com/avinassh/haxor/',
+    license='MIT',
+    description='Unofficial Python wrapper for Hacker News API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)

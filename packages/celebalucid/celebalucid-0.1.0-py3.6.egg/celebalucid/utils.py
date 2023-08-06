2a113eb1f30449e6d9f8c4f2f0b5bfa855f9a3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package celebalucid/0.1.0/celebalucid-0.1.0-py3.6.egg

---
+++ packages/celebalucid/celebalucid-0.1.0-py3.6.egg/celebalucid/utils.py
@@ -0,0 +1,12 @@
+import pkgutil
+
+def load_layer_info():
+    text = pkgutil.get_data(__name__, 'res/layer_info.txt').decode('utf-8')
+    data = []
+    #text = pkgutil.get_data(__name__, layer_info_txt).decode('utf8')
+    lines = text.split('\n')
+    for line in lines:
+        layer_name, n_channels = line.split(' ')
+        n_channels = int(n_channels)
+        data.append([layer_name, n_channels])
+    return data

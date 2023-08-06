f1a2c08cde10edfcee8c93f8aae3a2fdd982327b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:09 +0000

    Add package holiday-jp/20.12.2/holiday_jp-20.12.2.tar.gz

---
+++ packages/holiday-jp/holiday_jp-20.12.2.tar.gz/holiday_jp-20.12.2/holiday_jp/holiday_jp.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import datetime
+from .holidays import HolidayDataset
+
+
+class HolidayJp(object):
+  """Holiday in Japan."""
+
+  date_obj = '' # the date object
+  week = '' # the week day in jp ex: 木
+  week_en = '' # the week day in en ex: Thursday
+  name = '' # the name of the holiday in jp ex: 元日
+  name_en = '' # the name of the holiday in en ex: New Year's Day
+  is_holiday = False # if the day is a holiday day
+  is_business_day = False # if the day is from [monday, friday]
+
+  # defaul business day can be overwritten depends on context
+  BUSINESS_DAY = ['月', '火', '水', '木', '金']
+
+  HOLIDAY_DATASET = HolidayDataset.HOLIDAYS
+  DATASET_FORMAT = '%Y-%m-%d'
+
+  DATE_FORMAT = '%Y-%m-%d'
+
+  def __init__(self, date):
+    """init the date and fill the property.
+
+      date: string with the following format yyyy-mm-dd (default, overwrite DATE_FORMAT for other format)
+        or date object
+    """
+    self.date_obj = self._format_date(date)
+
+    date_str = self.date_obj.strftime(self.DATE_FORMAT)
+    if self.HOLIDAY_DATASET.get(date_str):
+      # as the date is in the dict
+      self.is_holiday = True
+      self.week = self.HOLIDAY_DATASET[date_str]['week']
+      self.week_en = self.HOLIDAY_DATASET[date_str]['week_en']
+      self.name = self.HOLIDAY_DATASET[date_str]['name']
+      self.name_en = self.HOLIDAY_DATASET[date_str]['name_en']
+      if self.week in self.BUSINESS_DAY:
+        self.is_business_day = True
+    else:
+      # Monday is 0 and Sunday is 6.
+      if self.date_obj.weekday() < 5:
+        self.is_business_day = True
+
+  @classmethod
+  def _format_date(cls, date):
+    """Format the date to date object."""
+    import unicodedata
+    date_obj = None
+    # if date is string support the following format otherwise must be a date or datetime object
+    if isinstance(date, str):
+      # normalize
+      date = unicodedata.normalize('NFKC', date)
+      # plus hyphen
+      date = date.replace('−', '-')
+
+      try:
+        date_obj = datetime.datetime.strptime(date, cls.DATE_FORMAT).date()
+      except Exception as e:
+        raise e
+    else:
+      date_obj = date
+
+    return date_obj
+
+  @classmethod
+  def between(cls, start, last):
+    """Return a tuple of HolidayJp objects between 2 dates.
+
+      start, last: string with the following format yyyy-mm-dd
+        or date object
+    """
+    from dateutil import relativedelta
+
+    start = cls._format_date(start)
+    last = cls._format_date(last)
+
+    result = []
+
+    if last < start:
+      raise ValueError('last must be greater than start.')
+
+    # include the limit in the format of the dataset
+    start_date_str = start.strftime(cls.DATASET_FORMAT)
+    if cls.HOLIDAY_DATASET.get(start_date_str):
+      result.append(cls(start))
+    # go day by day
+    next_day = start + relativedelta.relativedelta(days=1)
+
+    while next_day <= last:
+      next_day_str = next_day.strftime(cls.DATASET_FORMAT)
+      if cls.HOLIDAY_DATASET.get(next_day_str):
+        result.append(cls(next_day))
+      next_day += relativedelta.relativedelta(days=1)
+
+    return result
+
+  @classmethod
+  def year_holidays(cls, year):
+    """Return a list of HolidayJp for a given year."""
+    return cls.between('%s-01-01' % year, '%s-12-31' % year)
+
+  @classmethod
+  def month_holidays(cls, year, month):
+    """Return a list of HolidayJp for a given month year."""
+    import calendar
+    # get the last day of the month
+    last_day = calendar.monthrange(year, month)
+    return cls.between('%s-%s-01' % (year, month), '%s-%s-%s' % (year, month, last_day[1]))
---
+++ packages/holiday-jp/holiday_jp-20.12.2.tar.gz/holiday_jp-20.12.2/holiday_jp/test.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+import datetime
+from unittest import TestCase
+from holiday_jp import HolidayJp
+
+
+class HolidayJpTest(TestCase):
+  """Test the holiday class."""
+
+  def test_init(self):
+    """Check that the calss init correctly."""
+    HolidayJp('1990-01-01') # support string
+    HolidayJp(datetime.date(year=1990, month=1, day=1)) # date
+    with self.assertRaises(ValueError):
+      HolidayJp('Banana') # but not banana
+
+  def test_is_holiday(self):
+    """Check that is_holiday is working as expected."""
+    # first day of the year is holiday and business day so don't come to office
+    one_holiday_date = HolidayJp('1990-01-01')
+    self.assertEqual(one_holiday_date.is_holiday, True)
+    # today is working day as I'm in office
+    not_holiday_date = HolidayJp('2018-10-18')
+    self.assertEqual(not_holiday_date.is_holiday, False)
+    self.assertEqual(not_holiday_date.is_business_day, True)
+    # but 20 is weekend not holiday but don't come to office
+    not_business_date = HolidayJp('2018-10-20')
+    self.assertEqual(not_business_date.is_holiday, False)
+    self.assertEqual(not_business_date.is_business_day, False)
+
+  def test_other_data_holiday(self):
+    """Check that the data are correctly initialized."""
+    one_holiday_date = HolidayJp('1990-01-01')
+    self.assertEqual(one_holiday_date.week, '月')
+    self.assertEqual(one_holiday_date.week_en, 'Monday')
+    self.assertEqual(one_holiday_date.name, '元日')
+    self.assertEqual(one_holiday_date.name_en, "New Year's Day")
+    self.assertEqual(one_holiday_date.is_business_day, True)
+
+  def test_other_data_not_holiday(self):
+    """Check that for not holiday the data are empty."""
+    not_holiday_date = HolidayJp('2018-10-18')
+    self.assertEqual(not_holiday_date.week, '')
+    self.assertEqual(not_holiday_date.week_en, '')
+    self.assertEqual(not_holiday_date.name, '')
+    self.assertEqual(not_holiday_date.name_en, '')
+
+  def test_custom_holiday(self):
+    """Check that I can overwrite and add custom holiday."""
+    # default is not holiday
+    not_holiday_date = HolidayJp('2018-10-01')
+    self.assertEqual(not_holiday_date.is_holiday, False)
+
+    from holiday_jp.holidays import HolidayDataset
+    my_holiday = HolidayDataset.HOLIDAYS
+
+    my_holiday.update({'2018-10-01': {
+        'week': '月',
+        'week_en': 'Monday',
+        'name': '誕生日',
+        'name_en': 'Birthday',
+    }})
+
+    class MyHolidayJp(HolidayJp):
+      # overwrite the holiday dataset
+      HOLIDAY_DATASET = my_holiday
+
+    new_holiday_date = MyHolidayJp('2018-10-01')
+    self.assertEqual(new_holiday_date.is_holiday, True)
+
+  def test_between(self):
+    """Check that I can retrieved holiday date between 2 dates."""
+    # add a full width date to test the normalization
+    holidays = HolidayJp.between('２００９−０１−０１', '2009-01-31')
+    new_year_day = holidays[0]
+    self.assertEqual(datetime.date(year=2009, month=1, day=1), new_year_day.date_obj)
+    self.assertEqual('元日', new_year_day.name)
+    self.assertEqual("New Year's Day", new_year_day.name_en)
+    self.assertEqual('木', new_year_day.week)
+
+    self.assertEqual(datetime.date(year=2009, month=1, day=12), holidays[1].date_obj)
+    self.assertEqual('成人の日', holidays[1].name)
+
+    holidays = HolidayJp.between(datetime.date(year=2008, month=12, day=23), datetime.date(year=2009, month=1, day=12))
+
+    self.assertEqual(datetime.date(year=2008, month=12, day=23), holidays[0].date_obj)
+    self.assertEqual(datetime.date(year=2009, month=1, day=1), holidays[1].date_obj)
+    self.assertEqual(datetime.date(year=2009, month=1, day=12), holidays[2].date_obj)
+
+  def test_other_date_format(self):
+    """Check that we can overwrite the default date format."""
+    class MyHolidayJp(HolidayJp):
+      # overwrite the date format
+      DATE_FORMAT = '%d-%m-%Y'
+    # check init
+    not_holiday_date = MyHolidayJp('28-09-2019')
+    # and this format
+    MyHolidayJp('28-9-2019')
+
+    self.assertEqual(not_holiday_date.is_holiday, False)
+
+    # and between
+    holidays = MyHolidayJp.between('23-12-2008', '12-1-2009')
+
+    self.assertEqual(datetime.date(year=2008, month=12, day=23), holidays[0].date_obj)
+
+  def test_year_holidays(self):
+    """Check that the year call is correct."""
+    # let's take 2019 as there's more holidays due to Era changes.
+    one_year_holiday = HolidayJp.year_holidays(2019)
+    self.assertEqual(len(one_year_holiday), 22)
+
+  def test_month_holidays(self):
+    """Check the Golden week holidays."""
+    gw_holiday = HolidayJp.month_holidays(2020, 5)
+    # only 4 days for golden week need to take day off!
+    self.assertEqual(len(gw_holiday), 4)

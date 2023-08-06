f40b1abaf17f8cf75a5939218029b74acd6889f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:39 +0000

    Add package quantconnect-stubs/9930/quantconnect-stubs-9930.tar.gz

---
+++ packages/quantconnect-stubs/quantconnect-stubs-9930.tar.gz/quantconnect-stubs-9930/System/Runtime/Remoting/__init__.pyi
@@ -0,0 +1,14 @@
+import System
+import System.Runtime.Remoting
+
+
+class ObjectHandle(System.MarshalByRefObject):
+    """This class has no documentation."""
+
+    def __init__(self, o: System.Object) -> None:
+        ...
+
+    def Unwrap(self) -> System.Object:
+        ...
+
+

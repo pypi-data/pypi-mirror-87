3cfb58e0632df510c1000cf6ee4a1cc465da60a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:40 +0000

    Add package sadptprj-riclyap-adi/1.0.2/sadptprj_riclyap_adi-1.0.2.tar.gz

---
+++ packages/sadptprj-riclyap-adi/sadptprj_riclyap_adi-1.0.2.tar.gz/sadptprj_riclyap_adi-1.0.2/sadptprj_riclyap_adi/__init__.py
@@ -0,0 +1,4 @@
+from . import proj_ric_utils
+from . import lin_alg_utils
+
+__all__ = ["proj_ric_utils", "lin_alg_utils"]
---
+++ packages/sadptprj-riclyap-adi/sadptprj_riclyap_adi-1.0.2.tar.gz/sadptprj_riclyap_adi-1.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(name='sadptprj_riclyap_adi',
+                 version='1.0.2',
+                 description='Solve saddle-point problems as they occur' +
+                 ' in simulations, model reduction, and optimal control' +
+                 ' of incompressible flows.',
+                 license="MIT",
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 author='Jan Heiland',
+                 author_email='jnhlnd@gmail.com',
+                 url="https://github.com/highlando/sadptprj_riclyap_adi",
+                 packages=['sadptprj_riclyap_adi'],  # same as name
+                 install_requires=['numpy', 'scipy', 'krypy'],  # extrnl pckgs
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     ]
+                 )

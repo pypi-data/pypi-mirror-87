269c4098874317d5eefcdf2659d0a224499a9bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package sparpy/0.4.0.dev1/sparpy-0.4.0.dev1-py3-none-any.whl

---
+++ packages/sparpy/sparpy-0.4.0.dev1-py3-none-any.whl/sparpy/cli.py
@@ -0,0 +1,271 @@
+from pathlib import Path
+from shutil import rmtree
+
+import click
+
+from .cli_options import (common_spark_options, general_options,
+                          plugins_options, spark_interactive_options,
+                          spark_submit_options)
+from .logger import build_logger
+from .plugins import DownloadPlugins, DynamicGroup
+from .spark import SparkInteractiveCommand, SparkSubmitCommand
+
+
+@click.group(cls=DynamicGroup)
+def sparpy_runner():
+    pass
+
+
+def run_sparpy_runner():
+    sparpy_runner(obj={})
+
+
+@click.command(name='sparpy', context_settings={'ignore_unknown_options': True})
+@general_options
+@plugins_options
+@common_spark_options
+@spark_submit_options
+@click.pass_context
+def sparpy(ctx,
+           job_args,
+           **kwargs):
+    """
+    Submit an spark job defined on an sparpy plugin
+    """
+
+    job_args = list(job_args)
+    job_args.insert(0, str(Path(__file__).parent / 'run.py'))
+
+    return ctx.invoke(sparpy_submit,
+                      job_args=job_args,
+                      **kwargs)
+
+
+def run_sparpy():
+    sparpy(obj={})
+
+
+@click.command(name='sparpy-download')
+@general_options
+@plugins_options
+@click.option('--convert-to-zip', '-z',
+              type=bool,
+              default=False,
+              is_flag=True,
+              help='Whether packages should be converted to zip files')
+@click.option('--output-dir', '-o',
+              type=click.Path(file_okay=False, writable=True, resolve_path=True),
+              required=False,
+              help='Directory where to download packages. If it is not provided a temporal directory will be created.')
+@click.pass_context
+def sparpy_download(ctx,
+                    config,
+                    debug,
+                    # Plugin options
+                    plugin,
+                    requirements_file,
+                    extra_index_url,
+                    find_links,
+                    no_index,
+                    no_self,
+                    force_download,
+                    pre,
+                    # Output
+                    convert_to_zip,
+                    output_dir,
+                    *,
+                    logger=None):
+    """
+    Download all dependencies and store them in a directory
+    """
+
+    logger = logger or build_logger(config, debug)
+
+    download_command = DownloadPlugins(config=config,
+                                       plugins=plugin,
+                                       requirements_files=requirements_file,
+                                       extra_index_urls=extra_index_url,
+                                       find_links=find_links,
+                                       no_index=no_index,
+                                       no_self=no_self,
+                                       force_download=force_download,
+                                       pre=pre,
+                                       logger=logger,
+                                       convert_to_zip=convert_to_zip,
+                                       download_dir=output_dir)
+    try:
+        reqs_path = download_command.download(debug=debug)
+    except RuntimeError as ex:
+        click.echo(ex)
+        raise ctx.exit(-1)
+
+    click.echo(f'Packages directory: {reqs_path}')
+    return reqs_path
+
+
+def run_sparpy_download():
+    sparpy_download(obj={})
+
+
+@click.command(name='sparpy-submit', context_settings={'ignore_unknown_options': True})
+@general_options
+@plugins_options
+@common_spark_options
+@spark_submit_options
+@click.pass_context
+def sparpy_submit(ctx,
+                  config,
+                  debug,
+                  # Plugin options
+                  plugin,
+                  requirements_file,
+                  extra_index_url,
+                  find_links,
+                  no_index,
+                  no_self,
+                  force_download,
+                  pre,
+                  # Spark submit options
+                  spark_submit_executable,
+                  # Common Spark options
+                  master,
+                  deploy_mode,
+                  queue,
+                  conf,
+                  packages,
+                  repositories,
+                  env,
+                  # Job arguments
+                  job_args,
+                  *,
+                  logger=None):
+    """
+    Submit an spark job defined on an script
+    """
+
+    logger = logger or build_logger(config, debug)
+
+    reqs_path = ctx.invoke(sparpy_download,
+                           config=config,
+                           debug=debug,
+                           plugin=plugin,
+                           requirements_file=requirements_file,
+                           extra_index_url=extra_index_url,
+                           find_links=find_links,
+                           no_index=no_index,
+                           no_self=no_self,
+                           force_download=force_download,
+                           pre=pre,
+                           convert_to_zip=True,
+                           logger=logger)
+
+    reqs_paths = []
+    if reqs_path is not None:
+        reqs_paths.append(reqs_path)
+
+    spark_command = SparkSubmitCommand(config=config,
+                                       spark_executable=spark_submit_executable,
+                                       master=master,
+                                       deploy_mode=deploy_mode,
+                                       queue=queue,
+                                       conf=conf,
+                                       packages=packages,
+                                       repositories=repositories,
+                                       reqs_paths=reqs_paths,
+                                       env=dict(env or {}),
+                                       logger=logger)
+
+    try:
+        spark_command.run(job_args=job_args)
+    except RuntimeError as ex:
+        click.echo(ex)
+        raise ctx.exit(-1)
+    finally:
+        if reqs_path:
+            rmtree(reqs_path)
+
+
+def run_sparpy_submit():
+    sparpy(obj={})
+
+
+@click.command(name='isparpy')
+@general_options
+@plugins_options
+@common_spark_options
+@spark_interactive_options
+@click.pass_context
+def isparpy(ctx,
+            config,
+            debug,
+            # Plugin options
+            plugin,
+            requirements_file,
+            extra_index_url,
+            find_links,
+            no_index,
+            no_self,
+            force_download,
+            pre,
+            # Spark interactive options
+            pyspark_executable,
+            python_interactive_driver,
+            # Common Spark options
+            master,
+            deploy_mode,
+            queue,
+            conf,
+            packages,
+            repositories,
+            env,
+            *,
+            logger=None):
+    """
+    Start a pyspark interactive session with dependencies loaded
+    """
+
+    logger = logger or build_logger(config, debug)
+
+    reqs_path = ctx.invoke(sparpy_download,
+                           debug=debug,
+                           config=config,
+                           plugin=plugin,
+                           requirements_file=requirements_file,
+                           extra_index_url=extra_index_url,
+                           find_links=find_links,
+                           no_index=no_index,
+                           no_self=no_self,
+                           force_download=force_download,
+                           pre=pre,
+                           convert_to_zip=True,
+                           logger=logger)
+
+    reqs_paths = []
+    if reqs_path is not None:
+        reqs_paths.append(reqs_path)
+
+    spark_command = SparkInteractiveCommand(cmd_config=config,
+                                            pyspark_executable=pyspark_executable,
+                                            python_interactive_driver=python_interactive_driver,
+                                            master=master,
+                                            deploy_mode=deploy_mode,
+                                            queue=queue,
+                                            conf=conf,
+                                            packages=packages,
+                                            repositories=repositories,
+                                            reqs_paths=reqs_paths,
+                                            env=dict(env or {}),
+                                            logger=logger)
+
+    try:
+        spark_command.run()
+    except RuntimeError as ex:
+        click.echo(ex)
+        raise ctx.exit(-1)
+    finally:
+        if reqs_path:
+            rmtree(reqs_path)
+
+
+def run_isparpy():
+    isparpy(obj={})
---
+++ packages/sparpy/sparpy-0.4.0.dev1-py3-none-any.whl/sparpy/spark.py
@@ -0,0 +1,188 @@
+import os
+import sys
+from itertools import chain
+from logging import Logger, getLogger
+from pathlib import Path
+from typing import Dict, Iterable, Union
+
+from .config import ConfigParser
+from .processor import ProcessManager
+
+
+class BaseSparkCommand:
+
+    def __init__(self,
+                 config: ConfigParser = None,
+                 spark_executable: str = None,
+                 master: str = None,
+                 deploy_mode: str = None,
+                 queue: str = None,
+                 conf: Iterable[str] = None,
+                 packages: Union[Iterable[str], str] = None,
+                 repositories: Union[Iterable[str], str] = None,
+                 reqs_paths: Union[Iterable[str], str] = None,
+                 env: Dict[str, str] = None,
+                 logger: Logger = None):
+        try:
+            cmd_config = config['spark']
+        except KeyError:
+            config.add_section('spark')
+            cmd_config = config['spark']
+        except TypeError:
+            config = ConfigParser(default_sections=('spark', 'spark_env'))
+            cmd_config = config['spark']
+
+        try:
+            env_config = config['spark-env']
+        except KeyError:
+            config.add_section('spark-env')
+            env_config = config['spark-env']
+
+        self.spark_executable = spark_executable or cmd_config.get('spark-executable', fallback='spark-submit')
+        self.master = master or cmd_config.get('master', fallback='local')
+        self.deploy_mode = deploy_mode or cmd_config.get('deploy-mode', fallback='client')
+        self.queue = queue or cmd_config.get('queue')
+        self.conf = cmd_config.getlist('conf', fallback=[])
+        self.packages = cmd_config.getlist('packages', fallback=[])
+        self.repositories = cmd_config.getlist('repositories', fallback=[])
+        self.reqs_paths = cmd_config.getlist('reqs_paths', fallback=[])
+
+        try:
+            self.env = {k: v for k, v in env_config.items()}
+        except KeyError:
+            self.env = {}
+
+        self.env.update(env or {})
+
+        self.logger = logger or getLogger(__name__)
+
+        if conf:
+            self.conf.extend(conf)
+
+        if packages:
+            if isinstance(packages, str):
+                packages = packages.split(',')
+
+            packages = [p.strip() for p in packages if p.strip()]
+            if packages:
+                self.packages.extend(packages)
+
+        if repositories:
+            if isinstance(repositories, str):
+                repositories = repositories.split(',')
+
+            repositories = [p.strip() for p in repositories if p.strip()]
+            if repositories:
+                self.repositories.extend(repositories)
+
+        if reqs_paths:
+            if isinstance(reqs_paths, str):
+                reqs_paths = reqs_paths.split(',')
+            reqs_paths = [Path(p.strip()) for p in reqs_paths if p.strip()]
+
+            if reqs_paths:
+                self.reqs_paths.extend(reqs_paths)
+
+    def build_command(self, *, executable):
+        spark_cmd = [executable, '--master', self.master, '--deploy-mode', self.deploy_mode]
+
+        if self.queue:
+            spark_cmd.extend(['--queue', self.queue])
+
+        if self.conf:
+            spark_cmd.extend(chain(*[['--conf', c] for c in self.conf]))
+
+        if self.packages:
+            spark_cmd.extend(['--packages', ','.join(self.packages)])
+
+        if self.repositories:
+            spark_cmd.extend(['--repositories', ','.join(self.repositories)])
+
+        if self.reqs_paths:
+            ps = [r.strip() for r in chain(*[[str(p.resolve())
+                                              for p in chain(Path(rp).rglob('*.egg'),
+                                                             Path(rp).rglob('*.whl'),
+                                                             Path(rp).rglob('*.zip'))
+                                              if p.is_file()] for rp in self.reqs_paths]) if r.strip()]
+
+            if len(ps):
+                spark_cmd.extend(['--py-files', ','.join(ps)])
+
+        return spark_cmd
+
+
+class SparkSubmitCommand(BaseSparkCommand):
+
+    def build_command(self, *, job_args: Iterable[str], **kwargs):
+        kwargs.setdefault('executable', self.spark_executable)
+        spark_cmd = super(SparkSubmitCommand, self).build_command(**kwargs)
+
+        spark_cmd.extend(job_args)
+
+        return spark_cmd
+
+    def run(self, job_args: Iterable[str]):
+        self.logger.info('Executing Spark job...')
+        spark_command = self.build_command(job_args=job_args)
+
+        env = os.environ.copy()
+        env.update(self.env or {})
+        env['PYSPARK_PYTHON'] = sys.executable
+        env['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+        self.logger.info(' '.join(spark_command))
+
+        process = ProcessManager(spark_command, pass_through=True, env=env)
+        process.start_process()
+        process.wait()
+
+        if process.returncode != 0:
+            raise RuntimeError(f'Spark job failed with error: {process.returncode}')
+
+
+class SparkInteractiveCommand(BaseSparkCommand):
+
+    def __init__(self,
+                 cmd_config,
+                 *args,
+                 pyspark_executable: str = None,
+                 python_interactive_driver: str = None,
+                 **kwargs):
+        super(SparkInteractiveCommand, self).__init__(cmd_config, *args, **kwargs)
+
+        try:
+            cmd_config = cmd_config['interactive']
+        except (KeyError, TypeError):
+            cmd_config = ConfigParser(default_sections=('interactive',))
+            cmd_config = cmd_config['interactive']
+
+        self.pyspark_executable = pyspark_executable or cmd_config.get('pyspark-executable', fallback='pyspark')
+        self.python_interactive_driver = python_interactive_driver or cmd_config.get('python-interactive-driver',
+                                                                                     fallback=sys.executable)
+
+        # Force client deploy mode
+        self.deploy_mode = 'client'
+
+    def build_command(self, **kwargs):
+        kwargs.setdefault('executable', self.pyspark_executable)
+        return super(SparkInteractiveCommand, self).build_command(**kwargs)
+
+    def run(self):
+
+        self.logger.info('Executing Spark interactive...')
+
+        spark_command = self.build_command()
+
+        env = os.environ.copy()
+        env.update(self.env)
+        env['PYSPARK_PYTHON'] = sys.executable
+        env['PYSPARK_DRIVER_PYTHON'] = self.python_interactive_driver
+
+        self.logger.info(' '.join(spark_command))
+
+        process = ProcessManager(spark_command, pass_through=True, env=env)
+        process.start_process()
+        process.wait()
+
+        if process.returncode != 0:
+            raise RuntimeError(f'Interactive Spark failed with error: {process.returncode}')

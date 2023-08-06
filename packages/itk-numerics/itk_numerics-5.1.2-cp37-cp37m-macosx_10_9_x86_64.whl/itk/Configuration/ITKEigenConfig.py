9a10be32bbcf2495ed02d1c108af5dab413387c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:15 +0000

    Add package itk-numerics/5.1.2/itk_numerics-5.1.2-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/itk-numerics/itk_numerics-5.1.2-cp37-cp37m-macosx_10_9_x86_64.whl/itk/Configuration/ITKEigenConfig.py
@@ -0,0 +1,10 @@
+depends = ('ITKPyBase', 'ITKImageFilterBase', )
+templates = (
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF22', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,2 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF32', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,3 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterIF2IF2IVF42', True, 'itk::Image< float,2 >, itk::Image< float,2 >, itk::Image< itk::Vector< float,4 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterID2ID2IVF22', True, 'itk::Image< double,2 >, itk::Image< double,2 >, itk::Image< itk::Vector< float,2 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterID2ID2IVF32', True, 'itk::Image< double,2 >, itk::Image< double,2 >, itk::Image< itk::Vector< float,3 >,2 >'),
+  ('EigenAnalysis2DImageFilter', 'itk::EigenAnalysis2DImageFilter', 'itkEigenAnalysis2DImageFilterID2ID2IVF42', True, 'itk::Image< double,2 >, itk::Image< double,2 >, itk::Image< itk::Vector< float,4 >,2 >'),
+)
+snake_case_functions = ('eigen_analysis2_d_image_filter', )

47e884d072e8d9d460171e6f895846f4ff41ec87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:05 +0000

    Add package distributions-hippo/0.1/distributions_hippo-0.1.tar.gz

---
+++ packages/distributions-hippo/distributions_hippo-0.1.tar.gz/distributions_hippo-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_hippo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_hippo'],
+      zip_safe=False)

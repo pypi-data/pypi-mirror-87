5d1da455c7568ca969129b7628e26b4bd57bdf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:34 +0000

    Add package gepapy/1.1.6/gepapy-1.1.6.tar.gz

---
+++ packages/gepapy/gepapy-1.1.6.tar.gz/gepapy-1.1.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup  # type: ignore
+
+with open("README.rst", "r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name="gepapy",
+    version="1.1.6",
+    description="Developing a library with Cuda and Python for solving scheduling problems on GPU",
+    long_description=readme,
+    author="Jean Carlo Jimenez Giraldo",
+    author_email="mandalarotation@gmail.com",
+    license="MIT",
+    url="https://github.com/mandalarotation/GenSchedulingCuda-GSC",
+    package_data={
+        "gepapy": [
+            "py.typed",
+            "single_machine.pyi",
+            "flow_shop.pyi",
+            "job_shop.pyi",
+            "operations.pyi",
+        ],
+        "gepapy.kernels": [
+            "py.typed",
+            "crossA0001.pyi",
+            "fitnessA0001.pyi",
+            "fitnessSM0001.pyi",
+            "mutationA0001.pyi",
+            "permutationA0001.pyi",
+        ],
+    },
+    packages=[
+        "gepapy",
+        "gepapy.kernels",
+        "gepapy.exceptions",
+    ],
+    scripts=[],
+    install_requires=["numpy", "cupy", "numba", "typing"],
+)

238236b84f9808f158cb2b8066f0c3a746691737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package pybamm/0.3.0/pybamm-0.3.0-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/pybamm/pybamm-0.3.0-cp37-cp37m-macosx_10_15_x86_64.whl/pybamm/models/submodels/interface/kinetics/no_reaction.py
@@ -0,0 +1,29 @@
+#
+# Bulter volmer class
+#
+
+import pybamm
+from .base_kinetics import BaseKinetics
+
+
+class NoReaction(BaseKinetics):
+    """
+    Base submodel for when no reaction occurs
+
+    Parameters
+    ----------
+    param :
+        model parameters
+    domain : str
+        The domain to implement the model, either: 'Negative' or 'Positive'.
+    reaction : str
+        The name of the reaction being implemented
+
+    **Extends:** :class:`pybamm.interface.kinetics.BaseKinetics`
+    """
+
+    def __init__(self, param, domain, reaction):
+        super().__init__(param, domain, reaction)
+
+    def _get_kinetics(self, j0, ne, eta_r, T):
+        return pybamm.Scalar(0)

c90f7e436cf16440eb325881f69912ff353923a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:28 +0000

    Add package basxconnect/0.1.1/basxconnect-0.1.1.tar.gz

---
+++ packages/basxconnect/basxconnect-0.1.1.tar.gz/basxconnect-0.1.1/basxconnect/core/views.py
@@ -0,0 +1,210 @@
+import htmlgenerator as hg
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+from django.utils.html import mark_safe
+from django.utils.translation import gettext_lazy as _
+from haystack.query import SearchQuerySet
+
+from bread import layout
+from bread.forms.forms import generate_form
+from bread.utils.urlgenerator import registerurl
+
+from .models import (
+    Category,
+    JuristicPerson,
+    NaturalPerson,
+    PersonAssociation,
+    RelationshipType,
+    Term,
+)
+
+
+def single_item_fieldset(related_field, fieldname, queryset=None):
+    return layout.form.FormSetField(
+        related_field,
+        layout.form.FormField(fieldname),
+        formsetinitial={"queryset": queryset},
+        can_delete=False,
+        max_num=1,
+        extra=1,
+    )
+
+
+@registerurl
+def generalsettings(request):
+    instance = JuristicPerson.objects.get(id=1)  # must exists due to migration
+
+    formlayout = layout.BaseElement(
+        layout.grid.Grid(
+            layout.grid.Row(layout.grid.Col(layout.form.FormField("type"))),
+            layout.grid.Row(layout.grid.Col(layout.form.FormField("name"))),
+            layout.grid.Row(layout.grid.Col(layout.form.FormField("name_addition"))),
+        ),
+        layout.form.FormSetField(
+            "core_postal_list",
+            layout.grid.Grid(
+                layout.grid.Row(layout.grid.Col(layout.form.FormField("address"))),
+                layout.grid.Row(
+                    layout.grid.Col(layout.form.FormField("supplemental_address"))
+                ),
+                layout.grid.Row(
+                    layout.grid.Col(
+                        layout.form.FormField("postcode"), breakpoint="lg", width=2
+                    ),
+                    layout.grid.Col(
+                        layout.form.FormField("city"), breakpoint="lg", width=3
+                    ),
+                    layout.grid.Col(
+                        layout.form.FormField("country"), breakpoint="lg", width=3
+                    ),
+                ),
+            ),
+            can_delete=False,
+            max_num=1,
+            extra=1,
+        ),
+        layout.grid.Grid(
+            layout.grid.Row(
+                layout.grid.Col(single_item_fieldset("core_phone_list", "number")),
+                layout.grid.Col(single_item_fieldset("core_fax_list", "number")),
+            ),
+            layout.grid.Row(
+                layout.grid.Col(
+                    single_item_fieldset(
+                        "core_email_list",
+                        "email",
+                    )
+                ),
+                layout.grid.Col(single_item_fieldset("core_web_list", "url")),
+            ),
+        ),
+        layout.form.SubmitButton(_("Save")),
+    )
+
+    form = generate_form(request, JuristicPerson, formlayout, instance)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+
+    pagelayout = hg.BaseElement(
+        hg.H1(_("General")),
+        hg.H2(_("General information")),
+        layout.form.Form(form, formlayout),
+    )
+
+    return render(request, "bread/layout.html", {"layout": pagelayout})
+
+
+@registerurl
+def appearancesettings(request):
+    pagelayout = hg.BaseElement(hg.H2(_("Appearance")))
+    return render(request, "bread/layout.html", {"layout": pagelayout})
+
+
+@registerurl
+def personssettings(request):
+    from bread.admin import site
+
+    dist = hg.DIV(style="margin-bottom: 2rem")
+
+    pagelayout = hg.BaseElement(
+        hg.H2(_("Persons")),
+        # address type
+        layout.datatable.DataTable.from_queryset(
+            Term.objects.filter(category__slug="addresstype"),
+            fields=["term"],
+            title=_("Address types"),
+            addurl=site.get_default_admin(Term).reverse(
+                "add",
+                query_arguments={
+                    "category": Category.objects.get(slug="addresstype").id,
+                    "next": reverse("core.views.personssettings"),
+                },
+            ),
+        ),
+        dist,
+        # address origin
+        layout.datatable.DataTable.from_queryset(
+            Term.objects.filter(category__slug="addressorigin"),
+            fields=["term"],
+            title=_("Address origins"),
+            addurl=site.get_default_admin(Term).reverse(
+                "add",
+                query_arguments={
+                    "category": Category.objects.get(slug="addressorigin").id,
+                    "next": reverse("core.views.personssettings"),
+                },
+            ),
+        ),
+        dist,
+        # salutation
+        layout.datatable.DataTable.from_queryset(
+            Term.objects.filter(category__slug="salutation"),
+            fields=["term"],
+            title=_("Salutation"),
+            addurl=site.get_default_admin(Term).reverse(
+                "add",
+                query_arguments={
+                    "category": Category.objects.get(slug="salutation").id,
+                    "next": reverse("core.views.personssettings"),
+                },
+            ),
+        ),
+        dist,
+    )
+    return render(request, "bread/layout.html", {"layout": pagelayout})
+
+
+@registerurl
+def relationshipssettings(request):
+    from bread.admin import site
+
+    pagelayout = hg.BaseElement(
+        hg.H2(_("Relationships")),
+        # relationship type
+        layout.datatable.DataTable.from_queryset(
+            RelationshipType.objects.all(),
+            fields=["name"],
+            addurl=site.get_default_admin(RelationshipType).reverse("add"),
+        ),
+    )
+    return render(request, "bread/layout.html", {"layout": pagelayout})
+
+
+@registerurl
+def apikeyssettings(request):
+    pagelayout = hg.BaseElement(hg.H2(_("APK Keys")))
+    return render(request, "bread/layout.html", {"layout": pagelayout})
+
+
+@registerurl
+def searchperson(request):
+    query = request.GET.get("query")
+    if not query:
+        return HttpResponse("")
+
+    qs = (
+        SearchQuerySet()
+        .models(NaturalPerson, JuristicPerson, PersonAssociation)
+        .autocomplete(name_auto=query)
+    )
+    items = [
+        hg.LI(
+            result.object,
+            hg.DIV(
+                mark_safe(result.object.core_postal_list.first() or _("No address")),
+                style="font-size: small; margin-bottom: 1rem;",
+            ),
+        )
+        for result in qs
+    ]
+
+    return HttpResponse(
+        hg.UL(
+            *(items or [hg.LI(_("No results"))]),
+            _class="bx--tile",
+            style="margin-bottom: 2rem;"
+        ).render({})
+    )
---
+++ packages/basxconnect/basxconnect-0.1.1.tar.gz/basxconnect-0.1.1/basxconnect/core/wizards/add_person.py
@@ -0,0 +1,327 @@
+from django import forms
+from django.apps import apps
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from formtools.wizard.views import NamedUrlSessionWizardView
+
+from bread import layout
+from bread.forms.forms import breadmodelform_factory
+from bread.utils import pretty_modelname
+
+from ..models import (
+    JuristicPerson,
+    NaturalPerson,
+    Person,
+    PersonAssociation,
+    Postal,
+    Term,
+)
+
+ADD_FORM_LAYOUTS = {
+    NaturalPerson: layout.BaseElement(
+        layout.grid.Row(
+            layout.grid.Col(layout.form.FormField("first_name")),
+            layout.grid.Col(layout.form.FormField("last_name")),
+        ),
+        layout.grid.Row(
+            layout.grid.Col(layout.form.FormField("salutation")),
+            layout.grid.Col(layout.form.FormField("gender")),
+        ),
+    ),
+    JuristicPerson: layout.DIV(
+        layout.form.FormField("name"), layout.form.FormField("name_addition")
+    ),
+    PersonAssociation: layout.DIV(layout.form.FormField("name")),
+}
+ADD_ADDRESS_LAYOUT = layout.grid.Grid(
+    layout.grid.Row(
+        layout.grid.Col(_("Address"), style="font-weight: 700; margin-bottom: 2rem")
+    ),
+    layout.grid.Row(layout.grid.Col(layout.form.FormField("address"))),
+    layout.grid.Row(
+        layout.grid.Col(
+            layout.form.FormField("postcode"),
+        ),
+        layout.grid.Col(layout.form.FormField("city")),
+    ),
+    layout.grid.Row(layout.grid.Col(layout.form.FormField("country"))),
+)
+
+
+def generate_wizard_form(formlayout):
+    # needs to be rendered in view of type NamedUrlSessionWizardView in order to work correctly
+    def go_back_url(element, context):
+        url = reverse(
+            context["request"].resolver_match.view_name,
+            kwargs={"step": context["wizard"]["steps"].prev},
+        )
+        return f"document.location='{url}'"
+
+    return layout.form.Form(
+        layout.C("wizard.form"),
+        layout.form.Form(
+            layout.C("wizard.management_form"),
+            layout.form.FormField("current_step"),
+            standalone=False,
+        ),
+        formlayout,
+        layout.DIV(
+            layout.DIV(
+                layout.If(
+                    layout.C("wizard.steps.prev"),
+                    layout.button.Button(
+                        _("Back"),
+                        buttontype="secondary",
+                        onclick=layout.F(go_back_url),
+                    ),
+                ),
+                layout.If(
+                    layout.F(
+                        lambda e, c: c["wizard"]["steps"].last
+                        == c["wizard"]["steps"].current
+                    ),
+                    layout.button.Button(
+                        _("Complete"), type="submit", style="margin-left: 1rem"
+                    ),
+                    layout.button.Button(
+                        _("Continue"), type="submit", style="margin-left: 1rem"
+                    ),
+                ),
+            ),
+            style="align-items: flex-end",
+            _class="bx--form-item",
+        ),
+    )
+
+
+class SearchForm(forms.Form):
+    name_of_existing_person = forms.CharField(
+        label=_("Check for existing people before continuing"),
+        max_length=255,
+        required=False,
+    )
+
+    searchbutton = layout.search.Search(
+        widgetattributes={
+            "placeholder": _("Start typing to search for a person..."),
+            "hx_get": reverse_lazy("basxconnect.core.views.searchperson"),
+            "hx_trigger": "changed, keyup changed delay:100ms",
+            "hx_target": "#search-results",
+            "name": "query",
+        },
+    )
+    # clear search field when search box is emptied
+    searchbutton[3].attributes[
+        "onclick"
+    ] = "this.parentElement.nextElementSibling.innerHTML = ''"
+
+    title = _("Search person")
+    _layout = layout.DIV(
+        layout.DIV(
+            _(
+                "Before a new person can be added it must be confirmed that the person does not exists yet."
+            ),
+            style="margin-bottom: 2rem",
+        ),
+        searchbutton,
+        layout.DIV(id="search-results", style="margin-bottom: 2rem;"),
+    )
+
+
+class ChooseType(forms.Form):
+    PERSON_TYPES = {
+        "core.NaturalPerson": pretty_modelname(NaturalPerson),
+        "core.JuristicPerson": pretty_modelname(JuristicPerson),
+        "core.PersonAssociation": pretty_modelname(PersonAssociation),
+    }
+    persontype = forms.TypedChoiceField(
+        label=_("Type of person"),
+        choices=tuple(PERSON_TYPES.items()),
+        coerce=lambda a: apps.get_model(a),
+        empty_value=None,
+    )
+
+    title = _("Choose person main type")
+    _layout = layout.BaseElement(
+        layout.DIV(
+            _("Please choose what type of person you want to add:"),
+            style="margin-bottom: 2rem",
+        ),
+        layout.form.FormField("persontype"),
+    )
+
+
+class ChooseSubType(forms.Form):
+    ALLOWED_SUBTYPE_CATEGORY = {
+        Person: None,
+        NaturalPerson: None,
+        JuristicPerson: "legaltype",
+        PersonAssociation: "associationtype",
+    }
+    subtype = forms.ModelChoiceField(
+        label=_("Subtype of person"), queryset=Term.objects.all()
+    )
+
+    def __init__(self, persontype, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        subtype_category = ChooseSubType.ALLOWED_SUBTYPE_CATEGORY.get(persontype)
+        if subtype_category is None:
+            self.fields = {}
+        else:
+            self.fields["subtype"].queryset = Term.objects.filter(
+                category__slug=subtype_category
+            )
+
+    title = _("Choose person type")
+    _layout = layout.form.FormField("subtype")
+
+
+class AddPersonInformation(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    title = _("Add person")
+    _layout = layout.DIV("Please select a person type first")
+
+
+class ConfirmNewPerson(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    title = _("Finish")
+    _layout = layout.DIV("Please select a person type first")
+
+
+def generate_add_form_for(model, request, data, files, initial=None):
+    form = breadmodelform_factory(
+        request=request, model=model, layout=ADD_FORM_LAYOUTS[model]
+    )(data, files, initial=initial)
+    for fieldname, field in breadmodelform_factory(
+        request, Postal, ADD_ADDRESS_LAYOUT
+    )().fields.items():
+        form.fields[fieldname] = field
+
+    formlayout = layout.BaseElement(
+        layout.grid.Grid(ADD_FORM_LAYOUTS[model].copy(), style="margin-bottom: 2rem"),
+        ADD_ADDRESS_LAYOUT.copy(),
+    )
+    form._layout = formlayout
+    return form
+
+
+# The WizardView contains mostly control-flow logic and some configuration
+class AddPersonWizard(NamedUrlSessionWizardView):
+    kwargs = {"url_name": "core:person:add_wizard", "urlparams": {"step": "str"}}
+    urlparams = None
+
+    form_list = [
+        ("Search", SearchForm),
+        ("Type", ChooseType),
+        ("Subtype", ChooseSubType),
+        ("Information", AddPersonInformation),
+        ("Confirmation", ConfirmNewPerson),
+    ]
+    # translation detection
+    _("Search")
+    _("Type")
+    _("Subtype")
+    _("Information")
+    _("Confirmation")
+    template_name = "core/wizards/add_person.html"
+    condition_dict = {
+        "Subtype": lambda wizard: ChooseSubType.ALLOWED_SUBTYPE_CATEGORY.get(
+            (wizard.get_cleaned_data_for_step("Type") or {}).get("persontype")
+        )
+    }
+
+    def get_person_type(self):
+        return (self.get_cleaned_data_for_step("Type") or {}).get("persontype")
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        steps = []
+
+        for i, step in enumerate(self.get_form_list().keys()):
+            status = "incomplete"
+            if i < self.steps.index:
+                status = "complete"
+            if step == self.steps.current:
+                status = "current"
+            steps.append((_(step), status))
+
+        context["layout"] = layout.BaseElement(
+            layout.H1(_("Add new person"), style="margin-bottom: 2rem"),
+            layout.H2(self.get_form().title, style="margin-bottom: 2rem"),
+            layout.progress_indicator.ProgressIndicator(
+                steps,
+                style="margin-bottom: 2rem",
+            ),
+            generate_wizard_form(self.get_form()._layout),
+        )
+        return context
+
+    def get_form_kwargs(self, step):
+        ret = super().get_form_kwargs()
+        if step == "Subtype":
+            ret.update({"persontype": self.get_person_type()})
+        return ret
+
+    def get_form(self, step=None, data=None, files=None):
+        form = super().get_form(step, data, files)
+        if step is None:
+            step = self.steps.current
+
+        # step 3 and 4 depend on the select type, so we generate the forms dynamically
+        if step in ("Information", "Confirmation"):
+            persontype = self.get_person_type()
+            if persontype:
+                if step == "Information":
+                    form = generate_add_form_for(
+                        persontype,
+                        self.request,
+                        data,
+                        files,
+                    )
+                else:
+                    form = generate_add_form_for(
+                        persontype,
+                        self.request,
+                        data,
+                        files,
+                        self.get_cleaned_data_for_step("Information"),
+                    )
+                    form._layout.insert(
+                        0,
+                        layout.notification.InlineNotification(
+                            "",
+                            _("Review and confirm the entered information"),
+                            lowcontrast=True,
+                            hideclosebutton=True,
+                        ),
+                    )
+                    for fieldname in form.fields:
+                        form.fields[fieldname].disabled = True
+                        form.fields[fieldname].widget.attrs["style"] = "color: #000"
+                form.title = _("Add %s") % pretty_modelname(persontype)
+        return form
+
+    def done(self, form_list, **kwargs):
+        # in case the new person had a subtype set, we need to set the attribute here
+        subtype = (self.get_cleaned_data_for_step("Subtype") or {}).get("subtype")
+        if subtype:
+            list(form_list)[-1].instance.type = subtype
+        newperson = list(form_list)[-1].save()
+        newperson.core_postal_list.create(
+            **{
+                k: v
+                for k, v in list(form_list)[-1].cleaned_data.items()
+                if k in ("address", "city", "postcode", "country")
+            }
+        )
+        return redirect(
+            reverse(
+                f"core:{newperson._meta.model_name}:edit", kwargs={"pk": newperson.pk}
+            )
+            + "?next=/"
+        )

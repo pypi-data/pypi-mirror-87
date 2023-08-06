d8a4bd4b1a435a98e4d8923c2d0f40242f921ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:20 +0000

    Add package hacs-frontend/20201205161824/hacs_frontend-20201205161824-py3-none-any.whl

---
+++ packages/hacs-frontend/hacs_frontend-20201205161824-py3-none-any.whl/hacs_frontend/entrypoint.js
@@ -0,0 +1,9 @@
+
+try {
+  new Function("import('/hacsfiles/frontend/e.4add7a3d.js')")();
+} catch (err) {
+  var el = document.createElement('script');
+  el.src = '/hacsfiles/frontend/e.4add7a3d.js';
+  document.body.appendChild(el);
+}
+  

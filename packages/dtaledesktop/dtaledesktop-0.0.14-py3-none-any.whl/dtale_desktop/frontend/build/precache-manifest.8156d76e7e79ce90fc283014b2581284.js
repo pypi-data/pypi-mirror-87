d90b69e113185232ff2699d31c87fde10a958186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:44 +0000

    Add package dtaledesktop/0.0.14/dtaledesktop-0.0.14-py3-none-any.whl

---
+++ packages/dtaledesktop/dtaledesktop-0.0.14-py3-none-any.whl/dtale_desktop/frontend/build/precache-manifest.8156d76e7e79ce90fc283014b2581284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c924ee75f6031c0e176478ce22b69a5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d2b769f18d219fbdd89",
+    "url": "/static/css/2.13b92dd1.chunk.css"
+  },
+  {
+    "revision": "dda23abe77e5fe4498da",
+    "url": "/static/css/main.7968f145.chunk.css"
+  },
+  {
+    "revision": "0d2b769f18d219fbdd89",
+    "url": "/static/js/2.775b8d26.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.775b8d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda23abe77e5fe4498da",
+    "url": "/static/js/main.48c11554.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
---
+++ packages/dtaledesktop/dtaledesktop-0.0.14-py3-none-any.whl/dtale_desktop/frontend/build/precache-manifest.9edef258308cf61fe7653bdffc600b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827e09cc94241d119d7a828d168aeac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1a76cc21cbf1821f771",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "52ffccf8838e2fc242d1",
+    "url": "/static/css/main.ce00ed80.chunk.css"
+  },
+  {
+    "revision": "e1a76cc21cbf1821f771",
+    "url": "/static/js/2.0a1310cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0a1310cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ffccf8838e2fc242d1",
+    "url": "/static/js/main.e3513898.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);

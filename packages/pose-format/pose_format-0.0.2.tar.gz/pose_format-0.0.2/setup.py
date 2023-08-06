7290fda410738502e7f2f1ea5d5a823acc09d82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:13 +0000

    Add package pose-format/0.0.2/pose_format-0.0.2.tar.gz

---
+++ packages/pose-format/pose_format-0.0.2.tar.gz/pose_format-0.0.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='pose_format',
+    packages=find_packages(),
+    version='0.0.2',
+    description='Library for viewing, augmenting, and handling .pose files',
+    author='Amit Moryossef',
+    author_email='amitmoryossef@gmail.com',
+    url='https://github.com/AmitMY/pose-format',
+    keywords=['Pose Files', 'Pose Interpolation', 'Pose Augmentation'],
+    install_requires=['numpy', 'scipy', 'pytest', 'tqdm', 'opencv-python'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ]
+)

543456a65844860976a5cd8de019f1c928ac8653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:07 +0000

    Add package mvlearn/0.4.0/mvlearn-0.4.0-py3-none-any.whl

---
+++ packages/mvlearn/mvlearn-0.4.0-py3-none-any.whl/mvlearn/datasets/__init__.py
@@ -0,0 +1,9 @@
+from .base import load_UCImultifeature
+from .gaussian_mixture import make_gaussian_mixture
+from .factor_model import sample_joint_factor_model
+
+__all__ = [
+    "load_UCImultifeature",
+    "make_gaussian_mixture",
+    "sample_joint_factor_model"
+    ]

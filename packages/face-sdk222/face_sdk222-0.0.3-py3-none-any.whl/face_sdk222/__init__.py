452547045a829a0e0517ef4c7a44605a72616d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:53 +0000

    Add package face-sdk222/0.0.3/face_sdk222-0.0.3-py3-none-any.whl

---
+++ packages/face-sdk222/face_sdk222-0.0.3-py3-none-any.whl/face_sdk222/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from face_sdk222.config import *
+from face_sdk222.core import *
+from face_sdk222.logs import *
+from face_sdk222.models import *
+from face_sdk222.sample_api import *
+from face_sdk222.utils import *
+from face_sdk222 import *
+
+name = "face_sdk222"

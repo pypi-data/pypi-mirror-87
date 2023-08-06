34e706d876337caa5e5717f17f152052b0c3804e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:21 +0000

    Add package otter-grader/1.1.6/otter-grader-1.1.6.tar.gz

---
+++ packages/otter-grader/otter-grader-1.1.6.tar.gz/otter-grader-1.1.6/otter/__init__.py
@@ -0,0 +1,12 @@
+"""
+otter Python API
+"""
+
+import platform
+
+from .check import logs
+from .check.notebook import Notebook
+from .version import __version__
+
+# whether Otter is running on Window
+_WINDOWS = platform.system() == "Windows"

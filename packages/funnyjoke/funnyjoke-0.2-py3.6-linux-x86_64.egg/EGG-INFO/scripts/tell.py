30ed8017eec9586699a3f072b97b37a7bb0dbdd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:45 +0000

    Add package funnyjoke/0.2/funnyjoke-0.2-py3.6-linux-x86_64.egg

---
+++ packages/funnyjoke/funnyjoke-0.2-py3.6-linux-x86_64.egg/EGG-INFO/scripts/tell.py
@@ -0,0 +1,12 @@
+__requires__= 'funnyjoke==0.2'
+import pkg_resources
+pkg_resources.require("funnyjoke==0.2")
+import funnyjoke
+funnyjoke.tell()
+
+
+__requires__= 'fastjoke==0.2'
+import pkg_resources
+pkg_resources.require("fastjoke==0.2")
+import fastjoke
+fastjoke.tell()

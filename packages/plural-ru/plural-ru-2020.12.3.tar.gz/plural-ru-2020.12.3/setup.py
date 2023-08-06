109689171d7db831268b2a6c6f2c296651f9aa4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:16 +0000

    Add package plural-ru/2020.12.3/plural-ru-2020.12.3.tar.gz

---
+++ packages/plural-ru/plural-ru-2020.12.3.tar.gz/plural-ru-2020.12.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='plural-ru',
+    version='2020.12.3',
+    install_requires=open('requirements.txt').read().splitlines(),
+    packages=setuptools.find_packages(),
+    scripts=['scripts/plural']
+)

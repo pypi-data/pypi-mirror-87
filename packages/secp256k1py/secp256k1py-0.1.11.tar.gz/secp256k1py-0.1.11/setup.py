799b1160d0dc9ad4bedd9c7ef7ad76b5aaa27ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:33 +0000

    Add package secp256k1py/0.1.11/secp256k1py-0.1.11.tar.gz

---
+++ packages/secp256k1py/secp256k1py-0.1.11.tar.gz/secp256k1py-0.1.11/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from Cython.Build import cythonize, build_ext
+
+version = '0.1.11'
+
+
+setup(name='secp256k1py',
+      version=version,
+      description="Python version secp256k1 keypair generator signature and verify, ecdh secret sharing, for human mind",
+      long_description="""\
+Python version secp256k1 keypair generator signature and verify, ecdh secret sharing, for human mind""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='ecc,ecdh,secp256k1,signature,verification',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      url='https://github.com/ipconfiger/secp256k1-py',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      package_data={
+              'secp256k1py': ['*.pyx',]
+      },
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          "point",
+          "salsa20>=0.3.0"
+      ],
+      cmdclass={'build_ext': build_ext},
+      ext_modules=cythonize([Extension('*', ['secp256k1py/curv.c'])]),
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

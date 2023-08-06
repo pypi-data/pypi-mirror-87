58c0c3db3ddc5efb219a38607939892a7b230ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:15 +0000

    Add package pyvimond/0.1.3/pyvimond-0.1.3.tar.gz

---
+++ packages/pyvimond/pyvimond-0.1.3.tar.gz/pyvimond-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyvimond",
+    version="0.1.3",
+    author="Sam Stenvall",
+    author_email="sam.stenvall@nitor.com",
+    description="Tiny client library for various Vimond APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.mtv.fi/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

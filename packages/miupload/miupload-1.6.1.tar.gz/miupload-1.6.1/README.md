8fe6c1d88aacd7643a4fbf76d2d1151d15a20fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:30 +0000

    Add package miupload/1.6.1/miupload-1.6.1.tar.gz

---
+++ packages/miupload/miupload-1.6.1.tar.gz/miupload-1.6.1/README.md
@@ -0,0 +1,5 @@
+# MiUpload Library
+MiUpload library is small lighweight library for collecting and distribution of jupyter notebooks during Structured Study Sessions on Minerva Schools at KGI.
+
+Learn more about Minerva on <https://minerva.kgi.edu>
+    
---
+++ packages/miupload/miupload-1.6.1.tar.gz/miupload-1.6.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="miupload",
+    version="1.6.1",
+    author="Juraj Vasek",
+    author_email="juraj.vasek@minerva.kgi.edu",
+    description="Small lighweight library for collecting and distribution of jupyter notebooks during SSS on MSaKGI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    python_requires='>=3',
+    install_requires=['requests', "ipykernel", "ipython"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

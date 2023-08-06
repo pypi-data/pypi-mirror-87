5dc24587ccc2b4cbae7717391fc1f6ed2ded4cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:43 +0000

    Add package mlrun/0.6.0rc4/mlrun-0.6.0rc4-py3-none-any.whl

---
+++ packages/mlrun/mlrun-0.6.0rc4-py3-none-any.whl/mlrun/api/schemas/__init__.py
@@ -0,0 +1,32 @@
+# flake8: noqa  - this is until we take care of the F401 violations with respect to __all__ & sphinx
+
+from .artifact import ArtifactCategories
+from .project import Project, ProjectOut, ProjectCreate, ProjectInDB, ProjectUpdate
+from .schedule import (
+    SchedulesOutput,
+    ScheduleOutput,
+    ScheduleCronTrigger,
+    ScheduleKinds,
+    ScheduleUpdate,
+    ScheduleInput,
+    ScheduleRecord,
+)
+from .user import User, UserCreate, UserInDB, UserUpdate
+from .feature_store import (
+    Feature,
+    FeatureRecord,
+    Entity,
+    EntityRecord,
+    FeatureSetSpec,
+    FeatureSet,
+    FeatureSetRecord,
+    FeatureSetsOutput,
+    FeatureSetDigestOutput,
+    FeatureSetDigestSpec,
+    FeatureListOutput,
+    FeaturesOutput,
+    FeatureVector,
+    FeatureVectorRecord,
+    FeatureVectorsOutput,
+)
+from .object import ObjectMetadata, ObjectSpec, ObjectStatus, PatchMode, ObjectKind

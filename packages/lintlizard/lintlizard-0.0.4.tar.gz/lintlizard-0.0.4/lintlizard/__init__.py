5da84f46ff4e7039888d885bff74e279af53ae0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package lintlizard/0.0.4/lintlizard-0.0.4.tar.gz

---
+++ packages/lintlizard/lintlizard-0.0.4.tar.gz/lintlizard-0.0.4/lintlizard/__init__.py
@@ -0,0 +1,48 @@
+import subprocess
+from typing import Iterable, Tuple
+
+from attr import attrib, attrs
+
+__version__ = '0.0.4'
+
+
+@attrs(frozen=True)
+class CommandTool:
+    executable: str = attrib()
+    run_params: Tuple[str, ...] = attrib(default=())
+
+    def run_command(self) -> Tuple[str, ...]:
+        return (self.executable,) + self.run_params
+
+    def version_command(self) -> Tuple[str, ...]:
+        return self.executable, '--version'
+
+
+TOOLS = [
+    CommandTool('flake8'),
+    CommandTool('isort', run_params=('-rc', '-c', '.')),
+    CommandTool('mypy'),
+    CommandTool('black', run_params=('--check', '.')),
+]
+
+
+def execute_tools() -> Iterable[bool]:
+    for tool in TOOLS:
+        print('*' * 79)
+        try:
+            subprocess.run(args=tool.version_command(), check=True)
+            subprocess.run(args=tool.run_command(), check=True)
+        except subprocess.CalledProcessError:
+            yield False
+        else:
+            yield True
+
+
+def main() -> None:
+    tool_results = list(execute_tools())
+    if not all(tool_results):
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()

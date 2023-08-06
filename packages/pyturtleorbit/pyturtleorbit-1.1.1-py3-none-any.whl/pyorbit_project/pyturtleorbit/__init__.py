de8b70654eff3284b626b42366bb443ea269f0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:57 +0000

    Add package pyturtleorbit/1.1.1/pyturtleorbit-1.1.1-py3-none-any.whl

---
+++ packages/pyturtleorbit/pyturtleorbit-1.1.1-py3-none-any.whl/pyorbit_project/pyturtleorbit/__init__.py
@@ -0,0 +1,8 @@
+"""
+This __init__.py file allows Python to read your package files
+and compile them as a package. It is standard practice to leave 
+this empty if your package's modules and subpackages do not share
+any code.
+(If your package is just a module, then you can put that code here.)
+"""
+from orbit import *

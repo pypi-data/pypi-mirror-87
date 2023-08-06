46c768c54be625f87a898b730f302d75af873f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:36 +0000

    Add package manticoresearch/1.0.5/manticoresearch-1.0.5-py3-none-any.whl

---
+++ packages/manticoresearch/manticoresearch-1.0.5-py3-none-any.whl/manticoresearch/__init__.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+# flake8: noqa
+
+"""
+    Manticore Search Client
+
+    Contact: info@manticoresearch.com
+"""
+
+
+from __future__ import absolute_import
+
+__version__ = "1.0.5"
+
+# import apis into sdk package
+from manticoresearch.api.index_api import IndexApi
+from manticoresearch.api.search_api import SearchApi
+from manticoresearch.api.utils_api import UtilsApi
+
+# import ApiClient
+from manticoresearch.api_client import ApiClient
+from manticoresearch.configuration import Configuration
+from manticoresearch.exceptions import OpenApiException
+from manticoresearch.exceptions import ApiTypeError
+from manticoresearch.exceptions import ApiValueError
+from manticoresearch.exceptions import ApiKeyError
+from manticoresearch.exceptions import ApiAttributeError
+from manticoresearch.exceptions import ApiException
+# import models into sdk package
+from manticoresearch.models.bulk_response import BulkResponse
+from manticoresearch.models.delete_document_request import DeleteDocumentRequest
+from manticoresearch.models.delete_response import DeleteResponse
+from manticoresearch.models.error_response import ErrorResponse
+from manticoresearch.models.insert_document_request import InsertDocumentRequest
+from manticoresearch.models.percolate_request import PercolateRequest
+from manticoresearch.models.search_request import SearchRequest
+from manticoresearch.models.search_response import SearchResponse
+from manticoresearch.models.search_response_hits import SearchResponseHits
+from manticoresearch.models.success_response import SuccessResponse
+from manticoresearch.models.update_document_request import UpdateDocumentRequest
+from manticoresearch.models.update_response import UpdateResponse
+
---
+++ packages/manticoresearch/manticoresearch-1.0.5-py3-none-any.whl/manticoresearch/models/__init__.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+# flake8: noqa
+"""
+    Manticore Search Client
+
+    Contact: info@manticoresearch.com
+"""
+
+
+from __future__ import absolute_import
+
+# import models into model package
+from manticoresearch.models.bulk_response import BulkResponse
+from manticoresearch.models.delete_document_request import DeleteDocumentRequest
+from manticoresearch.models.delete_response import DeleteResponse
+from manticoresearch.models.error_response import ErrorResponse
+from manticoresearch.models.insert_document_request import InsertDocumentRequest
+from manticoresearch.models.percolate_request import PercolateRequest
+from manticoresearch.models.search_request import SearchRequest
+from manticoresearch.models.search_response import SearchResponse
+from manticoresearch.models.search_response_hits import SearchResponseHits
+from manticoresearch.models.success_response import SuccessResponse
+from manticoresearch.models.update_document_request import UpdateDocumentRequest
+from manticoresearch.models.update_response import UpdateResponse

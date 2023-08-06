5b605f6a89574a9f9a4eecdb92a909ee72714664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:27 +0000

    Add package zy/0.1.2/zy-0.1.2.tar.gz

---
+++ packages/zy/zy-0.1.2.tar.gz/zy-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='zy',
+      version='0.1.02',
+      description='zy 子永的包',
+      author='zy-only',
+      author_email='ui829475@126.com',
+      url='https://gitee.com/only_zy/python_small_project',
+      license='zy-wkr',
+      packages=['zy'],
+      zip_safe=False)

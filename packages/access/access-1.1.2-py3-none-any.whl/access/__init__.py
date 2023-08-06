aa80fb4599943275d0a23a72b092d5c603a7c462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:47 +0000

    Add package access/1.1.2/access-1.1.2-py3-none-any.whl

---
+++ packages/access/access-1.1.2-py3-none-any.whl/access/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.1.2"
+"""
+:mod:`access` --- Accessibility Metrics
+=================================================
+"""
+
+from .access import access
+from .datasets import datasets

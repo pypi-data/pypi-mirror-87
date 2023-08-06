5253ecdb3c0de336944cb022282f65fefd1b26b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:52 +0000

    Add package tanbih-pipeline/0.7.5/tanbih-pipeline-0.7.5.tar.gz

---
+++ packages/tanbih-pipeline/tanbih-pipeline-0.7.5.tar.gz/tanbih-pipeline-0.7.5/docs/index.rst
@@ -0,0 +1,18 @@
+========
+Contents
+========
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   authors
+   changelog
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/tanbih-pipeline/tanbih-pipeline-0.7.5.tar.gz/tanbih-pipeline-0.7.5/tests/test_message.py
@@ -0,0 +1,52 @@
+
+from unittest import TestCase
+
+from pipeline import Message
+
+
+class TestMessage(TestCase):
+    def test_default(self):
+        m = Message()
+        assert m.updated is False
+
+    def test_get_version(self):
+        m = Message()
+        assert not m.get_version('tester')['version']
+
+    def test_versions(self):
+        m = Message()
+        m.update_version('tester', [0, 1, 0])
+        assert m.updated is True
+        assert m.get_version('tester')['version'] == [0, 1, 0]
+        assert m.get_version('tester')['order'] == 1
+        m.updated = False
+        m.update_version('tester', [0, 1, 1])
+        assert m.updated is True
+        assert m.get_version('tester')['version'] == [0, 1, 1]
+        assert m.get_version('tester')['order'] == 1
+        m.updated = False
+        m.update_version('validator', [0, 0, 1])
+        assert m.updated is True
+        assert m.get_version('validator')['version'] == [0, 0, 1]
+        assert m.get_version('validator')['order'] == 2
+
+    def test_subclass(self):
+        class NewMessage(Message):
+            def __init__(self, other=None):
+                super().__init__()
+                if isinstance(other, dict):
+                    self.dct = other
+                else:
+                    self.header, self.dct = other.header, other.dct
+
+        newMessage1 = NewMessage({'key': 'new1'})
+        newMessage2 = NewMessage(newMessage1)
+        assert newMessage2.dct['key'] == 'new1'
+
+    def test_update_replace(self):
+        m = Message()
+        m.update({'key': 'value'})
+        assert m.get('key') == 'value'
+        m.replace({'newkey': 'value'})
+        assert m.get('key') is None
+        assert m.get('newkey') == 'value'

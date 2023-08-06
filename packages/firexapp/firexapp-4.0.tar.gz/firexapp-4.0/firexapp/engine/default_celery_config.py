1adf9ccddcd9de29e4470bc0e87396d371ee743b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:59 +0000

    Add package firexapp/4.0/firexapp-4.0.tar.gz

---
+++ packages/firexapp/firexapp-4.0.tar.gz/firexapp-4.0/firexapp/engine/default_celery_config.py
@@ -0,0 +1,35 @@
+from celery.utils.log import get_task_logger
+
+from firexapp.broker_manager.broker_factory import BrokerFactory
+from firexapp.engine.logging import add_hostname_to_log_records
+from firexapp.plugins import get_plugin_module_list
+from firexapp.discovery import find_firex_task_bundles
+
+add_hostname_to_log_records()
+logger = get_task_logger(__name__)
+
+
+broker_url = BrokerFactory.get_broker_url()
+result_backend = broker_url
+
+# find default tasks
+logger.debug("Beginning bundle discovery")
+bundles = find_firex_task_bundles()
+logger.debug("Bundle discovery completed")
+
+imports = tuple(bundles) + tuple(get_plugin_module_list()) + tuple(["firexapp.tasks.example",
+                                                                    "firexapp.tasks.core_tasks",
+                                                                    "firexapp.submit.report_trigger"])
+
+root_task = "firexapp.tasks.core_tasks.RootTask"
+
+accept_content = ['pickle']
+task_serializer = 'pickle'
+result_serializer = 'pickle'
+
+result_expires = None
+
+task_track_started = True
+task_acks_late = True
+
+worker_prefetch_multiplier = 1
---
+++ packages/firexapp/firexapp-4.0.tar.gz/firexapp-4.0/firexapp/submit/uid.py
@@ -0,0 +1,67 @@
+import os
+import datetime
+import pytz
+import tempfile
+from getpass import getuser
+import random
+
+from firexapp.submit.arguments import whitelist_arguments
+
+BASE_LOGGING_DIR_ENV_VAR_KEY = 'firex_base_logging_dir'
+
+class Uid(object):
+    debug_dirname = 'debug'
+
+    def __init__(self, identifier=None):
+        self.timestamp = datetime.datetime.now(tz=pytz.utc)
+        self.user = getuser()
+        if identifier:
+            self.identifier = identifier
+        else:
+            random.seed()
+            self.identifier = f'FireX-{self.user}-{self.timestamp.strftime("%y%m%d-%H%M%S")}-{random.randint(1, 65536)}'
+        self._base_logging_dir = None
+        self._logs_dir = None
+        self._debug_dir = None
+
+    @property
+    def base_logging_dir(self):
+        if not self._base_logging_dir:
+            self._base_logging_dir = os.environ.get(BASE_LOGGING_DIR_ENV_VAR_KEY, tempfile.gettempdir())
+        return self._base_logging_dir
+
+
+    @property
+    def logs_dir(self):
+        if not self._logs_dir:
+            self._logs_dir = self.create_logs_dir()
+            self._debug_dir = self.create_debug_dir()
+        return self._logs_dir
+
+    @property
+    def debug_dir(self):
+        if not self._debug_dir:
+            self._debug_dir = self.create_debug_dir()
+        return self._debug_dir
+
+    def create_logs_dir(self):
+        path = os.path.join(self.base_logging_dir, self.identifier)
+        os.makedirs(path, 0o777)
+        return path
+
+    def create_debug_dir(self):
+        path = os.path.join(self.logs_dir, self.debug_dirname)
+        os.makedirs(path, 0o777)
+        return path
+
+    def __str__(self):
+        return self.identifier
+
+    def __repr__(self):
+        return self.identifier
+
+    def __eq__(self, other):
+        return str(other) == self.identifier
+
+
+whitelist_arguments("uid")

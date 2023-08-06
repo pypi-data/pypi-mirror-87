6976efc66679ba1f7c37e4d37fa8dee35aa8a821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:49 +0000

    Add package distributions-pr11/0.1/distributions_pr11-0.1.tar.gz

---
+++ packages/distributions-pr11/distributions_pr11-0.1.tar.gz/distributions_pr11-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pr11',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pr11'],
+      author = 'mail.prithvi@gmail.com',
+      author_name = 'Prithvi Raj',
+      zip_safe=False)

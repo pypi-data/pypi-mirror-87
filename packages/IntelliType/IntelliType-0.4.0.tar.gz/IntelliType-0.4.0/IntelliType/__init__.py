21a18552a9a3bd8988e52a9de9cdb881c84f6dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package IntelliType/0.4.0/IntelliType-0.4.0.tar.gz

---
+++ packages/IntelliType/IntelliType-0.4.0.tar.gz/IntelliType-0.4.0/IntelliType/__init__.py
@@ -0,0 +1,17 @@
+from IntelliType import StoryScript
+import sys
+
+
+def run():
+    if len(sys.argv) < 2:
+        print("Usage: itt script.yaml")
+    else:
+        story_script = StoryScript.StoryScript(sys.argv[1])
+        story_script.parse()
+        story_script.scene.run()
+        for x in story_script.sections:
+            x.run()
+
+
+if __name__ == '__main__':
+    run()

47bc57ee97622337d150454d65a9971bc8824ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:57 +0000

    Add package n2yoasync/0.0.6/n2yoasync-0.0.6.tar.gz

---
+++ packages/n2yoasync/n2yoasync-0.0.6.tar.gz/n2yoasync-0.0.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="n2yoasync",
+    version="0.0.6",
+    author="Tim Empringham",
+    author_email="tim.empringham@live.ca",
+    description="Async Python wrapper for the N2YO API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/djtimca/n2yo-api",
+    packages=setuptools.find_packages(),
+    keywords=['N2YO','Satellite'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

d54f177de63a2efed9a4eff5c11e9cbcceee0141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:40 +0000

    Add package funnyjoke/0.1/funnyjoke-0.1.tar.gz

---
+++ packages/funnyjoke/funnyjoke-0.1.tar.gz/funnyjoke-0.1/README.md
@@ -0,0 +1,1 @@
+Small package example taken from https://python-packaging.readthedocs.io/en/latest/minimal.html
---
+++ packages/funnyjoke/funnyjoke-0.1.tar.gz/funnyjoke-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funnyjoke',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      license='MIT',
+      packages=['funnyjoke'],
+	  scripts=['tell.py'],
+      zip_safe=False)

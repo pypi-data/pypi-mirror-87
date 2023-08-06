fe01964d19c4646a31a26bc968e98c2a9964610e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:16 +0000

    Add package projetpy-fadoun-seydou/1.0.0/projetpy_fadoun_seydou-1.0.0.tar.gz

---
+++ packages/projetpy-fadoun-seydou/projetpy_fadoun_seydou-1.0.0.tar.gz/projetpy_fadoun_seydou-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os 
+import myprojet
+
+setup(name = 'projetpy_fadoun_seydou',
+      version = myprojet.__version__,
+      author = 'fadoun_seydou',
+      author_email = 'aliamagnefique123@gmail.com',
+      package = 'myprojet',
+      url='https://github.com/fadoun-seydou/projet',
+      description = 'creation d’un package python',
+      long_description = open(os.path.join(os.path.dirname(__file__), 'README.txt ')).read(),
+      long_description_content_type='text/markdown',
+      install_requires=[
+       'requests'
+         ],
+      python_requires='>=3.7',
+      classifiers = ['Programming Language :: Python :: 3.7 ',
+        ' License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'],
+     )

e1156ce514653202e455133bdf422e49409f3121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:46 +0000

    Add package dj-pony.applied-migrations-checker/0.1.2/dj-pony.applied-migrations-checker-0.1.2.tar.gz

---
+++ packages/dj-pony.applied-migrations-checker/dj-pony.applied-migrations-checker-0.1.2.tar.gz/dj-pony.applied-migrations-checker-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dj_pony',
+ 'dj_pony.applied_migrations_checker',
+ 'dj_pony.applied_migrations_checker.management',
+ 'dj_pony.applied_migrations_checker.management.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django>=1.8']
+
+setup_kwargs = {
+    'name': 'dj-pony.applied-migrations-checker',
+    'version': '0.1.2',
+    'description': 'Check that all django database migrations have been applied.',
+    'long_description': None,
+    'author': 'Samuel Bishop',
+    'author_email': 'sam@techdragon.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

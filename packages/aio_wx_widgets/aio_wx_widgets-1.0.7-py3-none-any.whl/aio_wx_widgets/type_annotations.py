91269269927b3f8d6466b9ef456cb3ca070d637b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:47 +0000

    Add package aio_wx_widgets/1.0.7/aio_wx_widgets-1.0.7-py3-none-any.whl

---
+++ packages/aio_wx_widgets/aio_wx_widgets-1.0.7-py3-none-any.whl/aio_wx_widgets/type_annotations.py
@@ -0,0 +1,32 @@
+"""All type annotations go here."""
+
+
+from typing import TYPE_CHECKING, Any, Union
+
+if TYPE_CHECKING:
+    from asyncio import AbstractEventLoop
+
+    import wx
+
+    from aio_wx_widgets.controller import BaseController
+    from aio_wx_widgets.core.base_widget import BaseWidget, CallableItem
+    from aio_wx_widgets.core.binding import Binding
+
+    try:
+        import Protocol  # type: ignore
+    except ImportError:
+        from typing_extensions import Protocol
+
+    assert BaseController
+    assert AbstractEventLoop
+    assert Binding
+    assert wx
+    assert BaseWidget
+
+    class Choice(Protocol):
+        """Typing protocol to be used for static type checking."""
+
+        label: str
+        value: Any
+
+    Widget = Union[BaseWidget, wx.Window, wx.BoxSizer, CallableItem]

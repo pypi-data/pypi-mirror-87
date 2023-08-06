18a74c7ca76e9dbec5973df6c9a231508e67ee3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:33 +0000

    Add package brainrender/2.0.0.4/brainrender-2.0.0.4.tar.gz

---
+++ packages/brainrender/brainrender-2.0.0.4.tar.gz/brainrender-2.0.0.4/setup.py
@@ -0,0 +1,71 @@
+from setuptools import setup, find_namespace_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+requirements = [
+    "numpy",
+    "pandas",
+    "vedo>=2020.4.1",
+    "k3d==2.7.4",
+    "msgpack",
+    "pyyaml>=5.3",
+    "morphapi>=0.1.2.4",
+    "requests",
+    "bg-atlasapi>=1.0.0",
+    "tables",
+    "pyinspect>=0.0.8",
+    "qtpy",
+    "napari",
+    "myterial",
+]
+
+setup(
+    name="brainrender",
+    version="2.0.0.4",
+    description="Visualisation and exploration of brain atlases and other anatomical data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    install_requires=requirements,
+    extras_require={
+        "nb": ["jupyter", "k3d"],
+        "dev": [
+            "pytest-cov",
+            "pytest",
+            "pytest-sugar",
+            "coveralls",
+            "coverage<=4.5.4",
+            "pre-commit",
+            "opencv-python",
+            "jupyter",
+            "allensdk",
+        ],
+        "pyside2": ["PySide2>=5.12.3"],
+        "pyqt5": ["PyQt5>=5.12.3"],
+    },
+    python_requires=">=3.6",
+    packages=find_namespace_packages(exclude=("tests, examples")),
+    entry_points={
+        "console_scripts": [
+            "brainrender-gui = brainrender.gui.__init__:clilaunch",
+        ]
+    },
+    include_package_data=True,
+    url="https://github.com/brainglobe/brainrender",
+    author="Federico Claudi, Adam Tyson, Luigi Petrucco",
+    zip_safe=False,
+)
---
+++ packages/brainrender/brainrender-2.0.0.4.tar.gz/brainrender-2.0.0.4/tests/test_scene.py
@@ -0,0 +1,157 @@
+from brainrender import Scene
+from brainrender.actor import Actor
+import pytest
+import shutil
+
+
+def test_scene_creation():
+    scene = Scene(root=False)
+    scene.root
+    assert scene.root.alpha() == 0
+
+    scene = Scene(atlas_name="mpin_zfish_1um")
+    assert isinstance(scene.root, Actor)
+
+    noinset = Scene(inset=False, title="TEST")
+    noinset.root
+    noinset.render(interactive=False)
+
+    scene.render(interactive=False)
+    del scene
+    noinset.render(interactive=False)
+    del noinset
+
+
+def test_scene_specials():
+    scene = Scene()
+    print(scene)
+    assert (
+        str(scene)
+        == f"A `brainrender.scene.Scene` with {len(scene.actors)} actors."
+    )
+    scene.content
+    scene.render(interactive=False)
+    del scene
+
+
+def test_brain_regions():
+    scene = Scene()
+    th = scene.add_brain_region("TH")
+    assert scene.actors[-1] == th
+    assert isinstance(th, Actor)
+
+    regs = scene.add_brain_region("MOs", "CA1")
+    assert isinstance(regs, list)
+    assert len(regs) == 2
+
+    noone = scene.add_brain_region("what is this")
+    assert noone is None
+
+    a1 = scene.add_brain_region("TH", hemisphere="left")
+    a2 = scene.add_brain_region("CA1", hemisphere="right")
+    assert isinstance(a1, Actor)
+    assert isinstance(a2, Actor)
+
+    scene.render(interactive=False)
+    del scene
+
+
+@pytest.mark.xfail(reason="No data in repo")
+def test_add_from_files():
+    scene = Scene()
+    obj = scene.add("tests/files/CC_134_1_ch1inj.obj", color="red")
+    assert isinstance(obj, Actor)
+
+    scene.render(interactive=False)
+    del scene
+
+
+def test_labels():
+    scene = Scene()
+    th = scene.add_brain_region("TH")
+    scene.add_label(th, "TH")
+    scene.render(interactive=False)
+    del scene
+
+
+def test_scene_render():
+    scene = Scene()
+    scene.add_brain_region("TH")
+
+    scene.render(interactive=False, zoom=1.4)
+
+    scene.render(
+        interactive=False,
+        camera=dict(
+            pos=(10705.845660949382, 7435.678067378925, -36936.3695486442,),
+            viewup=(
+                -0.0050579179155257475,
+                -0.9965615097647067,
+                -0.08270172139591858,
+            ),
+            clippingRange=(30461.81976236306, 58824.38622122339),
+        ),
+    )
+
+    scene.render(interactive=False, camera="sagittal")
+    del scene
+
+
+def test_scene_slice():
+    s = Scene()
+    s.add_brain_region("TH")
+
+    s.slice("frontal")
+
+    ret = s.slice("frontal",)
+    assert ret is None
+
+    s.slice("sagittal", close_actors=True)
+
+    s = Scene()
+    th = s.add_brain_region("TH")
+
+    plane = s.atlas.get_plane(pos=[1999, 1312, 3421], norm=[1, -1, 2])
+    s.slice(plane, actors=th)
+    ret = s.slice(plane, actors=[th, s.root],)
+
+    s.render(interactive=False)
+    del s
+
+
+@pytest.mark.parametrize(
+    "name, scale", [("test", 2), (None, None), (None, 1), ("test2", None)]
+)
+def test_scene_screenshot(name, scale):
+    s = Scene(screenshots_folder="tests/screenshots")
+    s.screenshot(name=name, scale=scale)
+    shutil.rmtree("tests/screenshots")
+
+    s.render(interactive=False)
+    del s
+
+
+def test_actor_removal():
+    s = Scene()
+    th = s.add_brain_region("TH")
+    assert len(s.actors) == 2
+
+    s.remove(th)
+    assert len(s.actors) == 1
+
+    s.remove("no actor")
+    assert len(s.actors) == 1
+
+
+def test_get_actors():
+    s = Scene()
+    th = s.add_brain_region("TH")
+
+    found1 = s.get_actors(name="TH")
+    assert len(found1) == 1
+    assert th in found1
+
+    found2 = s.get_actors(br_class="brain region")
+    assert len(found2) == 2
+    assert th in found2
+    assert s.root in found2

927357ead2e80b6576d93828c5f271876c238e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:49 +0000

    Add package asyncgui/0.1.3/asyncgui-0.1.3.tar.gz

---
+++ packages/asyncgui/asyncgui-0.1.3.tar.gz/asyncgui-0.1.3/README.md
@@ -0,0 +1,15 @@
+# AsyncGui
+
+An async library that works on top of an existing gui event loop.
+This is not for application developers, but for async-library developers.
+
+## Async-libraries who use this
+
+- [asynckivy](https://github.com/gottadiveintopython/asynckivy)
+- [asynctkinter](https://github.com/gottadiveintopython/asynctkinter)
+
+## TODO
+
+- `or_()` が中斷/完了のどちらか片方だけを待てるようにする
+- `Trio` -> `asyncgui` 方向へのadaptor
+
---
+++ packages/asyncgui/asyncgui-0.1.3.tar.gz/asyncgui-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asyncgui', 'asyncgui.adaptor']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'asyncgui',
+    'version': '0.1.3',
+    'description': 'An async library that works on top of an existing gui event loop',
+    'long_description': '# AsyncGui\n\nAn async library that works on top of an existing gui event loop.\nThis is not for application developers, but for async-library developers.\n\n## Async-libraries who use this\n\n- [asynckivy](https://github.com/gottadiveintopython/asynckivy)\n- [asynctkinter](https://github.com/gottadiveintopython/asynctkinter)\n\n## TODO\n\n- `or_()` が中斷/完了のどちらか片方だけを待てるようにする\n- `Trio` -> `asyncgui` 方向へのadaptor\n\n',
+    'author': 'Nattōsai Mitō',
+    'author_email': 'flow4re2c@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/gottadiveintopython/asyncgui',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

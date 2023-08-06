4451cdd4d56f5c64f5be97cd45ab13d1459b08a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package PortableTester/0.1/PortableTester-0.1.tar.gz

---
+++ packages/PortableTester/PortableTester-0.1.tar.gz/PortableTester-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PortableTester',
+    version='0.1',
+    packages=['PortableTester'],
+    url='',
+    license='',
+    author='waltermarchewka',
+    author_email='waltermarchewka@siemens.com',
+    description=''
+)

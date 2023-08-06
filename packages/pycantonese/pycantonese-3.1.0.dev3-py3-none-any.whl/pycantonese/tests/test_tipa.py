1c7f886e32e5c312a34a6822ebed131d22a1093f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:57 +0000

    Add package pycantonese/3.1.0.dev3/pycantonese-3.1.0.dev3-py3-none-any.whl

---
+++ packages/pycantonese/pycantonese-3.1.0.dev3-py3-none-any.whl/pycantonese/tests/test_tipa.py
@@ -0,0 +1,23 @@
+from pycantonese.jyutping.parse_jyutping import ONSETS, TONES
+from pycantonese.jyutping.tipa import ONSETS_TIPA, TONES_TIPA, jyutping_to_tipa
+
+
+# TODO def test_correct_nucleus_set():
+# TODO def test_correct_coda_set():
+
+
+def test_correct_onset_set():
+    assert set(ONSETS_TIPA.keys()) == ONSETS
+
+
+def test_correct_tone_set():
+    assert set(TONES_TIPA.keys()) == TONES
+
+
+def test_jyutping2tipa():
+    assert jyutping_to_tipa("m4goi1") == ["\\s{m}21", "kOY55"]
+    assert jyutping_to_tipa("gwong2dung1waa2") == [
+        "k\\super w ON25",
+        "tUN55",
+        "wa25",
+    ]

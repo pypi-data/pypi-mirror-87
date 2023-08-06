85c116fc5cb884e8297999ac71cd3e7d54c07b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package corva-sdk/0.0.1/corva-sdk-0.0.1.tar.gz

---
+++ packages/corva-sdk/corva-sdk-0.0.1.tar.gz/corva-sdk-0.0.1/corva/constants.py
@@ -0,0 +1,9 @@
+from typing import List, Union
+
+REDIS_STORED_VALUE_TYPE = Union[bytes, str, int, float]
+SCHEDULED_EVENT_TYPE = List[List[dict]]
+STREAM_EVENT_TYPE = List[dict]
+EVENT_TYPE = Union[
+    SCHEDULED_EVENT_TYPE,
+    STREAM_EVENT_TYPE
+]

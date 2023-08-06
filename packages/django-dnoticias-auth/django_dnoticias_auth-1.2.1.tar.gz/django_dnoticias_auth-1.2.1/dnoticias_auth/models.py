0be897dbb7447f6590d76900c5a301807bb1fb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:15 +0000

    Add package django-dnoticias-auth/1.2.1/django_dnoticias_auth-1.2.1.tar.gz

---
+++ packages/django-dnoticias-auth/django_dnoticias_auth-1.2.1.tar.gz/django_dnoticias_auth-1.2.1/dnoticias_auth/models.py
@@ -0,0 +1,12 @@
+import requests
+import logging
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+logger = logging.getLogger(__name__)
+
+# Create your models here.
+class User(AbstractUser):
+    def __str__(self):
+        return self.email

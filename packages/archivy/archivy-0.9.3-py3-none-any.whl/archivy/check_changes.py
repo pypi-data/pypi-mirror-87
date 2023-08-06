c3b6e2746a55e8fb753207aee9d08075766bdafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:08 +0000

    Add package archivy/0.9.3/archivy-0.9.3-py3-none-any.whl

---
+++ packages/archivy/archivy-0.9.3-py3-none-any.whl/archivy/check_changes.py
@@ -0,0 +1,116 @@
+import os
+import re
+import time
+from threading import Thread
+
+import flask
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from archivy import search, models
+
+DATAOBJ_REGEX = re.compile(r"^\d+-\d{2}-\d{2}-\d{2}-.*\.md$")
+SEP = os.path.sep
+
+
+class ModifHandler(FileSystemEventHandler):
+    def __init__(self, app: flask.Flask):
+        self.app = app
+        self.app.logger.info("Running watcher")
+        self.ELASTIC = app.config["ELASTICSEARCH_CONF"]["enabled"]
+        self.data_dir = os.path.join(app.config["USER_DIR"], "data" + SEP)
+        self.last_formatted = ""
+        self.time_formatted = time.time()
+
+    def is_unformatted(self, filename):
+        return (not re.match(DATAOBJ_REGEX, filename)
+                and not filename.startswith(".")
+                and filename.endswith(".md"))
+
+    def format_file(self, filepath):
+        # weird buggy errors with watchdog where event is triggered twice
+        if filepath == self.last_formatted and time.time() - self.time_formatted <= 40:
+            return
+
+        try:
+            new_file = open(filepath, "r")
+            file_contents = new_file.read()
+            new_file.close()
+        except FileNotFoundError:
+            return
+
+        # extract name of file
+        split_path = filepath.replace(self.data_dir, "").split(SEP)
+        file_title = split_path[-1].split(".")[0]
+        directory = "/".join(split_path[0:-1])
+        note_dataobj = {
+                "title": file_title,
+                "content": file_contents,
+                "type": "note",
+                "path": directory
+            }
+
+        dataobj = models.DataObj(**note_dataobj)
+        dataobj.insert()
+
+        self.last_formatted = filepath
+        self.time_formatted = time.time()
+        try:
+            os.remove(filepath)
+        except FileNotFoundError:
+            return
+
+    def on_modified(self, event):
+        with self.app.app_context():
+            filename = event.src_path.split(SEP)[-1]
+            if re.match(DATAOBJ_REGEX, filename) and self.ELASTIC:
+                self.app.logger.info(f"Detected changes to {event.src_path}")
+                with open(event.src_path) as f:
+                    dataobj = models.DataObj.from_md(f.read())
+                if dataobj.validate():
+                    search.add_to_index(
+                            self.app.config["ELASTICSEARCH_CONF"]["index_name"],
+                            dataobj)
+            elif self.is_unformatted(filename):
+                self.format_file(event.src_path)
+
+    def on_deleted(self, event):
+        with self.app.app_context():
+            filename = event.src_path.split(SEP)[-1]
+            if (re.match(DATAOBJ_REGEX, filename)
+                    and self.ELASTIC):
+                id = event.src_path.split(SEP)[-1].split("-")[0]
+                search.remove_from_index(self.app.config["ELASTICSEARCH_CONF"]["index_name"], id)
+                self.app.logger.info(f"{event.src_path} has been removed")
+
+    def on_created(self, event):
+        with self.app.app_context():
+            filename = event.src_path.split(SEP)[-1]
+            # check file is not formatted and is md file and is not temp file
+            if self.is_unformatted(filename):
+                self.format_file(event.src_path)
+
+
+class Watcher(Thread):
+
+    def __init__(self, app):
+        self.running = True
+        self.app = app
+        Thread.__init__(self)
+
+    def run(self):
+        event_handler = ModifHandler(self.app)
+        observer = Observer()
+        observer.schedule(
+            event_handler,
+            path=event_handler.data_dir,
+            recursive=True)
+        observer.start()
+
+        while self.running:
+            time.sleep(1)
+        observer.stop()
+        observer.join()
+
+    def stop(self):
+        self.running = False
---
+++ packages/archivy/archivy-0.9.3-py3-none-any.whl/archivy/cli.py
@@ -0,0 +1,121 @@
+import os
+from pkg_resources import iter_entry_points
+
+import click
+import pypandoc
+from click_plugins import with_plugins
+from flask.cli import FlaskGroup, load_dotenv, shell_command
+
+from archivy import app
+from archivy.check_changes import Watcher
+from archivy.config import Config
+from archivy.click_web import create_click_web_app
+from archivy.data import open_file
+from archivy.helpers import load_config, write_config
+from archivy.models import User
+
+
+def create_app():
+    return app
+
+
+@with_plugins(iter_entry_points('archivy.plugins'))
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    pass
+
+
+# add built in commands:
+cli.add_command(shell_command)
+
+
+@cli.command("init", short_help="Initialise your archivy application")
+@click.pass_context
+def init(ctx):
+    try:
+        load_config()
+        click.confirm("Config already found. Do you wish to reset it? "
+                      "Otherwise run `archivy config`", abort=True)
+    except FileNotFoundError:
+        pass
+
+    config = Config()
+    delattr(config, "SECRET_KEY")
+
+    click.echo("This is the archivy installation initialization wizard.")
+    data_dir = click.prompt("Enter the full path of the "
+                            "directory where you'd like us to store data.",
+                            type=str,
+                            default=os.getcwd())
+
+    es_enabled = click.confirm("Would you like to enable Elasticsearch? For this to work "
+                               "when you run archivy, you must have ES installed."
+                               "See https://archivy.github.io/setup-search/ for more info.")
+    if es_enabled:
+        config.ELASTICSEARCH_CONF["enabled"] = 1
+    else:
+        delattr(config, "ELASTICSEARCH_CONF")
+
+    create_new_user = click.confirm("Would you like to create a new admin user?")
+    if create_new_user:
+        username = click.prompt("Username")
+        password = click.prompt("Password", hide_input=True, confirmation_prompt=True)
+        if not ctx.invoke(create_admin, username=username, password=password):
+            return
+
+    config.HOST = click.prompt("Host [localhost (127.0.0.1)]",
+                               type=str, default="127.0.0.1", show_default=False)
+
+    try:
+        pypandoc.get_pandoc_version()
+    except OSError:
+        download_pandoc = click.confirm("Archivy requires Pandoc to be installed. "
+                                        "Do you want us to install it automatically?")
+        if download_pandoc:
+            pypandoc.download_pandoc()
+
+    config.override({"USER_DIR": data_dir})
+    app.config["USER_DIR"] = data_dir
+
+    # create data dir
+    os.makedirs(os.path.join(data_dir, "data"), exist_ok=True)
+
+    write_config(vars(config))
+    click.echo("Config successfully created at "
+               + os.path.join(app.config['INTERNAL_DIR'], 'config.yml'))
+
+
+@cli.command("config", short_help="Open archivy config.")
+def config():
+    open_file(os.path.join(app.config["INTERNAL_DIR"], "config.yml"))
+
+
+@cli.command("run", short_help="Runs archivy web application")
+def run():
+    click.echo('Running archivy...')
+    load_dotenv()
+    watcher = Watcher(app)
+    watcher.start()
+    os.environ["FLASK_RUN_FROM_CLI"] = "false"
+    app_with_cli = create_click_web_app(click, cli, app)
+    app_with_cli.run(host=app.config["HOST"], port=app.config["PORT"])
+    click.echo("Stopping archivy watcher")
+    watcher.stop()
+    watcher.join()
+
+
+@cli.command(short_help="Creates a new admin user")
+@click.argument("username")
+@click.password_option()
+def create_admin(username, password):
+    if len(password) < 8:
+        click.echo("Password length too short")
+        return False
+    else:
+        user = User(username=username, password=password, is_admin=True)
+        if user.insert():
+            click.echo(f"User {username} successfully created.")
+            return True
+        else:
+            click.echo("User with given username already exists.")
+            return False

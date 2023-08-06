322bbc6fc098de804b7a690be0030255ec3424ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package pygan/1.1.1/pygan-1.1.1.tar.gz

---
+++ packages/pygan/pygan-1.1.1.tar.gz/pygan-1.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+def read_readme(file_name):
+    from os import path
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, file_name), encoding='utf-8') as f:
+        long_description = f.read()
+
+    return long_description
+
+
+setup(
+    name='pygan',
+    version='1.1.1',
+    description='pygan is Python library to implement Generative Adversarial Networks(GANs), Conditional GANs, Adversarial Auto-Encoders(AAEs), and Energy-based Generative Adversarial Network(EBGAN).',
+    long_description=read_readme("README.md"),
+    long_description_content_type='text/markdown',
+    url='https://github.com/accel-brain/accel-brain-code/tree/master/Generative-Adversarial-Networks/',
+    author='accel-brain',
+    author_email='info@accel-brain.co.jp',
+    license='GPL2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Framework :: Robot Framework',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='Generative Adversarial Networks Adversarial Auto-Encoders autoencoder auto-encoder convolution deconvolution encoder decoder LSTM',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy', 'accel-brain-base'],
+)

9105883c559a2a247c91be875eabf52c7da16c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package caty/0.20.49.0/caty-0.20.49.0.tar.gz

---
+++ packages/caty/caty-0.20.49.0.tar.gz/caty-0.20.49.0/caty/plugins/__init__.py
@@ -0,0 +1,29 @@
+from .csv import CSV
+from .ini import INI
+from .json import JSON
+from .pickle import PICKLE
+from .sqlite import SQLITE
+from .toml import TOML
+from .txt import TXT
+from .yaml import YAML
+from .bin import BIN, OCT, DEC, HEX
+from .cfg import CFG
+from .html import HTML
+
+Plugins = (
+    CSV,
+    INI,
+    JSON,
+    PICKLE,
+    SQLITE,
+    TOML,
+    TXT,
+    YAML,
+    BIN,
+    OCT,
+    DEC,
+    HEX,
+    CFG,
+    HTML,
+)
+

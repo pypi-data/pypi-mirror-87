9099a2009e8ad4811185be89937f4e77a38b6a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:49 +0000

    Add package argo-events/0.1.0/argo_events-0.1.0-py3-none-any.whl

---
+++ packages/argo-events/argo_events-0.1.0-py3-none-any.whl/argo/events/client/__about__.py
@@ -0,0 +1,24 @@
+"""About this package."""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "argo-events"
+__summary__ = "Community Maintained Python client for Argo Events"
+__uri__ = "https://github.com/argoproj-labs/argo-events-client-python"
+
+__version__ = "v0.1.0"
+
+__author__ = "Yudi Xue"
+__email__ = "binarycrayon@gmail.com"
+
+__license__ = "Apache2"
+__copyright__ = "Copyright 2020 {0}".format(__author__)

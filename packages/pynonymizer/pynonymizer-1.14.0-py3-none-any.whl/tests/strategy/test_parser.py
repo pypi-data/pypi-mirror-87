542d30ed7d8e42aa869b2abf0c4181e444e8f5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:50 +0000

    Add package pynonymizer/1.14.0/pynonymizer-1.14.0-py3-none-any.whl

---
+++ packages/pynonymizer/pynonymizer-1.14.0-py3-none-any.whl/tests/strategy/test_parser.py
@@ -0,0 +1,252 @@
+import unittest
+from unittest.mock import Mock
+
+from pynonymizer.fake import UnsupportedFakeTypeError
+from pynonymizer.strategy import database
+from pynonymizer.strategy.exceptions import UnknownTableStrategyError, UnknownColumnStrategyError, ConfigSyntaxError
+from pynonymizer.strategy.table import TableStrategyTypes
+from pynonymizer.strategy.update_column import UpdateColumnStrategyTypes
+import copy
+import pytest
+
+
+@pytest.fixture
+def simple_config():
+    return {
+        "tables": {
+            "accounts": {
+                "columns": {
+                    "current_sign_in_ip": "ipv4_public",
+                    "username": "unique_login",
+                    "email": "unique_email",
+                    "name": "empty",
+                }
+            },
+            "secrets" : "delete",
+
+            "transactions": "truncate"
+        }
+    }
+
+@pytest.fixture
+def config_unsupported_fake_type():
+    return {
+            "tables": {
+                "accounts": {
+                    "columns": {
+                        "current_sign_in_ip": "ipv4_public",
+                        "username": "unique_login",
+                        "email": "NOT A VALID FAKE TYPE",
+                        "name": "empty",
+                    }
+                },
+
+                "transactions": "truncate"
+            }
+        }
+
+@pytest.fixture
+def fake_column_generator():
+    from pynonymizer.fake import FakeColumnGenerator
+    return FakeColumnGenerator()
+
+
+@pytest.fixture
+def strategy_parser(fake_column_generator):
+    from pynonymizer.strategy import parser
+    return parser.StrategyParser(fake_column_generator)
+
+
+def test_valid_parse_no_mutate(simple_config, strategy_parser):
+    old_valid_config = copy.deepcopy(simple_config)
+    strategy_parser.parse_config(simple_config)
+
+    assert simple_config == old_valid_config
+
+def test_simple_parse_creates_databasestrategy(simple_config, strategy_parser):
+    strategy = strategy_parser.parse_config(simple_config)
+    
+    assert isinstance(strategy, database.DatabaseStrategy)
+
+def test_simple_parse_update_columns(simple_config, strategy_parser):
+    strategy = strategy_parser.parse_config(simple_config)
+
+    table = strategy.table_strategies[0]
+    assert table.table_name == "accounts"
+    assert table.strategy_type == TableStrategyTypes.UPDATE_COLUMNS
+
+
+def test_simple_parse_truncate(simple_config, strategy_parser):
+    strategy = strategy_parser.parse_config(simple_config)
+
+    table = strategy.table_strategies[2]
+    assert table.table_name == "transactions"
+    assert table.strategy_type == TableStrategyTypes.TRUNCATE
+
+def test_simple_parse_delete(simple_config, strategy_parser):
+    strategy = strategy_parser.parse_config(simple_config)
+
+    table = strategy.table_strategies[1]
+    assert table.table_name == "secrets"
+    assert table.strategy_type == TableStrategyTypes.DELETE
+
+
+    # need a better matching technique than checking list indicies.
+    """
+    assert accounts_strategy.column_strategies[0].strategy_type == UpdateColumnStrategyTypes.FAKE_UPDATE
+
+    assert accounts_strategy.column_strategies[1].strategy_type == UpdateColumnStrategyTypes.UNIQUE_LOGIN
+    assert accounts_strategy.column_strategies[2].strategy_type == UpdateColumnStrategyTypes.UNIQUE_EMAIL
+    assert accounts_strategy.column_strategies[3].strategy_type == UpdateColumnStrategyTypes.EMPTY
+    """
+
+
+
+def test_unsupported_fake_column_type(strategy_parser, config_unsupported_fake_type):
+    """
+    get_fake_column's UnsupportedFakeType should kill a parse attempt
+    """
+
+    with pytest.raises(UnsupportedFakeTypeError):
+        strategy_parser.parse_config(config_unsupported_fake_type)
+
+
+def test_invalid_table_strategy_parse(strategy_parser):
+    with pytest.raises(UnknownTableStrategyError):
+        strategy_parser.parse_config({
+            "tables": {
+                "accounts": "cheesecake"
+            }
+        })
+
+
+def test_unknown_column_strategy(strategy_parser):
+    with pytest.raises(UnknownColumnStrategyError):
+        strategy_parser.parse_config({
+            "tables": {
+                "accounts": {
+                    "columns": {
+                        "current_sign_in_ip": {
+                            "type": "cheese"  # Not a valid strategy
+                        }
+                    }
+                },
+
+                "transactions": "truncate"
+            }
+        })
+
+
+def test_unknown_table_strategy_bad_dict(strategy_parser):
+    with pytest.raises(UnknownTableStrategyError):
+        strategy_parser.parse_config({
+            "tables": {
+                "accounts": {
+                    "not_columns": {
+                        "current_sign_in_ip": "ipv4_public",
+                        "username": "unique_login",
+                        "email": "unique_email",
+                        "name": "empty",
+                    }
+                },
+            }
+        })
+
+
+def test_valid_parse_before_after_script(strategy_parser):
+    parse_result = strategy_parser.parse_config({
+        "scripts": {
+            "before": [
+                "SELECT `before` from `students`;"
+            ],
+            "after": [
+                "SELECT `after` from `students`;",
+                "SELECT `after_2` from `example`;"
+            ]
+        },
+        "tables": {
+            "accounts": "truncate"
+        },
+    })
+
+    assert isinstance(parse_result, database.DatabaseStrategy)
+
+    assert len(parse_result.table_strategies) == 1
+    assert parse_result.table_strategies[0].strategy_type == TableStrategyTypes.TRUNCATE
+
+    assert parse_result.scripts["before"] == [
+                "SELECT `before` from `students`;"
+            ]
+    assert parse_result.scripts["after"] == [
+                "SELECT `after` from `students`;",
+                "SELECT `after_2` from `example`;"
+            ]
+
+
+def test_verbose_table_truncate(strategy_parser):
+    with pytest.raises(ConfigSyntaxError):
+        strategy = strategy_parser.parse_config({
+            "tables": {
+                "table1": {
+                    "type": "truncate",
+                    # parser should raise error when keys from other types when type is specified
+                    "columns": {}
+                }
+            }
+        })
+
+
+def test_verbose_table_update_columns(strategy_parser):
+    strategy = strategy_parser.parse_config({
+        "tables": {
+            "table1": {
+                "type": "update_columns",
+                "columns": {
+                }
+            }
+        }
+    })
+
+    assert len(strategy.table_strategies) == 1
+    assert strategy.table_strategies[0].table_name == "table1"
+    assert strategy.table_strategies[0].strategy_type == TableStrategyTypes.UPDATE_COLUMNS
+
+
+def test_verbose_table_list_duplicate(strategy_parser):
+    """parser should allow multiple tables of the same name in list-parse-mode"""
+    strategy = strategy_parser.parse_config({
+        "tables": [
+            {
+                "table_name": "table1",
+                "type": "truncate",
+            },
+            {
+                "table_name": "table1",
+                "type": "truncate",
+            }
+        ]
+    })
+
+    assert len(strategy.table_strategies) == 2
+    assert strategy.table_strategies[0].table_name == "table1"
+    assert strategy.table_strategies[0].strategy_type == TableStrategyTypes.TRUNCATE
+    assert strategy.table_strategies[1].table_name == "table1"
+    assert strategy.table_strategies[1].strategy_type == TableStrategyTypes.TRUNCATE
+
+
+def test_table_raises_when_given_unrelated_key(strategy_parser):
+    with pytest.raises(ConfigSyntaxError):
+        strategy_parser.parse_config({
+            "tables": {
+                "table1": {
+                    "type": "update_columns",
+                    "columns": {
+                        "column1": {
+                            "type": "empty",
+                            "where": "condition = 'value1'",
+                            "fake_type": "email"
+                        },
+                    }
+                }
+            }
+        })
---
+++ packages/pynonymizer/pynonymizer-1.14.0-py3-none-any.whl/tests/test_args.py
@@ -0,0 +1,199 @@
+from unittest.mock import patch, Mock
+from pynonymizer.__main__ import create_parser
+import pytest
+
+
+def test_legacy_positional_args():
+    """
+    When called with the positional args, argparse should return a working namespace with the correct values set
+    :return:
+    """
+    args = create_parser().parse_args(["input.sql", "strategyfile.yml", "output.sql"])
+    assert args.legacy_input == "input.sql"
+    assert args.legacy_strategyfile == "strategyfile.yml"
+    assert args.legacy_output == "output.sql"
+
+
+def mock_getenv_old(name):
+    """
+    a mock for getenv to return the OLD environment variable set with pre-defined values
+    """
+    if name == "DB_TYPE":
+        return "OLDENV_DB_TYPE"
+    elif name == "DB_HOST":
+        return "OLDENV_DB_HOST"
+    elif name == "DB_NAME":
+        return "OLDENV_DB_NAME"
+    elif name == "DB_USER":
+        return "OLDENV_DB_USER"
+    elif name == "DB_PASS":
+        return "OLDENV_DB_PASSWORD"
+    elif name == "FAKE_LOCALE":
+        return "OLDENV_FAKE_LOCALE"
+
+
+def mock_getenv_none():
+    """
+    Simulates an empty envrionment variable set
+    """
+    return None
+
+
+def mock_getenv_new(name):
+    """
+    a mock for getenv to return the NEW environment variable set with pre-defined values
+    """
+    if name == "PYNONYMIZER_DB_TYPE":
+        return "ENV_DB_TYPE"
+    elif name == "PYNONYMIZER_DB_HOST":
+        return "ENV_DB_HOST"
+    elif name == "PYNONYMIZER_DB_NAME":
+        return "ENV_DB_NAME"
+    elif name == "PYNONYMIZER_DB_USER":
+        return "ENV_DB_USER"
+    elif name == "PYNONYMIZER_DB_PASSWORD":
+        return "ENV_DB_PASSWORD"
+    elif name == "PYNONYMIZER_FAKE_LOCALE":
+        return "ENV_FAKE_LOCALE"
+    elif name == "PYNONYMIZER_INPUT":
+        return "ENV_INPUT"
+    elif name == "PYNONYMIZER_STRATEGY":
+        return "ENV_STRATEGY"
+    elif name == "PYNONYMIZER_OUTPUT":
+        return "ENV_OUTPUT"
+    elif name == "PYNONYMIZER_START_AT":
+        return "ANONYMIZE_DB"
+    elif name == "PYNONYMIZER_SKIP_STEPS":
+        return "ANONYMIZE_DB DUMP_DB"
+    elif name == "PYNONYMIZER_STOP_AT":
+        return "ANONYMIZE_DB"
+    elif name == "PYNONYMIZER_VERBOSE":
+        return "VERBOSE"
+
+
+def mock_getenv_old_new_combined(name):
+    """
+    combine the outputs from the two mocking function sets
+    """
+    return mock_getenv_new(name) or mock_getenv_old(name)
+
+
+@patch("os.getenv", Mock(side_effect=mock_getenv_old))
+def test_old_environmental_defaults():
+    """
+    When not specified, arguments should fall back to OLD env vars (if specified)
+    :return:
+    """
+    # Use positionals, since the old vars dont cover these
+    args = create_parser().parse_args(["input.sql", "strategyfile.yml", "output.sql"])
+    assert args.legacy_input        == "input.sql"
+    assert args.legacy_strategyfile == "strategyfile.yml"
+    assert args.legacy_output       == "output.sql"
+    assert args.db_type      == "OLDENV_DB_TYPE"
+    assert args.db_name      == "OLDENV_DB_NAME"
+    assert args.db_host      == "OLDENV_DB_HOST"
+    assert args.db_user      == "OLDENV_DB_USER"
+    assert args.db_password  == "OLDENV_DB_PASSWORD"
+    assert args.fake_locale  == "OLDENV_FAKE_LOCALE"
+
+
+@patch("os.getenv", Mock(side_effect=mock_getenv_old_new_combined))
+def test_environmental_defaults():
+    """
+    When not specified, arguments should fall back to NEW environmental defaults (PYNONYMIZER_*)
+    old envs should take the lowest precedence and be ignored
+    :return:
+    """
+    args = create_parser().parse_args([])
+    assert args.input         == "ENV_INPUT"
+    assert args.strategyfile  == "ENV_STRATEGY"
+    assert args.output        == "ENV_OUTPUT"
+    assert args.db_type       == "ENV_DB_TYPE"
+    assert args.db_name       == "ENV_DB_NAME"
+    assert args.db_host       == "ENV_DB_HOST"
+    assert args.db_user       == "ENV_DB_USER"
+    assert args.db_password   == "ENV_DB_PASSWORD"
+    assert args.fake_locale   == "ENV_FAKE_LOCALE"
+    assert args.start_at_step == "ANONYMIZE_DB"
+    assert args.skip_steps    == ["ANONYMIZE_DB", "DUMP_DB"]
+    assert args.stop_at_step  == "ANONYMIZE_DB"
+
+
+@patch("os.getenv", Mock(side_effect=mock_getenv_new))
+def test_all_legacy_new_mutex():
+    """
+    legacy posisitonals and new options should result in an error if specified together
+    """
+    with pytest.raises(SystemExit) as e_info:
+        create_parser().parse_args(["input.sql", "strategyfile.yml", "output.sql",  "--input", "input2.sql"])
+
+    with pytest.raises(SystemExit) as e_info:
+        create_parser().parse_args(["input.sql", "strategyfile.yml", "output.sql",  "--strategy", "strategyfile2.yml"])
+
+    with pytest.raises(SystemExit) as e_info:
+        create_parser().parse_args(["input.sql", "strategyfile.yml", "output.sql",  "--output", "output2.sql"])
+
+
+@patch("os.getenv", Mock(side_effect=mock_getenv_new))
+def test_all_args_precedence():
+    """
+    All arguments should be overridden by their argv counterparts
+    :return:
+    """
+    args = create_parser().parse_args([
+        "--input",  "input.sql",
+        "--strategy",  "strategyfile.yml",
+        "--output", "output.sql",
+        "--db-type", "ARG_DB_TYPE",
+        "--db-host", "ARG_DB_HOST",
+        "--db-name", "ARG_DB_NAME",
+        "--db-user", "ARG_DB_USER",
+        "--db-password", "ARG_DB_PASSWORD",
+        "--fake-locale", "ARG_FAKE_LOCALE",
+        "--start-at", "START",
+        "--skip-steps", "ANONYMIZE_DB",
+        "--stop-at", "END",
+        "--verbose"
+    ])
+    assert args.input          == "input.sql"
+    assert args.strategyfile   == "strategyfile.yml"
+    assert args.output         == "output.sql"
+    assert args.db_type        == "ARG_DB_TYPE"
+    assert args.db_name        == "ARG_DB_NAME"
+    assert args.db_host        == "ARG_DB_HOST"
+    assert args.db_user        == "ARG_DB_USER"
+    assert args.db_password    == "ARG_DB_PASSWORD"
+    assert args.fake_locale    == "ARG_FAKE_LOCALE"
+    assert args.start_at_step  == "START"
+    assert args.skip_steps     == ["ANONYMIZE_DB"]
+    assert args.stop_at_step   == "END"
+    assert args.verbose is True
+
+
+@patch("os.getenv", Mock(side_effect=mock_getenv_new))
+def test_all_short_args_precedence():
+    """
+    All arguments should be overridden by their argv counterparts (using short options!)
+    :return:
+    """
+    args = create_parser().parse_args([
+        "-i", "input.sql",
+        "-s", "strategyfile.yml",
+        "-o", "output.sql",
+        "-t", "ARG_DB_TYPE",
+        "-d", "ARG_DB_HOST",
+        "-n", "ARG_DB_NAME",
+        "-u", "ARG_DB_USER",
+        "-p", "ARG_DB_PASSWORD",
+        "-l", "ARG_FAKE_LOCALE"
+    ])
+    assert args.input        == "input.sql"
+    assert args.strategyfile == "strategyfile.yml"
+    assert args.output       == "output.sql"
+    assert args.db_type      == "ARG_DB_TYPE"
+    assert args.db_name      == "ARG_DB_NAME"
+    assert args.db_host      == "ARG_DB_HOST"
+    assert args.db_user      == "ARG_DB_USER"
+    assert args.db_password  == "ARG_DB_PASSWORD"
+    assert args.fake_locale  == "ARG_FAKE_LOCALE"
+    assert args.verbose is False

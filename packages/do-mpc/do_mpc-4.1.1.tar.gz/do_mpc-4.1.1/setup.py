0998eeeea6ffdacb7befa3925f69e6922209ef83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:44 +0000

    Add package do-mpc/4.1.1/do_mpc-4.1.1.tar.gz

---
+++ packages/do-mpc/do_mpc-4.1.1.tar.gz/do_mpc-4.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='do_mpc',
+    version='4.1.1',
+    packages=['do_mpc','do_mpc.tools'],
+    author='Sergio Lucia and Felix Fiedler',
+    author_email='sergio.lucia@tu-berlin.de',
+    url='https://www.do-mpc.com',
+    license='GNU LESSER GENERAL PUBLIC LICENSE',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "casadi",
+        "numpy",
+        "matplotlib"
+    ],
+)
